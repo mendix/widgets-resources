c963411a84c45070ed11f13fe27e24f1975566a5
Edwin P. Magezi <emagezi@gmail.com>
2016-12-13 12:48:42 +0300

    feat(switch): Replace active & hasSlider booleans with status

--- packages-web/boolean-slider/src/com/mendix/widget/BooleanSlider/components/Slider.ts
+++ packages-web/boolean-slider/src/com/mendix/widget/BooleanSlider/components/Slider.ts
@@ -4,27 +4,28 @@ import * as classNames from "classnames";
 import "../ui/Slider.sass";
 
 export interface SliderProps {
+    status: SliderStatus;
     isChecked: boolean;
-    enabled: boolean;
-    hasError: boolean;
-    showSlider: boolean;
+    hasError?: boolean;
     onClick?: (checked: boolean) => void;
 }
+
+export type SliderStatus = "enabled" | "disabled" | "no-context";
 
 export const Slider = (props: SliderProps & { children?: React.ReactNode }) =>
     DOM.div({ className: classNames("widget-boolean-slider", { "has-error": props.hasError }) },
         DOM.input({
             checked: props.isChecked,
-            className: classNames("widget-boolean-slider-checkbox", { enabled: props.enabled }),
+            className: classNames("widget-boolean-slider-checkbox", { enabled: props.status === "enabled" }),
             readOnly: true,
             type: "checkbox"
         }),
         DOM.div({
             className: classNames("widget-boolean-slider-btn", {
-                enabled: props.enabled,
-                "no-slider": !props.showSlider
+                enabled: props.status === "enabled",
+                "no-slider": props.status === "no-context"
             }),
-            onClick: props.enabled ? () => props.onClick(!props.isChecked) : null
+            onClick: props.status === "enabled" ? () => props.onClick(!props.isChecked) : null
         }),
         props.hasError ? props.children : null
     );
--- packages-web/boolean-slider/src/com/mendix/widget/BooleanSlider/components/__tests__/ValidationAlert.spec.ts
+++ packages-web/boolean-slider/src/com/mendix/widget/BooleanSlider/components/__tests__/ValidationAlert.spec.ts
@@ -1,24 +1,15 @@
-import { ShallowWrapper, shallow } from "enzyme";
+import { shallow } from "enzyme";
 import { DOM, createElement } from "react";
 
 import { ValidationAlert } from "../ValidationAlert";
 
 describe("ValidationAlert", () => {
-
-    let alert: ShallowWrapper<any, any>;
     const message = "This is an error";
-
-    beforeEach(() => {
-        alert = shallow(createElement(ValidationAlert, { message }));
-    });
+    const alert = shallow(createElement(ValidationAlert, { message }));
 
     it("renders structure correctly", () => {
         expect(alert).toBeElement(
             DOM.div({ className: "alert alert-danger widget-boolean-slider-validation-message" }, message)
         );
-    });
-
-    it("has the expected css classes", () => {
-        expect(alert.hasClass("alert alert-danger widget-boolean-slider-validation-message")).toBe(true);
     });
 });
