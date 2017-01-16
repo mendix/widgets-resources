cbc79e8580a5112da503b46ae5399fe7ab664bda
Acellam Guy <abiccel@yahoo.com>
2017-01-16 15:54:59 +0300

    feat(star-rating): Test star rating structure

--- packages-web/star-rating/src/com/mendix/widget/StarRating/components/__tests__/StarRating.spec.ts
+++ packages-web/star-rating/src/com/mendix/widget/StarRating/components/__tests__/StarRating.spec.ts
@@ -1,66 +1,79 @@
 import { shallow } from "enzyme";
 import { DOM, createElement } from "react";
 
-describe("Rating", () => {
+import * as Rating from "react-rating";
 
-   it("renders the structure correctly", () => {
-      //
+import { StarRating, StarRatingProps } from "../StarRating";
+
+describe("StarRating", () => {
+    const renderStarRating = (props: StarRatingProps) => shallow(createElement(StarRating, props));
+
+    it("renders the structure correctly", () => {
+        const starRating = renderStarRating({ fullColor: "#0000FF", emptyColor: "#FFFFFF" });
+
+        expect(starRating).toBeElement(
+            createElement(Rating, {
+                empty: "glyphicon glyphicon-star-empty custom custom-empty",
+                fractions: 2,
+                full: "glyphicon glyphicon-star custom custom-full"
+            })
+        );
     });
 
     it("creates a rating", () => {
-      //
+        //
     });
 
     it("sets the rating value", () => {
-      //
+        //
     });
 
     it("updates the rating value when the values are changed", () => {
-       
+        //
     });
 
-    describe ("should render as readonly", () => {
+    describe("should render as readonly", () => {
         it("when show total is false", () => {
             //
         });
 
         it("when user is not vote owner", () => {
-
+            //
         });
 
     });
 
     describe("with step", () => {
         it("not specified renders empty ui", () => {
-           //
+            //
         });
 
         it("stop  equal or less than 0 shows invalid", () => {
-           //
+            //
         });
 
         it("less than 0 show empty ui", () => {
-          
+            //
         });
 
         it("greater than the maximum show maximum", () => {
             //
         });
-    });   
-       
+    });
 
     describe("color", () => {
         it("should be red color is red", () => {
             // Fix me
         });
+
         it("should not be red when color not red", () => {
             // Fix me
-        })
+        });
     });
 
     describe("symbol", () => {
         it("should be star if set style is star", () => {
-           // Fix me
+            // Fix me
         });
 
         it("should not be star if set style is not star", () => {
@@ -70,11 +83,11 @@ describe("Rating", () => {
 
     describe("with an onClick microflow set", () => {
         it("executes the microflow when a rating is clicked", () => {
-           //
+            //
         });
 
         it("microflow selected it shows an error in configuration", () => {
-          //
+            //
         });
 
         it("invalid microflow shows an error when a rating is clicked", () => {
@@ -84,17 +97,17 @@ describe("Rating", () => {
 
     describe("with an onClick show page set", () => {
         it("opens the page when a rating is clicked", () => {
-           //
+            //
         });
 
         it("without a page selected it shows an error in configuration", () => {
-           //
+            //
         });
     });
 
     describe("without a on click", () => {
         it("should not respond on user click", () => {
-           //
+            //
         });
     });
 
