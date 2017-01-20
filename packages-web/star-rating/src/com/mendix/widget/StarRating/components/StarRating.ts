b910827033d0ad3f00ebace99475876f303ea517
Derrick Kwagala <derrick.kwagala@gmail.com>
2017-01-20 12:32:43 +0300

    feat(star-rating): - Removed unused functions as for now. - Changed star color to orange - Reintroduced starRating component

--- packages-web/star-rating/src/com/mendix/widget/StarRating/components/StarRating.ts
+++ packages-web/star-rating/src/com/mendix/widget/StarRating/components/StarRating.ts
@@ -5,10 +5,11 @@ import { Component, createElement, DOM } from "react";
 import * as Rating from "react-rating";
 
 export interface StarRatingProps {
-    maxStars?: number;
+    maximumStars?: number;
+    fractions?: number;
     fullColor?: string;
     emptyColor?: string;
-    activeRate?: number;
+    initialRate?: number;
     onChange?: (rate: number) => void;
     isCampaign?: boolean;
     isReadOnly?: boolean;
@@ -21,15 +22,26 @@ export class StarRating extends Component<StarRatingProps, {}> {
     private stars: Element[];
     render() {
         return createElement(Rating, {
-            empty: "glyphicon glyphicon-star-empty custom custom-empty",
-            fractions: 2,
-            full: "glyphicon glyphicon-star custom custom-full",
-            initialRate: this.props.activeRate,
+            empty: "glyphicon glyphicon-star-empty widget-starrating widget-starrating-empty",
+            fractions: this.props.fractions,
+            full: "glyphicon glyphicon-star widget-starrating widget-starrating-full",
+            initialRate: this.getRate(),
             onChange: this.props.onChange,
             readonly: this.props.isReadOnly,
             start: this.props.start,
             step: this.props.step,
-            stop: this.props.maxStars
+            stop: this.props.stop
         });
+
+    }
+    private getRate() {
+        const maximumValue = this.props.step * this.props.stop;
+        if (this.props.initialRate > maximumValue) {
+            return maximumValue;
+        } else if (this.props.initialRate < this.props.start) {
+            return this.props.start;
+        } else {
+            return Math.round(this.props.initialRate * this.props.fractions) / this.props.fractions;
+        }
     }
 }
