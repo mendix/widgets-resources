41f634f37ac11685cbf5e061e08bda31d868f6d1
Derrick Kwagala <derrick.kwagala@gmail.com>
2017-02-03 17:31:35 +0300

    feat(star-rating): Move data update and retrieval to wrapper

--- packages-web/star-rating/src/com/mendix/widget/StarRating/components/StarRating.ts
+++ packages-web/star-rating/src/com/mendix/widget/StarRating/components/StarRating.ts
@@ -4,15 +4,13 @@ import * as Rating from "react-rating";
 import { Alert } from "./Alert";
 
 export interface StarRatingProps {
-    campaignEntity: string;
-    contextObject?: mendix.lib.MxObject;
     initialRate?: number;
-    onChangeMicroflow?: string;
+    handleOnChange?: Function;
+    onRateMicroflow?: string;
+    ownerGUID?: string;
     readOnly: boolean;
     rateType: "single" | "average";
-    rateEntity: string;
-    rateAttribute: string;
-    averageAttribute: string;
+    configurationError?: string;
 }
 
 export interface StarRatingState {
@@ -20,33 +18,29 @@ export interface StarRatingState {
 }
 
 export class StarRating extends Component<StarRatingProps, StarRatingState> {
-    private fractions = 1;
-    private contextObject: mendix.lib.MxObject | undefined;
+    private fractions: number;
     private rateType: string;
-    private campaignEntity: string;
-    private ownerReference = "System.owner";
     private start: number;
     private step: number;
     private stop: number;
 
     constructor(props: StarRatingProps) {
         super(props);
+
         this.state = { errorMessage: "" };
+    }
+
+    render() {
         this.fractions = 2;
         this.start = 0;
         this.step = 1;
         this.stop = 5;
-    }
-
-    render() {
-        this.contextObject = this.props.contextObject || undefined;
         this.rateType = this.props.rateType;
-        this.campaignEntity = this.props.campaignEntity.split("/")[1];
 
-        if (this.hasValidConfig(this.props)) {
+        if (!this.props.configurationError) {
             this.fractions = this.rateType === "average" ? this.fractions : 1;
-            const readonly = this.props.readOnly || !(this.contextObject && this.rateType === "single"
-                && this.contextObject.get(this.ownerReference) === window.mx.session.getUserId());
+            const readonly = this.props.readOnly || this.rateType === "average" || !(this.rateType === "single"
+                && this.props.ownerGUID === window.mx.session.getUserId());
 
             return DOM.div({ className: "widget-starrating" },
                 createElement(Rating, {
@@ -54,7 +48,7 @@ export class StarRating extends Component<StarRatingProps, StarRatingState> {
                     fractions: this.fractions,
                     full: "glyphicon glyphicon-star widget-star-rating widget-star-rating-full",
                     initialRate: this.getRate(),
-                    onChange:  (rate: number) => this.submitData(rate),
+                    onChange: this.props.handleOnChange ? (rate: number) => this.onRate(rate) : undefined,
                     placeholder: "glyphicon glyphicon-star widget-star-rating widget-star-rating-placeholder",
                     readonly,
                     start: this.start,
@@ -63,20 +57,12 @@ export class StarRating extends Component<StarRatingProps, StarRatingState> {
                 }),
                 createElement(Alert, { message: this.state.errorMessage }));
         } else {
-            return createElement(Alert, { message: this.state.errorMessage });
+            return createElement(Alert, { message: this.props.configurationError });
         }
     }
 
     private getRate() {
-        // The contextObject entityType for "average" is different from that of "single"
-        // InitialRate for rateType "average" comes from averageAttribute on campaignEntity
-        // InitialRate for rateType "single" comes from rateAttribute on rateEntity
-        const initialRate = this.contextObject
-            ? this.props.rateType === "average"
-                ? this.contextObject.get(this.props.averageAttribute) as number
-                : this.contextObject.get(this.props.rateAttribute) as number
-            : 0 ;
-
+        const { initialRate } = this.props;
         const maximumValue = this.step * this.stop;
         if (initialRate > maximumValue) {
             return maximumValue;
@@ -88,47 +74,12 @@ export class StarRating extends Component<StarRatingProps, StarRatingState> {
             return Math.round(initialRate * this.fractions) / this.fractions;
         }
     }
-
-    private submitData(rate: number) {
-        if (this.contextObject) {
-            this.contextObject.set(this.props.rateAttribute, rate);
-            if (this.props.onChangeMicroflow) {
-                this.executeMicroflow(this.contextObject.getGuid(), this.props.onChangeMicroflow);
-            }
+    private onRate(rate: number) {
+        if (this.props.handleOnChange) {
+            const microflow = this.props.onRateMicroflow;
+            this.props.handleOnChange(rate, (error: Error) => { // this function is bound to the component's context
+                this.setState({ errorMessage: `Error while executing microflow: ${microflow}: ${error.message}` });
+            });
         }
-    }
-
-    private executeMicroflow(mendixGUID: string, microflow: string) {
-        window.mx.ui.action(microflow, {
-            error: (error: Error) =>
-                this.setState({ errorMessage: `Error while executing microflow: ${microflow}: ${error.message}` }),
-            params: {
-                applyto: "selection",
-                guids: [ mendixGUID ]
-            }
-        });
-    }
-
-    private hasValidConfig(props: StarRatingProps ) {
-        const errorMessage: string[] = [];
-
-        if (!this.contextObject) {
-            return true;
-        }
-        if ((this.rateType === "average") && this.contextObject.getEntity() !== this.campaignEntity) {
-            errorMessage.push(" - For rate type 'average', the contextObject should be campaign entity");
-        }
-        if ((this.rateType === "single") && this.contextObject.getEntity() !== this.props.rateEntity) {
-            errorMessage.push(` - For rate type 'Single', the contextObject be rate entity '${this.props.rateEntity}'`);
-        }
-        if (this.rateType === "single" && !this.contextObject.isReference(this.ownerReference)) {
-            errorMessage.push(` - Context object has no User / Owner association to it`);
-        }
-
-        if (errorMessage.length) {
-            errorMessage.unshift("Configuration Error: ");
-            this.state.errorMessage = errorMessage.join("\n");
-        }
-        return !errorMessage.length;
     }
 }
