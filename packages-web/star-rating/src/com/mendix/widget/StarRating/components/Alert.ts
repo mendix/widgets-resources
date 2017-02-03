73b531739055364d66bf83fa0ab9bace837e3945
Derrick Kwagala <derrick.kwagala@gmail.com>
2017-02-03 17:58:42 +0300

    feat(star-rating): Remove typing 'any'

--- packages-web/star-rating/src/com/mendix/widget/StarRating/components/Alert.ts
+++ packages-web/star-rating/src/com/mendix/widget/StarRating/components/Alert.ts
@@ -3,4 +3,4 @@ import { DOM, StatelessComponent } from "react";
 export const Alert: StatelessComponent<{ message: string }> = (props) =>
     props.message
     ? DOM.div({ className: "alert alert-danger widget-star-rating-alert" }, props.message)
-    : null as any;
+    : null;
