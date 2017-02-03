f42fe69c030fdf88202556655d50bd7b7842e266
Derrick Kwagala <derrick.kwagala@gmail.com>
2017-02-03 18:29:40 +0300

    feat(star-rating): Review: Update tests

--- packages-web/star-rating/src/com/mendix/widget/StarRating/components/__tests__/StarRating.spec.ts
+++ packages-web/star-rating/src/com/mendix/widget/StarRating/components/__tests__/StarRating.spec.ts
@@ -5,49 +5,20 @@ import * as Rating from "react-rating";
 import { Alert } from "../Alert";
 import { StarRating, StarRatingProps } from "../StarRating";
 
-import { mockMendix, mockMx } from "tests/mocks/Mendix";
 
 describe("StarRating", () => {
     let starProps: StarRatingProps;
     const renderStarRating = (props: StarRatingProps) => shallow(createElement(StarRating, props));
-    const spyOnGet = (rateValue?: number, averageValue?: number) => {
-        spyOn(starProps.contextObject, "get").and.callFake((attr: string) => {
-            if (attr === "rateAttribute") {
-                return rateValue || 2;
-            }
-            if (attr === "averageAttribute") {
-                return averageValue || 2.5;
-            }
-            return undefined;
-        });
-    };
-    const defaultMx = window.mx;
-    const defaultMendix = window.mendix;
-
-    beforeAll(() => {
-        window.mx = mockMx;
-        window.mendix = mockMendix;
-    });
 
     beforeEach(() => {
         starProps = {
-            averageAttribute: "averageAttribute",
-            campaignEntity: "Reference/campaignEntity",
-            contextObject: new mendix.lib.MxObject(),
-            onChangeMicroflow: "Microflow",
-            rateAttribute: "rateAttribute",
-            rateEntity: "rateEntity",
-            rateType: "single",
+            initialRate: 2,
+            rateType: "rating",
             readOnly: true
         };
-        spyOn(starProps.contextObject, "getEntity").and.callFake(() => {
-            return starProps.rateType === "single" ? "rateEntity" : "campaignEntity";
-        });
-        spyOn(starProps.contextObject, "isReference").and.callFake((att: string) => (att === "System.owner"));
     });
 
     it("should render with a rating structure", () => {
-        spyOnGet();
 
         const starRating = renderStarRating(starProps);
 
@@ -70,9 +41,7 @@ describe("StarRating", () => {
         );
     });
 
-    it("should render with whole numbers for the single rate type", () => {
-        spyOnGet();
-
+    it("should render with whole numbers for the rating rate type", () => {
         const starRating = renderStarRating(starProps).find(Rating);
 
         expect(starRating.props().fractions).toBe(1);
@@ -80,39 +49,25 @@ describe("StarRating", () => {
 
     it("should render with fractions for the average rate type", () => {
         starProps.rateType = "average";
-        spyOnGet();
 
         const starRating = renderStarRating(starProps).find(Rating);
 
         expect(starRating.props().fractions).toBe(2);
     });
 
-    it("should render without a contextObject", () => {
-        starProps.contextObject = undefined;
-
-        const starRating = renderStarRating(starProps).find(Rating);
-
-        expect(starRating.props().initialRate).toEqual(0);
-    });
-
     it("should render with negative rating", () => {
-        spyOnGet(-1);
-
+        starProps.initialRate = -1;
         const starRating = renderStarRating(starProps).find(Rating);
 
         expect(starRating.props().initialRate).toEqual(0);
     });
 
     it("should render with large positive rating", () => {
-        spyOnGet(100);
+        starProps.initialRate = 100;
 
         const starRating = renderStarRating(starProps).find(Rating);
 
         expect(starRating.props().initialRate).toEqual(5);
     });
 
-    afterAll(() => {
-        window.mx = defaultMx;
-        window.mendix = defaultMendix;
-    });
 });
