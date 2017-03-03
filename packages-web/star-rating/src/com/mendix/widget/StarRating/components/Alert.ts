71c49cc9de02b26365bdf6fbbfb054476dd38c5b
Derrick Kwagala <derrick.kwagala@gmail.com>
2017-03-04 00:46:57 +0300

    feat(star-rating): Fix subscription bug without onchange microflow

--- packages-web/star-rating/src/com/mendix/widget/StarRating/components/Alert.ts
+++ packages-web/star-rating/src/com/mendix/widget/StarRating/components/Alert.ts
@@ -4,3 +4,5 @@ export const Alert: StatelessComponent<{ message: string }> = (props) =>
     props.message
         ? DOM.div({ className: "alert alert-danger widget-star-rating-alert" }, props.message)
         : null;
+
+Alert.displayName = "Alert";
