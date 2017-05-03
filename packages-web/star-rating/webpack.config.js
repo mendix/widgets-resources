89d061809b1ee367b29e40b0bd20cb1c316f753e
Derrick Kwagala <derrick.kwagala@gmail.com>
2017-05-03 10:34:40 +0300

    feat(star-rating): Rename StarRating* to StarRatingInput*

--- packages-web/star-rating/webpack.config.js
+++ packages-web/star-rating/webpack.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: {
-        StarRating: "./src/components/StarRatingContainer.ts",
+        StarRatingInput: "./src/components/StarRatingInputContainer.ts",
         StarRatingView: "./src/components/StarRatingViewContainer.ts"
     },
     output: {
@@ -39,20 +39,9 @@ module.exports = {
         ], {
             copyUnmodified: true
         }),
-        new ExtractTextPlugin( {filename: "./src/com/mendix/widget/custom/StarRating/ui/StarRating.css" }),
+        new ExtractTextPlugin( {filename: "./src/com/mendix/widget/custom/StarRating/ui/StarRatingInput.css" }),
         new webpack.LoaderOptionsPlugin({
             debug: true
         })
-        // new webpack.optimize.CommonsChunkPlugin({
-        //     name: 'react-rating',
-        //     minChunks: function (module) {
-        //         // this assumes your vendor imports exist in the node_modules directory
-        //         return module.context && module.context.indexOf('node_modules') !== -1;
-        //     }
-        // }),
-        // //CommonChunksPlugin will now extract all the common modules from vendor and main bundles
-        // new webpack.optimize.CommonsChunkPlugin({ 
-        //     name: 'manifest' //But since there are no more common modules between them we end up with just the runtime code included in the manifest file
-        // })
     ]
 };
