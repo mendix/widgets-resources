a81ab93e0f84a120899394ef42a76179f63fafd0
Edwin P. Magezi <emagezi@gmail.com>
2017-05-10 15:02:18 +0300

    feat(switch): Add sass styles

--- packages-web/boolean-slider/src/Switch.webmodeler.ts
+++ packages-web/boolean-slider/src/Switch.webmodeler.ts
@@ -1,8 +1,14 @@
 import { Component, createElement } from "react";
 import { Switch, SwitchProps } from "./components/Switch";
 
+import * as css from "./ui/Switch.sass";
+
 // tslint:disable class-name
 export class preview extends Component<SwitchProps, {}> {
+    componentWillMount() {
+        this.addPreviewStyle("widget-switch");
+    }
+
     render() {
         return createElement(Switch, {
             bootstrapStyle: "success",
@@ -10,5 +16,19 @@ export class preview extends Component<SwitchProps, {}> {
             onClick: () => console.log("Clicked"),
             status: "enabled"
         });
+    }
+
+    private addPreviewStyle(styleId: string) {
+        // This workaround is to load style in the preview temporary till mendix has a better solution
+        const iFrame = document.getElementsByClassName("t-page-editor-iframe")[0] as HTMLIFrameElement;
+        const iFrameDoc = iFrame.contentDocument;
+        if (!iFrameDoc.getElementById(styleId)) {
+            const styleTarget = iFrameDoc.head || iFrameDoc.getElementsByTagName("head")[0];
+            const styleElement = document.createElement("style");
+            styleElement.setAttribute("type", "text/css");
+            styleElement.setAttribute("id", styleId);
+            styleElement.appendChild(document.createTextNode(css));
+            styleTarget.appendChild(styleElement);
+        }
     }
 }
