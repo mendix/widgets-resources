0fcb3e2ce730d36a870fca8f6189f2083b4413c2
Edwin P. Magezi <emagezi@gmail.com>
2017-05-15 13:46:45 +0300

    feat(switch): Fix orientation issues

--- packages-web/boolean-slider/src/components/Label.ts
+++ packages-web/boolean-slider/src/components/Label.ts
@@ -9,10 +9,10 @@ interface LabelProps {
 }
 
 const Label: SFC<LabelProps> = ({ children, className, label, style, weight }) =>
-    DOM.div({ className: classNames("form-horizontal", className), style },
+    DOM.div({ className: classNames("form-horizontal widget-switch-label", className), style },
         DOM.div({ className: "form-group" },
             DOM.div({ className: `col-sm-${weight} col-xs-${weight}` },
-                DOM.label({ className: "control-label widget-switch-label" }, label)
+                DOM.label({ className: "control-label" }, label)
             ),
             DOM.div({
                 className: `col-sm-${12 - weight} col-xs-${12 - weight}`
--- packages-web/boolean-slider/src/components/__tests__/Label.spec.ts
+++ packages-web/boolean-slider/src/components/__tests__/Label.spec.ts
@@ -11,10 +11,10 @@ describe("Label", () => {
         const labelComponent = renderLabel({ label });
 
         expect(labelComponent).toBeElement(
-            DOM.div({ className: "form-horizontal" },
+            DOM.div({ className: "form-horizontal widget-switch-label" },
                 DOM.div({ className: "form-group" },
                     DOM.div({ className: "col-sm-6 col-xs-6" },
-                        DOM.label({ className: "control-label widget-switch-label" }, label)
+                        DOM.label({ className: "control-label" }, label)
                     ),
                     DOM.div({ className: `col-sm-6` })
                 )
