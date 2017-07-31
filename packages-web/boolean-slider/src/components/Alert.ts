88b867718754a15e2bf1e3bf46f561b95fa9a38e
McDoyen <mrpoloh@yahoo.co.uk>
2017-07-31 09:35:46 +0300

    feat(switch): Update unit tests for Alert.ts

--- packages-web/boolean-slider/src/components/Alert.ts
+++ packages-web/boolean-slider/src/components/Alert.ts
@@ -2,14 +2,14 @@ import { SFC, createElement } from "react";
 import * as classNames from "classnames";
 
 export interface AlertProps {
-    message: string;
+    message?: string;
     className?: string;
-    bootstrapStyle: "default" | "primary" | "success" | "info" | "warning" | "danger";
+    bootstrapStyle?: "default" | "primary" | "success" | "info" | "warning" | "danger";
 }
 
 export const Alert: SFC<AlertProps> = ({ className, bootstrapStyle, message }) =>
     message
-        ? createElement("div", { className: classNames(`alert alert-${bootstrapStyle}`, className) }, message)
+        ? createElement("div", { className: classNames(`alert alert-${bootstrapStyle} ${className}`) }, message)
         : null;
 
 Alert.displayName = "Alert";
