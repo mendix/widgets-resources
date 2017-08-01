638a24611b918e8f7483ddea12feaa53836d6922
McDoyen <mrpoloh@yahoo.co.uk>
2017-08-01 09:43:13 +0300

    feat(star-rating): Update alert unit tests

--- packages-web/star-rating/src/components/__tests__/Alert.spec.ts
+++ packages-web/star-rating/src/components/__tests__/Alert.spec.ts
@@ -1,26 +1,56 @@
 import { shallow } from "enzyme";
 import { createElement } from "react";
-import { Alert } from "../Alert";
+
+import { Alert, AlertProps } from "../Alert";
 
 describe("Alert", () => {
+    const message = "This is an error";
+    const renderAlert = (props: AlertProps) => shallow(createElement(Alert, props));
+    const defaultProps: AlertProps = {
+        bootstrapStyle: "danger",
+        className: "widget-star-rating",
+        message
+    };
 
-    it("should render structure correctly", () => {
-        const message = "some alert message";
-        const renderAlert = shallow(createElement(Alert, {
-            bootstrapStyle: "danger",
-            className: "widget-star-rating-alert",
-            message
-        }));
+    it("renders the structure when an alert message is specified", () => {
+        const alert = renderAlert(defaultProps);
 
-        expect(renderAlert).toBeElement(
-            createElement("div", { className: "alert alert-danger widget-star-rating-alert" }, message)
+        expect(alert).toBeElement(
+            createElement("div", { className: "alert alert-danger widget-star-rating" }, message)
         );
     });
 
-    it("without message should not render anything", () => {
-        const renderAlert = shallow(createElement(Alert));
+    it("renders no structure when the alert message is not specified", () => {
+        const alert = shallow(createElement(Alert));
 
-        expect(renderAlert).toBeElement(null);
+        expect(alert).toBeElement(null);
     });
 
+    it("renders with the class of the specified bootstrap style", () => {
+        const alert = renderAlert(defaultProps);
+
+        expect(alert).toHaveClass("alert-danger");
+
+        alert.setProps({ bootstrapStyle: "default" });
+        expect(alert).toHaveClass("alert-default");
+
+        alert.setProps({ bootstrapStyle: "success" });
+        expect(alert).toHaveClass("alert-success");
+
+        alert.setProps({ bootstrapStyle: "primary" });
+        expect(alert).toHaveClass("alert-primary");
+
+        alert.setProps({ bootstrapStyle: "info" });
+        expect(alert).toHaveClass("alert-info");
+
+        alert.setProps({ bootstrapStyle: "warning" });
+        expect(alert).toHaveClass("alert-warning");
+    });
+
+    it("renders with the specified class name", () => {
+        defaultProps.className = "widget-unit-test-class";
+        const alert = renderAlert(defaultProps);
+
+        expect(alert).toHaveClass(defaultProps.className);
+    });
 });
