80f1e0ec767a748b05a6c226a3293a64df55f53a
Joseph D <j_seremba@live.com>
2017-08-23 18:56:13 +0300

    feat(switch): Update tests

--- packages-web/boolean-slider/tests/e2e/pages/home.page.ts
+++ packages-web/boolean-slider/tests/e2e/pages/home.page.ts
@@ -1,11 +1,14 @@
 class HomePage {
-    public get radioButton() { return browser.element(".mx-name-radioButtons5"); }
+    public get radioButton() { return browser.element(".mx-name-radioButtons6"); }
     public get switch1() { return browser.element(".mx-name-switch1"); }
-    public get switch2() { return browser.element(".mx-name-switch2"); }
-    public get switch3() { return browser.element(".mx-name-switch3"); }
+    public get switch2() {
+        return browser.element(".mx-name-switch2");
+    }
+    public get switch3() { return browser.element(".mx-name-switch4"); }
     public get popupWindow() { return browser.element(".mx-name-text1"); }
-    public get switch1Value() {
-        return browser.element(".widget-switch-btn-wrapper");
+    public get radioInput() { return browser.element(".mx-name-radioButtons6 input"); }
+    public get switchValue() {
+        return browser.element(".widget-switch-btn-wrapper-default.checked");
     }
 
     public open(): void {
