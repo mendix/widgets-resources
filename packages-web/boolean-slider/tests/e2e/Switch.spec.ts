ee4891c72c71b5a737deae8a2175546dd55345c3
Joseph D <j_seremba@live.com>
2017-08-24 10:13:52 +0300

    feat(switch): Code cleanup

--- packages-web/boolean-slider/tests/e2e/Switch.spec.ts
+++ packages-web/boolean-slider/tests/e2e/Switch.spec.ts
@@ -1,8 +1,8 @@
 import page from "./pages/home.page";
 
-const switchValue = "widget-switch-btn-wrapper-default.checked";
-const color = "rgba(100,189,99,1)";
-const defaultText = "false";
+const switchBgcolor = "rgba(100,189,99,1)";
+const switchBorder = "rgb(100,189,99)";
+const radioValue = "Yes";
 const popupValue = "IT WORKS";
 
 describe("Switch", () => {
@@ -12,7 +12,7 @@ describe("Switch", () => {
         page.switch1.waitForVisible();
 
         const content = page.switchValue.getCssProperty("background-color");
-        expect(content.value).toBe(color);
+        expect(content.value).toBe(switchBgcolor);
     });
 
     it("is updated by an attribute", () => {
@@ -21,17 +21,28 @@ describe("Switch", () => {
         page.radioButton.click();
 
         page.switch2.waitForVisible();
-        const changedslide = page.switch2.getHTML();
-        expect(page.switchValue.selector).toContain(switchValue);
+        const content = page.switchValue.getCssProperty("border-color");
+        expect(content.value).toBe(switchBorder);
+    });
+
+    it("updates attribute when clicked", () => {
+        page.open();
+        page.switch2.waitForVisible();
+        page.switch2.click();
+
+        page.radioButton.waitForVisible();
+        expect(page.radioButton.getText()).toContain(radioValue);
     });
 
     // Popup is not recognised by webdriverio
     xit("opens popup when clicked", () => {
         page.open();
         page.switch3.waitForVisible();
-        page.switch3.click();
+        page.switch3.clickAndWait();
 
-        const popupText = page.popupWindow.getText();
-        expect(page.popupWindow.alertText).toContain(popupValue);
+        page.popupWindow.waitForVisible();
+        if (browser.alertText()) {
+            expect(page.popupWindow.getText()).toContain(popupValue);
+        }
     });
 });
--- packages-web/boolean-slider/tests/e2e/pages/home.page.ts
+++ packages-web/boolean-slider/tests/e2e/pages/home.page.ts
@@ -1,14 +1,11 @@
 class HomePage {
-    public get radioButton() { return browser.element(".mx-name-radioButtons6"); }
+    public get radioButton() { return browser.element(".mx-name-radioButtons6 label"); }
     public get switch1() { return browser.element(".mx-name-switch1"); }
-    public get switch2() {
-        return browser.element(".mx-name-switch2");
-    }
-    public get switch3() { return browser.element(".mx-name-switch4"); }
-    public get popupWindow() { return browser.element(".mx-name-text1"); }
-    public get radioInput() { return browser.element(".mx-name-radioButtons6 input"); }
+    public get switch2() { return browser.element(".mx-name-switch2"); }
+    public get switch3() { return browser.element(".mx-name-switch3"); }
+    public get popupWindow() { return browser.alertAccept(); }
     public get switchValue() {
-        return browser.element(".widget-switch-btn-wrapper-default.checked");
+        return browser.element(".widget-switch-btn-wrapper");
     }
 
     public open(): void {
