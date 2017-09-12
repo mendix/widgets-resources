32d6a7504e0de1b360e06ce646758729dbc644e7
Edwin P. Magezi <emagezi@gmail.com>
2017-09-12 15:43:27 +0300

    feat(switch): Fix: add default danger class to alert

--- packages-web/boolean-slider/src/components/Alert.ts
+++ packages-web/boolean-slider/src/components/Alert.ts
@@ -4,7 +4,7 @@ import * as classNames from "classnames";
 export interface AlertProps {
     message?: string;
     className?: string;
-    bootstrapStyle: "default" | "primary" | "success" | "info" | "warning" | "danger";
+    bootstrapStyle?: "default" | "primary" | "success" | "info" | "warning" | "danger";
 }
 
 export const Alert: SFC<AlertProps> = ({ className, bootstrapStyle, message }) =>
@@ -12,4 +12,5 @@ export const Alert: SFC<AlertProps> = ({ className, bootstrapStyle, message }) =
         ? createElement("div", { className: classNames(`alert alert-${bootstrapStyle}`, className) }, message)
         : null;
 
+Alert.defaultProps = { bootstrapStyle: "danger" };
 Alert.displayName = "Alert";
