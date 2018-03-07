4b31ea83f37cf4fb342a61d3129852bb52c8a360
SamuelMuloki <samuellebron0@gmail.com>
2018-03-07 13:24:33 +0300

    feat(signature): code cleanUp

---
+++ packages-web/signature/src/components/canvas.ts
@@ -0,0 +1,27 @@
+import { Component, createElement } from "react";
+import * as classNames from "classnames";
+
+export interface CanvasProps {
+    width?: number;
+    gridx?: number;
+    gridy?: number;
+    height?: number;
+    gridBorder?: number;
+    getItemNode?: (ref: HTMLElement | null) => void;
+}
+
+export default class Canvas extends Component<CanvasProps> {
+    render() {
+        return createElement("canvas", {
+            gridx: this.props.gridx,
+            gridy: this.props.gridy,
+            height: this.props.height,
+            ref: node => { if (this.props.getItemNode) { this.props.getItemNode(node); } },
+            style: {
+                border: this.props.gridBorder + "px solid",
+                display: "block"
+            },
+            width: this.props.width
+        });
+    }
+}
