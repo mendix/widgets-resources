ad91f4692876e81fc76cd4bdc9060a5c9b38789a
SamuelMuloki <samuellebron0@gmail.com>
2018-03-20 12:43:26 +0300

    feat(signature): e2e implementation

--- packages-web/signature/tests/e2e/Signature.spec.ts
+++ packages-web/signature/tests/e2e/Signature.spec.ts
@@ -1,6 +1,7 @@
 import HomePage from "./pages/home.page";
 
 const border = "1px solid rgb(0, 0, 0)";
+const display = "block";
 
 describe("SignatureCanvas", () => {
 
@@ -10,5 +11,36 @@ describe("SignatureCanvas", () => {
 
         const canvasWidth = HomePage.canvas.getCssProperty("border");
         expect(canvasWidth.value).toBe(border);
+    });
+
+    it("renders signature", () => {
+        HomePage.open();
+        HomePage.canvas.waitForVisible();
+        HomePage.canvas.click();
+
+        HomePage.Signature.waitForVisible();
+        const displayButton = HomePage.Signature.getCssProperty("display");
+        expect(displayButton.value).toBe(display);
+    });
+
+    it("renders reset button", () => {
+        HomePage.open();
+        HomePage.canvas.waitForVisible();
+        HomePage.canvas.click();
+
+        HomePage.Signature.waitForVisible();
+        const displayButton = HomePage.button.getCssProperty("display");
+        expect(displayButton.value).toBe(display);
+    });
+
+    it("clears canvas on reset", () => {
+        HomePage.open();
+        HomePage.canvas.waitForVisible();
+        HomePage.canvas.click();
+
+        HomePage.button.waitForVisible();
+        HomePage.button.click();
+        const displayButton = HomePage.canvas.getCssProperty("display");
+        expect(displayButton.value).toBe(display);
     });
 });
