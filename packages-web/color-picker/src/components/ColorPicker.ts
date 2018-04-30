456b49312f51208c9f67da86a3cefd254d82768b
AKILENG ISAAC <isaacakileng56@gmail.com>
2018-04-30 16:28:47 +0300

    feat(color-picker-web): Initial color picker

--- packages-web/color-picker/src/components/ColorPicker.ts
+++ packages-web/color-picker/src/components/ColorPicker.ts
@@ -1,7 +1,17 @@
 import { Component, createElement } from "react";
+import { Color, ColorResult, SketchPicker } from "react-color";
 
-export class ColorPicker extends Component<{}, {}> {
+interface ColorPickerProps {
+    color?: Color;
+    type: PickerType;
+    onChange?: (color: ColorResult) => void;
+}
+
+export type PickerType = "sketch" | "photoshop" | "chrome" | "block" | "github" | "twitter" | "circle" | "hue" |
+    "aplha" | "slider" | "compact" | "material" | "swatches";
+
+export class ColorPicker extends Component<ColorPickerProps, {}> {
     render() {
-        return createElement("div", {}, "Color Picker");
+        return createElement(SketchPicker);
     }
 }
--- packages-web/color-picker/src/components/ColorPickerContainer.ts
+++ packages-web/color-picker/src/components/ColorPickerContainer.ts
@@ -1,8 +1,21 @@
 import { Component, createElement } from "react";
-import { ColorPicker } from "./ColorPicker";
+import { ColorPicker, PickerType } from "./ColorPicker";
 
-export default class ColorPickerContainer extends Component<{}, {}> {
+interface WrapperProps {
+    class: string;
+    mxObject?: mendix.lib.MxObject;
+    style: string;
+    readOnly: boolean;
+}
+
+interface ColorPickerContainerProps extends WrapperProps {
+    colorAttribute: string;
+    type: PickerType;
+    onChangeMicroflow: string;
+}
+
+export default class ColorPickerContainer extends Component<ColorPickerContainerProps, {}> {
     render() {
-        return createElement(ColorPicker);
+        return createElement(ColorPicker, { type: this.props.type });
     }
 }
