67edc18d83beb5f69503a577653fe834c0570f60
AKILENG ISAAC <isaacakileng56@gmail.com>
2018-04-30 18:43:02 +0300

    feat(color-picker-web): Update color value

--- packages-web/color-picker/src/components/ColorPickerContainer.ts
+++ packages-web/color-picker/src/components/ColorPickerContainer.ts
@@ -1,5 +1,6 @@
 import { Component, createElement } from "react";
 import { ColorPicker, PickerType } from "./ColorPicker";
+import { Color, ColorResult } from "react-color";
 
 interface WrapperProps {
     class: string;
@@ -14,8 +15,77 @@ interface ColorPickerContainerProps extends WrapperProps {
     onChangeMicroflow: string;
 }
 
-export default class ColorPickerContainer extends Component<ColorPickerContainerProps, {}> {
+interface ColorPickerContainerState {
+    alertMessage?: string;
+    color: Color;
+}
+
+export default class ColorPickerContainer extends Component<ColorPickerContainerProps, ColorPickerContainerState> {
+    private subscriptionHandles: number[];
+
+    constructor(props: ColorPickerContainerProps) {
+        super(props);
+
+        this.subscriptionHandles = [];
+        this.state = {
+            color: this.getValue(props.mxObject)
+        };
+    }
+
     render() {
-        return createElement(ColorPicker, { type: this.props.type });
+        return createElement(ColorPicker, {
+            color: this.state.color,
+            type: this.props.type,
+            onChange: this.updateColorValue
+        });
+    }
+
+    componentWillReceiveProps(newProps: ColorPickerContainerProps) {
+        this.resetSubscriptions(newProps.mxObject);
+        this.setState({
+            color: this.getValue(newProps.mxObject)
+        });
+    }
+
+    componentWillUnmount() {
+        this.subscriptionHandles.forEach(window.mx.data.unsubscribe);
+    }
+
+    private updateColorValue = (color: ColorResult) => {
+        const { mxObject, colorAttribute } = this.props;
+        if (color && mxObject) {
+            mxObject.set(colorAttribute, color.hex);
+        }
+    }
+
+    private getValue = (mxObject?: mendix.lib.MxObject): string => {
+        if (mxObject) {
+            return mxObject.get(this.props.colorAttribute) as string;
+        }
+
+        return "";
+    }
+
+    private resetSubscriptions(mxObject?: mendix.lib.MxObject) {
+        this.subscriptionHandles.forEach(window.mx.data.unsubscribe);
+        this.subscriptionHandles = [];
+
+        if (mxObject) {
+            this.subscriptionHandles.push(window.mx.data.subscribe({
+                attr: this.props.colorAttribute,
+                callback: this.handleSubscriptions,
+                guid: mxObject.getGuid()
+            }));
+            this.subscriptionHandles.push(window.mx.data.subscribe({
+                callback: this.handleSubscriptions,
+                guid: mxObject.getGuid()
+            }));
+        }
+    }
+
+    private handleSubscriptions = () => {
+        this.setState({
+            color: this.getValue(this.props.mxObject)
+        });
     }
 }
