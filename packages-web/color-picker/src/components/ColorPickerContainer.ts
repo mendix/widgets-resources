0891964873013ed71e9f88c06c6be27a90cc6097
AKILENG ISAAC <isaacakileng56@gmail.com>
2018-05-08 12:56:59 +0300

    feat(color-picker-web): Add on change event

--- packages-web/color-picker/src/components/ColorPickerContainer.ts
+++ packages-web/color-picker/src/components/ColorPickerContainer.ts
@@ -1,61 +1,83 @@
 import { Component, createElement } from "react";
-import { ColorPicker, PickerType, RenderMode } from "./ColorPicker";
+import { ColorPicker, Mode, PickerType } from "./ColorPicker";
 import { Color, ColorResult } from "react-color";
 
 interface WrapperProps {
     class: string;
     mxObject?: mendix.lib.MxObject;
+    mxform: mxui.lib.form._FormBase;
     style: string;
     readOnly: boolean;
+}
+
+interface Nanoflow {
+    nanoflow: object[];
+    paramsSpec: { Progress: string };
 }
 
 interface ColorPickerContainerProps extends WrapperProps {
     colorAttribute: string;
     type: PickerType;
-    renderMode: RenderMode;
-    onChangeMicroflow: string;
+    mode: Mode;
     format: string;
+    onChangeEvent: onChange;
+    onChangeMicroflow: string;
+    onChangePage: string;
+    onChangeNanoflow: Nanoflow;
 }
 
 interface ColorPickerContainerState {
     color: string;
+    alertMessage?: string;
 }
 
 type Format = "hex" | "rgb" | "rgba";
+type onChange = "doNothing" | "showPage" | "callMicroflow" | "callNanoflow";
 
 export default class ColorPickerContainer extends Component<ColorPickerContainerProps, ColorPickerContainerState> {
     private subscriptionHandles: number[];
+    private defaultColor = "#00ffff";
 
     constructor(props: ColorPickerContainerProps) {
         super(props);
 
         this.subscriptionHandles = [];
-        this.state = {
-            color: this.getValue(props.mxObject)
-        };
+        this.state = this.validateColor(props.mxObject);
     }
 
     render() {
-        const alertMessage = this.validateColor();
+        const alertMessage = this.state.alertMessage || ColorPickerContainer.validateProps(this.props);
 
         return createElement(ColorPicker, {
             alertMessage,
-            color: this.state.color || "#00ffff",
+            color: this.state.color,
             type: this.props.type,
-            mode: this.props.renderMode,
-            onChange: this.updateColorValue
+            mode: this.props.mode,
+            onChange: this.updateColorValue,
+            onChangeComplete: this.handleOnChange
         });
     }
 
     componentWillReceiveProps(newProps: ColorPickerContainerProps) {
         this.resetSubscriptions(newProps.mxObject);
-        this.setState({
-            color: this.getValue(newProps.mxObject)
-        });
+        this.setState(this.validateColor(newProps.mxObject));
     }
 
     componentWillUnmount() {
         this.subscriptionHandles.forEach(window.mx.data.unsubscribe);
+    }
+
+    public static validateProps(props: ColorPickerContainerProps): string {
+        let errorMessage = "";
+        if (props.onChangeEvent === "callMicroflow" && !props.onChangeMicroflow) {
+            errorMessage = "on change event is set to 'Call a microflow' but no microflow is selected";
+        } else if (props.onChangeEvent === "showPage" && !props.onChangePage) {
+            errorMessage = "on change event is set to 'Show a page' but no page is selected";
+        } else if (props.onChangeEvent === "callNanoflow" && (JSON.stringify(props.onChangeNanoflow) === JSON.stringify({}))) {
+            errorMessage = "on change event is set to 'Call a nanoflow' but no nanoflow is selected";
+        }
+
+        return errorMessage && `Error in color picker configuration: ${errorMessage}`;
     }
 
     private updateColorValue = (color: ColorResult) => {
@@ -102,12 +124,49 @@ export default class ColorPickerContainer extends Component<ColorPickerContainer
         });
     }
 
-    private validateColor = (): string => {
-        const { color } = this.state;
-        if (color.indexOf("#") === -1) {
-            return "Color value should be of format 'rgb', 'rgba' or 'hex'";
+    private validateColor = (mxObject?: mendix.lib.MxObject): ColorPickerContainerState => {
+        const color = this.getValue(mxObject);
+        if (color && color.indexOf("#") === -1) {
+            return {
+                alertMessage: "Color value should be of format 'rgb', 'rgba' or 'hex'",
+                color: this.defaultColor
+            };
         }
 
-        return "";
+        return {
+            alertMessage: "",
+            color
+        };
+    }
+
+    private handleOnChange = () => {
+        const { mxform, mxObject, onChangeEvent, onChangeMicroflow, onChangeNanoflow, onChangePage } = this.props;
+        if (mxObject) {
+            const context = new mendix.lib.MxContext();
+            context.setContext(mxObject.getEntity(), mxObject.getGuid());
+            if (onChangeEvent === "callMicroflow" && onChangeMicroflow) {
+                window.mx.ui.action(onChangeMicroflow, {
+                    error: error => window.mx.ui.error(`Error while executing microflow ${onChangeMicroflow}: ${error.message}`), // tslint:disable-line max-line-length
+                    params: {
+                        applyto: "selection",
+                        guids: [ mxObject.getGuid() ]
+                    },
+                    origin: mxform
+                });
+            } else if (onChangeEvent === "showPage" && onChangePage) {
+                window.mx.ui.openForm(onChangePage, {
+                    context,
+                    error: error => window.mx.ui.error(`Error while opening page ${onChangePage}: ${error.message}`),
+                    location: "content"
+                });
+            } else if (onChangeEvent === "callNanoflow" && onChangeNanoflow.nanoflow) {
+                window.mx.data.callNanoflow({
+                    context,
+                    error: error => window.mx.ui.error(`Error while executing the onchange nanoflow: ${error.message}`),
+                    nanoflow: onChangeNanoflow,
+                    origin: mxform
+                });
+            }
+        }
     }
 }
