2c1aeb2682bc46e85bd3512471d36f039b921115
AKILENG ISAAC <isaacakileng56@gmail.com>
2018-06-11 10:06:27 +0300

    feat(color-picker-web): Fix label alignment

--- packages-web/color-picker/src/components/__tests__/Label.spec.ts
+++ packages-web/color-picker/src/components/__tests__/Label.spec.ts
@@ -8,38 +8,45 @@ describe("Label", () => {
     const label = "Label";
 
     it("renders the structure correctly", () => {
-        const labelComponent = renderLabel({ label });
+        const labelComponent = renderLabel({ label, orientation: "horizontal" });
 
         expect(labelComponent).toBeElement(
-            createElement("div", { className: "widget-color-picker-label" },
-                createElement("div", { className: "form-group" },
-                    createElement("div", { className: "col-sm-6 col-xs-6" },
-                        createElement("label", { className: "control-label" }, label)
-                    ),
-                    createElement("div", { className: `col-sm-6` })
-                )
+            createElement("div", { className: "form-group" },
+                createElement("label", { className: "control-label col-sm-6" }, label),
+                createElement("div", { className: `col-sm-6` })
+            )
+        );
+    });
+
+    it("with orientation as vertical renders with the structure", () => {
+        const labelComponent = renderLabel({ label, orientation: "vertical" });
+
+        expect(labelComponent).toBeElement(
+            createElement("div", { className: "form-group" },
+                createElement("label", { className: "control-label" }, label),
+                createElement("div", { className: "" })
             )
         );
     });
 
     it("renders a label with the specified weight class", () => {
-        const labelComponent = renderLabel({ label, weight: 3 });
-        const colorLabel = labelComponent.childAt(0).childAt(0);
+        const labelComponent = renderLabel({ label, orientation: "horizontal", weight: 3 });
+        const colorLabel = labelComponent.childAt(0);
 
         expect(colorLabel).toHaveClass("col-sm-3");
     });
 
     it("renders the labeled element's wrapper with the calculated weight class", () => {
         const weight = 3;
-        const labelComponent = renderLabel({ label, weight });
-        const childrenWrapper = labelComponent.childAt(0).childAt(1);
+        const labelComponent = renderLabel({ label, orientation: "horizontal", weight });
+        const childrenWrapper = labelComponent.childAt(1);
 
         expect(childrenWrapper).toHaveClass(`col-sm-${12 - weight}`);
     });
 
     it("renders a label with calculated weight when specifed value is greater than 11", () => {
-        const labelComponent = renderLabel({ label, weight: 20 });
-        const colorLabel = labelComponent.childAt(0).childAt(0);
+        const labelComponent = renderLabel({ label, orientation: "horizontal", weight: 20 });
+        const colorLabel = labelComponent.childAt(0);
 
         expect(colorLabel).toHaveClass("col-sm-3");
     });
