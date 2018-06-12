842e3716aa5b15765028efb3f0fcc99bffaeb7db
Paul McDoyen <mrpoloh@yahoo.co.uk>
2018-06-12 09:41:30 +0300

    feat(calendar): Widget structure

---
+++ packages-web/calendar/src/Calendar.webmodeler.ts
@@ -0,0 +1,11 @@
+import { Component, createElement } from "react";
+
+import { Calendar } from "./components/Calendar";
+
+// tslint:disable-next-line class-name
+export class preview extends Component<{}> {
+    render() {
+        return createElement(Calendar, {
+        });
+    }
+}
