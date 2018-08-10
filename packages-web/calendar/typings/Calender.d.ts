012b2dd7cd749daf83cfbbce8b6e9c4f8f27aa2c
McDoyen <mrpoloh@yahoo.co.uk>
2018-08-10 17:09:36 +0300

    feat(calendar): Fix toolbar import

--- packages-web/calendar/typings/Calender.d.ts
+++ packages-web/calendar/typings/Calender.d.ts
@@ -2,4 +2,4 @@ declare module "globalize";
 declare module "react-big-calendar";
 declare module "react-big-calendar/lib/addons/dragAndDrop";
 declare module "react-big-calendar/lib/localizers/globalize";
-declare module "react-big-calendar/lib/ToolBar";
+declare module "react-big-calendar/lib/Toolbar";
