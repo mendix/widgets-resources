0522325d016b39ba65cc10afc64b6850a9cc6fc5
McDoyen <mrpoloh@yahoo.co.uk>
2018-08-13 13:06:49 +0300

    feat(calendar): Customise toolbar

--- packages-web/calendar/src/components/Toolbar.ts
+++ packages-web/calendar/src/components/Toolbar.ts
@@ -1,20 +1,31 @@
 import { createElement } from "react";
+import * as classNames from "classnames";
 import * as Toolbar from "react-big-calendar/lib/Toolbar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 export default class CustomToolbar extends Toolbar {
     render() {
         return (
-            createElement("div", { className: "rbc-toolbar" },
-                createElement("span", { className: "rbc-btn-group" },
-                    createElement("button", { onClick: () => this.navigate("PREV") }, "Back"),
-                    createElement("button", { onClick: () => this.navigate("TODAY") }, "Today"),
-                    createElement("button", { onClick: () => this.navigate("NEXT") }, "Next")
+            createElement("div", { className: classNames("rbc-toolbar", "mx-grid-controlbar") },
+                createElement("span", { className: "mx-grid-toolbar" },
+                    createElement("button", { className: "btn mx-button mx-name-paging-previous", onClick: () => this.navigate("PREV") },
+                        createElement("span", { className: "glyphicon glyphicon-backward" })),
+                    createElement("button", { className: "btn mx-button", onClick: () => this.navigate("TODAY") }, "Today"),
+                    createElement("button", { className: "btn mx-button mx-name-paging-next", onClick: () => this.navigate("NEXT") },
+                        createElement("span", { className: "glyphicon glyphicon-forward" }))
                 ),
                 createElement("span", { className: "rbc-toolbar-label" }, this.props.label),
-                createElement("span", { className: "rbc-btn-group" }, this.viewNamesGroup(this.props.messages))
+                createElement("span", { className: classNames("mx-grid-toolbar", "calendar-messages") }, this.viewNamesGroup(this.props.messages))
             )
         );
+    }
+
+    componentDidUpdate() {
+        const navigationButtonCollection = document.getElementsByClassName("calendar-messages");
+        const navigationButtonList = Array.from(navigationButtonCollection[0].getElementsByTagName("button"));
+        for (const value of navigationButtonList) {
+            value.classList.add("btn", "mx-button");
+        }
     }
 
     private navigate = (action: string) => {
