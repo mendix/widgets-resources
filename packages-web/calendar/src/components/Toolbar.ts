2d43e6a459e16b9f2ed8e7f2cc9aa2b2c900448e
Andries Smit <mr.andries.smit@gmail.com>
2018-09-04 11:37:52 +0200

    feat(calendar): Toolbar rendering generic for standard

--- packages-web/calendar/src/components/Toolbar.ts
+++ packages-web/calendar/src/components/Toolbar.ts
@@ -1,104 +1,84 @@
-import { createElement } from "react";
+import { ReactNode, createElement } from "react";
 import * as classNames from "classnames";
 import * as Toolbar from "react-big-calendar/lib/Toolbar";
 import { Style } from "../utils/namespaces";
-import { Button, Link } from "../components/Button";
+import { ToolbarButton } from "../components/Button";
+
 export default class CustomToolbar extends Toolbar {
 
     render() {
-        const leftButton = this.props.customViews.filter((customView: any) => customView.position === "left");
-        const centerButton = this.props.customViews.filter((customView: any) => customView.position === "center");
-        const rightButton = this.props.customViews.filter((customView: any) => customView.position === "right");
+        const leftButton = this.filterPosition("left");
+        const centerButton = this.filterPosition("center");
+        const rightButton = this.filterPosition("right");
 
-        if (this.props.viewOption === "standard") {
-            const standardViewButtons = [ "day", "week", "month" ];
-
-            return createElement("div", { className: classNames("calendar-toolbar") },
-                createElement("div", { className: "btn-group align-left" },
-                    createElement(Button, {
-                        onClick: () => this.props.onNavigate("PREV"),
-                        caption: createElement("span", { className: "glyphicon glyphicon-backward" })
-                    }),
-                    createElement(Button, { className: "btn", onClick: () => this.props.onNavigate("TODAY"), caption: "Today" }),
-                    createElement(Button, {
-                        onClick: () => this.props.onNavigate("NEXT"),
-                        caption: createElement("span", { className: "glyphicon glyphicon-forward" })
-                    })
-                ),
-                createElement("span", { className: "calendar-label" }, this.props.label),
-                createElement("span", { className: classNames("btn-group align-right") },
-                    standardViewButtons.map(button => createElement("button", {
-                        className: "btn",
-                        onClick: () => this.props.onViewChange(button)
-                    }, button.charAt(0).toUpperCase() + button.slice(1)))
-                )
-            );
-        } else {
-            return (
-                createElement("div", { className: classNames("calendar-toolbar") },
-                    this.createGroupButton(leftButton, "left"),
-                    this.createGroupButton(centerButton, "center"),
-                    this.createGroupButton(rightButton, "right")
-                )
-            );
-        }
+        return createElement("div", { className: classNames("calendar-toolbar") },
+            this.createGroupButton(leftButton, "left"),
+            this.createGroupButton(centerButton, "center"),
+            this.createGroupButton(rightButton, "right")
+        );
     }
 
-    private createGroupButton(views: string[], position: Style.Position) {
-        return createElement("div", { className: `btn-group align-${position}` },
-            (views).map((view) => this.createToolbarElement(view))
+    private filterPosition(position: string) {
+        return this.props.customViews.filter((customView: any) => customView.position === position);
+    }
+
+    // TODO wrap a group only buttons 2 or more buttons. without changing the order
+    // {group {left button}, {left button} }, {left title}, {group {right button}, {right button}} {right link}, {right button}
+    private createGroupButton(views: any[], position: Style.Position): ReactNode {
+        return createElement("div", { className: classNames(`align-${position}`, { "btn-group": true }) },
+            views.map(view => this.createToolbarElement(view))
         );
     }
 
     private createToolbarElement(view: any) {
         if (view.customView === "title") {
             return createElement("span", { className: "calendar-label" }, this.props.label);
-        } else if (view.customView === "previous") {
-            return createElement(Button, {
-                className: `btn-${view.buttonStyle}`,
-                title: view.buttonToolTip,
-                caption: createElement("span", {
-                    className: "glyphicon glyphicon-backward"
-                }),
-                onClick: () => this.props.onNavigate("PREV")
-            });
-        } else if (view.customView === "next") {
-            return createElement(Button, {
-                className: `btn-${view.buttonStyle}`,
-                title: view.buttonToolTip,
-                caption: createElement("span", {
-                    className: "glyphicon glyphicon-forward"
-                }),
-                onClick: () => this.props.onNavigate("NEXT")
-            });
-        } else if (view.customView === "today") {
-            if (view.renderMode === "button") {
-                return createElement(Button, {
-                    className: `btn-${view.buttonStyle}`,
-                    title: view.buttonToolTip,
-                    caption: view.customCaption,
-                    onClick: () => this.props.onNavigate("TODAY")
-                });
-            } else {
-                return createElement(Link, {
-                    title: view.buttonToolTip,
-                    onClick: () => this.props.onNavigate("TODAY"),
-                    caption: createElement("a", {}, view.customCaption)
-                });
-            }
-        } else if (view.renderMode === "button") {
-            return createElement(Button, {
-                className: `btn-${view.buttonStyle}`,
-                title: view.buttonToolTip,
-                caption: view.customCaption,
-                onClick: () => this.props.onViewChange(view.customView)
-            });
-        } else {
-            return createElement(Link, {
-                title: view.buttonToolTip,
-                onClick: () => this.props.onViewChange(view.customView),
-                caption: createElement("a", {}, view.customCaption)
-            });
         }
+
+        return createElement(ToolbarButton, {
+            renderMode: view.renderMode,
+            className: `toolbar-btn-${view.customView}`,
+            active: this.props.view === view.customView,
+            title: view.buttonToolTip,
+            icon: this.getIcon(view.customView),
+            iconPosition: this.getIconPosition(view.customView),
+            caption: view.customCaption,
+            onClick: this.getOnClickFunction(view.customView)
+        });
     }
+
+    private getOnClickFunction(customView: any): () => void {
+        if (customView === "title") {
+            return () => { /* */ };
+        } else if (customView === "previous") {
+            return () => this.props.onNavigate("PREV");
+        } else if (customView === "next") {
+            return () => this.props.onNavigate("NEXT");
+        } else if (customView === "today") {
+            return () => this.props.onNavigate("TODAY");
+        }
+
+        return () => this.props.onViewChange(customView);
+    }
+
+    private getIcon(view: any): string | undefined {
+        if (view === "previous") {
+            return "glyphicon glyphicon-backward";
+        } else if (view === "next") {
+            return "glyphicon glyphicon-forward";
+        }
+
+        return undefined;
+    }
+
+    private getIconPosition(view: any): "left" | "right" | undefined {
+        if (view === "previous") {
+            return "left";
+        } else if (view === "next") {
+            return "right";
+        }
+
+        return undefined;
+    }
+
 }
