83c5b6012ac0a63daf7a785924f2af4afe02bb26
SamuelMuloki <samuellebron0@gmail.com>
2018-11-05 09:58:30 +0300

    feat(signature): Update e2e tests

--- packages-web/signature/tests/e2e/pages/home.page.ts
+++ packages-web/signature/tests/e2e/pages/home.page.ts
@@ -1,9 +1,9 @@
 class HomePage {
-    public get canvases() { return browser.elements(".signature-unset canvas"); }
-    public get canvas() { return browser.element(".signature-unset canvas"); }
-    public get saveButton() { return browser.element(".signature-unset button.btn.btn-primary"); }
+    public get canvases() { return browser.elements(".widget-signature-wrapper canvas"); }
+    public get canvas() { return browser.element(".widget-signature-wrapper canvas.widget-Signature.signature-canvas"); }
+    public get saveButton() { return browser.element(".mx-dataview .mx-dataview-controls .mx-button:last-child"); }
     public get dialogBox() { return browser.element(".mx-dialog-content"); }
-    public get resetButton() { return browser.element(".signature-unset button.btn.btn-default"); }
+    public get resetCheckbox() { return browser.element("div#mxui_widget_RadioButtonGroup_0 > input[type='checkbox']"); }
 
     public open(): void {
         browser.url("/");
