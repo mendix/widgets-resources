f8bad19730de104e461561e52be7a712a42924a2
Andries Smit <mr.andries.smit@gmail.com>
2018-11-20 08:19:55 +0100

    feat(signature): Make grid stateless component

--- packages-web/signature/src/components/Grid.tsx
+++ packages-web/signature/src/components/Grid.tsx
@@ -5,20 +5,23 @@ interface GridBackgroundProps {
     gridCellHeight: number;
     gridBorderColor: string;
     gridBorderWidth: number;
+    showGrid?: boolean;
 }
-
-export default class Grid extends React.Component<GridBackgroundProps> {
-    render() {
-        const { gridCellWidth, gridCellHeight, gridBorderColor, gridBorderWidth } = this.props;
+export const Grid: React.SFC<GridBackgroundProps> =
+    ({ gridCellWidth, gridCellHeight, gridBorderColor, gridBorderWidth, showGrid }) => {
         const id = `grid${Math.floor(Math.random() * 1000000)}`;
-        return <svg className="widget-signature-grid" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
-            <defs>
-                <pattern id={id} width={gridCellWidth} height={gridCellHeight} patternUnits="userSpaceOnUse">
-                    <line x1="0" y1={gridCellHeight} x2={gridCellWidth} y2={gridCellHeight} stroke={gridBorderColor} stroke-width={gridBorderWidth} />
-                    <line x1={gridCellWidth} y1="0" x2={gridCellWidth} y2={gridCellHeight} stroke={gridBorderColor} stroke-width={gridBorderWidth} />
-                </pattern>
-            </defs>
-            <rect width="100%" height="100%" fill={`url(#${id})`} />
-        </svg>;
-    }
-}
+        return showGrid
+            ? <svg className="widget-signature-grid" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+                <defs>
+                    <pattern id={id} width={gridCellWidth} height={gridCellHeight} patternUnits="userSpaceOnUse">
+                        <line x1="0" y1={gridCellHeight} x2={gridCellWidth} y2={gridCellHeight} stroke={gridBorderColor} stroke-width={gridBorderWidth} />
+                        <line x1={gridCellWidth} y1="0" x2={gridCellWidth} y2={gridCellHeight} stroke={gridBorderColor} stroke-width={gridBorderWidth} />
+                    </pattern>
+                </defs>
+                <rect width="100%" height="100%" fill={`url(#${id})`} />
+            </svg>
+            : null;
+    };
+
+Grid.displayName = "Grid";
+Grid.defaultProps = { showGrid: true };
