1eff24765aa83e671b5e38b8ff688ceb6cf6b4bb
Andries Smit <mr.andries.smit@gmail.com>
2018-12-12 12:45:24 +0100

    feat(maps-web): Fix ration dimentions

--- packages-web/maps/src/utils/Utils.ts
+++ packages-web/maps/src/utils/Utils.ts
@@ -22,9 +22,13 @@ export default class Utils {
             width: props.widthUnit === "percentage" ? `${props.width}%` : `${props.width}px`
         };
         if (props.heightUnit === "percentageOfWidth") {
-            style.paddingBottom = props.widthUnit === "percentage"
-                ? `${props.height}%`
-                : `${props.width / 2}px`;
+            const ratio = props.height / 100 * props.width;
+            if (props.widthUnit === "percentage") {
+                style.height = "auto";
+                style.paddingBottom = `${ratio}%`;
+            } else {
+                style.height = `${ratio}px`;
+            }
         } else if (props.heightUnit === "pixels") {
             style.height = `${props.height}px`;
         } else if (props.heightUnit === "percentageOfParent") {
