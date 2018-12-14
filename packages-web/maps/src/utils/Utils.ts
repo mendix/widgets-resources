91d3eee66ddea308f8a4a91eba369c63b39a15c3
Andries Smit <mr.andries.smit@gmail.com>
2018-12-14 14:32:02 +0100

    feat(maps-web): Move parseStyle to Utils

--- packages-web/maps/src/utils/Utils.ts
+++ packages-web/maps/src/utils/Utils.ts
@@ -37,4 +37,24 @@ export default class Utils {
 
         return style;
     }
+
+    static parseStyle(style = ""): CSSProperties { // Doesn't support a few stuff.
+        try {
+            return style.split(";").reduce<{[key: string]: string}>((styleObject, line) => {
+                const pair = line.split(":");
+                if (pair.length === 2) {
+                    const name = pair[0].trim().replace(/(-.)/g, match => match[1].toUpperCase());
+                    styleObject[name] = pair[1].trim();
+                }
+
+                return styleObject;
+            }, {});
+        } catch (error) {
+            // tslint:disable-next-line no-console
+            window.console.log("Failed to parse style", style, error);
+        }
+
+        return {};
+    }
+
 }
