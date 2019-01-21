0afea4dd942334338407bfcbbb32983aa4d2e69c
Job van Stiphout <job.van.stiphout@mendix.com>
2019-01-21 11:21:03 +0100

    feat(progress-bar): switch to custom progress bar with more options

--- packages/progress-bar/src/ProgressBar.tsx
+++ packages/progress-bar/src/ProgressBar.tsx
@@ -1,33 +1,46 @@
 import React, { Component } from "react";
-import { Platform, ProgressBarAndroid, ProgressViewIOS } from "react-native";
+import { Bar } from "react-native-progress";
 
 interface Props {
-    progressValue: PluginWidget.EditableValue<number>;
-    progressMin: PluginWidget.EditableValue<number>;
-    progressMax: PluginWidget.EditableValue<number>;
+    indeterminate: boolean;
+    progressValue?: PluginWidget.EditableValue<number>;
+    progressMax?: PluginWidget.EditableValue<number>;
+    animated: boolean;
     color?: string;
+    unfilledColor?: string;
+    borderColor?: string;
+    borderWidth: number;
+
+    height?: number;
+    borderRadius?: number;
 }
 
 export class ProgressBar extends Component<Props> {
     private get progress(): number {
-        const min = this.props.progressMin.value || 0;
-        const max = this.props.progressMax.value || 100;
-        const value = this.props.progressValue.value || 0;
-        return value / (max - min);
+        const { progressMax, progressValue } = this.props;
+        const max = progressMax && progressMax.value != null ? Number(progressMax.value) : 100;
+        const value = progressValue && progressValue.value != null ? Number(progressValue.value) : 0;
+
+        if (this.props.indeterminate || max === 0) {
+            return 0;
+        }
+
+        return value / max;
     }
 
     render(): JSX.Element {
-        return Platform.select({
-            ios: this.renderIOS(),
-            android: this.renderAndroid()
-        });
-    }
-
-    private renderIOS(): JSX.Element {
-        return <ProgressViewIOS progress={this.progress} progressTintColor={this.props.color} />;
-    }
-
-    private renderAndroid(): JSX.Element {
-        return <ProgressBarAndroid progress={this.progress} color={this.props.color} />;
+        return (
+            <Bar
+                progress={this.progress}
+                indeterminate={this.props.indeterminate}
+                animated={this.props.animated}
+                {...(this.props.color ? { color: this.props.color } : {})}
+                {...(this.props.unfilledColor ? { color: this.props.unfilledColor } : {})}
+                {...(this.props.borderColor ? { color: this.props.borderColor } : {})}
+                borderWidth={this.props.borderWidth}
+                {...(this.props.height ? { height: this.props.height } : {})}
+                {...(this.props.borderRadius != null ? { borderRadius: this.props.borderRadius } : {})}
+            />
+        );
     }
 }
