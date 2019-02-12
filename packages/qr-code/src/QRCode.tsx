30341bb2cb1009a023bce859ab047fb3c639c589
Job van Stiphout <job.van.stiphout@mendix.com>
2019-02-12 14:05:36 +0100

    fix(qr-code): do not render on empty value

--- packages/qr-code/src/QRCode.tsx
+++ packages/qr-code/src/QRCode.tsx
@@ -4,7 +4,11 @@ import RNQRCode from "react-native-qrcode-svg";
 import { QRCodeProps } from "../typings/QRCodeProps";
 
 export class QRCode extends Component<QRCodeProps> {
-    render(): JSX.Element {
+    render(): JSX.Element | null {
+        if (!this.props.value.value) {
+            return null;
+        }
+
         return (
             <RNQRCode
                 value={this.props.value.value}
