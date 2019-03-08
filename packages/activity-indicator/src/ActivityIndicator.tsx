0e985cc38147dafc7456971fa98bc9340c6227dc
Diego Antonelli <diego.antonelli@mendix.com>
2019-03-08 10:06:37 +0100

    feat(activity-indicator): adding style for the main container

--- packages/activity-indicator/src/ActivityIndicator.tsx
+++ packages/activity-indicator/src/ActivityIndicator.tsx
@@ -1,9 +1,10 @@
 import { Component, createElement } from "react";
-import { ActivityIndicator as RNActivityIndicator } from "react-native";
+import { ActivityIndicator as RNActivityIndicator, ViewStyle } from "react-native";
 import { ActivityIndicatorProps } from "../typings/ActivityIndicatorProps";
 import { flattenStyles, Style } from "./utils/common";
 
 interface ActivityIndicatorStyle extends Style {
+    container: ViewStyle;
     indicator: {
         color: string;
     };
@@ -12,13 +13,20 @@ interface ActivityIndicatorStyle extends Style {
 const defaultActivityStyle: ActivityIndicatorStyle = {
     indicator: {
         color: "gray"
-    }
+    },
+    container: {}
 };
 
 export class ActivityIndicator extends Component<ActivityIndicatorProps<ActivityIndicatorStyle>> {
     private readonly styles = flattenStyles(defaultActivityStyle, this.props.style);
 
     render(): JSX.Element {
-        return <RNActivityIndicator size={this.props.size} color={this.styles.indicator.color} />;
+        return (
+            <RNActivityIndicator
+                size={this.props.size}
+                color={this.styles.indicator.color}
+                style={this.styles.container}
+            />
+        );
     }
 }
