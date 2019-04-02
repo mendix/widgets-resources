47850ecdde5f18f05e60b2c379ff64c871ab5012
Job van Stiphout <job.van.stiphout@mendix.com>
2019-04-02 10:45:14 +0200

    feat(nanoflow-commons): fix: specific location permission request for android

--- packages-native/nanoflow-commons/src/geolocation/RequestLocationPermission.ts
+++ packages-native/nanoflow-commons/src/geolocation/RequestLocationPermission.ts
@@ -4,20 +4,35 @@
 // - the code between BEGIN USER CODE and END USER CODE
 // Other code you write will be lost the next time you deploy the project.
 
+import ReactNative from "react-native";
+
 /**
  * @returns {boolean}
  */
-function RequestLocationPermission(): boolean {
+function RequestLocationPermission(): Promise<boolean> {
     // BEGIN USER CODE
 
-    if (navigator && navigator.geolocation) {
-        if (navigator.geolocation.requestAuthorization) {
-            navigator.geolocation.requestAuthorization();
+    if (navigator && navigator.product === "ReactNative") {
+        const RN: typeof ReactNative = require("react-native");
+
+        if (RN.Platform.OS === "android") {
+            const locationPermission = RN.PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION;
+
+            return RN.PermissionsAndroid.check(locationPermission).then(hasPermission =>
+                hasPermission
+                    ? true
+                    : RN.PermissionsAndroid.request(locationPermission).then(
+                          status => status === RN.PermissionsAndroid.RESULTS.GRANTED
+                      )
+            );
+        } else {
+            if (navigator.geolocation && navigator.geolocation.requestAuthorization) {
+                navigator.geolocation.requestAuthorization();
+            }
         }
-        return true;
     }
 
-    return false;
+    return Promise.resolve(true);
 
     // END USER CODE
 }
