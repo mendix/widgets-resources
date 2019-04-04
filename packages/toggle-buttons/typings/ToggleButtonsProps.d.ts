bfe5f4f84b217ff1a21961f844cf3155c036e068
Job van Stiphout <job.van.stiphout@mendix.com>
2019-04-04 09:39:01 +0200

    fix(toggle-buttons): remove wrong icon, add name property

--- packages/toggle-buttons/typings/ToggleButtonsProps.d.ts
+++ packages/toggle-buttons/typings/ToggleButtonsProps.d.ts
@@ -1,5 +1,5 @@
 /**
- * This file was generated from SegmentedControl.xml
+ * This file was generated from ToggleButtons.xml
  * WARNING: All changes made to this file will be overwritten
  * @author Mendix Widgets Team
  */
