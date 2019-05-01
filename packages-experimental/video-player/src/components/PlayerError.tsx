d7a3aea49a4031854d5694eda0a99e029b9342f0
Diego Antonelli <diego.antonelli@mendix.com>
2019-05-01 13:39:07 +0200

    feat(video-player): Changing error message

--- packages-experimental/video-player/src/components/PlayerError.tsx
+++ packages-experimental/video-player/src/components/PlayerError.tsx
@@ -4,7 +4,7 @@ export const PlayerError: FunctionComponent<{ preview?: boolean }> = props =>
     (
         <div className="widget-video-player-error-container">
             <div
-                className="video-error-label">{props.preview ? "The actual video rendering will be based on the attribute content." : "We are unable to show the video content :("}</div>
+                className="video-error-label">{props.preview ? "The actual video rendering will be based on the attribute content." : "The video failed to load :("}</div>
             <video height="100%" width="100%" controls={true}></video>
         </div>
     );
