a46c1df66d4e655a11026a134665c519560ae819
Wesley <wesley.sieses@mendix.com>
2019-05-15 11:56:54 +0200

    Revert "fix(checkbox): add disabled styling"
    This reverts commit d904b3498438f0937d48a200467f6650cfb1594c.

--- styles/native/core/widgets/checkbox.js
+++ styles/native/core/widgets/checkbox.js
@@ -28,10 +28,6 @@ export const CheckBox = {
         thumbColorOff: contrast.low,
         trackColorOff: `rgba(${hexToRgbString(contrast.low)},0.2)`,
     },
-    inputDisabled: {
-        // thumbColorOn, thumbColorOff, trackColorOn, trackColorOff and all TextStyle properties are allowed
-        opacity: 0.5,
-    },
     inputError: {
         // thumbColorOn, thumbColorOff, trackColorOn, trackColorOff and all TextStyle properties are allowed
         ...TextBox.inputError,
