772826799a8c08492d55e4771a22e18a507bbbc4
Diego Antonelli <45102481+diego-antonelli@users.noreply.github.com>
2019-06-18 14:10:03 +0200

    Aligning ESLint rules (#4)

--- packages-common/pluggable-widgets-generator/generators/app/templates/pluggable/native/fullTemplateTs/src/utils/common.ts
+++ packages-common/pluggable-widgets-generator/generators/app/templates/pluggable/native/fullTemplateTs/src/utils/common.ts
@@ -8,7 +8,7 @@ export interface Style {
     [key: string]: CustomStyle | ViewStyle | TextStyle | ImageStyle;
 }
 
-export function flattenStyles<T extends Style>(defaultStyle: T, overrideStyles: (T | undefined)[]): T {
+export function flattenStyles<T extends Style>(defaultStyle: T, overrideStyles: Array<(T | undefined)>): T {
     const styles = [defaultStyle, ...overrideStyles.filter((object): object is T => object !== undefined)];
 
     return Object.keys(defaultStyle).reduce(
