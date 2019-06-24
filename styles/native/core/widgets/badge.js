d2a55b7c9e239153e429174dcc4b706f33020680
Wesley <wesley.sieses@mendix.com>
2019-06-24 16:31:24 +0200

    fix(badge): Centered badge

--- styles/native/core/widgets/badge.js
+++ styles/native/core/widgets/badge.js
@@ -10,6 +10,7 @@ export const com_mendix_widget_native_badge_Badge = (Badge = {
     container: {
         // All ViewStyle properties are allowed
         backgroundColor: contrast.lower,
+        alignSelf: 'center',
     },
     caption: {
         // All TextStyle properties are allowed
