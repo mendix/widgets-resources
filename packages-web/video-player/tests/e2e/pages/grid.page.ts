1b7d6a334d6d20d70ba715cde604e18f916edc4a
Diego Antonelli <diego.antonelli@mendix.com>
2019-08-05 17:05:58 +0200

    chore: adding new tasks to use widget-tools

--- packages-web/video-player/tests/e2e/pages/grid.page.ts
+++ packages-web/video-player/tests/e2e/pages/grid.page.ts
@@ -1,15 +1,15 @@
 class GridPage {
-    // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
+    // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility,@typescript-eslint/explicit-function-return-type
     public get youtube(){
         return $(".widget-video-player.widget-video-player-container.mx-name-videoPlayer1.size-box iframe");
     }
 
-    // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
+    // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility,@typescript-eslint/explicit-function-return-type
     public get vimeo(){
         return $(".widget-video-player.widget-video-player-container.mx-name-videoPlayer2.size-box iframe");
     }
 
-    // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
+    // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility,@typescript-eslint/explicit-function-return-type
     public open(): void {
         browser.url("/p/grid");
     }
