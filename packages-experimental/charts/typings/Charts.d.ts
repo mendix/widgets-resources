6b113724b0ede7bb55983950a461911ae77b5c0b
Diego Antonelli <diego.antonelli@mendix.com>
2019-08-06 12:45:38 +0200

    chore: removing charts

--- packages-experimental/charts/typings/Charts.d.ts
+++
@@ -1,8 +0,0 @@
-import { PathProps } from "react-native-svg";
-import { ChartProps as RNChartProps } from "react-native-svg-charts";
-
-declare module "react-native-svg-charts" {
-    export interface ChartProps<T> extends RNChartProps<T> {
-        svgs?: Array<Partial<PathProps>>;
-    }
-}
