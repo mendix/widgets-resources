78b2ed7eac296dc1e574903f1b4c42171c08d69b
Diego Antonelli <diego.antonelli@mendix.com>
2019-08-08 10:04:50 +0200

    feat(badge-web): updating e2e tests to WDIO 5

--- packages-web/badge/tests/e2e/pages/home.page.ts
+++ packages-web/badge/tests/e2e/pages/home.page.ts
@@ -1,24 +1,41 @@
+/* eslint-disable */
 class HomePage {
+    public get badge() {
+        return $("#mxui_widget_ReactCustomWidgetWrapper_4");
+    }
 
-    public get badge() { return browser.element("#mxui_widget_ReactCustomWidgetWrapper_4"); }
+    public get badgeSuccess() {
+        return $(".mx-name-badge6");
+    }
 
-    public get badgeSuccess() { return browser.element(".mx-name-badge6"); }
+    public get badgeDanger() {
+        return $("#mxui_widget_ReactCustomWidgetWrapper_8");
+    }
 
-    public get badgeDanger() { return browser.element("#mxui_widget_ReactCustomWidgetWrapper_8"); }
+    public get labelSuccess() {
+        return $("#mxui_widget_ReactCustomWidgetWrapper_3");
+    }
 
-    public get labelSuccess() { return browser.element("#mxui_widget_ReactCustomWidgetWrapper_3"); }
+    public get labelDanger() {
+        return $("#mxui_widget_ReactCustomWidgetWrapper_9");
+    }
 
-    public get labelDanger() { return browser.element("#mxui_widget_ReactCustomWidgetWrapper_9"); }
+    public get input() {
+        return $("#mxui_widget_TextInput_0_input");
+    }
 
-    public get input() { return browser.element("#mxui_widget_TextInput_0_input"); }
+    public get label() {
+        return $("#mxui_widget_ReactCustomWidgetWrapper_1");
+    }
 
-    public get label() { return browser.element("#mxui_widget_ReactCustomWidgetWrapper_1"); }
-
-    public get saveButton() { return browser.element("##mxui_widget_ActionButton_0"); }
+    public get saveButton() {
+        return $("##mxui_widget_ActionButton_0");
+    }
 
     public open(): void {
         browser.url("/");
     }
 }
+
 const homepage = new HomePage();
 export default homepage;
