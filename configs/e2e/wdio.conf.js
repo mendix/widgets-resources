27cfd5ec130dfaaa8fd6372b31a63b999cf9e3e4
Diego Antonelli <diego.antonelli@mendix.com>
2019-08-08 14:57:12 +0200

    fix(videoplayer): fixing e2e tests

---
+++ configs/e2e/wdio.conf.js
@@ -0,0 +1,41 @@
+const debug = process.env.DEBUG;
+
+exports.config = {
+    before() {
+        require("ts-node").register({ files: true, project: "./tests/e2e/tsconfig.json" });
+    },
+    host: "127.0.0.1",
+    port: 4444,
+    specs: ["./tests/e2e/*.spec.ts"],
+    maxInstances: debug ? 1 : 5,
+    capabilities: [
+        {
+            maxInstances: debug ? 1 : 5,
+            browserName: "chrome"
+        }
+    ],
+    sync: true,
+    logLevel: "silent",
+    coloredLogs: true,
+    bail: 0,
+    screenshotPath: "./dist/wdio/",
+    baseUrl: debug ? "http://localhost:8080/" : process.env.URL,
+    waitforTimeout: 30000,
+    connectionRetryTimeout: 90000,
+    connectionRetryCount: 0,
+    services: ["selenium-standalone"],
+
+    framework: "jasmine",
+    reporters: ["spec"],
+    execArgv: debug ? ["--inspect"] : undefined,
+    // Options to be passed to Jasmine.
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: debug ? 60 * 60 * 1000 : 30 * 1000,
+        expectationResultHandler(passed, assertion) {
+            if (passed) {
+                return;
+            }
+            browser.saveScreenshot(`./dist/wdio/assertionError_${assertion.error.message}.png`);
+        }
+    }
+};
