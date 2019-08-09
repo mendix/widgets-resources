e0617ee8458f4ee1baa571b741482b25b62ba2f2
Andries Smit <Andries.Smit@mendix.com>
2019-08-09 10:40:26 +0200

    chore(utils-react-widgets): clean up code

--- packages-web/utils-react-widgets/configs/variables.js
+++ packages-web/utils-react-widgets/configs/variables.js
@@ -7,10 +7,6 @@ let pathToJoin = "";
 if (indexOf > -1 && args.length > indexOf + 1) {
     pathToJoin = args[indexOf + 1];
 }
-const gruntArg = args.find(a => a.startsWith("--subprojectPath="));
-if (gruntArg) {
-    pathToJoin = gruntArg.replace("--subprojectPath=", "");
-}
 
 const newPath = path.join(__dirname, "../../../", pathToJoin);
 const pkg = require(path.join(newPath, "package.json"));
@@ -24,9 +20,6 @@ try {
 } catch (err) {
     extension = "ts";
 }
-// const dirCont = fs.readdirSync(path.join(newPath, "/src/"));
-// let regex = new RegExp(pkg.widgetName, "gi");
-// const files = dirCont.filter(f => f.match(regex))[0].;
 
 module.exports = {
     path: newPath,
