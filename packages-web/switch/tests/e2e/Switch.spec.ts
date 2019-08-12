a6582f694cdc7d5b227bf75b0a8a562eb3926f3a
Diego Antonelli <diego.antonelli@mendix.com>
2019-08-12 13:04:19 +0200

    fix(switch): fixing e2e tests

--- packages-web/switch/tests/e2e/Switch.spec.ts
+++ packages-web/switch/tests/e2e/Switch.spec.ts
@@ -6,41 +6,40 @@ const radioValueTrue = "Yes";
 const popupValue = "IT WORKS";
 
 describe("Switch", () => {
-
     it("changes color when checked", () => {
         page.open();
-        page.switch1.waitForVisible();
+        page.switch1.waitForDisplayed();
 
-        const content = page.switchValue.getCssProperty("background-color");
+        const content = page.switchValue.getCSSProperty("background-color");
         expect(content.value).toBe(switchBackgroundColor);
     });
 
     it("is updated by an attribute", () => {
         page.open();
-        page.radioButton.waitForVisible();
+        page.radioButton.waitForDisplayed();
         page.radioButton.click();
 
-        page.switch2.waitForVisible();
-        const content = page.switchValue.getCssProperty("border-color");
+        page.switch2.waitForDisplayed();
+        const content = page.switchValue.getCSSProperty("border-color");
         expect(content.value).toBe(switchBorderColor);
     });
 
     it("updates attribute when clicked", () => {
         page.open();
-        page.switch2.waitForVisible();
+        page.switch2.waitForDisplayed();
         page.switch2.click();
 
-        page.radioButton.waitForVisible();
+        page.radioButton.waitForDisplayed();
         expect(page.radioButton.getText()).toContain(radioValueTrue);
     });
 
     it("opens popup when clicked", () => {
         page.open();
-        page.switch3.waitForVisible();
+        page.switch3.waitForDisplayed();
         page.switch3.click();
 
         setTimeout(() => {
-            page.popup.waitForVisible();
+            page.popup.waitForDisplayed();
             expect(page.popup.getText()).toContain(popupValue);
         }, 3000);
     });
--- packages-web/switch/tests/e2e/pages/home.page.ts
+++ packages-web/switch/tests/e2e/pages/home.page.ts
@@ -1,11 +1,22 @@
+/* eslint-disable */
 class HomePage {
-    public get radioButton() { return browser.element(".mx-name-radioButtons6 label"); }
-    public get switch1() { return browser.element(".mx-name-switch1"); }
-    public get switch2() { return browser.element(".mx-name-switch2"); }
-    public get switch3() { return browser.element(".mx-name-switch3"); }
-    public get popup() { return browser.element("#mxui_widget_Window_0"); }
+    public get radioButton() {
+        return $(".mx-name-radioButtons6 label");
+    }
+    public get switch1() {
+        return $(".mx-name-switch1");
+    }
+    public get switch2() {
+        return $(".mx-name-switch2");
+    }
+    public get switch3() {
+        return $(".mx-name-switch3");
+    }
+    public get popup() {
+        return $("#mxui_widget_Window_0");
+    }
     public get switchValue() {
-        return browser.element(".widget-switch-btn-wrapper");
+        return $(".widget-switch-btn-wrapper");
     }
 
     public open(): void {
