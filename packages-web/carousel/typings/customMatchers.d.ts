b8f51da57815d1a5f9890f8dcbf347a329a6e4c4
Diego Antonelli <diego.antonelli@mendix.com>
2019-08-14 11:46:51 +0200

    feat(carousel): removing files

--- packages-web/carousel/typings/customMatchers.d.ts
+++
@@ -1,9 +0,0 @@
-// Note: cannot use ReactElement here, because importing a module from the declaration of a namespace is not allowed.
-// tslint:disable-next-line
-declare namespace jasmine {
-    interface Matchers {
-        toBeElement(expected: any): boolean;
-        toHaveClass(expected: string): boolean;
-        toMatchStructure(expected: any): boolean;
-    }
-}
