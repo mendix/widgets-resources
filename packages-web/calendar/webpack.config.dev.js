316d6ca189840ac303e1a54efc74f0d664efe344
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2019-08-27 16:58:55 +0200

    fix(calendar): fix IE11 bugs
    - Persisting problem for all browsers: resizing and moving in drag and drop calendar causes unwanted event manipulations

---
+++ packages-web/calendar/webpack.config.dev.js
@@ -0,0 +1,46 @@
+const merge = require("webpack-merge");
+const baseConfig = require("../utils-react-widgets/configs/webpack.config.dev");
+
+const overridingConfig = {
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        cacheDirectory: true,
+                        presets: [
+                            [
+                                "@babel/preset-env",
+                                {
+                                    corejs: 3,
+                                    debug: true,
+                                    targets: {
+                                        chrome: "49",
+                                        firefox: "52",
+                                        edge: "16",
+                                        safari: "9",
+                                        ie: "11",
+                                        ios: "9",
+                                        android: "5"
+                                    },
+                                    useBuiltIns: "usage"
+                                }
+                            ],
+                            "@babel/preset-react"
+                        ],
+                        plugins: [
+                            ["@babel/plugin-proposal-class-properties", { loose: true }],
+                            ["@babel/plugin-transform-react-jsx", { pragma: "createElement" }],
+                            "react-hot-loader/babel"
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+};
+
+module.exports = [baseConfig[0], merge(baseConfig[1], overridingConfig)];
