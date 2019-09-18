0033076958036e4fecf23ee3dc23af6fa44daecc
Diego Antonelli <diego.antonelli@mendix.com>
2019-09-18 11:06:05 +0200

    feat(listview-swipe): adding typings comments

--- packages-native/listview-swipe/typings/ReactNativeSwipeableRow.d.ts
+++ packages-native/listview-swipe/typings/ReactNativeSwipeableRow.d.ts
@@ -6,50 +6,62 @@ declare module "react-native-swipeable-row" {
         children: ReactNode;
         /**
          * Left content visible during pull action
+         * @default null
          */
         leftContent?: ReactNode;
         /**
          * Right content visible during pull action
+         * @default null
          */
         rightContent?: ReactNode;
         /**
          * Array of buttons, first being the innermost; ignored if leftContent present
+         * @default null
          */
         leftButtons?: ReactNode[];
         /**
          * Array of buttons, first being the innermost; ignored if rightContent present
+         * @default null
          */
         rightButtons?: ReactNode[];
         /**
          * Minimum swipe distance to activate left action
+         * @default 125
          */
         leftActionActivationDistance?: number;
         /**
          * User has swiped beyond leftActionActivationDistance and released
+         * @default null
          */
         onLeftActionRelease?: () => void;
         /**
          * Minimum swipe distance to activate right action
+         * @default 125
          */
         rightActionActivationDistance?: number;
         /**
          * User has swiped beyond rightActionActivationDistance and released
+         * @default null
          */
         onRightActionRelease?: () => void;
         /**
          * Resting visible peek of each left button after buttons are swiped ope
+         * @default 75
          */
         leftButtonWidth?: number;
         /**
          * Resting visible peek of each right button after buttons are swiped open
+         * @default 75
          */
         rightButtonWidth?: number;
         /**
          * To alert the user that swiping is possible
+         * @default false
          */
         bounceOnMount?: boolean;
         /**
          * Disable swiping
+         * @default null
          */
         disable?: boolean;
         style?: ViewStyle;
@@ -59,6 +71,11 @@ declare module "react-native-swipeable-row" {
         rightButtonContainerStyle?: ViewStyle;
         contentContainerStyle?: ViewStyle;
 
+        /**
+         * Gets the reference to the object
+         * @default null
+         * @param ref
+         */
         onRef?: (ref: Swipeable) => void;
         onSwipeStart?: () => void;
         onSwipeMove?: () => void;
