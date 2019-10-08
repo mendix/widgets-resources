31668ac557126477acea863c33bf00e39289af2a
Wesley <wesley.sieses@mendix.com>
2019-10-08 13:59:13 +0200

    feat(native): Change app theme based on OS theme

--- styles/native/app/custom-variables.js
+++ styles/native/app/custom-variables.js
@@ -19,8 +19,10 @@ export const brand = {
     danger: "#ed1c24",
 };
 
-// Dark Mode
-const darkMode = false;
+// Dark Mode - Inherits OS theme if possible
+const darkMode = NativeModules && NativeModules.RNDarkMode && NativeModules.RNDarkMode.initialMode
+                 ? NativeModules.RNDarkMode.initialMode === "dark"
+                 : false;
 
 // Background Colors
 const backgroundColor = darkMode ? "#000" : "#FFF";
