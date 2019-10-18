d9aa4e7becce030ed691d0ced82f82cc6c8e11d3
Diego Antonelli <diego.antonelli@mendix.com>
2019-10-18 16:54:50 +0200

    feat(intro-screen): adding unit tests for attribute

--- packages-native/intro-screen/src/__tests__/IntroScreen.spec.tsx
+++ packages-native/intro-screen/src/__tests__/IntroScreen.spec.tsx
@@ -4,6 +4,8 @@ import { IntroScreen } from "../IntroScreen";
 import { IntroScreenProps } from "../../typings/IntroScreenProps";
 import { IntroScreenStyle } from "../ui/Styles";
 import { View } from "react-native";
+import { EditableValueBuilder } from "@native-mobile-resources/util-widgets";
+import { Big } from "big.js";
 
 jest.mock("mendix/components/native/Icon", () => require.requireActual("./__mocks__/mendix/components/native/Icon"));
 
@@ -44,6 +46,16 @@ describe("Intro Screen", () => {
 
     it("renders with 2 bottom button", () => {
         const component = render(<IntroScreen {...defaultProps} slideIndicators={"above"} buttonPattern={"all"} />);
+        expect(component.toJSON()).toMatchSnapshot();
+    });
+
+    it("renders with active layout attribute", () => {
+        const component = render(
+            <IntroScreen
+                {...defaultProps}
+                activeSlideAttribute={new EditableValueBuilder<BigJs.Big>().withValue(new Big(1)).build()}
+            />
+        );
         expect(component.toJSON()).toMatchSnapshot();
     });
 });
