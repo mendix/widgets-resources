02a037b8be04e62fa85d0460f8e189d8038f5b9f
Diego Antonelli <diego.antonelli@mendix.com>
2019-10-30 10:38:59 +0100

    refactor(piw-utils): removing dependencies

--- packages-common/piw-utils/src/components.tsx
+++ packages-common/piw-utils/src/components.tsx
@@ -1,15 +1,15 @@
-import { createElement, ReactNode } from "react";
+import { Children, createElement, ReactNode } from "react";
 import classNames from "classnames";
 
 export interface AlertProps {
-    message: string;
+    children?: ReactNode;
     className?: string;
     bootstrapStyle: "default" | "primary" | "success" | "info" | "warning" | "danger";
 }
 
-export const Alert = ({ className, bootstrapStyle, message }: AlertProps): ReactNode =>
-    message
-        ? createElement("div", { className: classNames(`alert alert-${bootstrapStyle}`, className) }, message)
-        : null;
+export const Alert = ({ className, bootstrapStyle, children }: AlertProps): ReactNode =>
+    Children.count(children) > 0 ? (
+        <div className={classNames(`alert alert-${bootstrapStyle}`, className)}>{children}</div>
+    ) : null;
 
 Alert.displayName = "Alert";
