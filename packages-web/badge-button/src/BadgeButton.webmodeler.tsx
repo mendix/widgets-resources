91809a3c0164b033efbd6898c129bc697a0c389c
Diego Antonelli <diego.antonelli@mendix.com>
2019-10-31 15:39:18 +0100

    refactor(badge-button): removing parseStyle

--- packages-web/badge-button/src/BadgeButton.webmodeler.tsx
+++ packages-web/badge-button/src/BadgeButton.webmodeler.tsx
@@ -5,14 +5,14 @@ import { BadgeButtonPreviewProps } from "../typings/BadgeButtonProps";
 declare function require(name: string): string;
 
 export const preview = (props: BadgeButtonPreviewProps): ReactElement => {
-    const { bootstrapStyle, class: className, label, style, value } = props;
+    const { bootstrapStyle, class: className, label, styleObject, value } = props;
 
     return (
         <BadgeButton
             bootstrapStyle={bootstrapStyle}
             className={className}
             label={label}
-            style={parseStyle(style as string)}
+            style={styleObject}
             value={value}
         />
     );
@@ -21,21 +21,3 @@ export const preview = (props: BadgeButtonPreviewProps): ReactElement => {
 export function getPreviewCss(): string {
     return require("./ui/BadgeButton.css");
 }
-
-const parseStyle = (style = ""): { [key: string]: string } => {
-    try {
-        return style.split(";").reduce<{ [key: string]: string }>((styleObject, line) => {
-            const pair = line.split(":");
-            if (pair.length === 2) {
-                const name = pair[0].trim().replace(/(-.)/g, match => match[1].toUpperCase());
-                styleObject[name] = pair[1].trim();
-            }
-            return styleObject;
-        }, {});
-    } catch (error) {
-        // eslint-disable-next-line no-console
-        console.log("Failed to parse style", style, error);
-    }
-
-    return {};
-};
