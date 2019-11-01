430119d51424453aea5c805ab4919f771bdd5daf
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2019-11-01 13:46:28 +0100

    chore(badge): adjust preview
    - Adjust xml config

--- packages-web/badge/src/Badge.webmodeler.tsx
+++ packages-web/badge/src/Badge.webmodeler.tsx
@@ -1,53 +1,26 @@
-import { Component, createElement, ReactNode } from "react";
-import { Badge, BadgeProps } from "./components/Badge";
-import { Alert } from "./components/Alert";
-import BadgeContainer, { BadgeContainerProps } from "./components/BadgeContainer";
+import { createElement, ReactElement } from "react";
+
+import { BadgePreviewProps } from "../typings/BadgeProps";
+import { Badge } from "./components/Badge";
 
 declare function require(name: string): string;
 
-type VisibilityMap = {
-    [P in keyof BadgeContainerProps]: boolean;
+export const preview = (props: BadgePreviewProps): ReactElement => {
+    const { class: classname, styleObject, type, defaultValue, valueAttribute, bootstrapStyle, onClick } = props;
+
+    return (
+        <Badge
+            type={type}
+            defaultValue={defaultValue}
+            value={valueAttribute}
+            clickable={onClick.type !== "NoClientAction"}
+            bootstrapStyle={bootstrapStyle}
+            className={classname}
+            style={styleObject}
+        />
+    );
 };
-
-export class preview extends Component<BadgeContainerProps, {}> {
-    render(): ReactNode {
-        const message = BadgeContainer.validateProps(this.props);
-        return createElement(
-            "div",
-            { ref: this.parentInline },
-            createElement(Alert, { bootstrapStyle: "danger", message, className: "widget-badge-alert" }),
-            createElement(Badge, this.transformProps(this.props))
-        );
-    }
-
-    private parentInline(node?: HTMLElement | null): void {
-        if (node && node.parentElement) {
-            node.parentElement.style.display = "inline-block";
-        }
-    }
-
-    private transformProps(props: BadgeContainerProps): BadgeProps {
-        const valueAttribute = props.valueAttribute ? props.valueAttribute.split(".")[2] : "";
-        return {
-            badgeType: props.badgeType,
-            bootstrapStyle: props.bootstrapStyle,
-            className: props.class,
-            clickable: false,
-            style: BadgeContainer.parseStyle(props.style),
-            value: valueAttribute ? "[" + valueAttribute + "]" : props.badgeValue
-        };
-    }
-}
 
 export function getPreviewCss(): string {
     return require("./ui/Badge.css");
-}
-
-export function getVisibleProperties(valueMap: BadgeContainerProps, visibilityMap: VisibilityMap): VisibilityMap {
-    visibilityMap.microflow = valueMap.onClickEvent === "callMicroflow";
-    visibilityMap.nanoflow = valueMap.onClickEvent === "callNanoflow";
-    visibilityMap.page = valueMap.onClickEvent === "showPage";
-    visibilityMap.openPageAs = valueMap.onClickEvent === "showPage";
-
-    return visibilityMap;
 }
