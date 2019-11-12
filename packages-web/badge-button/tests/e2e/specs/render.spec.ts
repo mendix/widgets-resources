d705a3f4cc1dbfac4b0f7bd928e7b293d4f3d648
Diego Antonelli <diego.antonelli@mendix.com>
2019-11-12 16:31:24 +0100

    fix(badge-button): fixing e2e tests

--- packages-web/badge-button/tests/e2e/specs/render.spec.ts
+++ packages-web/badge-button/tests/e2e/specs/render.spec.ts
@@ -1,6 +1,10 @@
 import homepage from "../pages/home.page";
 
 describe("BadgeButton rendering", () => {
+    beforeAll(() => {
+        homepage.open();
+    });
+
     it("should render ", () => {
         const badgeButton = homepage.badgeButton;
         badgeButton.waitForDisplayed();
