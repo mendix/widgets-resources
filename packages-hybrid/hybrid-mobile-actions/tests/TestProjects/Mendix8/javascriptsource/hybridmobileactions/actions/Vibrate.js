0627347cebef32a46099d8ff004c3087c164508a
Andries Smit <Andries.Smit@mendix.com>
2019-11-14 17:07:53 +0100

    chore(hybrid-mobile-actions): convert structure

--- packages-hybrid/hybrid-mobile-actions/tests/TestProjects/Mendix8/javascriptsource/hybridmobileactions/actions/Vibrate.js
+++ packages-hybrid/hybrid-mobile-actions/tests/TestProjects/Mendix8/javascriptsource/hybridmobileactions/actions/Vibrate.js
@@ -1,16 +1,15 @@
-// BEGIN EXTRA CODE
-// END EXTRA CODE
 /**
  * @param {Big} duration - Android only setting. The time (in milliseconds) the device should vibrate. Set to empty to use the default value 500.
- * @returns {Promise.<void>}
+ * @returns {boolean}
  */
-export async function Vibrate(duration) {
+// eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
+function Vibrate(duration) {
     // BEGIN USER CODE
     // Requires cordova-plugin-vibration
     if (!navigator.vibrate) {
         throw new Error("Vibrate action requires cordova-plugin-vibration to be installed in the app");
     }
     navigator.vibrate(parseInt(String(duration), 10));
-    return Promise.resolve();
+    return true;
     // END USER CODE
 }
