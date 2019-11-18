911f720a51abae95e889b94cfc5756365a4d3ef9
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2019-11-18 16:18:58 +0100

    test(badge-web): expand e2e tests
    - Update test project

--- packages-web/badge/tests/e2e/pages/home.page.ts
+++ packages-web/badge/tests/e2e/pages/home.page.ts
@@ -13,12 +13,38 @@ class HomePage {
         return this.getWidget("badgeStaticSuccess");
     }
 
+    public get badgeInfo() {
+        return this.getWidget("badgeInfo");
+    }
+    public get badgeDefault() {
+        return this.getWidget("badgeDefault");
+    }
+    public get badgeInverse() {
+        return this.getWidget("badgeInverse");
+    }
+    public get badgeWarning() {
+        return this.getWidget("badgeWarning");
+    }
+
     public get badgeDanger() {
         return this.getWidget("badgeDanger");
     }
 
     public get labelSuccess() {
         return this.getWidget("labelStaticSuccess");
+    }
+
+    public get labelInfo() {
+        return this.getWidget("labelInfo");
+    }
+    public get labelDefault() {
+        return this.getWidget("labelDefault");
+    }
+    public get labelInverse() {
+        return this.getWidget("labelInverse");
+    }
+    public get labelWarning() {
+        return this.getWidget("labelWarning");
     }
 
     public get labelDanger() {
