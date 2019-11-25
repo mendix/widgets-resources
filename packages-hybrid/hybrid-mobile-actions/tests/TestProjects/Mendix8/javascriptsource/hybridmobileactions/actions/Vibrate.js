e0b6b22c9330159b608e442464d77015082192b4
Andries Smit <Andries.Smit@mendix.com>
2019-11-25 12:00:13 +0100

    chore(hybrid-mobile-actions): update test project

--- packages-hybrid/hybrid-mobile-actions/tests/TestProjects/Mendix8/javascriptsource/hybridmobileactions/actions/Vibrate.js
+++ packages-hybrid/hybrid-mobile-actions/tests/TestProjects/Mendix8/javascriptsource/hybridmobileactions/actions/Vibrate.js
@@ -12,16 +12,15 @@ import { Big } from "big.js";
 
 /**
  * @param {Big} duration - Android only setting. The time (in milliseconds) the device should vibrate. Set to empty to use the default value 500.
- * @returns {boolean}
+ * @returns {Promise.<void>}
  */
-// eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
-function Vibrate(duration) {
+export async function Vibrate(duration) {
     // BEGIN USER CODE
     // Requires cordova-plugin-vibration
     if (!navigator.vibrate) {
         throw new Error("Vibrate action requires cordova-plugin-vibration to be installed in the app");
     }
     navigator.vibrate(parseInt(String(duration), 10));
-    return true;
+    return Promise.resolve();
     // END USER CODE
 }
