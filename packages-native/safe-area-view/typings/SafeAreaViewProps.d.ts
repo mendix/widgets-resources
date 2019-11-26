e9bb792aee4a1868a2d54ec346325c9f0fd2d6ee
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2019-11-26 17:09:23 +0100

    revert: pluggable widgets tools upgrade

--- packages-native/safe-area-view/typings/SafeAreaViewProps.d.ts
+++ packages-native/safe-area-view/typings/SafeAreaViewProps.d.ts
@@ -7,7 +7,7 @@ import { ReactNode } from "react";
 
 interface CommonProps<Style> {
     name: string;
-    style: Array<Partial<Style>>;
+    style: Style[];
 }
 
 export interface SafeAreaViewProps<Style> extends CommonProps<Style> {
