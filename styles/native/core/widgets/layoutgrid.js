08dbd117b816690bcb9eb3cee84156f0a1ebf902
Wesley <wesley.sieses@mendix.com>
2019-11-28 17:21:46 +0100

    fix(native-layoutgrid): updated styles

--- styles/native/core/widgets/layoutgrid.js
+++ styles/native/core/widgets/layoutgrid.js
@@ -1,81 +1,99 @@
+const gutterSize = 15;
+
+
 export const LayoutGrid = {
-    container: {
-        flex: 1,
-    },
+    container: {},
 };
 
 export const row = {
     container: {
         flexDirection: "row",
         flexWrap: "wrap",
+        marginHorizontal: -gutterSize,
     },
 };
 export const col = {
-    container: {},
+    container: {
+        flex: 1,
+        paddingHorizontal: gutterSize,
+    },
 };
 export const colFitToContent = {
     container: {
         flex: -1,
+        paddingHorizontal: gutterSize,
     },
 };
 export const col1 = {
     container: {
         flexBasis: "8.333%",
+        paddingHorizontal: gutterSize,
     },
 };
 export const col2 = {
     container: {
         flexBasis: "16.666%",
+        paddingHorizontal: gutterSize,
     },
 };
 export const col3 = {
     container: {
         flexBasis: "25%",
+        paddingHorizontal: gutterSize,
     },
 };
 export const col4 = {
     container: {
         flexBasis: "33.333%",
+        paddingHorizontal: gutterSize,
     },
 };
 export const col5 = {
     container: {
         flexBasis: "41.666%",
+        paddingHorizontal: gutterSize,
     },
 };
 export const col6 = {
     container: {
         flexBasis: "50%",
+        paddingHorizontal: gutterSize,
     },
 };
 export const col7 = {
     container: {
         flexBasis: "58.333%",
+        paddingHorizontal: gutterSize,
     },
 };
 export const col8 = {
     container: {
         flexBasis: "66.666%",
+        paddingHorizontal: gutterSize,
     },
 };
 export const col9 = {
     container: {
         flexBasis: "75%",
+        paddingHorizontal: gutterSize,
     },
 };
 export const col10 = {
     container: {
         flexBasis: "83.333%",
+        paddingHorizontal: gutterSize,
     },
 };
 export const col11 = {
     container: {
         flexBasis: "91.666%",
+        paddingHorizontal: gutterSize,
     },
 };
 export const col12 = {
     container: {
         flexBasis: "100%",
+        paddingHorizontal: gutterSize,
     },
 };
 export const noGutters = {
@@ -87,5 +105,11 @@ export const noGutters = {
         paddingBottom: 0,
         paddingVertical: 0,
         paddingHorizontal: 0,
+    },
+};
+
+export const rowGutters = {
+    container: {
+        paddingVertical: gutterSize,
     },
 };
