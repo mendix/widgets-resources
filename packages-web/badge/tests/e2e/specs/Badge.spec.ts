1a6c6bc0f0112dcc85c96a06a34b0c290f44cbd9
Olga Shprygova <olga.shprygova@mendix.com>
2019-12-06 15:55:25 +0100

    test(badge): page object, badge object

--- packages-web/badge/tests/e2e/specs/Badge.spec.ts
+++ packages-web/badge/tests/e2e/specs/Badge.spec.ts
@@ -1,105 +1,101 @@
-import homepage from "../pages/home.page";
+import page from "../pages/page";
+import badgeWidget from "../Objects/badge.widget";
 
 describe("badge-web", () => {
-    beforeEach(() => {
-        homepage.open(); // resets page
+    beforeAll(() => {
+        page.open(); // resets page
     });
 
     describe("type: badge", () => {
         it("should render a badge with a static caption", () => {
-            const caption = "Static";
-
-            const badge = homepage.badgeSuccess.getHTML();
-            expect(badge).toContain(caption);
+            const badge = new badgeWidget("badgeStaticSuccess");
+            expect(badge.getBadgeText()).toEqual("Static");
         });
 
         it("renders with brand style success", () => {
-            const backgroundColor = homepage.badgeSuccess.getCSSProperty("background-color").value;
-            expect(backgroundColor).toContain("rgba(118,202,2,1)");
+            const badge = new badgeWidget("badgeStaticSuccess");
+            expect(badge.getColors()).toEqual(badge.defaultStyles.SuccessBackground);
         });
 
         it("renders with brand style info", () => {
-            const backgroundColor = homepage.badgeInfo.getCSSProperty("background-color").value;
-            expect(backgroundColor).toContain("rgba(72,176,247,1)");
+            const badge = new badgeWidget("badgeInfo");
+            expect(badge.getColors()).toEqual(badge.defaultStyles.InfoBackground);
         });
 
         it("renders with brand style default", () => {
-            const backgroundColor = homepage.badgeDefault.getCSSProperty("background-color").value;
-            expect(backgroundColor).toContain("rgba(221,221,221,1)");
+            const badge = new badgeWidget("badgeDefault");
+            expect(badge.getColors()).toEqual(badge.defaultStyles.DefaultBackground);
         });
 
         it("renders with brand style inverse", () => {
-            const backgroundColor = homepage.badgeInverse.getCSSProperty("background-color").value;
-            expect(backgroundColor).toContain("rgba(37,44,54,1)");
+            const badge = new badgeWidget("badgeInverse");
+            expect(badge.getColors()).toEqual(badge.defaultStyles.InverseBackground);
         });
 
         it("renders with brand style warning", () => {
-            const backgroundColor = homepage.badgeWarning.getCSSProperty("background-color").value;
-            expect(backgroundColor).toContain("rgba(249,155,29,1)");
+            const badge = new badgeWidget("badgeWarning");
+            expect(badge.getColors()).toEqual(badge.defaultStyles.WarningBackground);
         });
 
         it("renders with brand style danger", () => {
-            const backgroundColor = homepage.badgeDanger.getCSSProperty("background-color").value;
-            expect(backgroundColor).toContain("rgba(237,28,36,1)");
+            const badge = new badgeWidget("badgeDanger");
+            expect(badge.getColors()).toEqual(badge.defaultStyles.DangerBackground);
         });
 
         it("changes caption when attribute value is changed", () => {
             const newAttributeValue = "Test";
+            const badge = new badgeWidget("badgeDanger");
+            expect(badge.getText()).not.toContain(newAttributeValue);
 
-            expect(homepage.badgeDanger.getText()).not.toContain(newAttributeValue);
-
-            homepage.input.setValue(newAttributeValue); // setValue isn't working correctly with inputs in the Mendix Client, therefore, newAttributeValue gets appended to existing value
+            page.input.setValue(newAttributeValue); // setValue isn't working correctly with inputs in the Mendix Client, therefore, newAttributeValue gets appended to existing value
             browser.keys("\uE007");
-            expect(homepage.badgeDanger.getText()).toContain(newAttributeValue); // Badge text: NewSuccessTest
+            expect(badge.getText()).toContain(newAttributeValue); // Badge text: NewSuccessTest
         });
     });
 
     describe("type: label", () => {
         it("renders a label with a static caption", () => {
-            const caption = "Static";
-
-            const badge = homepage.labelSuccess.getHTML();
-            expect(badge).toContain(caption);
+            const badge = new badgeWidget("labelStaticSuccess");
+            expect(badge.getBadgeText()).toEqual("Static");
         });
 
         it("renders with brand style success", () => {
-            const backgroundColor = homepage.labelSuccess.getCSSProperty("background-color").value;
-            expect(backgroundColor).toContain("rgba(118,202,2,1)");
+            const badge = new badgeWidget("labelStaticSuccess");
+            expect(badge.getColors()).toEqual(badge.defaultStyles.SuccessBackground);
         });
 
         it("renders with brand style info", () => {
-            const backgroundColor = homepage.labelInfo.getCSSProperty("background-color").value;
-            expect(backgroundColor).toContain("rgba(72,176,247,1)");
+            const badge = new badgeWidget("labelInfo");
+            expect(badge.getColors()).toEqual(badge.defaultStyles.InfoBackground);
         });
 
         it("renders with brand style default", () => {
-            const backgroundColor = homepage.labelDefault.getCSSProperty("background-color").value;
-            expect(backgroundColor).toContain("rgba(221,221,221,1)");
+            const badge = new badgeWidget("labelDefault");
+            expect(badge.getColors()).toEqual(badge.defaultStyles.DefaultBackground);
         });
 
         it("renders with brand style inverse", () => {
-            const backgroundColor = homepage.labelInverse.getCSSProperty("background-color").value;
-            expect(backgroundColor).toContain("rgba(37,44,54,1)");
+            const badge = new badgeWidget("labelInverse");
+            expect(badge.getColors()).toEqual(badge.defaultStyles.InverseBackground);
         });
 
         it("renders with brand style warning", () => {
-            const backgroundColor = homepage.labelWarning.getCSSProperty("background-color").value;
-            expect(backgroundColor).toContain("rgba(249,155,29,1)");
+            const badge = new badgeWidget("labelWarning");
+            expect(badge.getColors()).toEqual(badge.defaultStyles.WarningBackground);
         });
 
         it("renders with brand style danger", () => {
-            const backgroundColor = homepage.labelDanger.getCSSProperty("background-color").value;
-            expect(backgroundColor).toContain("rgba(237,28,36,1)");
+            const badge = new badgeWidget("labelDanger");
+            expect(badge.getColors()).toEqual(badge.defaultStyles.DangerBackground);
         });
 
         it("changes caption when attribute value is changed", () => {
             const newAttributeValue = "Test";
+            const badge = new badgeWidget("labelDanger");
 
-            expect(homepage.badgeDanger.getText()).not.toContain(newAttributeValue);
-
-            homepage.input.setValue(newAttributeValue); // setValue isn't working correctly with inputs in the Mendix Client, therefore, newAttributeValue gets appended to existing value
+            page.input.setValue(newAttributeValue); // setValue isn't working correctly with inputs in the Mendix Client, therefore, newAttributeValue gets appended to existing value
             browser.keys("\uE007");
-            expect(homepage.badgeDanger.getText()).toContain(newAttributeValue); // Badge text: NewSuccessTest
+            expect(badge.getText()).toContain(newAttributeValue); // Badge text: NewSuccessTest
         });
     });
 });
