32acd7e8cb59b0b2c9c542ffe8d1024076ca35ca
Diego Antonelli <diego.antonelli@mendix.com>
2019-12-16 16:05:05 +0100

    fix: removing android machine

--- detox/jest.startup.js
+++ detox/jest.startup.js
@@ -4,7 +4,7 @@ const specReporter = require("detox/runners/jest/specReporter");
 const config = require("./package.json").detox;
 
 // Set the default timeout
-jest.setTimeout(1200000);
+jest.setTimeout(120000);
 jasmine.getEnv().addReporter(adapter);
 
 // This takes care of generating status logs on a per-spec basis. By default, jest only reports at file-level.
@@ -14,7 +14,7 @@ jasmine.getEnv().addReporter(specReporter);
 beforeAll(async () => {
     await init(config, { initGlobals: false, launchApp: false });
     await device.launchApp({ newInstance: true, launchArgs: { detoxPrintBusyIdleResources: "YES" } });
-}, 1200000);
+}, 120000);
 
 beforeEach(async () => {
     await adapter.beforeEach();
