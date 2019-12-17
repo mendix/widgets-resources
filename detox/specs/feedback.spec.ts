94926121d0af8b624860b4c4041b94ef18eaca3e
Diego Antonelli <diego.antonelli@mendix.com>
2019-12-17 18:16:57 +0100

    fix: changing tests

--- detox/specs/feedback.spec.ts
+++ detox/specs/feedback.spec.ts
@@ -2,6 +2,8 @@ import { device, expect } from "detox";
 import { Feedback, Pages } from "./elements";
 
 describe("Feedback", () => {
+    const feedback = Feedback("feedback1");
+
     beforeEach(async () => {
         await Pages().openFeedback();
     });
@@ -11,40 +13,65 @@ describe("Feedback", () => {
     });
 
     it("should open on click floating button", async () => {
-        const feedback = Feedback("feedback1");
         const button = await feedback.getFloatingButton();
-        await expect(button).toExist();
+        await expect(button).toBeVisible();
         await button.tap();
 
         const input = await feedback.getInput();
-        await expect(input).toExist();
+        await expect(input).toBeVisible();
     }, 12000);
 
     it("should change data in feedback and send", async () => {
-        const feedback = Feedback("feedback1");
         const button = await feedback.getFloatingButton();
-        await expect(button).toExist();
+        await expect(button).toBeVisible();
         await button.tap();
 
         const input = await feedback.getInput();
+        await expect(input).toBeVisible();
+        await input.tap();
         await input.clearText();
         await input.typeText("Testing feedback on Detox");
 
         await expect(input).toHaveText("Testing feedback on Detox");
 
         const checkbox = await feedback.getSwitch();
-        await expect(checkbox).toExist();
+        await expect(checkbox).toBeVisible();
         await checkbox.tap();
 
         const sendButton = await feedback.getSendButton();
-        await expect(sendButton).toExist();
+        await expect(sendButton).toBeVisible();
         await sendButton.tap();
 
         const successMessage = await feedback.getSuccessMessage();
-        await expect(successMessage).toExist();
+        await expect(successMessage).toBeVisible();
 
         const successOk = await feedback.getSuccessOkButton();
-        await expect(successOk).toExist();
+        await expect(successOk).toBeVisible();
+        await successOk.tap();
+    });
+
+    it("should send screenshot", async () => {
+        const button = await feedback.getFloatingButton();
+        await expect(button).toBeVisible();
+        await button.tap();
+
+        const input = await feedback.getInput();
+        await expect(input).toBeVisible();
+        await input.tap();
+        await input.clearText();
+        await input.typeText("Testing feedback with screenshot on Detox");
+
+        await expect(input).toHaveText("Testing feedback with screenshot on Detox");
+
+        const sendButton = await feedback.getSendButton();
+        await expect(sendButton).toBeVisible();
+        await sendButton.tap();
+
+        const successMessage = await feedback.getSuccessMessage();
+        await expect(successMessage).toBeVisible();
+
+        const successOk = await feedback.getSuccessOkButton();
+        await expect(successOk).toBeVisible();
         await successOk.tap();
     });
 });
