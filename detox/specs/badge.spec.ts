547ca1c558ef88243529b1ff5361f6486f0bd938
Diego Antonelli <diego.antonelli@mendix.com>
2019-12-18 16:34:12 +0100

    feat: adding detox for badge

---
+++ detox/specs/badge.spec.ts
@@ -0,0 +1,47 @@
+import { by, device, element, expect, waitFor } from "detox";
+import { Alert, Badge, Pages } from "./elements";
+
+describe("Badge", () => {
+    beforeEach(async () => {
+        await Pages().openBadge();
+
+        const textbox = await element(by.id("textBox1"));
+        await waitFor(textbox)
+            .toBeVisible()
+            .withTimeout(20000);
+        await textbox.tap();
+        await textbox.clearText();
+        await textbox.typeText("Test");
+    });
+
+    afterEach(async () => {
+        await device.reloadReactNative();
+    });
+
+    it("should render normal badge", async () => {
+        const badge = Badge("badge1");
+        const badgeContainer = await badge.getBadge();
+        await expect(badgeContainer).toBeVisible();
+        await badgeContainer.tap();
+
+        const badgeCaption = badge.getCaption();
+        await expect(badgeCaption).toBeVisible();
+        await expect(badgeCaption).toHaveText("Test");
+    });
+
+    it("should render a badge with actions", async () => {
+        const text = await element(by.id("text2"));
+        const badge = Badge("badge4");
+        const badgeContainer = await badge.getBadge();
+        await expect(badgeContainer).toBeVisible();
+        await text.tap();
+
+        const badgeCaption = badge.getCaption();
+        await expect(badgeCaption).toBeVisible();
+        await expect(badgeCaption).toHaveText("Test");
+
+        await badgeContainer.tap();
+        await expect(Alert().getMessage("Action test: Test")).toBeVisible();
+        await Alert().confirm();
+    });
+});
--- detox/specs/elements/index.ts
+++ detox/specs/elements/index.ts
@@ -1,2 +1,4 @@
+export * from "./Alert";
+export * from "./Badge";
 export * from "./Feedback";
 export * from "./Pages";
