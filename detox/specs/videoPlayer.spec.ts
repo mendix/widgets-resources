afe0b09c9429b4b1840e4278eed4c62a6d6cc858
Diego Antonelli <diego.antonelli@mendix.com>
2019-12-19 16:54:59 +0100

    feat: adding waitfor video player

--- detox/specs/videoPlayer.spec.ts
+++ detox/specs/videoPlayer.spec.ts
@@ -1,5 +1,5 @@
 import { Pages, VideoPlayer } from "./elements";
-import { expect } from "detox";
+import { expect, waitFor } from "detox";
 
 describe("Video Player", () => {
     beforeAll(async () => {
@@ -8,12 +8,18 @@ describe("Video Player", () => {
 
     it("should render the default video player", async () => {
         const videoPlayer = VideoPlayer("videoPlayer1");
+        await waitFor(videoPlayer)
+            .toBeVisible()
+            .withTimeout(120000);
         await expect(videoPlayer).toBeVisible();
         await videoPlayer.tap();
     });
 
     it("should render the custom video player", async () => {
         const videoPlayer = VideoPlayer("videoPlayer2");
+        await waitFor(videoPlayer)
+            .toBeVisible()
+            .withTimeout(120000);
         await expect(videoPlayer).toBeVisible();
         await videoPlayer.tap();
     });
