10b27a0f3bd06256204d425ae0180ac4375e1bc5
Diego Antonelli <diego.antonelli@mendix.com>
2019-12-19 11:57:05 +0100

    fix: removing feedback test

--- detox/specs/feedback.spec.ts
+++ detox/specs/feedback.spec.ts
@@ -16,7 +16,7 @@ describe("Feedback", () => {
         await feedback.getCancelButton().tap();
     }, 12000);
 
-    it("should change data in feedback and send", async () => {
+    it("should send the feedback including a screenshot", async () => {
         await expect(feedback.getFloatingButton()).toBeVisible();
         await feedback.getFloatingButton().tap();
 
@@ -26,36 +26,6 @@ describe("Feedback", () => {
         await feedback.getInput().typeText("Detox");
 
         await expect(feedback.getInput()).toHaveText("Detox");
-
-        await expect(feedback.getSwitch()).toBeVisible();
-        await expect(feedback.getSwitch()).toHaveValue("1");
-        await feedback.getSwitch().tap();
-        await expect(feedback.getSwitch()).toHaveValue("0");
-
-        await expect(feedback.getSendButton()).toBeVisible();
-        await feedback.getSendButton().tap();
-
-        await expect(feedback.getSuccessMessage()).toBeVisible();
-
-        await expect(feedback.getSuccessOkButton()).toBeVisible();
-        await feedback.getSuccessOkButton().tap();
-    });
-
-    it("should send screenshot", async () => {
-        await expect(feedback.getFloatingButton()).toBeVisible();
-        await feedback.getFloatingButton().tap();
-
-        await expect(feedback.getInput()).toBeVisible();
-        await feedback.getInput().tap();
-        await feedback.getInput().clearText();
-        await feedback.getInput().typeText("Detox");
-
-        await expect(feedback.getInput()).toHaveText("Detox");
-
-        await expect(feedback.getSwitch()).toBeVisible();
-        await expect(feedback.getSwitch()).toHaveValue("0");
-        await feedback.getSwitch().tap();
-        await expect(feedback.getSwitch()).toHaveValue("1");
 
         await expect(feedback.getSendButton()).toBeVisible();
         await feedback.getSendButton().tap();
