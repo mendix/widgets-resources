0244183b9c671915566e4d36e07da14467b2ad3a
Diego Antonelli <diego.antonelli@mendix.com>
2020-01-02 13:16:26 +0100

    test: android

--- detox/scripts/ci.android.sh
+++ detox/scripts/ci.android.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -e
 
-if [[ "$TRAVIS" != "true" ]]; then
-    # Approve unapproved SDK licenses
-    yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses
-fi
-
 ### Wait for emulator
 if [[ "$TRAVIS" == "true" ]]; then
     $(dirname "$0")/ci.android-waitforemulator.sh
