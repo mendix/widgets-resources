f4d9a74e63ef36e9beda8589777c6ab4d4e9b219
Mehmet Ali IZCI <mehmet.izci@mendix.com>
2020-01-06 14:07:12 +0100

    fix(native-carousel): improve styling by merging card/fullscreen styles

--- packages-native/native-carousel/src/styles/styles.ts
+++ packages-native/native-carousel/src/styles/styles.ts
@@ -1,4 +1,3 @@
-import { Style } from "@native-mobile-resources/util-widgets";
 import { TextStyle, ViewStyle } from "react-native";
 
 interface DotStyle {
@@ -14,61 +13,44 @@ interface ContainerStyle extends ViewStyle {
     height?: number;
 }
 
-export interface NativeCarouselStyle extends Style {
-    container: ViewStyle;
-    carousel: ContainerStyle;
-    inactiveSlideItem: {
+interface LayoutStyle {
+    container?: ViewStyle;
+    carousel?: ContainerStyle;
+    slideItem?: {
         opacity?: number;
         scale?: number;
     };
-    slideItem: ViewStyle;
-    paginationContainer: ViewStyle;
-    paginationText: TextStyle;
-    dotStyle: DotStyle;
-    activeDotStyle: ActiveDotStyle;
+    activeSlideItem?: ViewStyle;
+    paginationContainer?: ViewStyle;
+    paginationText?: TextStyle;
+    dotStyle?: DotStyle;
+    activeDotStyle?: ActiveDotStyle;
 }
 
-export const defaultNativeCarouselStyle: NativeCarouselStyle = {
-    container: {
-        width: "100%"
-    },
-    carousel: {},
-    slideItem: {
-        width: "70%"
-    },
-    inactiveSlideItem: {
-        opacity: 0.7,
-        scale: 0.7
-    },
-    paginationContainer: {},
-    paginationText: {},
-    dotStyle: {
-        color: "gray",
-        opacity: 0.4,
-        scale: 0.5
-    },
-    activeDotStyle: {
-        color: "black",
-        width: 8,
-        height: 8,
-        borderRadius: 4,
-        marginHorizontal: 8
-    }
-};
+export interface CarouselStyle {
+    cardLayout: LayoutStyle;
+    fullWidthLayout: LayoutStyle;
+}
 
-export const defaultNativeCarouselFullWidthStyle: NativeCarouselStyle = {
-    container: {},
-    carousel: {},
-    slideItem: {
-        width: "100%"
+export const defaultCarouselStyle: CarouselStyle = {
+    cardLayout: {
+        container: {
+            width: "100%"
+        },
+        activeSlideItem: {
+            width: "70%"
+        }
     },
-    pagination: {},
-    inactiveSlideItem: {
-        opacity: 1,
-        scale: 1
-    },
-    paginationContainer: {},
-    paginationText: {},
-    dotStyle: {},
-    activeDotStyle: {}
+    fullWidthLayout: {
+        container: {
+            width: "100%"
+        },
+        activeSlideItem: {
+            width: "100%"
+        },
+        slideItem: {
+            opacity: 1,
+            scale: 1
+        }
+    }
 };
