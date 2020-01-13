4528c5a621438fdee33cfa104fedc10dd63879c3
Wesley <wesley.sieses@mendix.com>
2020-01-13 17:29:59 +0100

    feat(native-carousel): update styles

--- styles/native/core/widgets/carousel.js
+++ styles/native/core/widgets/carousel.js
@@ -1,5 +1,5 @@
-import { shadeBlendConvert }           from "../helpers/_functions/shadeblendconvert.js";
-import { background, brand, contrast } from "../variables.js";
+import { shadeBlendConvert }                 from "../helpers/_functions/shadeblendconvert.js";
+import { background, brand, contrast, font } from "../variables.js";
 
 //
 // DISCLAIMER:
@@ -14,19 +14,67 @@ import { background, brand, contrast } from "../variables.js";
     Default Class For Mendix Carousel Widget
 ========================================================================== */
 
-
 // Card
-export const com_mendix_widget_native_nativecarousel_NativeCarousel = {
+const carouselFullWidthLayout = {
     container: {
         // All ViewStyle properties are allowed
     },
     carousel: {
         // All ViewStyle properties are allowed, with the exception that width and height should be a number
     },
-    inactiveSlideItem: {
+    slideItem: {
         // Only opacity and scale are allowed
+        opacity: .8,
+    },
+    activeSlideItem: {
+        // All ViewStyle properties are allowed
+    },
+    paginationContainer: {
+        // All ViewStyle properties are allowed
+        position: "absolute",
+        bottom: 0,
+        width: "100%",
+        justifyContent: "center",
+        paddingHorizontal: 20,
+        paddingBottom: 20,
+        paddingVertical: undefined,
+    },
+    paginationText: {
+        // All TextStyle properties are allowed
+        color: "#FFF",
+        fontSize: font.size,
+        fontFamily: font.family,
+        textAlign: "center",
+        textShadowColor: "#888",
+        textShadowRadius: 3,
+        textShadowOffset: {
+            width: 0,
+            height: 1,
+        },
+    },
+    dotStyle: {
+        // Only opacity, scale and color are allowed
+        color: "rgba(255, 255, 255, 0.45)",
+        scale: 1,
+    },
+    activeDotStyle: {
+        // Opacity, scale, color and all ViewStyle properties are allowed
+        color: "#FFF",
+    },
+};
+
+const carouselCardLayout = {
+    container: {
+        // All ViewStyle properties are allowed
+    },
+    carousel: {
+        // All ViewStyle properties are allowed, with the exception that width and height should be a number
     },
     slideItem: {
+        // Only opacity and scale are allowed
+        opacity: .8,
+    },
+    activeSlideItem: {
         // All ViewStyle properties are allowed
         paddingBottom: 20,
         elevation: 1.5,
@@ -43,58 +91,27 @@ export const com_mendix_widget_native_nativecarousel_NativeCarousel = {
         paddingHorizontal: 20,
         paddingBottom: 20,
         paddingVertical: undefined,
+
     },
     paginationText: {
         // All TextStyle properties are allowed
+        color: font.color,
+        fontSize: font.size,
+        fontFamily: font.family,
+        textAlign: "center",
     },
     dotStyle: {
         // Only opacity, scale and color are allowed
-        color: contrast.high,
-        marginHorizontal: 0,
+        color: contrast.low,
         scale: 1,
     },
     activeDotStyle: {
         // Opacity, scale, color and all ViewStyle properties are allowed
         color: brand.primary,
-        marginHorizontal: 0,
     },
 };
 
-// Full width
-// export const com_mendix_widget_native_nativecarousel_NativeCarousel = {
-//     container: {
-//         // All ViewStyle properties are allowed
-//     },
-//     carousel: {
-//         // All ViewStyle properties are allowed, with the exception that width and height should be a number
-//     },
-//     inactiveSlideItem: {
-//         // Only opacity and scale are allowed
-//     },
-//     slideItem: {
-//         // All ViewStyle properties are allowed
-//     },
-//     paginationContainer: {
-//         // All ViewStyle properties are allowed
-//         position: "absolute",
-//         bottom: 0,
-//         width: "100%",
-//         padding: 20,
-//         paddingHorizontal: undefined,
-//         paddingVertical: undefined,
-//     },
-//     paginationText: {
-//         // All TextStyle properties are allowed
-//     },
-//     dotStyle: {
-//         // Only opacity, scale and color are allowed
-//         color: contrast.high,
-//         marginHorizontal: 0,
-//         scale: 1,
-//     },
-//     activeDotStyle: {
-//         // Opacity, scale, color and all ViewStyle properties are allowed
-//         color: "#FFF",
-//         marginHorizontal: 0,
-//     },
-// };
+export const com_mendix_widget_native_nativecarousel_NativeCarousel = {
+    fullWidthLayout: carouselFullWidthLayout,
+    cardLayout: carouselCardLayout,
+};
