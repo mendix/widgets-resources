04b751280f0b8930f2fb7792f2152fb029a05593
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-01-21 16:57:50 +0100

    feat(bottom-drawer): first implementation

--- packages-native/bottom-drawer/src/BottomDrawer.tsx
+++ packages-native/bottom-drawer/src/BottomDrawer.tsx
@@ -1,5 +1,36 @@
-import { ReactNode } from "react";
+import { ReactNode, createElement, useRef, useEffect } from "react";
+import BottomSheet from "reanimated-bottom-sheet";
+import { View, Text, ViewStyle } from "react-native";
+import { BottomDrawerProps } from "../typings/BottomDrawerProps";
+import { Style, flattenStyles } from "@native-mobile-resources/util-widgets";
 
-export function BottomDrawer(): ReactNode {
-    return null;
+interface BottomDrawerStyle extends Style {
+    container: ViewStyle;
+}
+
+export function BottomDrawer(props: BottomDrawerProps<BottomDrawerStyle>): ReactNode {
+    const styles = flattenStyles({ container: { backgroundColor: "#FFF" } }, props.style);
+
+    const bottomSheetRef = useRef<BottomSheet>(null);
+
+    useEffect(() => {
+        console.warn("test");
+        bottomSheetRef.current?.snapTo(0);
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <BottomSheet
+                snapPoints={[450, 300, 0]}
+                initialSnap={1}
+                renderContent={() => <Text>Content</Text>}
+                renderHeader={() => (
+                    <View style={{ backgroundColor: "#FFF" }}>
+                        <Text>Header</Text>
+                    </View>
+                )}
+                ref={bottomSheetRef}
+            />
+        </View>
+    );
 }
