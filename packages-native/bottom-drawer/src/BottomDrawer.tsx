42346f4c6ac660f07e8f3c56576891b6126f9d49
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-01-23 11:13:13 +0100

    feat(bottom-drawer): working example

--- packages-native/bottom-drawer/src/BottomDrawer.tsx
+++ packages-native/bottom-drawer/src/BottomDrawer.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createElement, useRef, useEffect } from "react";
+import { ReactNode, createElement, useRef, useEffect, useCallback } from "react";
 import BottomSheet from "reanimated-bottom-sheet";
 import { View, Text, ViewStyle } from "react-native";
 import { BottomDrawerProps } from "../typings/BottomDrawerProps";
@@ -9,26 +9,38 @@ interface BottomDrawerStyle extends Style {
 }
 
 export function BottomDrawer(props: BottomDrawerProps<BottomDrawerStyle>): ReactNode {
-    const styles = flattenStyles({ container: { backgroundColor: "#FFF" } }, props.style);
+    const styles = flattenStyles({ container: { flex: 1, backgroundColor: "#FFF" } }, props.style);
 
     const bottomSheetRef = useRef<BottomSheet>(null);
 
+    const renderHeader = useCallback(() => {
+        return (
+            <View style={{ backgroundColor: "blue" }}>
+                <Text>Header</Text>
+            </View>
+        );
+    }, []);
+    const renderContent = useCallback(() => {
+        return (
+            <View style={{ backgroundColor: "green" }}>
+                <Text>Content</Text>
+            </View>
+        );
+    }, []);
+
     useEffect(() => {
         console.warn("test");
-        bottomSheetRef.current?.snapTo(0);
+        console.warn(!!bottomSheetRef.current);
+        bottomSheetRef.current!.snapTo(0);
     }, []);
 
     return (
         <View style={styles.container}>
             <BottomSheet
-                snapPoints={[450, 300, 0]}
-                initialSnap={1}
-                renderContent={() => <Text>Content</Text>}
-                renderHeader={() => (
-                    <View style={{ backgroundColor: "#FFF" }}>
-                        <Text>Header</Text>
-                    </View>
-                )}
+                snapPoints={["20%", "70%"]}
+                initialSnap={0}
+                renderContent={renderContent}
+                renderHeader={renderHeader}
                 ref={bottomSheetRef}
             />
         </View>
