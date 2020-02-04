0575f33a7ac752e3166a7122a36a169c77f53857
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-02-04 13:45:03 +0100

    chore(bottom-drawer): clean up code
    - Remove implementation that uses callbackNode prop

--- packages-native/bottom-drawer/src/BottomDrawer.tsx
+++ packages-native/bottom-drawer/src/BottomDrawer.tsx
@@ -11,96 +11,48 @@ export function BottomDrawer(props: BottomDrawerProps<BottomDrawerStyle>): React
     const styles = flattenStyles(defaultBottomDrawerStyle, props.style);
 
     const bottomSheetRef = useRef<BottomSheet>(null);
-    const currentBottomSheetPosition = useMemo(() => new Animated.Value(0), []);
+    const currentHeaderPosition = useMemo(() => new Animated.Value(0), []);
 
     const renderHeader = useCallback(() => props.headerContent, [props.headerContent]);
     const renderContent = useCallback(() => props.mainContent, [props.mainContent]);
 
-    const [snapPoints, snapPointsInPoints, normalizedSnapPointsDesc] = useMemo(() => {
+    const [snapPoints, snapPointsInDp] = useMemo(() => {
         const snapPoints = props.snapPoints.map(snapPoint =>
             snapPoint.distanceUnit === "percentage" ? snapPoint.distance + "%" : snapPoint.distance
         );
 
-        const snapPointsInPoints = snapPoints.map(snapPoint => {
+        const snapPointsInDp = snapPoints.map(snapPoint => {
             if (typeof snapPoint === "string") {
                 // convert percentages into dp
                 return BottomSheet.renumber(snapPoint);
-                // return (Dimensions.get("window").height / 100) * Number(snapPoint.slice(0, -1));
             }
 
             return snapPoint;
         });
 
-        console.warn("snap points in points", snapPointsInPoints);
-
-        const snapPointsDesc = snapPointsInPoints
-            .map((e, i) => {
-                return { distance: e, originalIndex: i };
-            })
-            .sort((a, b) => b.distance - a.distance);
-
-        const normalizedSnapPointsDesc = snapPointsDesc.map((e, _i, a) => {
-            return {
-                distance: 1 - (e.distance - a[a.length - 1].distance) / (a[0].distance - a[a.length - 1].distance),
-                inaccuracyDistance: 0.0001 / (a[0].distance - a[a.length - 1].distance),
-                originalIndex: e.originalIndex
-            };
-        });
-
-        return [snapPoints, snapPointsInPoints, normalizedSnapPointsDesc];
+        return [snapPoints, snapPointsInDp];
     }, [props.snapPoints]);
 
-    console.warn("Normalized snap points desc", normalizedSnapPointsDesc);
-
-    const test = (index: number) => {
-        return debounce(
-            () => {
-                console.warn("snap point index", index);
-            },
-            0,
-            undefined
-        );
+    const test = (snapPoint: number) => {
+        return () => {
+            console.warn("snap point index", snapPoint);
+        };
     };
 
-    // const renderSnapPointListeners = useCallback(() => {
-    //     const conditions = normalizedSnapPointsDesc.map(snapPoint => {
-    //         return Animated.cond(
-    //             Animated.and(
-    //                 Animated.greaterOrEq(currentBottomSheetPosition, snapPoint.distance - snapPoint.inaccuracyDistance),
-    //                 Animated.lessOrEq(currentBottomSheetPosition, snapPoint.distance + snapPoint.inaccuracyDistance)
-    //             ),
-    //             // Animated.eq(currentBottomSheetPosition, snapPoint.distance),
-    //             Animated.call([currentBottomSheetPosition], test(snapPoint.originalIndex))
-    //         );
-    //     });
-
-    //     return <Animated.Code exec={Animated.block(conditions)} />;
-    // }, [normalizedSnapPointsDesc]);
-
-    // === headerPosition ===
-
-    const currentHeaderPosition = useMemo(() => new Animated.Value(0), []);
     const renderHeaderListeners = useCallback(() => {
-        console.warn("renderHeaderListeners", snapPointsInPoints);
-        const conditions = snapPointsInPoints.map(snapPoint => {
+        const conditions = snapPointsInDp.map(snapPoint => {
             return Animated.cond(
-                // Animated.and(
-                //     Animated.greaterOrEq(currentHeaderPosition, snapPoint - 25),
-                //     Animated.lessOrEq(currentHeaderPosition, snapPoint + 25)
-                // ),
                 Animated.eq(currentHeaderPosition, Dimensions.get("window").height - snapPoint),
                 Animated.call([currentHeaderPosition], test(snapPoint))
             );
         });
 
         return <Animated.Code exec={Animated.block(conditions)} />;
-    }, [normalizedSnapPointsDesc]);
-
-    // ======================
+    }, [snapPointsInDp]);
 
     useEffect(() => {
-        if (props.currentSnapPointIndex.status === ValueStatus.Available) {
-            bottomSheetRef.current!.snapTo(Number(props.currentSnapPointIndex.value.toFixed(0)));
+        if (bottomSheetRef.current && props.currentSnapPointIndex.status === ValueStatus.Available) {
+            bottomSheetRef.current.snapTo(Number(props.currentSnapPointIndex.value.toFixed(0)));
         }
     }, [props.currentSnapPointIndex]);
 
@@ -111,36 +63,9 @@ export function BottomDrawer(props: BottomDrawerProps<BottomDrawerStyle>): React
                 snapPoints={snapPoints}
                 renderHeader={renderHeader}
                 renderContent={renderContent}
-                callbackNode={currentBottomSheetPosition}
                 headerPosition={currentHeaderPosition}
             />
-            {/* {renderSnapPointListeners()} */}
             {renderHeaderListeners()}
         </View>
     );
-}
-
-// Returns a function, that, as long as it continues to be invoked, will not
-// be triggered. The function will be called after it stops being called for
-// N milliseconds. If `immediate` is passed, trigger the function on the
-// leading edge, instead of the trailing.
-//@ts-ignore
-function debounce(func, wait, immediate) {
-    //@ts-ignore
-    var timeout;
-    return function() {
-        // @ts-ignore
-        var context = this,
-            args = arguments;
-        var later = function() {
-            timeout = null;
-            if (!immediate) func.apply(context, args); // use destructure ...
-        };
-        //@ts-ignore
-        var callNow = immediate && !timeout;
-        // @ts-ignore
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-    };
 }
