401b7266a2fca0f0b3e891c3943e9e6fb2c87069
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-02-11 10:13:51 +0100

    fix(signature): e2e test scripts

--- packages-web/signature/tests/e2e/specs/Signature.spec.ts
+++ packages-web/signature/tests/e2e/specs/Signature.spec.ts
@@ -1,5 +1,5 @@
-import HomePage from "./pages/home.page";
-import GridPage from "./pages/grid.page";
+import HomePage from "../pages/home.page";
+import GridPage from "../pages/grid.page";
 
 describe("SignatureCanvas", () => {
     it("renders Canvas", () => {
