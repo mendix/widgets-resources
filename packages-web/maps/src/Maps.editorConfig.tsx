e3567b55eca8ba801f54f02026d3e253fce83f78
Diego Antonelli <diego.antonelli@mendix.com>
2020-02-11 11:25:30 +0100

    refactor(maps-web): changing helper funcs to generics

--- packages-web/maps/src/Maps.editorConfig.tsx
+++ packages-web/maps/src/Maps.editorConfig.tsx
@@ -1,36 +1,6 @@
 import { MapsPreviewProps } from "../typings/MapsProps";
-
-declare type Option<T> = T | undefined;
-
-type Properties = PropertyGroup[];
-
-type PropertyGroup = {
-    caption: string;
-    propertyGroups?: PropertyGroup[];
-    properties?: Property[];
-};
-
-type Property = {
-    key: string;
-    caption: string;
-    description?: string;
-    properties?: Properties[];
-};
-
-type Problem = {
-    property?: string; // key of the property, at which the problem exists
-    severity?: "error" | "warning" | "deprecation"; // default = "error"
-    message: string; // description of the problem
-    studioMessage?: string; // studio-specific message, defaults to message
-    url?: string; // link with more information about the problem
-    studioUrl?: string; // studio-specific link
-};
-
-const customMarkerConfig = {
-    key: "customMarker",
-    caption: "Custom marker image",
-    description: "Use a custom image as marker"
-};
+import { Problem, Properties } from "../typings/PageEditor";
+import { changeProperty, hideProperty } from "./utils/PageEditorUtils";
 
 export function getProperties(
     values: MapsPreviewProps,
@@ -43,13 +13,18 @@ export function getProperties(
         if (target === "web") {
             return defaultProperties;
         }
-        hideProperty("shape", defaultProperties);
-        hideProperty("customMarker", defaultProperties);
-        changeProperty("type", "description", "Now the map is configured for Cate", defaultProperties);
+        hideProperty<MapsPreviewProps>("shape", defaultProperties);
+        hideProperty<MapsPreviewProps>("customMarker", defaultProperties);
+        changeProperty<MapsPreviewProps>(
+            "type",
+            "description",
+            "Now the map is configured for Cate",
+            defaultProperties
+        );
     } else {
-        changeProperty("type", "description", "Now the map is configured for Tom", defaultProperties);
+        changeProperty<MapsPreviewProps>("type", "description", "Now the map is configured for Tom", defaultProperties);
         if (values.shape === "default") {
-            hideProperty("customMarker", defaultProperties);
+            hideProperty<MapsPreviewProps>("customMarker", defaultProperties);
         }
     }
     return defaultProperties;
@@ -74,50 +49,4 @@ export function check(values: MapsPreviewProps): Problem[] {
         });
     }
     return errors;
-}
-
-function hideProperty(key: keyof MapsPreviewProps, properties?: Option<Properties>): void {
-    properties?.forEach(property => {
-        property?.properties?.forEach((prop, index, array) => {
-            if (prop.key === key) {
-                array.splice(index, 1);
-            } else {
-                prop.properties?.forEach(propArray => hideProperty(key, propArray));
-            }
-        });
-        hideProperty(key, property.propertyGroups);
-    });
-}
-
-function includeKey(
-    key: keyof MapsPreviewProps,
-    properties: Option<Properties>,
-    newItem: Property,
-    after = true
-): void {
-    properties?.forEach(property => {
-        property?.properties?.forEach((prop, index, array) => {
-            if (prop.key === key) {
-                if (!array.some(p => p.key === newItem.key)) {
-                    array.splice(after ? index : index - 1, 0, newItem);
-                }
-            } else {
-                prop.properties?.forEach(propArray => includeKey(key, propArray, newItem));
-            }
-        });
-        includeKey(key, property.propertyGroups, newItem);
-    });
-}
-
-function changeProperty(key: string, targetKey: keyof Property, value: any, properties: Option<Properties>): void {
-    properties?.forEach(property => {
-        property?.properties?.forEach(prop => {
-            if (prop.key === key) {
-                prop[targetKey] = value;
-            } else {
-                prop.properties?.forEach(propArray => changeProperty(key, targetKey, value, propArray));
-            }
-        });
-        changeProperty(key, targetKey, value, property.propertyGroups);
-    });
 }
