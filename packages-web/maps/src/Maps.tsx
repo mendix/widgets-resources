19bc36d4f4cc73e36fdacd4195ba8d97f875896c
Diego Antonelli <diego.antonelli@mendix.com>
2020-02-12 18:32:57 +0100

    feat(maps-web): implementing geodecode and current location

--- packages-web/maps/src/Maps.tsx
+++ packages-web/maps/src/Maps.tsx
@@ -1,11 +1,15 @@
 import { hot } from "react-hot-loader/root";
 import { createElement, ReactNode } from "react";
 import { MapsContainerProps } from "../typings/MapsProps";
-import GoogleMap from "./components/GoogleMap";
+import GoogleMap, { ModeledMarker } from "./components/GoogleMap";
 import { translateZoom } from "./utils/Utils";
 import { ValueStatus } from "mendix";
 
 const Maps = (props: MapsContainerProps): ReactNode => {
+    const locations: ModeledMarker[] = props.markers.map(location => ({
+        location: location.location.value!,
+        action: location.onClick?.execute
+    }));
     return (
         <GoogleMap
             autoZoom
@@ -13,6 +17,7 @@ const Maps = (props: MapsContainerProps): ReactNode => {
             mapsToken={props.apiKey && props.apiKey.status === ValueStatus.Available ? props.apiKey.value : undefined}
             defaultCenterLatitude={51.906855}
             defaultCenterLongitude={4.488367}
+            locations={locations}
         />
     );
 };
