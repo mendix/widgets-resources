dbcfb6d54e8e436500bc7ee4c24a0cf8d8de5a6e
Andries Smit <Andries.Smit@mendix.com>
2020-02-17 15:07:39 +0100

    chore: move prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,25 @@
 const base = require("@mendix/pluggable-widgets-tools/configs/prettier.base.json");
 
 module.exports = {
-    ...base
+    ...base,
+    overrides: [
+        {
+            files: ["CHANGELOG.md", ".travis.yml"],
+            options: {
+                proseWrap: "preserve"
+            }
+        },
+        {
+            files: ["package.json", "package.json"],
+            options: {
+                tabWidth: 2
+            }
+        },
+        {
+            files: "*.md",
+            options: {
+                proseWrap: "preserve"
+            }
+        }
+    ]
 };
