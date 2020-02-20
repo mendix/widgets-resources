ca5eeeb34f3a961978dc2ab5519702971985e7fe
Diego Antonelli <diego.antonelli@mendix.com>
2020-02-20 16:09:30 +0100

    fix(actions): fixing device info to be compatible with 8.8

--- packages-native/actions/src/platform/GetDeviceInfo.ts
+++ packages-native/actions/src/platform/GetDeviceInfo.ts
@@ -5,7 +5,7 @@
 // Other code you write will be lost the next time you deploy the project.
 
 import DeviceInfo from "react-native-device-info";
-import RNLocalize from "react-native-localize";
+import { getLocales, getCountry, getTimeZone, uses24HourClock } from "react-native-localize";
 
 /**
  * @returns {Promise.<MxObject>}
@@ -19,39 +19,54 @@ export async function GetDeviceInfo(): Promise<mendix.lib.MxObject> {
         DeviceInfo.getFontScale(),
         DeviceInfo.getFreeDiskStorage(),
         DeviceInfo.getTotalDiskCapacity(),
-        DeviceInfo.getTotalMemory()
-    ]).then(async ([mxObject, batteryLevel, fontScale, freeDiskStorage, totalDiskCapacity, totalMemory]) => {
-        const locales = RNLocalize.getLocales();
-        mxObject.set("ApplicationName", DeviceInfo.getApplicationName());
-        mxObject.set("BatteryLevel", new Big(batteryLevel.toFixed(2)));
-        mxObject.set("Brand", DeviceInfo.getBrand());
-        mxObject.set("BuildNumber", String(DeviceInfo.getBuildNumber()));
-        mxObject.set("BundleId", DeviceInfo.getBundleId());
-        mxObject.set("Carrier", DeviceInfo.getCarrier());
-        mxObject.set("DeviceCountry", RNLocalize.getCountry());
-        mxObject.set("DeviceId", DeviceInfo.getDeviceId());
-        mxObject.set("DeviceLocale", locales && locales.length > 0 ? locales[0].languageTag : "");
-        mxObject.set("FontScale", new Big(fontScale.toFixed(2)));
-        mxObject.set("FreeDiskStorage", new Big(freeDiskStorage));
-        mxObject.set("Manufacturer", DeviceInfo.getManufacturer());
-        mxObject.set("Model", DeviceInfo.getModel());
-        mxObject.set("ReadableVersion", DeviceInfo.getReadableVersion());
-        mxObject.set("SystemName", DeviceInfo.getSystemName());
-        mxObject.set("SystemVersion", DeviceInfo.getSystemVersion());
-        mxObject.set("Timezone", RNLocalize.getTimeZone());
-        mxObject.set("TotalDiskCapacity", new Big(totalDiskCapacity));
-        mxObject.set("TotalMemory", new Big(totalMemory));
-        mxObject.set("UniqueId", DeviceInfo.getUniqueId());
-        mxObject.set("UserAgent", DeviceInfo.getUserAgent());
-        mxObject.set("Version", DeviceInfo.getVersion());
-        mxObject.set("Is24Hour", RNLocalize.uses24HourClock());
-        mxObject.set("IsEmulator", DeviceInfo.isEmulator());
-        mxObject.set("IsTablet", DeviceInfo.isTablet());
-        mxObject.set("IsLandscape", DeviceInfo.isLandscape());
-        mxObject.set("HasNotch", DeviceInfo.hasNotch());
+        DeviceInfo.getTotalMemory(),
+        DeviceInfo.getCarrier(),
+        DeviceInfo.getManufacturer(),
+        DeviceInfo.getUserAgent()
+    ]).then(
+        async ([
+            mxObject,
+            batteryLevel,
+            fontScale,
+            freeDiskStorage,
+            totalDiskCapacity,
+            totalMemory,
+            carrier,
+            manufacturer,
+            userAgent
+        ]) => {
+            const locales = getLocales();
+            mxObject.set("ApplicationName", DeviceInfo.getApplicationName());
+            mxObject.set("BatteryLevel", new Big(batteryLevel.toFixed(2)));
+            mxObject.set("Brand", DeviceInfo.getBrand());
+            mxObject.set("BuildNumber", String(DeviceInfo.getBuildNumber()));
+            mxObject.set("BundleId", DeviceInfo.getBundleId());
+            mxObject.set("Carrier", carrier);
+            mxObject.set("DeviceCountry", getCountry());
+            mxObject.set("DeviceId", DeviceInfo.getDeviceId());
+            mxObject.set("DeviceLocale", locales && locales.length > 0 ? locales[0].languageTag : "");
+            mxObject.set("FontScale", new Big(fontScale.toFixed(2)));
+            mxObject.set("FreeDiskStorage", new Big(freeDiskStorage));
+            mxObject.set("Manufacturer", manufacturer);
+            mxObject.set("Model", DeviceInfo.getModel());
+            mxObject.set("ReadableVersion", DeviceInfo.getReadableVersion());
+            mxObject.set("SystemName", DeviceInfo.getSystemName());
+            mxObject.set("SystemVersion", DeviceInfo.getSystemVersion());
+            mxObject.set("Timezone", getTimeZone());
+            mxObject.set("TotalDiskCapacity", new Big(totalDiskCapacity));
+            mxObject.set("TotalMemory", new Big(totalMemory));
+            mxObject.set("UniqueId", DeviceInfo.getUniqueId());
+            mxObject.set("UserAgent", userAgent);
+            mxObject.set("Version", DeviceInfo.getVersion());
+            mxObject.set("Is24Hour", uses24HourClock());
+            mxObject.set("IsEmulator", DeviceInfo.isEmulator());
+            mxObject.set("IsTablet", DeviceInfo.isTablet());
+            mxObject.set("IsLandscape", DeviceInfo.isLandscape());
+            mxObject.set("HasNotch", DeviceInfo.hasNotch());
 
-        return mxObject;
-    });
+            return mxObject;
+        }
+    );
 
     function createMxObject(entity: string): Promise<mendix.lib.MxObject> {
         return new Promise((resolve, reject) => {
