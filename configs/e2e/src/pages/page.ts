f6c41729b95105e4dac426ce55e49361b651f8cf
Olga Shprygova <olga.shprygova@mendix.com>
2020-02-24 10:10:24 +0100

    test(carousel): initial tests

--- configs/e2e/src/pages/page.ts
+++ configs/e2e/src/pages/page.ts
@@ -3,15 +3,18 @@ class Page {
         browser.url("/" + url);
     }
 
-    getElement(name: string): WebdriverIO.Element {
-        const element = $(name);
+    existing(selector: string): boolean {
+        return $(selector).isExisting();
+    }
+    getElement(selector: string): WebdriverIO.Element {
+        const element = $(selector);
         element.waitForDisplayed();
         return element;
     }
 
-    getElements(name: string): WebdriverIO.Element[] {
-        $(name).waitForDisplayed();
-        const elements = $$(name);
+    getElements(selector: string): WebdriverIO.Element[] {
+        $(selector).waitForDisplayed();
+        const elements = $$(selector);
         return elements;
     }
 
@@ -21,6 +24,10 @@ class Page {
 
     getWidgets(widgetName: string): WebdriverIO.Element[] {
         return this.getElements(`.mx-name-${widgetName}`);
+    }
+
+    get headerElement(): WebdriverIO.Element {
+        return this.getWidget("pageTitle1");
     }
 
     get header(): string {
