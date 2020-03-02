a8fc6aa990f253c989522a6c8999f394cf81405c
mehmet <mehmet.izci@mendix.com>
2020-03-02 15:03:27 +0100

    fix(carousel-native): update jest

--- packages-native/carousel/src/__tests__/Carousel.spec.tsx
+++ packages-native/carousel/src/__tests__/Carousel.spec.tsx
@@ -42,7 +42,6 @@ describe("Carousel", () => {
             layout: "card",
             showPagination: true,
             activeSlideAlignment: "center",
-            loop: false,
             style: []
         };
     });
