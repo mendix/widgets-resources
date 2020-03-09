2e622641e93a9ef80d5aa72875671d0ccac304ba
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-03-09 15:28:42 +0100

    feat(background-image): adjust image availability behavior

--- packages-native/background-image/src/BackgroundImage.tsx
+++ packages-native/background-image/src/BackgroundImage.tsx
@@ -1,22 +1,24 @@
-import { createElement, ReactNode } from "react";
+import { createElement } from "react";
 import { BackgroundImageStyle, defaultBackgroundImageStyle } from "./ui/Styles";
 import { BackgroundImageProps } from "../typings/BackgroundImageProps";
 import { ImageBackground } from "react-native";
 import { ValueStatus } from "mendix";
 import { flattenStyles } from "@native-mobile-resources/util-widgets";
 
-export function BackgroundImage(props: BackgroundImageProps<BackgroundImageStyle>): ReactNode {
+export function BackgroundImage(props: BackgroundImageProps<BackgroundImageStyle>): JSX.Element | null {
     const styles = flattenStyles(defaultBackgroundImageStyle, props.style);
 
-    const backgroundImage =
-        props.backgroundImage.status === ValueStatus.Available ? props.backgroundImage.value : undefined;
+    const backgroundImage = props.backgroundImage;
 
-    if (!backgroundImage) {
+    if (
+        backgroundImage.status === ValueStatus.Unavailable ||
+        (backgroundImage.status === ValueStatus.Loading && !backgroundImage.value)
+    ) {
         return null;
     }
 
     return (
-        <ImageBackground source={backgroundImage} style={styles.container}>
+        <ImageBackground source={backgroundImage.value!} style={styles.container}>
             {props.content}
         </ImageBackground>
     );
