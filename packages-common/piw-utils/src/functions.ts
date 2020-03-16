e0bca24a7c11e25cabea67d41fe4a483b502ecbb
Diego Antonelli <diego.antonelli@mendix.com>
2020-03-16 09:48:23 +0100

    feat(progress-bar): fixing validations

--- packages-common/piw-utils/src/functions.ts
+++ packages-common/piw-utils/src/functions.ts
@@ -1,7 +1,11 @@
-import { ActionValue } from "mendix";
+import { ActionValue, DynamicValue, EditableValue, ValueStatus } from "mendix";
 
 export const executeAction = (action?: ActionValue): void => {
     if (action && action.canExecute && !action.isExecuting) {
         action.execute();
     }
+};
+
+export const isAvailable = (property: DynamicValue<any> | EditableValue<any>): boolean => {
+    return property.status === ValueStatus.Available && property.value != null;
 };
