4ba43a9f69e96bd4c4831372c1db19eb82be4b1f
Mehmet Ali IZCI <mehmet.izci@mendix.com>
2020-03-18 17:56:36 +0100

    feat(popup-menu): improve the properties

--- packages-native/popup-menu/typings/PopupMenuProps.d.ts
+++ packages-native/popup-menu/typings/PopupMenuProps.d.ts
@@ -3,7 +3,7 @@
  * WARNING: All changes made to this file will be overwritten
  * @author Mendix Widgets Team
  */
-import { ActionValue, DynamicValue } from "mendix";
+import { ActionValue } from "mendix";
 import { ReactNode } from "react";
 
 interface CommonProps<Style> {
@@ -11,24 +11,35 @@ interface CommonProps<Style> {
     style: Style[];
 }
 
-export type TypeEnum = "basic" | "freeModaling";
+export type RenderModeEnum = "basic" | "custom";
+
+export type ItemTypeEnum = "item" | "divider";
+
+export type StyleClassEnum = "defaultStyle" | "primaryStyle" | "dangerStyle" | "customStyle";
 
 export interface ItemsBasicType {
+    itemType: ItemTypeEnum;
     caption: string;
-    color?: string;
-    condition?: DynamicValue<boolean>;
     action?: ActionValue;
+    styleClass: StyleClassEnum;
 }
 
-export type RenderModeEnum = "contextMenu" | "notAnimatedContextMenu" | "slideInMenu" | "popOpver";
+export interface ItemsComplexType {
+    name: string;
+    content: ReactNode;
+}
+
+export type TypePopUpEnum = "ContextMenu" | "Popover";
 
 export type PreferredLocationEnum = "auto" | "bottom" | "left" | "right" | "top";
 
 export interface PopupMenuProps<Style> extends CommonProps<Style> {
     menuTriggerer?: ReactNode;
-    type: TypeEnum;
-    itemsBasic: ItemsBasicType[];
     renderMode: RenderModeEnum;
+    itemsBasic: ItemsBasicType[];
+    itemsComplex: ItemsComplexType[];
+    typePopUp: TypePopUpEnum;
+    animate: boolean;
     preferredLocation: PreferredLocationEnum;
     handleBackButtonAndroid: boolean;
 }
