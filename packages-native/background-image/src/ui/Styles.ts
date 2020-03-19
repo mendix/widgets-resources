9684aa226ec63298c9886b05cddf9c53d9043592
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-03-19 15:43:47 +0100

    feat(background-image): add color styling for SVGs

--- packages-native/background-image/src/ui/Styles.ts
+++ packages-native/background-image/src/ui/Styles.ts
@@ -1,9 +1,13 @@
 import { ViewStyle, ImageStyle } from "react-native";
 import { Style } from "@native-mobile-resources/util-widgets";
 
+interface customImageStyle extends ImageStyle {
+    svgColor?: string;
+}
+
 export interface BackgroundImageStyle extends Style {
     container: ViewStyle;
-    image: ImageStyle;
+    image: customImageStyle;
 }
 
 export const defaultBackgroundImageStyle: BackgroundImageStyle = {
