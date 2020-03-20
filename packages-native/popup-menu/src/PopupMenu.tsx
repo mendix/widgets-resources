965cd0298ae82c5dfae154d66e8e93ab98798f30
Mehmet Ali IZCI <mehmet.izci@mendix.com>
2020-03-20 17:12:57 +0100

    fix(popup-menu): add dividers and align xml with actions for content

--- packages-native/popup-menu/src/PopupMenu.tsx
+++ packages-native/popup-menu/src/PopupMenu.tsx
@@ -1,20 +1,27 @@
 import { createElement, ReactElement } from "react";
 import { PopupMenuProps } from "../typings/PopupMenuProps";
-import { PopUpMenuStyle } from "./ui/Styles";
+import { dividerStyle, PopUpMenuStyle } from "./ui/Styles";
 import { Menu, MenuOption, MenuOptions, MenuProvider, MenuTrigger, renderers } from "react-native-popup-menu";
 import { executeAction } from "@widgets-resources/piw-utils";
+import { View } from "react-native";
 
 export function PopupMenu(props: PopupMenuProps<PopUpMenuStyle>): ReactElement {
     const renderMenuOptions = () => {
         if (props.renderMode === "basic") {
-            return props.itemsBasic.map(item => (
-                <MenuOption value={item.caption} text={item.caption} onSelect={() => executeAction(item.action!)} />
-            ));
+            return props.itemsBasic.map(item => {
+                if (item.itemType === "divider") {
+                    return <View style={dividerStyle} />;
+                }
+                return (
+                    <MenuOption value={item.caption} text={item.caption} onSelect={() => executeAction(item.action)} />
+                );
+            });
         } else {
-            return props.itemsComplex.map(item => <MenuOption>{item.content}</MenuOption>);
+            return props.itemsComplex.map(item => (
+                <MenuOption onSelect={() => executeAction(item.action)}>{item.content}</MenuOption>
+            ));
         }
     };
-    // TODO: incorporate new action for Complex and divider for Basic
     // TODO: styling is off, they are adding flex:1
     return (
         <MenuProvider backHandler={props.handleBackButtonAndroid}>
--- packages-native/popup-menu/src/ui/Styles.ts
+++ packages-native/popup-menu/src/ui/Styles.ts
@@ -1,2 +1,9 @@
 import { Style } from "@native-mobile-resources/util-widgets";
 export interface PopUpMenuStyle extends Style {}
+
+export const dividerStyle = {
+    marginVertical: 5,
+    marginHorizontal: 2,
+    borderBottomWidth: 1,
+    borderColor: "#ccc"
+};
