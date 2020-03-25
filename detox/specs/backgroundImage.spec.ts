38bd77b1df1d786dd97d4381ffe7f81ae35b2077
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-03-25 13:17:31 +0100

    chore(background-image): disable detox tests

--- detox/specs/backgroundImage.spec.ts
+++ detox/specs/backgroundImage.spec.ts
@@ -1,43 +1,43 @@
-import { by, device, element, expect, waitFor } from "detox";
-import { Alert, BackgroundImage, Pages } from "./elements";
+// import { by, device, element, expect, waitFor } from "detox";
+// import { Alert, BackgroundImage, Pages } from "./elements";
 
-describe("Background image", () => {
-    beforeAll(async () => {
-        await Pages().openBackgroundImage();
-    });
+// describe("Background image", () => {
+//     beforeAll(async () => {
+//         await Pages().openBackgroundImage();
+//     });
 
-    it("renders static image", async () => {
-        const backgroundImage = BackgroundImage("backgroundImage");
-        await expect(backgroundImage.getbackgroundImage()).toBeVisible();
-        await expect(backgroundImage.getImage()).toBeVisible();
-    });
-    it("renders static SVG image", () => {});
-    it("renders dynamic image", () => {});
+//     it("renders static image", async () => {
+//         const backgroundImage = BackgroundImage("backgroundImage");
+//         await expect(backgroundImage.getbackgroundImage()).toBeVisible();
+//         await expect(backgroundImage.getImage()).toBeVisible();
+//     });
+//     it("renders static SVG image", () => {});
+//     it("renders dynamic image", () => {});
 
-    // it("should render normal badge", async () => {
-    //     const badge = Badge("badgeNormal");
-    //     await expect(badge.getBadge()).toBeVisible();
-    //     await badge.getBadge().tap();
+//     // it("should render normal badge", async () => {
+//     //     const badge = Badge("badgeNormal");
+//     //     await expect(badge.getBadge()).toBeVisible();
+//     //     await badge.getBadge().tap();
 
-    //     await expect(badge.getCaption()).toBeVisible();
-    //     await expect(badge.getCaption()).toHaveText("Detox");
-    // });
+//     //     await expect(badge.getCaption()).toBeVisible();
+//     //     await expect(badge.getCaption()).toHaveText("Detox");
+//     // });
 
-    // it("should render a badge with actions", async () => {
-    //     const text = await element(by.id("actionLabel"));
-    //     const badge = Badge("badgeAction");
-    //     await expect(badge.getBadge()).toBeVisible();
-    //     await text.tap();
+//     // it("should render a badge with actions", async () => {
+//     //     const text = await element(by.id("actionLabel"));
+//     //     const badge = Badge("badgeAction");
+//     //     await expect(badge.getBadge()).toBeVisible();
+//     //     await text.tap();
 
-    //     await expect(badge.getCaption()).toBeVisible();
-    //     await expect(badge.getCaption()).toHaveText("Detox");
+//     //     await expect(badge.getCaption()).toBeVisible();
+//     //     await expect(badge.getCaption()).toHaveText("Detox");
 
-    //     await badge.getBadge().tap();
-    //     await expect(Alert().getMessage("Action test: Detox")).toBeVisible();
-    //     await Alert().confirm();
-    // });
+//     //     await badge.getBadge().tap();
+//     //     await expect(Alert().getMessage("Action test: Detox")).toBeVisible();
+//     //     await Alert().confirm();
+//     // });
 
-    afterAll(async () => {
-        await device.reloadReactNative();
-    });
-});
+//     afterAll(async () => {
+//         await device.reloadReactNative();
+//     });
+// });
