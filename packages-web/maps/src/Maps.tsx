11c03e7262591ccc45c27cb0ed1b6e353ce08f4d
Diego Antonelli <diego.antonelli@mendix.com>
2020-03-30 18:13:47 +0200

    refactor(maps-web): refactoring code to utils

--- packages-web/maps/src/Maps.tsx
+++ packages-web/maps/src/Maps.tsx
@@ -1,18 +1,16 @@
 import { hot } from "react-hot-loader/root";
 import { createElement, ReactNode, useState } from "react";
-import { DynamicMarkersType, MapsContainerProps, MarkersType } from "../typings/MapsProps";
+import { MapsContainerProps } from "../typings/MapsProps";
 import GoogleMap, { ModeledMarker } from "./components/GoogleMap";
-import { translateZoom } from "./utils/Utils";
-import { ObjectItem, ValueStatus } from "mendix";
+import { analyzeDynamicMarker, analyzeStaticMarker, translateZoom } from "./utils/Utils";
+import { ValueStatus } from "mendix";
 import "./ui/Maps.css";
 
 const Maps = (props: MapsContainerProps): ReactNode => {
     const currentMarkers: ModeledMarker[] = [];
     const [locations, setLocations] = useState<ModeledMarker[]>(currentMarkers);
 
-    props.markers.map(marker => {
-        currentMarkers.push(analyzeStaticMarker(marker));
-    });
+    currentMarkers.push(...props.markers.map(marker => analyzeStaticMarker(marker)));
     props.dynamicMarkers.forEach(marker => {
         if (marker.markersDS && marker.markersDS.status === ValueStatus.Available) {
             console.warn("Extracting markers");
@@ -21,54 +19,6 @@ const Maps = (props: MapsContainerProps): ReactNode => {
             });
         }
     });
-
-    const analyzeStaticMarker = (marker: MarkersType): ModeledMarker => {
-        console.warn("Analyzing static marker");
-        let location;
-        if (marker.dataSourceType === "static") {
-            if (marker.locationType === "address") {
-                location = marker.address!;
-            } else {
-                location = `${marker.latitude},${marker.longitude}`;
-            }
-        } else {
-            if (marker.locationType === "address") {
-                if (marker.propertyContext === "attribute") {
-                    location = marker.addressAttribute?.value!;
-                } else {
-                    location = marker.addressExpression?.value!;
-                }
-            } else {
-                if (marker.propertyContext === "attribute") {
-                    location = `${marker.latitudeAttribute?.value},${marker.longitudeAttribute?.value}`;
-                } else {
-                    location = `${marker.latitudeExpression?.value},${marker.longitudeExpression?.value}`;
-                }
-            }
-        }
-
-        return {
-            location,
-            action: marker.onClick?.execute,
-            customMarker: marker.customMarker?.value?.uri
-        };
-    };
-
-    const analyzeDynamicMarker = (marker: DynamicMarkersType, item: ObjectItem): ModeledMarker => {
-        console.warn("Analyzing dynamic marker");
-        const { locationType, address, latitude, longitude, onClickAttribute } = marker;
-        let location;
-        if (locationType === "address") {
-            location = address ? address(item).value! : "";
-        } else {
-            location = `${latitude ? latitude(item).value : 0},${longitude ? longitude(item).value : 0}`;
-        }
-        return {
-            location,
-            action: onClickAttribute ? onClickAttribute(item).execute : undefined,
-            customMarker: marker.customMarkerDynamic?.value?.uri
-        };
-    };
 
     if (currentMarkers.length !== locations.length) {
         setLocations(currentMarkers);
