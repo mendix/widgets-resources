be4152009384b5d2d66e49c06be6102a3b6b2248
Mitya Kononchuk <mitya.kononchuk@mendix.com>
2020-04-06 12:24:25 +0200

    Split plugin and translation logic

--- packages-common/typing-library/src/index.ts
+++ packages-common/typing-library/src/index.ts
@@ -1,9 +1,7 @@
-import { transformPackageContent } from "./functions";
-import { parseString } from "xml2js";
 import PluginError from "plugin-error";
 import File from "vinyl";
 import map from "map-stream";
-import { PackageContent } from "./PackageContent";
+import { transformPackage } from "./transformPackage";
 
 function generateTypings(file: File, cb: (error: Error | null) => void): void {
     if (!file || file.isNull() || !file.contents) {
@@ -18,22 +16,10 @@ function generateTypings(file: File, cb: (error: Error | null) => void): void {
         return cb(new PluginError("pluggable-widgets-typing-generator", "Streaming not supported"));
     }
 
-    parseString(file.contents!.toString("utf8"), {}, function (err: Error, content?: PackageContent) {
-        if (err) cb(err);
-
-        if (!content) {
-            return cb(
-                new PluginError(
-                    "pluggable-widgets-typing-generator",
-                    `Empty XML, please check your src folder for file package.xml`
-                )
-            );
-        }
-
-        transformPackageContent(content, file.base + "/");
-
-        cb(null);
-    });
+    transformPackage(file.contents!.toString("utf8"), file.base).then(
+        () => cb(null),
+        (err) => cb(new PluginError("pluggable-widgets-typing-generator", err))
+    );
 }
 
 export = () => map(generateTypings);
