2e7c9b5050dca8bf0e4c2885ddeda1f3530cbd78
Mitya Kononchuk <mitya.kononchuk@mendix.com>
2020-04-06 11:42:11 +0200

    Adhere more to callback-style APIs

--- packages-common/typing-library/src/index.ts
+++ packages-common/typing-library/src/index.ts
@@ -4,28 +4,21 @@ import PluginError from "plugin-error";
 import File from "vinyl";
 import map from "map-stream";
 
-function typingGenerator() {
-    function modifyContents(file: File, cb: (error: Error | null) => void): void {
-        if (!file || file.isNull()) {
-            return cb(
-                new PluginError(
-                    "pluggable-widgets-typing-generator",
-                    `Empty XML, please check your src folder for file package.xml`
-                )
-            );
-        }
-        if (file.isStream()) {
-            return cb(new PluginError("pluggable-widgets-typing-generator", "Streaming not supported"));
-        }
+function generateTypings(file: File, cb: (error: Error | null) => void): void {
+    if (!file || file.isNull() || !file.contents) {
+        return cb(
+            new PluginError(
+                "pluggable-widgets-typing-generator",
+                `Empty XML, please check your src folder for file package.xml`
+            )
+        );
+    }
+    if (file.isStream()) {
+        return cb(new PluginError("pluggable-widgets-typing-generator", "Streaming not supported"));
+    }
 
-        let content = null;
-
-        if (file.contents) {
-            parseString(file.contents.toString("utf8"), {}, function (err: Error, result: any) {
-                if (err) cb(err);
-                content = result;
-            });
-        }
+    parseString(file.contents.toString("utf8"), {}, function (err: Error, content: any) {
+        if (err) cb(err);
 
         if (!content) {
             return cb(
@@ -39,9 +32,7 @@ function typingGenerator() {
         transformPackageContent(content, file.base + "/");
 
         cb(null);
-    }
-
-    return map(modifyContents);
+    });
 }
 
-export = typingGenerator;
+export = () => map(generateTypings);
