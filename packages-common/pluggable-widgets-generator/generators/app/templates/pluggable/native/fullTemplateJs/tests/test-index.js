3d058fb47b3cd643bac5644c6b9ac3adb14b2109
Diego Antonelli <diego.antonelli@mendix.com>
2020-04-09 16:52:26 +0200

    Updating dependencies and introducing editorPreview

--- packages-common/pluggable-widgets-generator/generators/app/templates/pluggable/native/fullTemplateJs/tests/test-index.js
+++ packages-common/pluggable-widgets-generator/generators/app/templates/pluggable/native/fullTemplateJs/tests/test-index.js
@@ -3,11 +3,7 @@ import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-jest.mock("react-native", () => ({
-    Platform: { OS: "android", select: jest.fn(dict => dict.android) },
-    StyleSheet: jest.requireActual("StyleSheet"),
-    Text: jest.requireActual("Text"),
-    TouchableNativeFeedback: jest.requireActual("TouchableNativeFeedback"),
-    TouchableOpacity: jest.requireActual("TouchableOpacity"),
-    View: jest.requireActual("View")
+jest.mock("react-native/Libraries/Utilities/Platform", () => ({
+    OS: "android",
+    select: jest.fn(dict => dict.android)
 }));
