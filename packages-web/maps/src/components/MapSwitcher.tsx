780bdb4f70e1146236c5262ab81604f1b20e7c69
Diego Antonelli <diego.antonelli@mendix.com>
2020-04-10 11:53:18 +0200

    refactor(maps-web): refectoring code

--- packages-web/maps/src/components/MapSwitcher.tsx
+++ packages-web/maps/src/components/MapSwitcher.tsx
@@ -5,8 +5,5 @@ import { LeafletMap, LeafletProps } from "./LeafletMap";
 interface SwitcherProps extends GoogleMapsProps, LeafletProps {}
 
 export const MapSwitcher = (props: SwitcherProps): ReactElement => {
-    if (props.mapProvider === "googleMaps") {
-        return <GoogleMap {...props} />;
-    }
-    return <LeafletMap {...props} />;
+    return props.mapProvider === "googleMaps" ? <GoogleMap {...props} /> : <LeafletMap {...props} />;
 };
