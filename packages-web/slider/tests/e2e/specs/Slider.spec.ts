1b6c28326730a01f64dc401f8db56fabb197f0ac
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-04-14 16:30:09 +0200

    test(slider-web): add general tests

--- packages-web/slider/tests/e2e/specs/Slider.spec.ts
+++ packages-web/slider/tests/e2e/specs/Slider.spec.ts
@@ -27,26 +27,58 @@ describe("Slider widget", () => {
         expect(sliderWidget.getSliderHandle().getAttribute("style")).toBe("left: 50%;");
     });
 
-    it("renders without context");
-    it("listens to a grid");
-    // it("renders in a list view");
-    // it("renders in a template grid");
-    // it("renders in a tab container");
-    it("triggers a microflow after slide");
-    it("triggers a nanoflow after slide");
+    it("renders without context", () => {
+        page.open("p/no-context");
 
-    describe("Alert message", () => {
-        it("warns when the minimum value is higher than the maximum value");
-        it("warns when the value is higher than the maximum value");
-        it("warns when the value is lower than the minimum value");
-        it("warns about an invalid step size value");
+        const sliderWidget = new SliderWidget("sliderNoContext");
+
+        expect(sliderWidget.getSliderRoot().getAttribute("class")).toContain("rc-slider-disabled");
+        expect(sliderWidget.getMinimumSliderMark().getText()).toBe("0");
+        expect(sliderWidget.getMaximumSliderMark().getText()).toBe("100");
+        expect(sliderWidget.getSliderHandle().getAttribute("style")).toBe("left: 50%;");
+        expect(sliderWidget.isSliderTrackDisplayed()).toBe(false);
     });
 
-    describe("Tooltip", () => {
-        it("doesn't render when there's no title");
-        it("renders a static title");
-        it("renders the slider's value");
-        it("renders without context the slider's value");
+    it("listens to a grid", () => {
+        page.open("p/listen-to-grid");
+
+        const sliderWidget = new SliderWidget("slider");
+        const dataGrid = page.getWidget("grid");
+        const dataGridRows = dataGrid.$$("td");
+
+        expect(sliderWidget.isDisabled()).toBe(true);
+
+        dataGridRows[0].click();
+        expect(sliderWidget.isDisabled()).toBe(false);
+        expect(sliderWidget.getSliderHandle().getAttribute("style")).toBe("left: 50%;");
+
+        dataGridRows[1].click();
+        expect(sliderWidget.isDisabled()).toBe(false);
+        expect(sliderWidget.getSliderHandle().getAttribute("style")).toBe("left: 80%;");
+    });
+
+    it("triggers a microflow after slide", () => {
+        page.open("p/after-slide");
+
+        const sliderWidget = new SliderWidget("sliderMicroflow");
+
+        sliderWidget.dragSliderHandleToMinimum();
+
+        const modalDialogText = $(".modal-dialog .mx-dialog-body > p");
+        modalDialogText.waitForDisplayed();
+        expect(modalDialogText.getText()).toContain("0");
+    });
+
+    it("triggers a nanoflow after slide", () => {
+        page.open("p/after-slide");
+
+        const sliderWidget = new SliderWidget("sliderNanoflow");
+
+        sliderWidget.dragSliderHandleToMinimum();
+
+        const modalDialogText = $(".modal-dialog .mx-name-text1");
+        modalDialogText.waitForDisplayed();
+        expect(modalDialogText.getText()).toContain("0");
     });
 
     describe("Slider", () => {
@@ -54,6 +86,10 @@ describe("Slider widget", () => {
         it("renders multiple markers");
         it("renders decimal values");
         it("renders long values");
+        // const textBoxValueWidget = page.getWidget("textBoxValue");
+        // textBoxValueWidget.$("input").setValue("75.0"); // setValue isn't working correctly with inputs in the Mendix Client, therefore, "s" gets appended to existing value
+        // browser.keys("\uE007");
+        // expect(sliderWidget.getSliderHandle().getAttribute("style")).toBe("left: 75%;");
         it("slides with a step size");
 
         describe("Style", () => {
@@ -65,5 +101,19 @@ describe("Slider widget", () => {
             it("renders with warning style");
             it("renders with danger style");
         });
+    });
+
+    describe("Tooltip", () => {
+        it("doesn't render when there's no title");
+        it("renders a static title");
+        it("renders the slider's value");
+        it("renders without context the slider's value");
+    });
+
+    describe("Alert message", () => {
+        it("warns when the minimum value is higher than the maximum value");
+        it("warns when the value is higher than the maximum value");
+        it("warns when the value is lower than the minimum value");
+        it("warns about an invalid step size value");
     });
 });
