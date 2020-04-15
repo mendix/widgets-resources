a1dabaa008a4f91ca980caaf93f1abcdf58314f6
Diego Antonelli <diego.antonelli@mendix.com>
2020-04-15 16:39:02 +0200

    feat(piw-utils): adding typings for PageEditorUtils

--- packages-common/piw-utils/src/utils/PageEditorUtils.ts
+++ packages-common/piw-utils/src/utils/PageEditorUtils.ts
@@ -6,18 +6,20 @@ import { Property, PropertyGroup } from "../typings";
 
 declare type Option<T> = T | undefined;
 
-export function hidePropertyIn<T>(propertyGroups: PropertyGroup[], key: keyof T): void;
-export function hidePropertyIn<T>(
+export function hidePropertyIn<T, TKey extends keyof T>(propertyGroups: PropertyGroup[], _value: T, key: TKey): void;
+export function hidePropertyIn<T, TKey extends keyof T>(
     propertyGroups: PropertyGroup[],
-    key: keyof T,
+    _value: T,
+    key: TKey,
     nestedPropIndex: number,
-    nestedPropKey: string
+    nestedPropKey: T[TKey] extends Array<infer TChild> ? keyof TChild : never
 ): void;
-export function hidePropertyIn<T>(
+export function hidePropertyIn<T, TKey extends keyof T>(
     propertyGroups: PropertyGroup[],
-    key: keyof T,
+    _value: T,
+    key: TKey,
     nestedPropIndex?: number,
-    nestedPropKey?: string
+    nestedPropKey?: T[TKey] extends Array<infer TChild> ? keyof TChild : never
 ): void {
     modifyProperty(
         (_, index, container) => container.splice(index, 1),
@@ -28,40 +30,43 @@ export function hidePropertyIn<T>(
     );
 }
 
-export function hidePropertiesIn<T>(propertyGroups: PropertyGroup[], keys: Array<keyof T>): void {
+export function hidePropertiesIn<T>(propertyGroups: PropertyGroup[], _value: T, keys: Array<keyof T>): void {
     keys.forEach(key =>
         modifyProperty((_, index, container) => container.splice(index, 1), propertyGroups, key, undefined, undefined)
     );
 }
 
-export function changePropertyIn<T>(
+export function changePropertyIn<T, TKey extends keyof T>(
     propertyGroups: PropertyGroup[],
+    _value: T,
     modify: (prop: Property) => void,
-    key: keyof T
+    key: TKey
 ): void;
-export function changePropertyIn<T>(
+export function changePropertyIn<T, TKey extends keyof T>(
     propertyGroups: PropertyGroup[],
+    _value: T,
     modify: (prop: Property) => void,
-    key: keyof T,
+    key: TKey,
     nestedPropIndex: number,
     nestedPropKey: string
 ): void;
-export function changePropertyIn<T>(
+export function changePropertyIn<T, TKey extends keyof T>(
     propertyGroups: PropertyGroup[],
+    _value: T,
     modify: (prop: Property) => void,
-    key: keyof T,
+    key: TKey,
     nestedPropIndex?: number,
     nestedPropKey?: string
 ): void {
     modifyProperty(modify, propertyGroups, key, nestedPropIndex, nestedPropKey);
 }
 
-function modifyProperty<T>(
+function modifyProperty(
     modify: (prop: Property, index: number, container: Property[]) => void,
     propertyGroups: PropertyGroup[],
-    key: keyof T,
+    key: string | number | symbol,
     nestedPropIndex?: number,
-    nestedPropKey?: string
+    nestedPropKey?: string | number | symbol
 ) {
     propertyGroups.forEach(propGroup => {
         if (propGroup.propertyGroups) {
