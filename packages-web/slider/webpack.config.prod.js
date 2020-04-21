b8d35fa3d92c6995950052cb7c2ec4e781b17622
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-04-21 09:47:40 +0200

    chore(slider-web): add webpack configs
    - Make sure older browsers are supported

---
+++ packages-web/slider/webpack.config.prod.js
@@ -0,0 +1,44 @@
+const merge = require("webpack-merge");
+const baseConfig = require("../utils-react-widgets/configs/webpack.config.dev");
+
+const overridingConfig = {
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        cacheDirectory: true,
+                        presets: [
+                            [
+                                "@babel/preset-env",
+                                {
+                                    corejs: 2,
+                                    targets: {
+                                        chrome: "49",
+                                        firefox: "52",
+                                        edge: "16",
+                                        safari: "9",
+                                        ie: "11",
+                                        ios: "9",
+                                        android: "5"
+                                    },
+                                    useBuiltIns: "usage"
+                                }
+                            ],
+                            "@babel/preset-react"
+                        ],
+                        plugins: [
+                            ["@babel/plugin-proposal-class-properties", { loose: true }],
+                            ["@babel/plugin-transform-react-jsx", { pragma: "createElement" }]
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+};
+
+module.exports = [merge(baseConfig[0], overridingConfig), merge(baseConfig[1], overridingConfig)];
