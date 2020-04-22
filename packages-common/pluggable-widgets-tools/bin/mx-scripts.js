1b29472a6658738d0873ebe4dbfd57588b0c06a2
Mitya Kononchuk <mitya.kononchuk@mendix.com>
2020-04-22 15:12:18 +0200

    feat(pluggable-widgets-tools): fix windows build

--- packages-common/pluggable-widgets-tools/bin/mx-scripts.js
+++ packages-common/pluggable-widgets-tools/bin/mx-scripts.js
@@ -1,14 +1,14 @@
 #! /usr/bin/env node
-'use strict';
+"use strict";
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', err => {
+process.on("unhandledRejection", err => {
     throw err;
 });
 
-const spawn = require('child_process').spawnSync;
+const spawn = require("child_process").spawnSync;
 const path = require("path");
 const args = process.argv.slice(2);
 const script = args[0];
@@ -39,9 +39,12 @@ switch (script) {
     case "release:js":
     case "release:ts":
     case "release:js:native":
-    case "release:ts:native":
-    {
-        console.log(`Running MX Widget Tools script: "${script}" ${argsFiltered.length > 0 ? `with options "${argsFiltered.join(" ")}"`:""}`);
+    case "release:ts:native": {
+        console.log(
+            `Running MX Widget Tools script: "${script}" ${
+                argsFiltered.length > 0 ? `with options "${argsFiltered.join(" ")}"` : ""
+            }`
+        );
         executeScript(script);
         break;
     }
@@ -50,32 +53,29 @@ switch (script) {
         break;
 }
 
-function executeScript(script){
+function executeScript(script) {
     const libraryPath = getLibraryPath();
-    const spawnParams = {stdio: 'inherit'};
     let args = ["run", script];
     if (argsFiltered.length > 0) {
         args.push("--");
         args = args.concat(argsFiltered);
     }
-    if (/.*node_modules[\/|\\]@mendix[\/|\\]pluggable-widgets-tools[\/|\\]?$/.test(libraryPath)) {
-        spawnParams.cwd = libraryPath;
-    }
-    const result = spawn(
-        /^win/.test(process.platform) ? 'npm.cmd' : 'npm', args, spawnParams
-    );
+    const result = spawn(/^win/.test(process.platform) ? "npm.cmd" : "npm", args, {
+        stdio: "inherit",
+        cwd: libraryPath
+    });
     if (result.signal) {
-        if (result.signal === 'SIGKILL') {
+        if (result.signal === "SIGKILL") {
             console.log(
-                'The build failed because the process exited too early. ' +
-                'This probably means the system ran out of memory or someone called ' +
-                '`kill -9` on the process.'
+                "The build failed because the process exited too early. " +
+                    "This probably means the system ran out of memory or someone called " +
+                    "`kill -9` on the process."
             );
-        } else if (result.signal === 'SIGTERM') {
+        } else if (result.signal === "SIGTERM") {
             console.log(
-                'The build failed because the process exited too early. ' +
-                'Someone might have called `kill` or `killall`, or the system could ' +
-                'be shutting down.'
+                "The build failed because the process exited too early. " +
+                    "Someone might have called `kill` or `killall`, or the system could " +
+                    "be shutting down."
             );
         }
         process.exit(1);
@@ -83,10 +83,10 @@ function executeScript(script){
     process.exit(result.status);
 }
 
-function getLibraryPath(){
+function getLibraryPath() {
     const currentPath = process.argv[1];
 
-    if (currentPath.endsWith("mx-scripts.js")){
+    if (currentPath.endsWith("mx-scripts.js")) {
         return gulpSlash(path.join(currentPath, "../../"));
     }
     const isBinFolder = currentPath.indexOf(gulpSlash("/node_modules/.bin")) !== -1;
