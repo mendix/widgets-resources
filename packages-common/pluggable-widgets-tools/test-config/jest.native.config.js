f5a34d65596db66f293cd1c6e4728b6729b547ff
Mitya Kononchuk <mitya.kononchuk@mendix.com>
2020-04-22 13:10:14 +0200

    feat(pluggable-widgets-tools): make all script pass

--- packages-common/pluggable-widgets-tools/test-config/jest.native.config.js
+++ packages-common/pluggable-widgets-tools/test-config/jest.native.config.js
@@ -7,55 +7,28 @@ const nativeConfig = {
     rootDir: "../../../../",
     globals: {
         "ts-jest": {
-            "tsConfig": {
+            tsConfig: {
                 module: "commonjs"
             }
         }
     },
     haste: {
         defaultPlatform: "android",
-        platforms: [
-            "android",
-            "ios",
-            "native"
-        ],
-        providesModuleNodeModules: [
-            "react-native"
-        ]
+        platforms: ["android", "ios", "native"],
+        providesModuleNodeModules: ["react-native"]
     },
-    moduleDirectories: [
-        "<rootDir>/node_modules",
-        "<rootDir>/src"
-    ],
-    moduleFileExtensions: [
-        "ts",
-        "tsx",
-        "js",
-        "jsx"
-    ],
-    modulePathIgnorePatterns: [
-        "<rootDir>/node_modules/react-native/Libraries/react-native/"
-    ],
-    setupFilesAfterEnv: [
-        "<rootDir>/node_modules/@mendix/pluggable-widgets-tools/test-config/test-index-native.js"
-    ],
-    snapshotSerializers: [
-        "enzyme-to-json/serializer"
-    ],
-    testMatch: [
-        "<rootDir>/src/**/?(*.)(spec|test).[jt]s?(x)"
-    ],
-    testPathIgnorePatterns: [
-        "<rootDir>/dist",
-        "<rootDir>/node_modules"
-    ],
+    moduleDirectories: ["<rootDir>/node_modules", "<rootDir>/src"],
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+    modulePathIgnorePatterns: ["<rootDir>/node_modules/react-native/Libraries/react-native/"],
+    setupFilesAfterEnv: [__dirname + "/test-index-native.js"],
+    snapshotSerializers: ["enzyme-to-json/serializer"],
+    testMatch: ["<rootDir>/src/**/?(*.)(spec|test).[jt]s?(x)"],
+    testPathIgnorePatterns: ["<rootDir>/dist", "<rootDir>/node_modules"],
     reporters: ["default"],
-    transformIgnorePatterns: [
-        "/node_modules/(?!react-native)/.+"
-    ],
+    transformIgnorePatterns: ["/node_modules/(?!react-native)/.+"],
     transform: {
         "^.+\\.tsx?$": "ts-jest",
-        "^.+\\.jsx?$": "<rootDir>/node_modules/@mendix/pluggable-widgets-tools/test-config/transform-native.js"
+        "^.+\\.jsx?$": __dirname + "/transform-native.js"
     },
     coverageDirectory: "<rootDir>/dist/coverage"
 };
--- scripts/test/jest.web.config.js
+++ scripts/test/jest.web.config.js
@@ -12,7 +12,7 @@ module.exports = {
             tsConfig: `${cwd}/tsconfig.spec.json`
         }
     },
-    moduleDirectories: ["<rootDir>/node_modules", `${cwd}/node_modules`, `${cwd}/src`],
+    moduleDirectories: undefined,
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
             "<rootDir>/__mocks__/fileMock.js",
