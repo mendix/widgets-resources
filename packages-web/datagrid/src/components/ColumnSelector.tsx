9def9da2916ca5b5dd7fbcc94599a79435610d10
Diego Antonelli <diego.antonelli@mendix.com>
2020-05-29 16:40:07 +0200

    feat(datagrid): implementing usable column selector

--- packages-web/datagrid/src/components/ColumnSelector.tsx
+++ packages-web/datagrid/src/components/ColumnSelector.tsx
@@ -1,4 +1,4 @@
-import { createElement, ReactElement } from "react";
+import { createElement, ReactElement, Fragment, useState } from "react";
 
 interface ColumnSelectorProps {
     allColumns: any[];
@@ -6,25 +6,34 @@ interface ColumnSelectorProps {
 }
 
 export function ColumnSelector({ allColumns, columnsConfig }: ColumnSelectorProps): ReactElement {
+    const [show, setShow] = useState(false);
+    const toggleColumnSelector = (): void => {
+        setShow(s => !s);
+    };
     return (
-        <div className="column-selectors">
-            {allColumns
-                .sort((c1, c2) => c1.Header.localeCompare(c2.Header))
-                .map((column: any, index: number) => {
-                    const { hidable } = columnsConfig[column.id];
-                    return hidable !== "no" ? (
-                        <div key={`fragment_${index}`} className="column-selector">
-                            <label htmlFor={`checkbox_toggle_${index}`}>{column.render("Header")}</label>
-                            <input
-                                id={`checkbox_toggle_${index}`}
-                                type="checkbox"
-                                checked={column.isVisible}
-                                onClick={() => column.toggleHidden()}
-                                {...column.getToggleHiddenProps()}
-                            />
-                        </div>
-                    ) : null;
-                })}
-        </div>
+        <Fragment>
+            <button className="btn btn-default" onClick={toggleColumnSelector}>
+                Columns
+            </button>
+            {show && (
+                <ul className="column-selectors">
+                    {allColumns.map((column: any, index: number) => {
+                        const { hidable } = columnsConfig[column.id];
+                        return hidable !== "no" ? (
+                            <li key={`fragment_${index}`}>
+                                <input
+                                    id={`checkbox_toggle_${index}`}
+                                    type="checkbox"
+                                    checked={column.isVisible}
+                                    onClick={() => column.toggleHidden()}
+                                    {...column.getToggleHiddenProps()}
+                                />
+                                <label htmlFor={`checkbox_toggle_${index}`}>{column.render("Header")}</label>
+                            </li>
+                        ) : null;
+                    })}
+                </ul>
+            )}
+        </Fragment>
     );
 }
