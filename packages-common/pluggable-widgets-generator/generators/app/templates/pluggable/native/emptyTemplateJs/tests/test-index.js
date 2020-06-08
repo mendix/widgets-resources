0945bb0dc77d15bad3a28d005a7c74a90e6b73b6
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-06-08 09:55:16 +0200

    chore(generator-widget): remove e2e test files

--- packages-common/pluggable-widgets-generator/generators/app/templates/pluggable/native/emptyTemplateJs/tests/test-index.js
+++
@@ -1,9 +0,0 @@
-import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-
-Enzyme.configure({ adapter: new Adapter() });
-
-jest.mock("react-native/Libraries/Utilities/Platform", () => ({
-    OS: "android",
-    select: jest.fn(dict => dict.android)
-}));
