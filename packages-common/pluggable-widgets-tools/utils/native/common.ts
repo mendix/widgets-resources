1d4945287536a1cc5ce9b247689b5290ac53c780
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-06-16 14:41:46 +0200

    refactor(generator-widget): move utils to PIW-tools
    - Fix template placeholders for packageName

--- packages-common/pluggable-widgets-tools/utils/native/common.ts
+++ packages-common/pluggable-widgets-tools/utils/native/common.ts
@@ -8,19 +8,16 @@ export interface Style {
     [key: string]: CustomStyle | ViewStyle | TextStyle | ImageStyle;
 }
 
-export function flattenStyles<T extends Style>(defaultStyle: T, overrideStyles: Array<(T | undefined)>): T {
+export function flattenStyles<T extends Style>(defaultStyle: T, overrideStyles: Array<T | undefined>): T {
     const styles = [defaultStyle, ...overrideStyles.filter((object): object is T => object !== undefined)];
 
-    return Object.keys(defaultStyle).reduce(
-        (flattened, currentKey) => {
-            const styleItems = styles.map(object => object[currentKey]);
-            return {
-                ...flattened,
-                [currentKey]: flattenObjects(styleItems)
-            };
-        },
-        {} as T
-    );
+    return Object.keys(defaultStyle).reduce((flattened, currentKey) => {
+        const styleItems = styles.map(object => object[currentKey]);
+        return {
+            ...flattened,
+            [currentKey]: flattenObjects(styleItems)
+        };
+    }, {} as T);
 }
 
 function flattenObjects<T extends object>(objects: T[]): T {
