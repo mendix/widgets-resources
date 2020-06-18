db162725f134766240269c767b21e671393054aa
mehmet <mehmet.izci@mendix.com>
2020-06-18 16:37:18 +0200

    fix(): rename NativeMobileActions to NativeMobileResources

--- packages-native/actions/src/platform/OpenInAppBrowser.ts
+++ packages-native/actions/src/platform/OpenInAppBrowser.ts
@@ -9,7 +9,7 @@ import InAppBrowser, { InAppBrowserOptions } from "react-native-inappbrowser-reb
 /**
  * @param {string} url - This field is required.
  * @param {string} toolbarColor - An optional custom background color for the browser toolbar. For example: 'red' or '#6200EE'.
- * @param {"NativeMobileActions.InAppBrowserDismissButtonStyle.done"|"NativeMobileActions.InAppBrowserDismissButtonStyle.close"|"NativeMobileActions.InAppBrowserDismissButtonStyle.cancel"} iosDismissButtonStyle - iOS only setting. The text that should be used for the button that closes the in app browser. Set to empty to use default value 'close'.
+ * @param {"NativeMobileResources.InAppBrowserDismissButtonStyle.done"|"NativeMobileResources.InAppBrowserDismissButtonStyle.close"|"NativeMobileResources.InAppBrowserDismissButtonStyle.cancel"} iosDismissButtonStyle - iOS only setting. The text that should be used for the button that closes the in app browser. Set to empty to use default value 'close'.
  * @param {boolean} androidShowTitle - Android only setting. Set to true to show the title of the web page in the toolbar.
  * @returns {Promise.<void>}
  */
