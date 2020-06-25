ac352d6ed5fbb78c197e9a5604fe2c77522b8bb0
mehmet <mehmet.izci@mendix.com>
2020-06-25 16:53:58 +0200

    fix(accessibility-helper): add more empty cases to fill

--- packages-web/accessibility-helper/tests/e2e/specs/AccessibilityHelper.spec.ts
+++ packages-web/accessibility-helper/tests/e2e/specs/AccessibilityHelper.spec.ts
@@ -11,4 +11,24 @@ describe("accessibility-helper", () => {
         $(".mx-name-radiButtons1 input").click();
         expect(elementToBeChanged.getAttribute("yolo1")).toEqual("yolo1");
     });
+
+    it("hides attributes when condition is false", () => {
+        expect(true).toBe(true);
+    });
+
+    it("updates target attributes when attributes are dynamic", () => {
+        expect(true).toBe(true);
+    });
+
+    it("doesnt change forbidden attributes", () => {
+        expect(true).toBe(true);
+    });
+
+    it("sets target attributes even though target's props changed eg: textinput", () => {
+        expect(true).toBe(true);
+    });
+
+    it("sets target attributes even though target is conditionally shown after being hidden", () => {
+        expect(true).toBe(true);
+    });
 });
