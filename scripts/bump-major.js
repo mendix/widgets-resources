135fa582dd3ee235a80d9d38838a91979773207c
Wesley <wesley.sieses@mendix.com>
2020-07-01 14:14:54 +0200

    bump version

--- scripts/bump-major.js
+++ scripts/bump-major.js
@@ -5,7 +5,7 @@ const { writeVersionFiles } = require("./lib/writeFile.js");
     const paths = getVersionFilePaths();
     const files = readVersionFiles(paths);
     const versionNumbers = files.pkg.version.split(".");
-    const majorNumber = Number(versionNumbers[1]) + 1;
+    const majorNumber = Number(versionNumbers[0]) + 1;
     const majorVersion = majorNumber + "." + 0 + "." + 0;
     files.pkg.version = files.manifest.ts.version = files.manifest.js.version = files.manifest.sass.version = majorVersion;
     writeVersionFiles(files, paths);
