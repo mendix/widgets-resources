52728b60ce28ca1707154b928b54d098d15c1561
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-07-01 13:42:44 +0200

    chore(pluggable-widgets-tools): use jest projects

--- packages-common/pluggable-widgets-tools/jest.native.config.js
+++
@@ -1,8 +0,0 @@
-const cwd = process.cwd();
-
-const base = require("../../scripts/test/jest.native.config.js");
-
-module.exports = {
-    ...base,
-    testMatch: [`${cwd}/src/native/**/*.spec.{ts,tsx}`]
-};
--- packages-common/pluggable-widgets-tools/jest.web.config.js
+++
@@ -1,8 +0,0 @@
-const cwd = process.cwd();
-
-const base = require("../../scripts/test/jest.web.config.js");
-
-module.exports = {
-    ...base,
-    testMatch: [`${cwd}/src/web/**/?(*.)(spec|test).[jt]s?(x)`]
-};
