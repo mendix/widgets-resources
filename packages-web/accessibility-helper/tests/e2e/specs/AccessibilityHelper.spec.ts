30f038385f1c2311daa9a811ba50b56115cde665
mehmet <mehmet.izci@mendix.com>
2020-07-06 18:46:39 +0200

    fix(accessibility-helper): add attributes to all matched elements

--- packages-web/accessibility-helper/tests/e2e/specs/AccessibilityHelper.spec.ts
+++ packages-web/accessibility-helper/tests/e2e/specs/AccessibilityHelper.spec.ts
@@ -5,55 +5,144 @@ describe("accessibility-helper", () => {
         page.open("");
     });
 
-    it("sets attributes when condition is true", () => {
-        $(".mx-name-radioButtons2 input").click();
-        const elementToBeChanged = page.getWidget("text3");
-        expect(elementToBeChanged.getAttribute("trueCondition")).toEqual("true");
+    describe("with single target", () => {
+        it("sets attributes when condition is true", () => {
+            $(".mx-name-radioButtons2 input").click();
+            const elementToBeChanged = page.getWidget("text3");
+
+            expect(elementToBeChanged.getAttribute("trueCondition")).toEqual("true");
+        });
+
+        it("hides attributes when condition is false", () => {
+            $(".mx-name-radioButtons2 input").click();
+            const elementToBeChanged = page.getWidget("text3");
+
+            expect(elementToBeChanged.getAttribute("a11yhelper")).not.toBe("a11yhelper");
+        });
+
+        it("updates target attributes when attributes are expression", () => {
+            $(".mx-name-radioButtons2 input").click();
+            const elementToBeChanged = page.getWidget("text3");
+            const textBox = page.getWidget("textBox1");
+            textBox.$("input").setValue("test");
+            $(".mx-name-radioButtons1 input").click();
+
+            expect(elementToBeChanged.getAttribute("expressionValue")).toEqual("test");
+        });
+        it("updates target attributes using a NF", () => {
+            $(".mx-name-radioButtons2 input").click();
+            $(".mx-name-radioButtons1 input").click();
+            const elementToBeChanged = page.getWidget("text3");
+            $(".mx-name-actionButton1").click();
+            browser.pause(1000);
+
+            expect(elementToBeChanged.getAttribute("expressionValue")).toEqual("NF changes");
+        });
+
+        it("sets target attributes even though target's props changed eg: textinput", () => {
+            $(".mx-name-radioButtons2 input").click();
+            $(".mx-name-radioButtons1 input").click();
+            const elementToBeChanged = page.getWidget("text3");
+            expect(elementToBeChanged.getAttribute("a11yhelper")).toContain("a11yhelper");
+            const textBox = page.getWidget("textBox1");
+            textBox.$("input").setValue("test");
+
+            expect(elementToBeChanged.getAttribute("a11yhelper")).toContain("a11yhelper");
+        });
+
+        it("sets target attributes even though target is conditionally shown after being hidden", () => {
+            const radioVisibility = $(".mx-name-radioButtons2 input");
+            const radioAtt = $(".mx-name-radioButtons1 input");
+            radioVisibility.click();
+            radioAtt.click();
+            const elementToBeChanged = page.getWidget("text3");
+
+            expect(elementToBeChanged.getAttribute("a11yhelper")).toContain("a11yhelper");
+
+            const radioNo = $(".mx-name-radioButtons1").$("label*=No");
+            radioNo.click();
+            radioAtt.click();
+
+            expect(elementToBeChanged.getAttribute("a11yhelper")).toContain("a11yhelper");
+        });
     });
 
-    it("hides attributes when condition is false", () => {
-        $(".mx-name-radioButtons2 input").click();
-        const elementToBeChanged = page.getWidget("text3");
-        expect(elementToBeChanged.getAttribute("a11yhelper")).not.toBe("a11yhelper");
-    });
+    describe("with multiple targets", () => {
+        it("sets attributes when condition is true", () => {
+            $(".mx-name-radioButtons2 input").click();
+            const elementToBeChanged = page.getWidget("text3");
+            const elementToBeChanged2 = page.getWidget("text4");
 
-    it("updates target attributes when attributes are expression", () => {
-        $(".mx-name-radioButtons2 input").click();
-        const elementToBeChanged = page.getWidget("text3");
-        const textBox = page.getWidget("textBox1");
-        textBox.$("input").setValue("test");
-        $(".mx-name-radioButtons1 input").click();
-        expect(elementToBeChanged.getAttribute("expressionValue")).toEqual("test");
-    });
-    it("updates target attributes using a NF", () => {
-        $(".mx-name-radioButtons2 input").click();
-        $(".mx-name-radioButtons1 input").click();
-        const elementToBeChanged = page.getWidget("text3");
-        $(".mx-name-actionButton1").click();
-        browser.pause(1000);
-        expect(elementToBeChanged.getAttribute("expressionValue")).toEqual("NF changes");
-    });
+            expect(elementToBeChanged.getAttribute("trueCondition")).toEqual("true");
+            expect(elementToBeChanged2.getAttribute("trueCondition")).toEqual("true");
+        });
 
-    it("sets target attributes even though target's props changed eg: textinput", () => {
-        $(".mx-name-radioButtons2 input").click();
-        $(".mx-name-radioButtons1 input").click();
-        const elementToBeChanged = page.getWidget("text3");
-        expect(elementToBeChanged.getAttribute("a11yhelper")).toContain("a11yhelper");
-        const textBox = page.getWidget("textBox1");
-        textBox.$("input").setValue("test");
-        expect(elementToBeChanged.getAttribute("a11yhelper")).toContain("a11yhelper");
-    });
+        it("hides attributes when condition is false", () => {
+            $(".mx-name-radioButtons2 input").click();
+            const elementToBeChanged = page.getWidget("text3");
+            const elementToBeChanged2 = page.getWidget("text4");
 
-    it("sets target attributes even though target is conditionally shown after being hidden", () => {
-        const radioVisibility = $(".mx-name-radioButtons2 input");
-        const radioAtt = $(".mx-name-radioButtons1 input");
-        radioVisibility.click();
-        radioAtt.click();
-        const elementToBeChanged = page.getWidget("text3");
-        expect(elementToBeChanged.getAttribute("a11yhelper")).toContain("a11yhelper");
-        const radioNo = $(".mx-name-radioButtons1").$("label*=No");
-        radioNo.click();
-        radioAtt.click();
-        expect(elementToBeChanged.getAttribute("a11yhelper")).toContain("a11yhelper");
+            expect(elementToBeChanged.getAttribute("a11yhelper")).not.toBe("a11yhelper");
+            expect(elementToBeChanged2.getAttribute("a11yhelper")).not.toBe("a11yhelper");
+        });
+
+        it("updates target attributes when attributes are expression", () => {
+            $(".mx-name-radioButtons2 input").click();
+            const elementToBeChanged = page.getWidget("text3");
+            const elementToBeChanged2 = page.getWidget("text4");
+            const textBox = page.getWidget("textBox1");
+            textBox.$("input").setValue("test");
+            $(".mx-name-radioButtons1 input").click();
+
+            expect(elementToBeChanged.getAttribute("expressionValue")).toEqual("test");
+            expect(elementToBeChanged2.getAttribute("expressionValue")).toEqual("test");
+        });
+        it("updates target attributes using a NF", () => {
+            $(".mx-name-radioButtons2 input").click();
+            $(".mx-name-radioButtons1 input").click();
+            const elementToBeChanged = page.getWidget("text3");
+            const elementToBeChanged2 = page.getWidget("text4");
+            $(".mx-name-actionButton1").click();
+            browser.pause(1000);
+
+            expect(elementToBeChanged.getAttribute("expressionValue")).toEqual("NF changes");
+            expect(elementToBeChanged2.getAttribute("expressionValue")).toEqual("NF changes");
+        });
+
+        it("sets target attributes even though target's props changed eg: textinput", () => {
+            $(".mx-name-radioButtons2 input").click();
+            $(".mx-name-radioButtons1 input").click();
+            const elementToBeChanged = page.getWidget("text3");
+            const elementToBeChanged2 = page.getWidget("text4");
+
+            expect(elementToBeChanged.getAttribute("a11yhelper")).toContain("a11yhelper");
+            expect(elementToBeChanged2.getAttribute("a11yhelper")).toContain("a11yhelper");
+
+            const textBox = page.getWidget("textBox1");
+            textBox.$("input").setValue("test");
+
+            // TODO: we should check the changed attribute, not the static one
+            expect(elementToBeChanged.getAttribute("a11yhelper")).toContain("a11yhelper");
+            expect(elementToBeChanged.getAttribute("a11yhelper")).toContain("a11yhelper");
+        });
+
+        it("sets target attributes even though target is conditionally shown after being hidden", () => {
+            const radioVisibility = $(".mx-name-radioButtons2 input");
+            const radioAtt = $(".mx-name-radioButtons1 input");
+            radioVisibility.click();
+            radioAtt.click();
+            const elementToBeChanged = page.getWidget("text3");
+            const elementToBeChanged2 = page.getWidget("text4");
+
+            expect(elementToBeChanged.getAttribute("a11yhelper")).toContain("a11yhelper");
+            expect(elementToBeChanged2.getAttribute("a11yhelper")).toContain("a11yhelper");
+
+            const radioNo = $(".mx-name-radioButtons1").$("label*=No");
+            radioNo.click();
+            radioAtt.click();
+
+            expect(elementToBeChanged.getAttribute("a11yhelper")).toContain("a11yhelper");
+            expect(elementToBeChanged2.getAttribute("a11yhelper")).toContain("a11yhelper");
+        });
     });
 });
