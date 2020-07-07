bd625f41aeb62d4d768a198f3c2a446f303cfef8
mehmet <mehmet.izci@mendix.com>
2020-07-07 12:02:27 +0200

    fix(nanoflow-commons): add typings for mx.data.update

--- packages-common/nanoflow-commons/src/types/mx.d.ts
+++ packages-common/nanoflow-commons/src/types/mx.d.ts
@@ -3,4 +3,8 @@ declare namespace mx {
     interface ui extends mx.ui {
         toggleSidebar: () => void;
     }
+    // eslint-disable-next-line @typescript-eslint/class-name-casing
+    interface data extends mx.data {
+        update: (param: { guid?: string | undefined; entity?: string | undefined; callback?: () => void }) => void;
+    }
 }
