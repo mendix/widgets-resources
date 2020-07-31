5409d0aa7969361ff92bb5a3c63eeceffef9c181
andries-smit <mr.andries.smit@gmail.com>
2020-07-31 14:00:08 +0200

    test(pluggable-widgets-tools): match all tests

--- packages/tools/pluggable-widgets-tools/jest.config.js
+++ packages/tools/pluggable-widgets-tools/jest.config.js
@@ -4,13 +4,13 @@ const nativeBaseConfig = require("./test-config/jest.native.config.js");
 const webConfig = {
     ...webBaseConfig,
     rootDir: ".",
-    testMatch: ["<rootDir>/src/{web,typings-generator}/**/*.spec.{ts,tsx}"]
+    testMatch: ["<rootDir>/src/@(web|typings-generator)/**/*.spec.ts"]
 };
 
 const nativeConfig = {
     ...nativeBaseConfig,
     rootDir: ".",
-    testMatch: ["<rootDir>/src/native/**/*.spec.{ts,tsx}"]
+    testMatch: ["<rootDir>/src/native/**/*.spec.ts"]
 };
 
 module.exports = {
