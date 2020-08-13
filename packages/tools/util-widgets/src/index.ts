b1335a1a9edd0c84040c9e7751165e78519ac491
mehmet <mehmet.izci@mendix.com>
2020-08-13 17:28:24 +0200

    fix(timeline): add/rm necessary libs since refactor changed them

--- packages/tools/util-widgets/src/index.ts
+++ packages/tools/util-widgets/src/index.ts
@@ -1,4 +1,3 @@
 export { flattenStyles, Style } from "./styles";
 export { only, exclude } from "./objectFilters";
 export { available, toNumber, unavailable } from "./properties";
-export * from "../../piw-utils/src/builders/DynamicActionValueBuilder";
