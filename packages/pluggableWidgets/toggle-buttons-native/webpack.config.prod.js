c346cd5cc088b0eea0a2efc4d9c5b880bb46ea5e
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-08-27 13:36:28 +0200

    chore(toggle-buttons-native): simplify webpack configs

--- packages/pluggableWidgets/toggle-buttons-native/webpack.config.prod.js
+++ packages/pluggableWidgets/toggle-buttons-native/webpack.config.prod.js
@@ -1,61 +1,8 @@
 const merge = require("webpack-merge");
 const baseConfig = require("@mendix/pluggable-widgets-tools/configs/webpack.native.config.js");
 
-const customConfig = {
-    mode: "production",
-    devtool: false,
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader",
-                options: {
-                    compilerOptions: {
-                        target: "esnext"
-                    }
-                }
-            },
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        cacheDirectory: true,
-                        presets: ["@babel/preset-react"],
-                        plugins: [
-                            ["@babel/plugin-proposal-class-properties", { loose: true }],
-                            ["@babel/plugin-transform-react-jsx", { pragma: "createElement" }]
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.jsx?$/,
-                include: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        cacheDirectory: true,
-                        presets: ["@babel/preset-react"],
-                        plugins: [
-                            ["@babel/plugin-proposal-class-properties", { loose: true }],
-                            "@babel/plugin-transform-react-jsx",
-                            "@babel/plugin-transform-flow-strip-types"
-                        ]
-                    }
-                }
-            }
-        ]
-    }
-};
+baseConfig[0].module.rules[2].use.options.plugins.push("@babel/plugin-transform-flow-strip-types");
+baseConfig[0].mode = "production";
+baseConfig[0].devtool = false;
 
-const customConfigurations = [merge.smartStrategy({ "module.rules": "replace" })(baseConfig[0], customConfig)];
-
-const hasEditorConfig = baseConfig.length === 2;
-
-if (hasEditorConfig) {
-    customConfigurations.push(baseConfig[1]);
-}
-
-module.exports = customConfigurations;
+module.exports = baseConfig;
