a26081cc491aa28adb3d914b82681d5df81f70eb
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-09-09 15:28:53 +0200

    chore(line-chart-native): improve styling
    - Add default styles

---
+++ packages/pluggableWidgets/line-chart-native/src/ui/Styles.ts
@@ -0,0 +1,27 @@
+import { VictoryAxisCommonProps, VictoryCommonProps, VictoryLabelProps } from "victory-core";
+import { VictoryLineProps } from "victory-line";
+import { VictoryScatterProps } from "victory-scatter";
+
+export interface LineChartStyle {
+    chart?: { padding?: VictoryCommonProps["padding"] };
+    xAxis?: VictoryAxisCommonProps["style"] & { axisLabel?: { verticalOffset?: VictoryLabelProps["dy"] } };
+    yAxis?: VictoryAxisCommonProps["style"] & { axisLabel?: { horizontalOffset?: VictoryLabelProps["dy"] } };
+    series?: { [key: string]: LineChartSeriesStyle };
+}
+
+export interface LineChartSeriesStyle {
+    line?: VictoryLineProps["style"];
+    markers?: VictoryScatterProps["style"] & { size?: VictoryScatterProps["size"] };
+}
+
+export const defaultLineChartStyle: LineChartStyle = {
+    chart: {
+        padding: { left: 75, top: 25, bottom: 50, right: 25 }
+    },
+    xAxis: {
+        axisLabel: { verticalOffset: 10 }
+    },
+    yAxis: {
+        axisLabel: { horizontalOffset: -10 }
+    }
+};
