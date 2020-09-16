4d95c7bf6d7f0548f2566fb0ea3a58c36ad2e7bb
Wesley <wesley.sieses@mendix.com>
2020-09-16 14:48:37 +0200

    feat(signature-native): finalised widget

--- packages/pluggableWidgets/signature-native/src/Signature.tsx
+++ packages/pluggableWidgets/signature-native/src/Signature.tsx
@@ -1,50 +1,113 @@
 import { flattenStyles } from "@native-mobile-resources/util-widgets";
 import { executeAction } from "@widgets-resources/piw-utils";
-import { createElement, ReactElement, useRef, useState } from "react";
-import { View, Image } from "react-native";
+import { createElement, ReactElement, useRef } from "react";
+import { View, Text } from "react-native";
 import SignatureScreen, { SignatureViewRef } from "react-native-signature-canvas";
+import { extractStyles } from "../../../tools/util-widgets/src/styles";
+import { Touchable } from "../components/Touchable";
 
 import { SignatureProps } from "../typings/SignatureProps";
 import { SignatureStyle, defaultSignatureStyle } from "./ui/Styles";
 
-export function Signature(props: SignatureProps<SignatureStyle>): ReactElement {
-    const styles = flattenStyles(defaultSignatureStyle, props.style);
+export type Props = SignatureProps<SignatureStyle>;
+
+export function Signature(props: Props): ReactElement {
     const ref = useRef<SignatureViewRef>(null);
-    const [signature, setSign] = useState("");
+    const styles = flattenStyles(defaultSignatureStyle, props.style);
+    const [signatureProps, containerStyles] = extractStyles(styles.container, ["penColor", "backgroundColor"]);
+    const [buttonClearContainerProps, buttonClearContainerStyles] = extractStyles(styles.buttonClearContainer, [
+        "rippleColor",
+        "activeOpacity",
+        "underlayColor"
+    ]);
+    const [buttonSaveContainerProps, buttonSaveContainerStyles] = extractStyles(styles.buttonSaveContainer, [
+        "rippleColor",
+        "activeOpacity",
+        "underlayColor"
+    ]);
+    const buttonCaptionClear = props.buttonCaptionClear?.value ?? "Clear";
+    const buttonCaptionSave = props.buttonCaptionSave?.value ?? "Save";
+    const webStyles = `
+                    .m-signature-pad {
+                        border: none;
+                    }
+                    .m-signature-pad--body {
+                      left: 0;
+                      right: 0;
+                      top: 0;
+                      bottom: 0;
+                      border: none;
+                    }
+                    .m-signature-pad--body canvas {
+                        border-radius: 0;
+                        box-shadow: none;
+                    }
+                    .m-signature-pad--footer {
+                        display: none;
+                    }
+                `;
 
-    const handleSignature = (base64signature: string) => {
-        console.warn("Signature => " + base64signature);
+    const handleSignature = (base64signature: string): void => {
         props.imageAttribute.setValue(base64signature);
-        setSign(base64signature);
-        executeAction(props.javascriptAction);
+        executeAction(props.onSave);
     };
 
-    const handleEmpty = () => {
-        console.warn("Empty");
+    const handleEmpty = (): void => {
+        executeAction(props.onEmpty);
     };
 
-    const handleClear = () => {
-        console.warn("clear success!");
+    const handleEnd = (): void => {
+        executeAction(props.onEnd);
     };
 
-    const handleEnd = () => {
-        // ref.current?.readSignature();
+    const handleClear = (): void => {
+        executeAction(props.onClear);
+    };
+
+    const onPressClearHandler = (): void => {
+        ref.current?.clearSignature();
+    };
+
+    const onPressSaveHandler = (): void => {
+        ref.current?.readSignature();
     };
 
     return (
-        <View style={[{ flex: 1 }, styles.container]}>
-            {signature ? <Image source={{ uri: signature }} style={{ height: 100, width: 100 }} /> : null}
+        <View style={[{ flex: 1 }, containerStyles]}>
             <SignatureScreen
                 ref={ref}
+                autoClear
+                onEmpty={handleEmpty}
                 onEnd={handleEnd}
                 onOK={handleSignature}
-                onEmpty={handleEmpty}
                 onClear={handleClear}
-                autoClear={true}
-                descriptionText={"Description"}
-                clearText="Clear"
-                confirmText="Save me"
+                webStyle={webStyles}
+                {...signatureProps}
             />
+            <View style={styles.buttonWrapper}>
+                <Touchable
+                    testID={`${buttonCaptionClear}$Touchable`}
+                    onPress={onPressClearHandler}
+                    accessible={false}
+                    style={buttonClearContainerStyles}
+                    {...buttonClearContainerProps}
+                >
+                    <Text testID={`${buttonCaptionClear}$caption`} style={styles.buttonClearCaption}>
+                        {buttonCaptionClear}
+                    </Text>
+                </Touchable>
+                <Touchable
+                    testID={`${buttonCaptionSave}$Touchable`}
+                    onPress={onPressSaveHandler}
+                    accessible={false}
+                    style={buttonSaveContainerStyles}
+                    {...buttonSaveContainerProps}
+                >
+                    <Text testID={`${buttonCaptionSave}$caption`} style={styles.buttonSaveCaption}>
+                        {buttonCaptionSave}
+                    </Text>
+                </Touchable>
+            </View>
         </View>
     );
 }
--- packages/pluggableWidgets/signature-native/src/ui/Styles.ts
+++ packages/pluggableWidgets/signature-native/src/ui/Styles.ts
@@ -1,12 +1,72 @@
 import { Style } from "@native-mobile-resources/util-widgets";
-import { ViewStyle } from "react-native";
+import { ViewStyle, TextStyle } from "react-native";
+
+interface ButtonContainerType extends ViewStyle {
+    rippleColor?: string;
+    activeOpacity?: number;
+    underlayColor?: string;
+}
 
 export interface SignatureStyle extends Style {
-    container: ViewStyle;
+    container: ViewStyle & {
+        penColor?: string;
+    };
+    buttonWrapper: ViewStyle;
+    buttonClearContainer: ButtonContainerType;
+    buttonSaveContainer: ButtonContainerType;
+    buttonClearCaption: TextStyle;
+    buttonSaveCaption: TextStyle;
 }
+
+const buttonContainer: ButtonContainerType = {
+    // rippleColor, underlayColor, activeOpacity and all ViewStyle properties are allowed
+    flex: 1,
+    borderWidth: 1,
+    borderStyle: "solid",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+
+    paddingVertical: 8,
+    paddingHorizontal: 8
+};
 
 export const defaultSignatureStyle: SignatureStyle = {
     container: {
-        backgroundColor: "transparent"
+        // penColor and all ViewStyle properties are allowed
+        penColor: "blue",
+        backgroundColor: "white"
+    },
+    buttonWrapper: {
+        // All ViewStyle properties are allowed
+        flexDirection: "row"
+    },
+    buttonClearContainer: {
+        // rippleColor, underlayColor, activeOpacity and all ViewStyle properties are allowed
+        ...buttonContainer,
+        borderColor: "#0595DB",
+        backgroundColor: "transparent",
+        marginRight: 8
+    },
+    buttonSaveContainer: {
+        // rippleColor, underlayColor, activeOpacity and all ViewStyle properties are allowed
+        ...buttonContainer,
+        borderColor: "#0595DB",
+        backgroundColor: "#0595DB",
+        marginLeft: 8
+    },
+    buttonClearCaption: {
+        // All TextStyle properties are allowed
+        color: "#0595DB",
+        fontSize: 12,
+        lineHeight: 18,
+        fontWeight: "bold"
+    },
+    buttonSaveCaption: {
+        // All TextStyle properties are allowed
+        color: "#FFF",
+        fontSize: 12,
+        lineHeight: 18,
+        fontWeight: "bold"
     }
 };
