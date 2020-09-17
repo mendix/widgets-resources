763bdd669818c45e0726dce1b5d4f5f9599491a2
Jordan Hunt <jordan.hunt@mendix.com>
2020-09-17 15:33:34 +0200

    feat(atlas): move content into separate dir

--- packages/theming/atlas/content/styles.js
+++ packages/theming/atlas/content/styles.js
@@ -1,6 +1,6 @@
-import * as custom from "./styles/native/js/app/custom";
-import mergeobjects from "./styles/native/js/core/helpers/_functions/mergeobjects";
-import * as main from "./styles/native/js/main";
+import * as custom from "../styles/native/js/app/custom";
+import mergeobjects from "../styles/native/js/core/helpers/_functions/mergeobjects";
+import * as main from "../styles/native/js/main";
 
 Object.keys(custom).forEach(key => {
     if (main[key]) {
