143c50af209578081c24c2cd8ca85dbf703022e2
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-09-17 14:09:08 +0200

    refactor(line-chart-native): split up dynamic series loading

--- packages/pluggableWidgets/line-chart-native/src/LineChart.tsx
+++ packages/pluggableWidgets/line-chart-native/src/LineChart.tsx
@@ -17,10 +17,6 @@ export function LineChart(props: LineChartProps<LineChartStyle>): ReactElement |
     const [chartSeries, setChartSeries] = useState<Array<LineChartSeries>>([]);
 
     useEffect(() => {
-        // 1) Set ready to true
-        // 2) Prepare series
-        // 3) Stop as soon as something is not available. (set ready to false & return result)
-
         let allDataAvailable = true;
 
         const chartSeriesResult = series.reduce<Array<LineChartSeries>>((result, series) => {
@@ -28,17 +24,7 @@ export function LineChart(props: LineChartProps<LineChartStyle>): ReactElement |
                 return result;
             }
 
-            const {
-                dataSource,
-                groupByAttribute,
-                interpolation,
-                lineStyle,
-                seriesNameAttribute,
-                stylePropertyNameAttribute,
-                type,
-                xValue,
-                yValue
-            } = series;
+            const { dataSource, interpolation, lineStyle, type } = series;
 
             let interpolationSetting: InterpolationPropType;
 
@@ -62,87 +48,8 @@ export function LineChart(props: LineChartProps<LineChartStyle>): ReactElement |
                 return result;
             }
 
-            const groupedDataSourceItems = dataSource.items!.reduce<
-                Array<{
-                    groupByAttributeValue: string;
-                    seriesNameAttributeValue?: string;
-                    stylePropertyNameAttributeValue?: string;
-                    items: Array<ObjectItem>;
-                }>
-            >((dataSourceItemsResult, item) => {
-                if (!allDataAvailable) {
-                    return dataSourceItemsResult;
-                }
-
-                const groupByAttributeValue = groupByAttribute!(item);
-
-                if (groupByAttributeValue.status !== ValueStatus.Available) {
-                    allDataAvailable = false;
-                    return dataSourceItemsResult;
-                }
-
-                const group = dataSourceItemsResult.find(
-                    group => group.groupByAttributeValue === groupByAttributeValue.value
-                );
-
-                if (group) {
-                    group.items.push(item);
-                } else {
-                    const seriesNameAttributeValue = seriesNameAttribute!(item);
-                    const stylePropertyNameAttributeValue = stylePropertyNameAttribute!(item);
-
-                    if (
-                        stylePropertyNameAttributeValue.status !== ValueStatus.Available ||
-                        seriesNameAttributeValue.status !== ValueStatus.Available
-                    ) {
-                        allDataAvailable = false;
-                        return dataSourceItemsResult;
-                    }
-
-                    dataSourceItemsResult.push({
-                        groupByAttributeValue: groupByAttributeValue.value!,
-                        seriesNameAttributeValue: seriesNameAttributeValue.value,
-                        stylePropertyNameAttributeValue: stylePropertyNameAttributeValue.value,
-                        items: [item]
-                    });
-                }
-
-                return dataSourceItemsResult;
-            }, []);
-
-            groupedDataSourceItems.forEach(itemGroup => {
-                result.push({
-                    dataPoints: itemGroup.items.reduce<Array<LineChartDataPoint>>((dataPointsResult, item) => {
-                        if (!allDataAvailable) {
-                            return dataPointsResult;
-                        }
-
-                        const x = xValue(item);
-                        const y = yValue(item);
-
-                        if (x.status !== ValueStatus.Available || y.status !== ValueStatus.Available) {
-                            allDataAvailable = false;
-                            return dataPointsResult;
-                        }
-
-                        dataPointsResult.push({ x: Number(x.value!.toFixed()), y: Number(y.value!.toFixed()) });
-
-                        return dataPointsResult;
-                    }, []),
-                    interpolation: interpolationSetting, // TODO: use attribute
-                    name: itemGroup.seriesNameAttributeValue,
-                    stylePropertyName: itemGroup.stylePropertyNameAttributeValue // TODO: use attribute
-                });
-            });
-
-            // 1. group data by group attr
-            // 2. get serie group value
-            // 3. check existing groups
-            // 4. drop dss item in array of group
-            // 5. For each array create a line chart series and push onto the chartSeries array
-
-            // How are you going to deal with styles? Have a special attribute that contains the style property name.
-
+            const groupedDataSourceItems = groupDataSourceItems(series, allDataAvailable);
+            loadDynamicSeries(result, allDataAvailable, series, interpolationSetting, groupedDataSourceItems);
             return result;
         }, []);
 
@@ -182,7 +89,7 @@ export function LineChart(props: LineChartProps<LineChartStyle>): ReactElement |
             loadedSeries: LineChartSeries[],
             allDataAvailable: boolean,
             series: SeriesType,
-            interpolationSetting: InterpolationPropType
+            interpolation: InterpolationPropType
         ): void => {
             const { dataSource, seriesName } = series;
 
@@ -193,9 +100,91 @@ export function LineChart(props: LineChartProps<LineChartStyle>): ReactElement |
 
             loadedSeries.push({
                 dataPoints: convertToDataPoints(dataSource.items!, series, allDataAvailable),
-                interpolation: interpolationSetting,
+                interpolation,
                 name: seriesName ? seriesName.value : undefined,
                 stylePropertyName: series.stylePropertyName
+            });
+        },
+        [convertToDataPoints]
+    );
+
+    const groupDataSourceItems = useCallback((series: SeriesType, allDataAvailable: boolean): Array<{
+        groupByAttributeValue: string;
+        seriesNameAttributeValue?: string;
+        stylePropertyNameAttributeValue?: string;
+        items: Array<ObjectItem>;
+    }> => {
+        const { dataSource, groupByAttribute, seriesNameAttribute, stylePropertyNameAttribute } = series;
+
+        return dataSource.items!.reduce<
+            Array<{
+                groupByAttributeValue: string;
+                seriesNameAttributeValue?: string;
+                stylePropertyNameAttributeValue?: string;
+                items: Array<ObjectItem>;
+            }>
+        >((dataSourceItemsResult, item) => {
+            if (!allDataAvailable) {
+                return dataSourceItemsResult;
+            }
+
+            const groupByAttributeValue = groupByAttribute!(item);
+
+            if (groupByAttributeValue.status !== ValueStatus.Available) {
+                allDataAvailable = false;
+                return dataSourceItemsResult;
+            }
+
+            const group = dataSourceItemsResult.find(
+                group => group.groupByAttributeValue === groupByAttributeValue.value
+            );
+
+            if (group) {
+                group.items.push(item);
+            } else {
+                const seriesNameAttributeValue = seriesNameAttribute!(item);
+                const stylePropertyNameAttributeValue = stylePropertyNameAttribute!(item);
+
+                if (
+                    stylePropertyNameAttributeValue.status !== ValueStatus.Available ||
+                    seriesNameAttributeValue.status !== ValueStatus.Available
+                ) {
+                    allDataAvailable = false;
+                    return dataSourceItemsResult;
+                }
+
+                dataSourceItemsResult.push({
+                    groupByAttributeValue: groupByAttributeValue.value!,
+                    seriesNameAttributeValue: seriesNameAttributeValue.value,
+                    stylePropertyNameAttributeValue: stylePropertyNameAttributeValue.value,
+                    items: [item]
+                });
+            }
+
+            return dataSourceItemsResult;
+        }, []);
+    }, []);
+
+    const loadDynamicSeries = useCallback(
+        (
+            loadedSeries: LineChartSeries[],
+            allDataAvailable: boolean,
+            series: SeriesType,
+            interpolation: InterpolationPropType,
+            groupedDataSourceItems: Array<{
+                groupByAttributeValue: string;
+                seriesNameAttributeValue?: string;
+                stylePropertyNameAttributeValue?: string;
+                items: Array<ObjectItem>;
+            }>
+        ): void => {
+            groupedDataSourceItems.forEach(itemGroup => {
+                loadedSeries.push({
+                    dataPoints: convertToDataPoints(itemGroup.items, series, allDataAvailable),
+                    interpolation,
+                    name: itemGroup.seriesNameAttributeValue,
+                    stylePropertyName: itemGroup.stylePropertyNameAttributeValue
+                });
             });
         },
         [convertToDataPoints]
