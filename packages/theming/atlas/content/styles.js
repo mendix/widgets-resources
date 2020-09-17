d478909ab3e5dbc31aff8d5daa7902d4c96c8890
Jordan Hunt <jordan.hunt@mendix.com>
2020-09-17 16:03:32 +0200

    feat(atlas): rename dir to src

--- packages/theming/atlas/content/styles.js
+++ packages/theming/atlas/content/styles.js
@@ -1,6 +1,6 @@
-import * as custom from "../styles/native/js/app/custom";
-import mergeobjects from "../styles/native/js/core/helpers/_functions/mergeobjects";
-import * as main from "../styles/native/js/main";
+import * as custom from "../src/native/js/app/custom";
+import mergeobjects from "../src/native/js/core/helpers/_functions/mergeobjects";
+import * as main from "../src/native/js/main";
 
 Object.keys(custom).forEach(key => {
     if (main[key]) {
