e2c96dce6f540a3570fad30f0760167fc80575c0
Jordan Hunt <jordan.hunt@mendix.com>
2020-09-22 17:20:04 +0200

    chore(atlas): simplify build script

--- packages/theming/atlas/scripts/build.js
+++ packages/theming/atlas/scripts/build.js
@@ -1,36 +1,33 @@
-const { exec } = require("shelljs");
+const { rm, cd } = require("shelljs");
+const { join } = require("path");
+const concurrently = require("concurrently");
 
-function build() {
-    const mode = process.argv[2] || "production";
-    const MX_PROJECT_PATH = process.env.ATLAS_MX_PROJECT_PATH;
+const mode = process.argv[2] || "production";
+const MX_PROJECT_PATH = process.env.ATLAS_MX_PROJECT_PATH; // should be an absolute path.
+const outputDir =
+    mode !== "production" && MX_PROJECT_PATH ? join(MX_PROJECT_PATH, "theme") : join(__dirname, "../dist/theme");
 
-    if (mode === "production") {
-        console.info("Building for production...");
-        exec("shx rm -rf ./dist");
-        exec("shx mkdir -p ./dist/theme/styles/web/css/fonts");
-        exec("shx cp ./src/web/sass/core/_legacy/bootstrap/fonts/* ./dist/theme/styles/web/css/fonts");
-        exec("shx cp -R ./content/. ./dist/theme/");
-        exec(
-            "concurrently -n 'sass,tsc'" +
-                " 'sass --embed-sources --no-charset -s compressed ./src/web/sass/main.scss ./dist/theme/styles/web/css/main.css'" +
-                " 'tsc -p ./tsconfig.json'"
-        );
-        exec("tsc -p ./tsconfig.json");
-        console.info("Done.");
-    } else {
-        console.info("Building for development...");
-        exec("shx rm -rf ./dist");
-        exec("shx mkdir -p ./dist/theme/styles/web/css/fonts");
-        exec("shx cp ./src/web/sass/core/_legacy/bootstrap/fonts/* ./dist/theme/styles/web/css/fonts");
-        exec(
-            "concurrently -n 'content,resources,sass,tsc,workingProject'" +
-                " 'copy-and-watch --watch content/* dist/theme'" +
-                " 'copy-and-watch --watch content/resources/* dist/theme/resources'" +
-                " 'sass --watch --embed-sources --no-charset ./src/web/sass/main.scss ./dist/theme/styles/web/css/main.css'" +
-                " 'tsc -w -p ./tsconfig.json'" +
-                ` 'copy-and-watch --watch dist/theme ${MX_PROJECT_PATH}/theme'`
-        );
+console.info(`Building for ${mode}...`);
+const watchArg = mode !== "production" ? "--watch" : "";
+const compressArg = mode === "production" ? "--style compressed" : "";
+
+cd(join(__dirname, ".."));
+rm("-rf", outputDir);
+concurrently([
+    {
+        name: "content",
+        command: `copy-and-watch ${watchArg} 'content/**/*' '${outputDir}'`
+    },
+    {
+        name: "fonts",
+        command: `copy-and-watch ${watchArg} ./src/web/sass/core/_legacy/bootstrap/fonts/* '${outputDir}/styles/web/css/fonts'`
+    },
+    {
+        name: "sass",
+        command: `sass ${watchArg} --embed-sources ${compressArg} --no-charset ./src/web/sass/main.scss '${outputDir}/styles/web/css/main.css'`
+    },
+    {
+        name: "tsc",
+        command: `tsc ${watchArg} --project ./tsconfig.json --outDir '${outputDir}/styles/native'`
     }
-}
-
-build();
+]);
