b6162f2d1109179e6130a3025ca853932a66bd35
Jordan Hunt <jordan.hunt@mendix.com>
2020-09-23 14:45:29 +0200

    chore(atlas): copy theme to deployment/web

--- packages/theming/atlas/scripts/build.js
+++ packages/theming/atlas/scripts/build.js
@@ -6,6 +6,7 @@ const mode = process.argv[2] || "production";
 const MX_PROJECT_PATH = process.env.ATLAS_MX_PROJECT_PATH; // should be an absolute path.
 const outputDir =
     mode !== "production" && MX_PROJECT_PATH ? join(MX_PROJECT_PATH, "theme") : join(__dirname, "../dist/theme");
+const projectDeployDir = mode !== "production" && MX_PROJECT_PATH ? join(MX_PROJECT_PATH, "deployment/web") : null;
 
 console.info(`Building for ${mode}...`);
 const watchArg = mode !== "production" ? "--watch" : "";
@@ -13,21 +14,30 @@ const compressArg = mode === "production" ? "--style compressed" : "";
 
 cd(join(__dirname, ".."));
 rm("-rf", outputDir);
-concurrently([
-    {
-        name: "content",
-        command: `copy-and-watch ${watchArg} 'content/**/*' '${outputDir}'`
-    },
-    {
-        name: "fonts",
-        command: `copy-and-watch ${watchArg} ./src/web/sass/core/_legacy/bootstrap/fonts/* '${outputDir}/styles/web/css/fonts'`
-    },
-    {
-        name: "sass",
-        command: `sass ${watchArg} --embed-sources ${compressArg} --no-charset ./src/web/sass/main.scss '${outputDir}/styles/web/css/main.css'`
-    },
-    {
-        name: "tsc",
-        command: `tsc ${watchArg} --project ./tsconfig.json --outDir '${outputDir}/styles/native'`
-    }
-]);
+concurrently(
+    [
+        {
+            name: "content",
+            command: `copy-and-watch ${watchArg} 'content/**/*' '${outputDir}'`
+        },
+        {
+            name: "fonts",
+            command: `copy-and-watch ${watchArg} ./src/web/sass/core/_legacy/bootstrap/fonts/* '${outputDir}/styles/web/css/fonts'`
+        },
+        {
+            name: "sass",
+            command: `sass ${watchArg} --embed-sources ${compressArg} --no-charset ./src/web/sass/main.scss '${outputDir}/styles/web/css/main.css'`
+        },
+        {
+            name: "tsc",
+            command: `tsc ${watchArg} --project ./tsconfig.json --outDir '${outputDir}/styles/native'`
+        }
+    ].concat(
+        projectDeployDir
+            ? {
+                  name: "project",
+                  command: `copy-and-watch --watch '${outputDir}/' '${projectDeployDir}'`
+              }
+            : []
+    )
+);
