e8a4f8fd1137b96fae3002610dac4b1d6ffb41e4
tieniber <tieniber@gmail.com>
2020-09-30 20:59:16 -0400

    Add backwards-compatibility
    Now supported both parsed and unparsed query strings

--- packages/jsActions/native-mobile-resources/src/deeplink/ParseUrlToObject.ts
+++ packages/jsActions/native-mobile-resources/src/deeplink/ParseUrlToObject.ts
@@ -23,9 +23,10 @@ async function createParamObject(entity: string, url: string): Promise<mendix.li
 }
 
 function splitUrlToObject(url: string): Records {
-    const urlObject = new UrlParse(url, true);
-    const queryValues: Records = {};
-    const query = urlObject.query;
+    const urlObject = new UrlParse(url);
+    const parsedUrlObject = new UrlParse(url, true);
+    const queryValues = {};
+    const query = parsedUrlObject.query;
 
     for (const [key, value] of Object.entries(query)) {
         queryValues[key.toLowerCase()] = value !== undefined ? value : "";
