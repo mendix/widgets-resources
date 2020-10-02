e62c63369f71e07b03c31df039e22b2fe096e41d
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-10-02 09:39:43 +0200

    feat(line-chart-native): add separate dss description for static and dynamic type

--- packages/pluggableWidgets/line-chart-native/src/LineChart.editorConfig.ts
+++ packages/pluggableWidgets/line-chart-native/src/LineChart.editorConfig.ts
@@ -5,12 +5,18 @@ import { LineChartPreviewProps } from "../typings/LineChartProps";
 export function getProperties(values: LineChartPreviewProps, defaultProperties: Properties): Properties {
     values.series.forEach((series, index) => {
         if (series.type === "static") {
+            hidePropertyIn(defaultProperties, values, "series", index, "dynamicDataSource");
             hidePropertyIn(defaultProperties, values, "series", index, "groupByAttribute");
             hidePropertyIn(defaultProperties, values, "series", index, "dynamicSeriesName");
+            hidePropertyIn(defaultProperties, values, "series", index, "dynamicXValue");
+            hidePropertyIn(defaultProperties, values, "series", index, "dynamicYValue");
             hidePropertyIn(defaultProperties, values, "series", index, "dynamicStylePropertyName");
         } else {
-            hidePropertyIn(defaultProperties, values, "series", index, "seriesName");
-            hidePropertyIn(defaultProperties, values, "series", index, "stylePropertyName");
+            hidePropertyIn(defaultProperties, values, "series", index, "staticDataSource");
+            hidePropertyIn(defaultProperties, values, "series", index, "staticSeriesName");
+            hidePropertyIn(defaultProperties, values, "series", index, "staticXValue");
+            hidePropertyIn(defaultProperties, values, "series", index, "staticYValue");
+            hidePropertyIn(defaultProperties, values, "series", index, "staticStylePropertyName");
         }
     });
     return defaultProperties;
