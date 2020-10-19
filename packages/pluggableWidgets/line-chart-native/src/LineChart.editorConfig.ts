2b595e14616879a9fe7e9e00a025b39ba48ff9ee
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-10-19 14:05:26 +0200

    feat(line-chart-native): add required consistency checks

--- packages/pluggableWidgets/line-chart-native/src/LineChart.editorConfig.ts
+++ packages/pluggableWidgets/line-chart-native/src/LineChart.editorConfig.ts
@@ -1,4 +1,4 @@
-import { hidePropertyIn, Properties } from "@widgets-resources/piw-utils";
+import { hidePropertyIn, Problem, Properties } from "@widgets-resources/piw-utils";
 
 import { LineChartPreviewProps } from "../typings/LineChartProps";
 
@@ -22,6 +22,81 @@ export function getProperties(values: LineChartPreviewProps, defaultProperties: 
         }
     });
     return defaultProperties;
+}
+
+export function check(values: LineChartPreviewProps): Problem[] {
+    const errors: Problem[] = [];
+
+    // property?: string; // key of the property, at which the problem exists
+    // severity?: "error" | "warning" | "deprecation"; // default = "error"
+    // message: string; // description of the problem
+    // studioMessage?: string; // studio-specific message, defaults to message
+    // url?: string; // link with more information about the problem
+    // studioUrl?: string; // studio-specific link
+
+    values.series.forEach((series, index) => {
+        if (series.type === "static") {
+            if (!series.staticDataSource) {
+                errors.push({
+                    property: "staticDataSource",
+                    severity: "error",
+                    message: `No data source configured for static series located at position ${index + 1}.`
+                });
+            }
+
+            if (!series.staticXValue) {
+                errors.push({
+                    property: "staticXValue",
+                    severity: "error",
+                    message: `No X attribute configured for static series located at position ${index + 1}.`
+                });
+            }
+
+            if (!series.staticYValue) {
+                errors.push({
+                    property: "staticYValue",
+                    severity: "error",
+                    message: `No Y attribute configured for static series located at position ${index + 1}.`
+                });
+            }
+        } else {
+            if (!series.dynamicDataSource) {
+                errors.push({
+                    property: "dynamicDataSource",
+                    severity: "error",
+                    message: `No data source configured for dynamic series located at position ${index + 1}.`
+                });
+            }
+
+            if (!series.dynamicXValue) {
+                errors.push({
+                    property: "dynamicXValue",
+                    severity: "error",
+                    message: `No X attribute configured for dynamic series located at position ${index + 1}.`
+                });
+            }
+
+            if (!series.dynamicYValue) {
+                errors.push({
+                    property: "dynamicYValue",
+                    severity: "error",
+                    message: `No Y attribute configured for dynamic series located at position ${index + 1}.`
+                });
+            }
+
+            if (!series.groupByAttribute) {
+                errors.push({
+                    property: "groupByAttribute",
+                    severity: "error",
+                    message: `No group by attribute configured for dynamic series located at position ${index + 1}.`
+                });
+            }
+        }
+    });
+
+    // check inter series datatypes
+
+    return errors;
 }
 
 // TODO: Add consistency checks
