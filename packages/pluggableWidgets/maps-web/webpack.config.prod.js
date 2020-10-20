f883c349e4d2adc9b4a0db364a423517f71f1b64
Mitya Kononchuk <mitya.kononchuk@mendix.com>
2020-10-20 23:51:41 +0200

    chore(maps-web): do not try to (incorrectly) rename resulting css

--- packages/pluggableWidgets/maps-web/webpack.config.prod.js
+++ packages/pluggableWidgets/maps-web/webpack.config.prod.js
@@ -1,17 +1,10 @@
 const { merge } = require("webpack-merge");
 const baseConfig = require("@mendix/pluggable-widgets-tools/configs/webpack.config.prod");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const customConfig = {
     output: {
         filename: "widgets/com/mendix/widget/custom/Maps/Maps.js"
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "./widgets/com/mendix/widget/custom/Maps/ui/Maps.css",
-            ignoreOrder: false
-        })
-    ]
+    }
 };
 
 module.exports = [merge(baseConfig[0], customConfig), ...baseConfig.slice(1)];
