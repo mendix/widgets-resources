e9891764f1918518920ad9811963218e3e65175a
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-10-21 12:01:17 +0200

    refactor(line-chart-native): legend rendering
    - Make use of filter instead of for/of loop

--- packages/pluggableWidgets/line-chart-native/src/components/Legend.tsx
+++ packages/pluggableWidgets/line-chart-native/src/components/Legend.tsx
@@ -16,33 +16,31 @@ export interface LegendSeries {
 export function Legend(props: LegendProps): ReactElement | null {
     const { series, style } = props;
 
-    const legendItems = useMemo(() => {
-        const legendItems = [];
+    const legendItems = useMemo(
+        () =>
+            series
+                .map((series, index) => {
+                    if (!series.name || !series.stylePropertyName || !style.series) {
+                        return undefined;
+                    }
 
-        for (let i = 0; i < series.length; i++) {
-            const currentSeries = series[i];
+                    const seriesStyle = style.series[series.stylePropertyName];
+                    const backgroundColor = seriesStyle?.line?.data?.stroke;
 
-            if (!currentSeries.name || !currentSeries.stylePropertyName || !style.series) {
-                continue;
-            }
+                    if (!(typeof backgroundColor === "string")) {
+                        return undefined;
+                    }
 
-            const seriesStyle = style.series[currentSeries.stylePropertyName];
-            const backgroundColor = seriesStyle?.line?.data?.stroke;
-
-            if (!(typeof backgroundColor === "string")) {
-                continue;
-            }
-
-            legendItems.push(
-                <View key={i} style={style.legend?.item}>
-                    <View style={[{ backgroundColor }, style.legend?.indicator]} />
-                    <Text style={style.legend?.label}>{currentSeries.name}</Text>
-                </View>
-            );
-        }
-
-        return legendItems;
-    }, [series]);
+                    return [
+                        <View key={index} style={style.legend?.item}>
+                            <View style={[{ backgroundColor }, style.legend?.indicator]} />
+                            <Text style={style.legend?.label}>{series.name}</Text>
+                        </View>
+                    ];
+                })
+                .filter(item => item),
+        [series]
+    );
 
     return legendItems.length > 0 ? <View style={style.legend?.container}>{legendItems}</View> : null;
 }
