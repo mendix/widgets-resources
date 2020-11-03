e29b7ac2a70755f161627f2d91b2ba8c0400e160
mehmet <mehmet.izci@mendix.com>
2020-11-03 17:32:10 +0100

    fix(pluggable-widgets-tools): check if emu started correctly

--- packages/tools/pluggable-widgets-tools/scripts/android-wait-for-emulator.sh
+++ packages/tools/pluggable-widgets-tools/scripts/android-wait-for-emulator.sh
@@ -5,24 +5,20 @@
 set +e
 
 bootanim=""
-keyevent="INIT"
 failcounter=0
 timeout_in_sec=300 # 5 minutes
 
-until [[ "$keyevent" =~ "done" ]]; do
-  keyevent=`adb shell input keyevent 82 &`
-  if [[ "$keyevent" =~ "no devices/emulators found" || "$keyevent" =~ "no emulators found" || "$keyevent" =~ "running" || "$keyevent" =~  "error: no emulators found" ]]; then
+until [[ "$bootanim" =~ "stopped" ]]; do
+  bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
+  if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline" || "$bootanim" =~ "running" || "$bootanim" =~  "error: no emulators found" ]]; then
     let "failcounter += 1"
-    bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
     echo "Waiting for emulator to start, device is currently '($bootanim)'"
     if [[ $failcounter -gt timeout_in_sec ]]; then
       echo "Timeout ($timeout_in_sec seconds) reached; failed to start emulator"
       exit 1
     fi
   fi
-  echo " "
   sleep 1
 done
 
 echo "Emulator is ready '($bootanim)'"
-echo "Key event result '(keyevent)'"
