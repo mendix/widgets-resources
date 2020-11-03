17c8619d5d647566e0507f65c642c19d7fa1505d
mehmet <mehmet.izci@mendix.com>
2020-11-03 17:11:19 +0100

    fix(pluggable-widgets-tools): correctly check keypress

--- packages/tools/pluggable-widgets-tools/scripts/android-wait-for-emulator.sh
+++ packages/tools/pluggable-widgets-tools/scripts/android-wait-for-emulator.sh
@@ -9,9 +9,9 @@ keyevent="INIT"
 failcounter=0
 timeout_in_sec=300 # 5 minutes
 
-until [[ "keyevent" = "" ]]; do
+until [[ "$keyevent" =~ "done" ]]; do
   keyevent=`adb shell input keyevent 82 &`
-  if [[ "keyevent" =~ "no devices/emulators found" || "keyevent" =~ "no emulators found" || "keyevent" =~ "running" || "keyevent" =~  "error: no emulators found" ]]; then
+  if [[ "$keyevent" =~ "no devices/emulators found" || "$keyevent" =~ "no emulators found" || "$keyevent" =~ "running" || "$keyevent" =~  "error: no emulators found" ]]; then
     let "failcounter += 1"
     bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
     echo "Waiting for emulator to start, device is currently '($bootanim)'"
@@ -20,6 +20,7 @@ until [[ "keyevent" = "" ]]; do
       exit 1
     fi
   fi
+  echo " "
   sleep 1
 done
 
