fd14236e550078920d81a1c9334e9aadff4d7554
mehmet <mehmet.izci@mendix.com>
2020-11-03 17:38:39 +0100

    fix(pluggable-widgets-tools): remove & unlock emulator

--- packages/tools/pluggable-widgets-tools/scripts/android-wait-for-emulator.sh
+++ packages/tools/pluggable-widgets-tools/scripts/android-wait-for-emulator.sh
@@ -9,7 +9,7 @@ failcounter=0
 timeout_in_sec=300 # 5 minutes
 
 until [[ "$bootanim" =~ "stopped" ]]; do
-  bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
+  bootanim=`adb -e shell getprop init.svc.bootanim 2>&1`
   if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline" || "$bootanim" =~ "running" || "$bootanim" =~  "error: no emulators found" ]]; then
     let "failcounter += 1"
     echo "Waiting for emulator to start, device is currently '($bootanim)'"
