bfba7651ee60c5f790a0e286a694d484565db6d7
Mitya Kononchuk <mitya.kononchuk@mendix.com>
2020-11-13 00:48:16 +0100

    feat(pluggable-widgets-tools): compile editor preview and config

--- packages/tools/pluggable-widgets-tools/configs/rollup.config.js
+++ packages/tools/pluggable-widgets-tools/configs/rollup.config.js
@@ -9,10 +9,9 @@ import scss from "rollup-plugin-scss";
 import { terser } from "rollup-plugin-terser";
 
 const variables = require("./variables");
-const MODULES_SHIPPED_WITH_CLIENT = ["react", "big.js"];
 
 const outDir = join(variables.sourcePath, "/dist/tmp/widgets/");
-const outFile = join(
+const outWidgetFile = join(
     variables.package.packagePath.replace(/\./g, "/"),
     variables.package.widgetName.toLowerCase(),
     `${variables.package.widgetName}.js`
@@ -25,17 +24,25 @@ export default args => {
         throw new Error("Must pass --configPlatform=web|native parameter");
     }
 
-    return {
+    const extensions = [".tsx", ".ts", ".jsx", ".js", ".scss", ".sass", ".css"];
+    const widgetConfig = {
         input: variables.widgetEntry,
         output: {
             format: "amd",
-            file: join(outDir, outFile),
+            file: join(outDir, outWidgetFile),
             sourcemap: !production
         },
-        external: [...MODULES_SHIPPED_WITH_CLIENT, /^mendix($|\/)/],
+        treeshake: { moduleSideEffects: false },
+        external: [/^mendix($|\/)/, "react", "react-dom", "big.js"],
         plugins: [
-            nodeResolve({ browser: true, mainFields: ["browser", "module", "main"], preferBuiltins: false }),
-            commonjs(),
+            scss({ failOnError: true, sass: require("sass") }),
+            nodeResolve({
+                browser: true,
+                extensions,
+                mainFields: ["browser", "module", "main"],
+                preferBuiltins: false
+            }),
+            commonjs({ extensions, transformMixedEsModules: true }),
             babel({
                 presets: [["@babel/preset-env", { targets: { safari: "12" } }]],
                 babelHelpers: "bundled",
@@ -48,19 +55,57 @@ export default args => {
             replace({
                 "process.env.NODE_ENV": production ? "'production'" : "'development'"
             }),
-            scss({ failOnError: true, sass: require("sass") }),
+           ...(production ? [terser()] : []),
             copy({
                 targets: [{ src: join(variables.sourcePath, "src/**/*.xml").replace("\\", "/"), dest: outDir }]
-            }),
-            ...(production ? [terser()] : [])
+            })
         ],
-        onwarn: function (warning, warn) {
-            if (["CIRCULAR_DEPENDENCY", "THIS_IS_UNDEFINED", "UNUSED_EXTERNAL_IMPORT"].includes(warning.code)) {
-                warn(warning);
-            } else {
-                console.error(warning);
-                process.exit(1);
-            }
-        }
+        onwarn
     };
+
+    const previewConfig = {
+        ...widgetConfig,
+        input: variables.previewEntry,
+        output: {
+            format: "commonjs",
+            file: join(outDir, `${variables.package.widgetName}.editorPreview.js`),
+            sourcemap: !production
+        },
+        external: widgetConfig.external.slice(0, 3),
+        plugins: [
+            scss({ output: false, failOnError: true, sass: require("sass") }),
+            ...widgetConfig.plugins.slice(1, -1)
+        ]
+    };
+
+    const editorConfig = {
+        input: variables.editorConfigEntry,
+        output: {
+            format: "commonjs",
+            file: join(outDir, `${variables.package.widgetName}.editorConfig.js`)
+        },
+        treeshake: { moduleSideEffects: false },
+        plugins: [
+            nodeResolve({ preferBuiltins: false }),
+            commonjs({ transformMixedEsModules: true }),
+            babel({
+                presets: [["@babel/preset-env", { targets: { ie: "11" } }]],
+                babelHelpers: "bundled",
+                plugins: ["@babel/plugin-proposal-class-properties"]
+            }),
+            typescript({ noEmitOnError: true, sourceMap: false })
+        ],
+        onwarn
+    };
+
+    return [widgetConfig, previewConfig, editorConfig].filter(c => c.input);
 };
+
+function onwarn(warning, warn) {
+    if (["CIRCULAR_DEPENDENCY", "THIS_IS_UNDEFINED", "UNUSED_EXTERNAL_IMPORT"].includes(warning.code)) {
+        warn(warning);
+    } else {
+        console.error(warning);
+        process.exit(1);
+    }
+}
