da1c8478fedfc82eb64c725b67d7bf1d09f52fa3
Diego Antonelli <diego.antonelli@mendix.com>
2020-11-16 18:51:50 +0100

    feat(pluggable-widgets-tools): adding support for hooks in jest

--- packages/tools/pluggable-widgets-tools/test-config/test-index.js
+++ packages/tools/pluggable-widgets-tools/test-config/test-index.js
@@ -1,4 +1,6 @@
-const Enzyme = require("enzyme");
-const Adapter = require("@wojtekmaj/enzyme-adapter-react-17");
+import Enzyme from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import enableHooks from "jest-react-hooks-shallow";
 
 Enzyme.configure({ adapter: new Adapter() });
+enableHooks(jest);
