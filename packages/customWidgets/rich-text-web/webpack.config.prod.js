1c555bdb36d0e8bbdfef8e32432bbaf7a6c484bd
mehmet <mehmet.izci@mendix.com>
2020-12-16 14:39:18 +0100

    fix(rich-text-web): resolve path in alias

---
+++ packages/customWidgets/rich-text-web/webpack.config.prod.js
@@ -0,0 +1,12 @@
+const { merge } = require("webpack-merge");
+const baseConfig = require("@widgets-resources/utils-react-widgets/configs/webpack.config.prod");
+
+const overridingConfig = {
+    resolve: {
+        alias: {
+            path: "path-browserify"
+        }
+    }
+};
+
+module.exports = [merge(baseConfig[0], overridingConfig), merge(baseConfig[1], overridingConfig)];
