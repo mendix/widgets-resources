86fb4a64c3e736e4e122698aeb466b3719fa6e27
Leonardo de Souza <leonardo.de.souza@mendix.com>
2020-12-21 14:02:05 +0100

    test(datagrid-web): fix getAllRows()

--- packages/pluggableWidgets/datagrid-web/tests/e2e/objects/datagrid.widget.ts
+++ packages/pluggableWidgets/datagrid-web/tests/e2e/objects/datagrid.widget.ts
@@ -1,10 +1,9 @@
 class Datagrid {
     private rows: string[];
-    getAllRows(item: WebdriverIO.ElementArray): string[] {
+    getAllRows(items: WebdriverIO.ElementArray): string[] {
         this.rows = [];
-        item.forEach(item => {
-            this.rows.push(item.getText());
-        });
+        /* TODO: When I tried to use getText() in a loop to get the text it's taking almost 1min to process. So instead of using getText I am using getHTML with replace for this case. Maybe in the future will be possible to back to getText() */
+        items.map(item => this.rows.push(item.getHTML().replace(/(<([^>]+)>)/gi, "")));
         return this.rows;
     }
 }
