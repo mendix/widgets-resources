2860ed8994664bf213a89c02abf07910bd219611
Jordan Hunt <jordan.hunt@mendix.com>
2020-12-23 16:26:33 +0200

    feat(bar-chart-native): revert bar order

--- packages/pluggableWidgets/bar-chart-native/src/components/BarChart.tsx
+++ packages/pluggableWidgets/bar-chart-native/src/components/BarChart.tsx
@@ -228,7 +228,6 @@ export function BarChart({
                                         }
                                         style={mapToGridStyle(style.grid)}
                                     >
-                                        {groupedOrStacked}
                                         <VictoryAxis
                                             orientation={"bottom"}
                                             dependentAxis
@@ -244,6 +243,7 @@ export function BarChart({
                                                 ? { tickFormat: firstSeries.yFormatter }
                                                 : undefined)}
                                         />
+                                        {groupedOrStacked}
                                     </VictoryChart>
                                 ) : null}
                             </View>
