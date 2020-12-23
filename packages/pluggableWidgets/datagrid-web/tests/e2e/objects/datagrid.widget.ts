187f98d42b0a3cad6b0e7c3fe84ae8c4ce944cd8
Leonardo de Souza <leonardo.de.souza@mendix.com>
2020-12-23 09:36:00 +0100

    test(datagrid-web): update getAllRows()

--- packages/pluggableWidgets/datagrid-web/tests/e2e/objects/datagrid.widget.ts
+++ packages/pluggableWidgets/datagrid-web/tests/e2e/objects/datagrid.widget.ts
@@ -1,9 +1,7 @@
 class Datagrid {
     getAllRows(items: WebdriverIO.ElementArray): string[] {
-        const rows = [];
         /* TODO: When I tried to use getText() in a loop to get the text it's taking almost 1min to process. So instead of using getText I am using getHTML with replace for this case. Maybe in the future will be possible to back to getText() */
-        items.map(item => rows.push(item.getHTML().replace(/(<([^>]+)>)/gi, "")));
-        return rows;
+        return items.map(item => item.getHTML().replace(/(<([^>]+)>)/gi, ""));
     }
 }
 export default new Datagrid();
