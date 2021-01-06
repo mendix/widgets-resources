fc018ad47c73d607749a2b4a64a2b8460d1f2718
Wesley <wesley.sieses@mendix.com>
2021-01-06 16:51:42 +0100

    feat(floating-action-button-native): add preview

---
+++ packages/pluggableWidgets/floating-action-button-native/src/FloatingActionButton.editorConfig.ts
@@ -0,0 +1,29 @@
+import { ImageProps, ContainerProps } from "@widgets-resources/piw-utils";
+import { FloatingActionButtonPreviewProps } from "../typings/FloatingActionButtonProps";
+import { FABPrimarySVG } from "./assets/FloatingActionButton_Primary";
+
+const rowLayoutContainer: ContainerProps = { type: "Container", children: [] };
+let rowLayoutImage: ImageProps = {
+    type: "image",
+    document: FABPrimarySVG(),
+    data: "",
+    width: 56,
+    height: 56
+};
+
+export const getPreview = (values: FloatingActionButtonPreviewProps) => ({
+    type: "RowLayout",
+    borders: false,
+    padding: 8,
+    columnSize: "grow",
+    children: (() => {
+        switch (values.horizontalPosition) {
+            case "left":
+                return [rowLayoutImage];
+            case "center":
+                return [rowLayoutContainer, rowLayoutImage, rowLayoutContainer];
+            case "right":
+                return [rowLayoutContainer, rowLayoutImage];
+        }
+    })()
+});
