701f5be3605a20569af248b837f0d1d2e2a390a3
mehmet <mehmet.izci@mendix.com>
2021-01-12 09:59:56 +0100

    fix(): rename development to start, production to release

--- packages/theming/atlas/scripts/build.js
+++ packages/theming/atlas/scripts/build.js
@@ -9,21 +9,21 @@ let projectDeployDir;
 
 switch (mode) {
     case "build":
-    case "development":
+    case "start":
         // Build will work with two different folders: project's theme, project's deployment
         outputDir = MX_PROJECT_PATH ? join(MX_PROJECT_PATH, "theme") : join(__dirname, "../tests/testProject/theme");
         projectDeployDir = MX_PROJECT_PATH
             ? join(MX_PROJECT_PATH, "deployment/web")
             : join(__dirname, "../tests/testProject/deployment/web");
         break;
-    case "production":
+    case "release":
         outputDir = join(__dirname, "../dist/theme");
         break;
 }
 
 console.info(`Building for ${mode}...`);
-const watchArg = mode === "development" ? "--watch" : "";
-const compressArg = mode === "production" ? "--style compressed" : "";
+const watchArg = mode === "start" ? "--watch" : "";
+const compressArg = mode === "release" ? "--style compressed" : "";
 const copyAndWatchFonts = command(`copy-and-watch ${watchArg} 'src/web/sass/core/_legacy/bootstrap/fonts/*'`);
 const compileSass = command(`sass ${watchArg} --embed-sources ${compressArg} --no-charset src/web/sass/main.scss`);
 cd(join(__dirname, ".."));
@@ -55,7 +55,7 @@ concurrently(
             command: `tsc ${watchArg} --project tsconfig.json --outDir '${outputDir}/styles/native'`
         }
     ].concat(
-        mode === "development" || mode === "build"
+        mode === "start" || mode === "build"
             ? [
                   {
                       name: "fonts-development",
