4adde03afb682510425125afce5e27d8afa90ad3
Wesley <wesley.sieses@mendix.com>
2021-01-14 17:27:29 +0100

    feat(animation-native): add preview

---
+++ packages/pluggableWidgets/animation-native/src/Animation.editorConfig.ts
@@ -0,0 +1,40 @@
+import { StructurePreviewProps } from "@widgets-resources/piw-utils";
+import { AnimationProps } from "../typings/AnimationProps";
+
+export function getPreview(values: AnimationProps<any>): StructurePreviewProps {
+    const prettifyText = (text: string) => text.replace(/([A-Z])/g, " $1").toLowerCase();
+    const animationType =
+        values.animationType === "in" ? "Entrance" : values.animationType === "attention" ? "Attention" : "Exit";
+    const animation =
+        values.animationType === "in"
+            ? `${prettifyText(values.animationIn)}`
+            : values.animationType === "attention"
+            ? `${prettifyText(values.animationAttention)}`
+            : `${prettifyText(values.animationOut)}`;
+
+    const content =
+        `${animationType} ${animation} ${values.duration}ms` + (values.delay ? `, delay ${values.delay}ms` : "");
+
+    return {
+        type: "Container",
+        borders: false,
+        children: [
+            {
+                type: "Container",
+                padding: 4,
+                borders: false,
+                children: [
+                    {
+                        type: "Text",
+                        bold: true,
+                        content
+                    }
+                ]
+            },
+            {
+                type: "DropZone",
+                property: values.content as object
+            }
+        ]
+    };
+}
