498297c150552935837ecc29d508549b3b871624
Diego Antonelli <diego.antonelli@mendix.com>
2021-01-27 15:17:22 +0100

    feat: bumping widgets and module versions

--- packages/pluggableWidgets/rating-native/typings/RatingProps.d.ts
+++ packages/pluggableWidgets/rating-native/typings/RatingProps.d.ts
@@ -5,18 +5,7 @@
  */
 import { ActionValue, DynamicValue, EditableValue, NativeImage } from "mendix";
 
-export type AnimationEnum =
-    | "pulse"
-    | "bounce"
-    | "flash"
-    | "jello"
-    | "rotate"
-    | "rubberBand"
-    | "shake"
-    | "swing"
-    | "tada"
-    | "wobble"
-    | "none";
+export type AnimationEnum = "pulse" | "bounce" | "flash" | "jello" | "rotate" | "rubberBand" | "shake" | "swing" | "tada" | "wobble" | "none";
 
 export type EditableEnum = "default" | "never";
 
