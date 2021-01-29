8945d885711e9d13f3d6d6b035a33d688e8ec7b5
mehmet <mehmet.izci@mendix.com>
2021-01-29 11:29:29 +0100

    feat(notifications-native): remove redundant import

--- packages/jsActions/native-mobile-resources/src/notifications/ClearAllDeliveredNotifications.ts
+++ packages/jsActions/native-mobile-resources/src/notifications/ClearAllDeliveredNotifications.ts
@@ -5,7 +5,6 @@
 // - the code between BEGIN USER CODE and END USER CODE
 // - the code between BEGIN EXTRA CODE and END EXTRA CODE
 // Other code you write will be lost the next time you deploy the project.
-import { Big } from "big.js";
 import { NativeModules } from "react-native";
 
 // BEGIN EXTRA CODE
