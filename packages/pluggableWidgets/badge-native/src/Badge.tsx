72f9f735986562772833018ecfa9924607e3bef5
mehmet <mehmet.izci@mendix.com>
2021-02-05 16:00:39 +0100

    chore(badge-native): convert from class to func component

--- packages/pluggableWidgets/badge-native/src/Badge.tsx
+++ packages/pluggableWidgets/badge-native/src/Badge.tsx
@@ -1,51 +1,40 @@
 import { flattenStyles } from "@native-mobile-resources/util-widgets";
-import { Component, createElement } from "react";
+import { createElement, ReactElement, useCallback } from "react";
 import { Platform, Text, TouchableNativeFeedback, TouchableOpacity, View } from "react-native";
 
 import { BadgeProps } from "../typings/BadgeProps";
 import { BadgeStyle, defaultBadgeStyle } from "./ui/Styles";
-import { executeAction } from "@widgets-resources/piw-utils";
+import { executeAction, isAvailable } from "@widgets-resources/piw-utils";
 
 export type Props = BadgeProps<BadgeStyle>;
 
-export class Badge extends Component<Props> {
-    private readonly onClickHandler = this.onClick.bind(this);
-    private readonly styles = flattenStyles(defaultBadgeStyle, this.props.style);
+export function Badge(props: BadgeProps<BadgeStyle>): ReactElement {
+    const styles = flattenStyles(defaultBadgeStyle, props.style);
 
-    render(): JSX.Element {
-        const isAndroid = Platform.OS === "android";
-
+    const renderText = useCallback(() => {
         return (
-            <View style={this.styles.container} testID={this.props.name}>
-                {this.props.onClick ? (
-                    isAndroid ? (
-                        <TouchableNativeFeedback
-                            background={TouchableNativeFeedback.SelectableBackgroundBorderless()}
-                            onPress={this.onClickHandler}
-                        >
-                            {this.renderText()}
-                        </TouchableNativeFeedback>
-                    ) : (
-                        <TouchableOpacity onPress={this.onClickHandler}>{this.renderText()}</TouchableOpacity>
-                    )
-                ) : (
-                    this.renderText()
-                )}
-            </View>
-        );
-    }
-
-    private renderText(): JSX.Element {
-        const value = this.props.caption.value || "";
-
-        return (
-            <Text testID={`${this.props.name}$caption`} style={this.styles.caption}>
-                {value}
+            <Text testID={`${props.name}$caption`} style={styles.caption}>
+                {isAvailable(props.caption) ? props.caption.value : ""}
             </Text>
         );
-    }
+    }, [props.caption.value, props.caption.status, styles]);
 
-    private onClick(): void {
-        executeAction(this.props.onClick);
-    }
+    return (
+        <View style={styles.container} testID={props.name}>
+            {props.onClick ? (
+                Platform.OS === "android" ? (
+                    <TouchableNativeFeedback
+                        background={TouchableNativeFeedback.SelectableBackgroundBorderless()}
+                        onPress={() => executeAction(props.onClick)}
+                    >
+                        {renderText()}
+                    </TouchableNativeFeedback>
+                ) : (
+                    <TouchableOpacity onPress={() => executeAction(props.onClick)}>{renderText()}</TouchableOpacity>
+                )
+            ) : (
+                renderText()
+            )}
+        </View>
+    );
 }
