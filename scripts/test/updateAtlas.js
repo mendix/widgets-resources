f6cc2a1e631b2dd89b9bddc348395cc5896ab03e
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2021-02-08 14:37:27 +0100

    feat: make web e2e test execution configurable
    - E2e test on specified mx major version
    - Allow usage of Atlas from mono repo or latest released atlas on github atlas-ui-framework
    - Update all test:e2e scripts for widgets to use mx major version 8

--- scripts/test/updateAtlas.js
+++ scripts/test/updateAtlas.js
@@ -1,11 +1,16 @@
 const { exec } = require("child_process");
 const {
+    createWriteStream,
     promises: { access }
 } = require("fs");
+const fetch = require("node-fetch");
+const { tmpdir } = require("os");
 const { join } = require("path");
-const { promisify } = require("util");
-const { rm } = require("shelljs");
 const rCopy = require("recursive-copy");
+const semverCompare = require("semver/functions/rcompare");
+const { rm } = require("shelljs");
+const { pipeline } = require("stream");
+const { promisify } = require("util");
 
 main().catch(e => {
     console.error(e);
@@ -16,15 +21,14 @@ async function main() {
     if (!(await exists("tests/testProject"))) {
         throw new Error("Cannot find a tests/testProject. Did you run the script in the widget folder?");
     }
-    try {
-        await promisify(exec)("unzip --help", { stdio: "ignore" });
-    } catch (e) {
-        throw new Error("This script requires unzip command to be available on the PATH!");
-    }
 
     rm("-rf", "tests/testProject/theme", "tests/testProject/themesource");
 
-    await copyLatestAtlas();
+    if (process.argv.includes("--latest-atlas")) {
+        await copyLatestAtlas();
+    } else {
+        await copyLatestReleasedAtlas();
+    }
 }
 
 async function exists(filePath) {
@@ -52,35 +56,49 @@ async function copyLatestAtlas() {
     }
 }
 
-// === Preserved for future use ===
+async function copyLatestReleasedAtlas() {
+    try {
+        await promisify(exec)("unzip --help", { stdio: "ignore" });
+    } catch (e) {
+        throw new Error("This script requires unzip command to be available on the PATH!");
+    }
 
-// async function getLatestAtlasArchive() {
-//     let latestAtlasVersions;
-//     try {
-//         const releasesResponse = await fetch("https://api.github.com/repos/mendix/Atlas-UI-Framework/releases");
-//         const suitableReleases = (await releasesResponse.json()).map(r => r.tag_name).filter(t => t.startsWith("2."));
-//         suitableReleases.sort(semverCompare);
-//         latestAtlasVersions = suitableReleases.slice(0, 2);
-//     } catch (e) {
-//         throw new Error("Couldn't reach api.github.com. Make sure you are connected to internet.");
-//     }
-//     if (!latestAtlasVersions.length) {
-//         throw new Error("Couldn't retrieve latest Atlas package from api.github.com. Try again later.");
-//     }
-//
-//     for (const latestAtlasVersion of latestAtlasVersions) {
-//         const downloadedArchivePath = join(tmpdir(), `${latestAtlasVersion}.mpk`);
-//
-//         const appstoreUrl = `https://files.appstore.mendix.com/5/104730/${latestAtlasVersion}/Atlas_UI_Resources_${latestAtlasVersion}.mpk`;
-//         console.log(`Trying to download Atlas from ${appstoreUrl}`);
-//         try {
-//             await promisify(pipeline)((await fetch(appstoreUrl)).body, createWriteStream(downloadedArchivePath));
-//             return downloadedArchivePath;
-//         } catch (e) {
-//             console.log(`Url is not available :(`);
-//             rm("-f", downloadedArchivePath);
-//         }
-//     }
-//
-//     throw new Error("Cannot find suitable Atlas in appstore.mendix.com. Try again later.");
-// }
+    const atlasArchivePath = await getLatestAtlasArchive();
+
+    try {
+        await promisify(exec)(`unzip -o ${atlasArchivePath} -x '*.mpr' '*.xml' -d tests/testProject`);
+    } catch (e) {
+        throw new Error("Failed to unzip the Atlas mpk into tests/testProject");
+    }
+}
+
+async function getLatestAtlasArchive() {
+    let latestAtlasVersions;
+    try {
+        const releasesResponse = await fetch("https://api.github.com/repos/mendix/Atlas-UI-Framework/releases");
+        const suitableReleases = (await releasesResponse.json()).map(r => r.tag_name).filter(t => t.startsWith("2."));
+        suitableReleases.sort(semverCompare);
+        latestAtlasVersions = suitableReleases.slice(0, 2);
+    } catch (e) {
+        throw new Error("Couldn't reach api.github.com. Make sure you are connected to internet.");
+    }
+    if (!latestAtlasVersions.length) {
+        throw new Error("Couldn't retrieve latest Atlas package from api.github.com. Try again later.");
+    }
+
+    for (const latestAtlasVersion of latestAtlasVersions) {
+        const downloadedArchivePath = join(tmpdir(), `${latestAtlasVersion}.mpk`);
+
+        const appstoreUrl = `https://files.appstore.mendix.com/5/104730/${latestAtlasVersion}/Atlas_UI_Resources_${latestAtlasVersion}.mpk`;
+        console.log(`Trying to download Atlas from ${appstoreUrl}`);
+        try {
+            await promisify(pipeline)((await fetch(appstoreUrl)).body, createWriteStream(downloadedArchivePath));
+            return downloadedArchivePath;
+        } catch (e) {
+            console.log(`Url is not available :(`);
+            rm("-f", downloadedArchivePath);
+        }
+    }
+
+    throw new Error("Cannot find suitable Atlas in appstore.mendix.com. Try again later.");
+}
