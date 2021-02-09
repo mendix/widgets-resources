59c474abc7ad931ae3c043f32880ff3e83d5ecb0
Jordan Hunt <jordan.hunt@mendix.com>
2021-02-09 16:12:51 +0200

    fix(timeline-web): flesh out structure preview

--- packages/pluggableWidgets/timeline-web/src/Timeline.editorConfig.ts
+++ packages/pluggableWidgets/timeline-web/src/Timeline.editorConfig.ts
@@ -1,5 +1,12 @@
-import { hidePropertiesIn, hidePropertyIn, Problem, Properties } from "@widgets-resources/piw-utils";
+import {
+    hidePropertiesIn,
+    hidePropertyIn,
+    Problem,
+    Properties,
+    StructurePreviewProps
+} from "@widgets-resources/piw-utils";
 import { TimelinePreviewProps } from "../typings/TimelineProps";
+import { lineAndC } from "./preview/preview";
 
 export function getProperties(values: TimelinePreviewProps, defaultProperties: Properties): Properties {
     if (values.customVisualization) {
@@ -66,3 +73,75 @@ export function check(values: TimelinePreviewProps): Problem[] {
     }
     return errors;
 }
+
+export function getPreview(values: TimelinePreviewProps): StructurePreviewProps {
+    return {
+        type: "Container",
+        children: [
+            {
+                type: "RowLayout",
+                children: [
+                    {
+                        type: "Container",
+                        children: [{ content: "Today", type: "Text", fontSize: 12, bold: true, grow: 0 }],
+                        borderRadius: 15,
+                        borderWidth: 1,
+                        borders: true,
+                        grow: 0,
+                        padding: 100
+                    },
+                    { content: "", type: "Text", grow: 1 }
+                ],
+                columnSize: "grow"
+            },
+            buildRow(values),
+            buildRow(values),
+            buildRow(values),
+            buildRow(values)
+        ]
+    };
+}
+
+function buildRow(values: TimelinePreviewProps): StructurePreviewProps {
+    return {
+        type: "RowLayout",
+        children: [
+            {
+                type: "Image",
+                document: lineAndC,
+                width: 65,
+                grow: 0
+            },
+            {
+                type: "Container",
+                children: [
+                    { type: "Text", content: "-", fontColor: "white" },
+                    { type: "Text", content: "-", fontColor: "white" },
+                    {
+                        type: "RowLayout",
+                        children: [
+                            { type: "Text", content: values.title },
+                            { type: "Text", content: "", grow: 3 },
+                            { type: "Text", content: values.timeIndication }
+                        ]
+                    },
+                    { type: "Text", content: values.description }
+                ]
+            }
+        ],
+        columnSize: "grow"
+    };
+}
+/*
+
+ TODO(jordan):
+. padding to text inside bubble isn't taking effect. Isa says newer modeler will work. 9.0.4
+
+. image width and height are fixed, is this okay for all situations?
+
+. the two texts "-" are used for layout. coloured white. is this okay? what about dark mode?
+
+. figure out where the svg code should live
+
+
+ */
