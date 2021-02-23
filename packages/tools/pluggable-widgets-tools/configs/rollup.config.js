02ccae72c13c0128791e0a1f43e17813c439ede8
Diego Antonelli <diego.antonelli@mendix.com>
2021-02-23 14:15:06 +0100

    fix(pluggable-widgets-tools): fix replace and post test

--- packages/tools/pluggable-widgets-tools/configs/rollup.config.js
+++ packages/tools/pluggable-widgets-tools/configs/rollup.config.js
@@ -90,7 +90,7 @@ export default async args => {
                 external: nativeExternal,
                 plugins: [
                     replace({
-                        "Platform.OS": `"${os}"`,
+                        values: { "Platform.OS": `"${os}"` },
                         preventAssignment: true
                     }),
                     ...(i === 0 ? getClientComponentPlugins() : []),
@@ -199,7 +199,8 @@ export default async args => {
             }),
             commonjs({ extensions: config.extensions, transformMixedEsModules: true, requireReturnsDefault: true }),
             replace({
-                "process.env.NODE_ENV": production ? "'production'" : "'development'"
+                values: { "process.env.NODE_ENV": production ? "'production'" : "'development'" },
+                preventAssignment: true
             }),
             config.transpile
                 ? getBabelOutputPlugin({
