d5c11d0af9fddac6cbff5214512429b720bf2816
Keraito <chakshunyu@gmail.com>
2021-03-15 17:18:26 +0100

    feat(atlas-ui): create .bat file to archive & init theme folder

---
+++ packages/theming/atlas/src/themesource/atlas_core/migration/migration.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+IF EXIST ..\..\..\theme_atlas2\ (
+    ECHO The folder 'theme_atlas2' already exists in the project folder, please rename and rerun this tool.
+) ELSE IF EXIST theme.zip (
+    MOVE ..\..\..\theme ..\..\..\theme_atlas2
+    tar -xf theme.zip -C ..\..\..\
+    CLS
+    ECHO Migration tool ran successfully, please continue with the other steps of the migration documentation.
+) ELSE (
+    ECHO No 'theme.zip' file could be found inside the migration folder. If the file is present with a different name, change it to 'theme.zip'. Otherwise, please download Atlas again from Marketplace.
+)
+
+pause
