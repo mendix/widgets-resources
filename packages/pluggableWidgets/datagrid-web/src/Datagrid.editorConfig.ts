bc4c9e98ffcd034254042c894355137924c5a197
Diego Antonelli <diego.antonelli@mendix.com>
2021-03-28 09:43:49 +0200

    fix(datagrid-web): fixing consistency check

--- packages/pluggableWidgets/datagrid-web/src/Datagrid.editorConfig.ts
+++ packages/pluggableWidgets/datagrid-web/src/Datagrid.editorConfig.ts
@@ -270,7 +270,7 @@ export function check(values: DatagridPreviewProps): Problem[] {
                 property: `columns/${index + 1}/attribute`,
                 message: `An attribute is required when 'Show' is set to 'Attribute'. Select the 'Attribute' property for column ${column.header}`
             });
-        } else if (!column.attribute && (column.sortable || values.columnsFilterable)) {
+        } else if (!column.attribute && ((values.columnsSortable && column.sortable) || values.columnsFilterable)) {
             errors.push({
                 property: `columns/${index + 1}/attribute`,
                 message: `An attribute is required when filtering or sorting is enabled. Select the 'Attribute' property for column ${column.header}`
