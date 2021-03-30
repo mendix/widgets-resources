f1d82e90b9a8813fb0aef55d9167b1bfd9865408
Leonardo de Souza <leonardo.de.souza@mendix.com>
2021-03-30 17:00:36 +0200

    test(badge-button-web): update testProject and specs

--- packages/pluggableWidgets/badge-button-web/tests/e2e/specs/render.spec.ts
+++ packages/pluggableWidgets/badge-button-web/tests/e2e/specs/render.spec.ts
@@ -1,12 +1,12 @@
 import page from "../../../../../../configs/e2e/src/pages/page";
 import badgeButtonWidget from "../objects/badgeButton.widget";
 
-describe("BadgeButton rendering", () => {
+describe("BadgeButton", () => {
     beforeAll(() => {
         page.open();
     });
 
-    it("should display correctly dynamic data", () => {
+    it("displays correctly dynamic data", () => {
         const badgeButton = new badgeButtonWidget("badgeButtonDynamic");
 
         badgeButton.element.waitForDisplayed();
@@ -14,15 +14,7 @@ describe("BadgeButton rendering", () => {
         expect(badgeButton.getBadgeText()).toEqual("New");
     });
 
-    it("should display correctly fallback data", () => {
-        const badgeButton = new badgeButtonWidget("badgeButtonFallback");
-
-        badgeButton.element.waitForDisplayed();
-        expect(badgeButton.getText()).toEqual("Button");
-        expect(badgeButton.getBadgeText()).toEqual("");
-    });
-
-    it("should update text value", () => {
+    it("updates text value", () => {
         const textInput = page.getWidget("textBox1");
         textInput.$(".form-control").setValue("\uE003\uE003\uE003"); // Chrome is not clearing value before set new value (https://github.com/webdriverio/webdriverio/issues/3024)
         textInput.$(".form-control").setValue("Newer");
@@ -35,72 +27,11 @@ describe("BadgeButton rendering", () => {
         expect(badgeButton.getBadgeText()).toEqual("Newer");
     });
 
-    it("should update number value", () => {
-        const textInput = page.getWidget("textBox2");
-        textInput.$(".form-control").setValue("0987654");
-        textInput.$(".control-label").click();
-
-        const badgeButton = new badgeButtonWidget("badgeButtonFallback");
-
-        badgeButton.element.waitForDisplayed();
-        expect(badgeButton.getText()).toEqual("Button");
-        expect(badgeButton.getBadgeText()).toEqual("987654");
-    });
-
-    it("should display correctly static data", () => {
-        const badgeButton = new badgeButtonWidget("badgeButtonStatic");
-
-        badgeButton.element.waitForDisplayed();
-        expect(badgeButton.getText()).toEqual("Button");
-        expect(badgeButton.getBadgeText()).toEqual("Static");
-    });
-
-    it("should display correctly primary style", () => {
-        const badgeButton = new badgeButtonWidget("badgeButtonStatic");
-
-        badgeButton.element.waitForDisplayed();
-        expect(badgeButton.getColors()).toEqual(badgeButton.defaultStyles.PrimaryBackground);
-    });
-
-    it("should display correctly default style", () => {
-        const badgeButton = new badgeButtonWidget("badgeButtonDynamic");
-
-        badgeButton.element.waitForDisplayed();
-        expect(badgeButton.getColors()).toEqual(badgeButton.defaultStyles.DefaultBackground);
-    });
-
-    it("should display correctly success style", () => {
-        const badgeButton = new badgeButtonWidget("badgeButtonSuccess");
-
-        badgeButton.element.waitForDisplayed();
-        expect(badgeButton.getColors()).toEqual(badgeButton.defaultStyles.SuccessBackground);
-    });
-
-    it("should display correctly info style", () => {
-        const badgeButton = new badgeButtonWidget("badgeButtonInfo");
-
-        badgeButton.element.waitForDisplayed();
-        expect(badgeButton.getColors()).toEqual(badgeButton.defaultStyles.InfoBackground);
-    });
-
-    it("should display correctly warning style", () => {
-        const badgeButton = new badgeButtonWidget("badgeButtonWarning");
-
-        badgeButton.element.waitForDisplayed();
-        expect(badgeButton.getColors()).toEqual(badgeButton.defaultStyles.WarningBackground);
-    });
-
-    it("should display correctly danger style", () => {
-        const badgeButton = new badgeButtonWidget("badgeButtonDanger");
-
-        badgeButton.element.waitForDisplayed();
-        expect(badgeButton.getColors()).toEqual(badgeButton.defaultStyles.DangerBackground);
-    });
-
-    it("should display correctly inverse style", () => {
-        const badgeButton = new badgeButtonWidget("badgeButtonInverse");
-
-        badgeButton.element.waitForDisplayed();
-        expect(badgeButton.getColors()).toEqual(badgeButton.defaultStyles.InverseBackground);
+    it("compares with a screenshot baseline and checks if all badge buttons elements are rendered as expected", () => {
+        browser.setWindowRect(0, 0, 1200, 900);
+        const screenshotElem = $(".mx-name-table1");
+        screenshotElem.waitForDisplayed({ timeout: 5000 });
+        browser.saveElement(screenshotElem, "badgeButtonPageContent");
+        expect(browser.checkElement(screenshotElem, "badgeButtonPageContent")).toEqual(0);
     });
 });
