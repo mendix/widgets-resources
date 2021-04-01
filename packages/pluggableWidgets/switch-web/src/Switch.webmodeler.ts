abf823af1bdc293c8172bcce8f51bfbf181377f0
Jordan Hunt <jordan.hunt@mendix.com>
2021-04-01 12:42:06 +0200

    feat(switch-web): temp:wip

--- packages/pluggableWidgets/switch-web/src/Switch.webmodeler.ts
+++ packages/pluggableWidgets/switch-web/src/Switch.webmodeler.ts
@@ -1,29 +1,31 @@
-import { Component, ReactNode, createElement } from "react";
+// import { Component, ReactNode, createElement } from "react";
+//
+// import { Switch } from "./Switch";
+// import SwitchContainer, { SwitchContainerProps } from "./components/SwitchContainer";
+//
+// declare function require(name: string): string;
+//
+// // tslint:disable class-name
+// export class preview extends Component<SwitchContainerProps, {}> {
+//     render(): ReactNode {
+//         return this.renderSwitch();
+//     }
+//
+//     private renderSwitch(hasLabel = false): ReactNode {
+//         return createElement(Switch, {
+//             className: !hasLabel ? this.props.class : undefined,
+//             colorStyle: this.props.colorStyle,
+//             deviceStyle: this.props.deviceStyle,
+//             isChecked: true,
+//             onClick: undefined as any,
+//             status: this.props.editable === "default" ? "enabled" : "disabled",
+//             style: !hasLabel ? SwitchContainer.parseStyle(this.props.style) : undefined
+//         });
+//     }
+// }
+//
+// export function getPreviewCss(): string {
+//     return require("./ui/Switch.scss");
+// }
 
-import { Switch } from "./Switch";
-import SwitchContainer, { SwitchContainerProps } from "./components/SwitchContainer";
-
-declare function require(name: string): string;
-
-// tslint:disable class-name
-export class preview extends Component<SwitchContainerProps, {}> {
-    render(): ReactNode {
-        return this.renderSwitch();
-    }
-
-    private renderSwitch(hasLabel = false): ReactNode {
-        return createElement(Switch, {
-            className: !hasLabel ? this.props.class : undefined,
-            colorStyle: this.props.colorStyle,
-            deviceStyle: this.props.deviceStyle,
-            isChecked: true,
-            onClick: undefined as any,
-            status: this.props.editable === "default" ? "enabled" : "disabled",
-            style: !hasLabel ? SwitchContainer.parseStyle(this.props.style) : undefined
-        });
-    }
-}
-
-export function getPreviewCss(): string {
-    return require("./ui/Switch.scss");
-}
+console.log("hi");
