e0dd31940028be6aa6bb01be3a4504e08f7fe605
Jordan Hunt <jordan.hunt@mendix.com>
2021-04-01 12:26:19 +0200

    feat(switch-web): refactor switch

--- packages/customWidgets/switch-web/src/Switch.tsx
+++ packages/customWidgets/switch-web/src/Switch.tsx
@@ -1,10 +1,10 @@
-import { SFC, createElement } from "react";
+import { SFC, createElement, KeyboardEvent } from "react";
 import classNames from "classnames";
 
-import { Alert } from "./Alert";
-import { ColorStyle, DeviceStyle } from "./SwitchContainer";
+import { Alert } from "./components/Alert";
+import { ColorStyle, DeviceStyle } from "./components/SwitchContainer";
 
-import "../ui/Switch.scss";
+import "./ui/Switch.scss";
 
 export interface SwitchProps {
     alertMessage?: string;
@@ -20,49 +20,44 @@ export interface SwitchProps {
 
 export type SwitchStatus = "enabled" | "disabled" | "no-context";
 
-export const Switch: SFC<SwitchProps> = props =>
-    createElement(
-        "div",
-        {
-            className: classNames("widget-switch", props.className, props.deviceStyle),
-            style: props.style
-        },
-        createElement("input", {
-            checked: props.isChecked,
-            className: classNames("widget-switch-checkbox", { enabled: props.status === "enabled" }),
-            readOnly: true,
-            type: "checkbox"
-        }),
-        createElement(
-            "div",
-            {
-                className: classNames(`widget-switch-btn-wrapper widget-switch-btn-wrapper-${props.colorStyle}`, {
-                    checked: props.isChecked,
-                    disabled: props.status === "disabled",
-                    "no-switch": props.status === "no-context",
-                    "un-checked": !props.isChecked
-                }),
-                onClick: props.status === "enabled" ? props.onClick : undefined,
-                onKeyDown: (e: KeyboardEvent) => {
-                    if (props.status === "enabled" && e.key === " ") {
-                        e.preventDefault();
-                        props.onClick();
-                    }
-                },
-                tabIndex: 0,
-                role: "checkbox",
-                "aria-checked": props.isChecked,
-                "aria-labelledby": props.labelId
-            },
-            createElement("small", {
-                className: classNames("widget-switch-btn", {
+export const Switch: SFC<SwitchProps> = props => (
+    <div className={classNames("widget-switch", props.className, props.deviceStyle)} style={props.style}>
+        <input
+            checked={props.isChecked}
+            className={classNames("widget-switch-checkbox", { enabled: props.status === "enabled" })}
+            readOnly={true}
+            type={"checkbox"}
+        />
+        ,
+        <div
+            className={classNames(`widget-switch-btn-wrapper widget-switch-btn-wrapper-${props.colorStyle}`, {
+                checked: props.isChecked,
+                disabled: props.status === "disabled",
+                "no-switch": props.status === "no-context",
+                "un-checked": !props.isChecked
+            })}
+            onClick={props.status === "enabled" ? props.onClick : undefined}
+            onKeyDown={(e: KeyboardEvent<HTMLDivElement>) => {
+                if (props.status === "enabled" && e.key === " ") {
+                    e.preventDefault();
+                    props.onClick();
+                }
+            }}
+            tabIndex={0}
+            role={"checkbox"}
+            aria-checked={props.isChecked}
+            aria-labelledby={props.labelId}
+        >
+            <small
+                className={classNames("widget-switch-btn", {
                     left: !props.isChecked,
                     right: props.isChecked
-                })
-            })
-        ),
-        createElement(Alert, { message: props.alertMessage })
-    );
+                })}
+            />
+        </div>
+        <Alert message={props.alertMessage} />
+    </div>
+);
 
 Switch.defaultProps = {
     colorStyle: "default",
