34e9ff51644aa28a73a8070f2f80317287e650b3
Jordan Hunt <jordan.hunt@mendix.com>
2021-04-01 12:24:29 +0200

    feat(switch-web): refactor alert

--- packages/customWidgets/switch-web/src/components/Alert.tsx
+++ packages/customWidgets/switch-web/src/components/Alert.tsx
@@ -1,16 +1,10 @@
-import { SFC, createElement } from "react";
-import classNames from "classnames";
+import { FunctionComponent, createElement } from "react";
 
 export interface AlertProps {
     message?: string;
-    className?: string;
-    bootstrapStyle?: "default" | "primary" | "success" | "info" | "warning" | "danger";
 }
 
-export const Alert: SFC<AlertProps> = ({ className, bootstrapStyle, message }) =>
-    message
-        ? createElement("div", { className: classNames(`alert alert-${bootstrapStyle}`, className) }, message)
-        : null;
+export const Alert: FunctionComponent<AlertProps> = ({ message }) =>
+    message ? <div className={"alert alert-danger"}>{message}</div> : null;
 
-Alert.defaultProps = { bootstrapStyle: "danger" };
 Alert.displayName = "Alert";
