141cd9edcfc840c2a9044607c555b8a043efb86d
I. Isaku <16718722+IIsaku@users.noreply.github.com>
2021-04-02 10:16:05 +0200

    feat(shrinking-header-web): calculate wrapping div height automatically

--- packages/pluggableWidgets/shrinking-header-web/README.md
+++ packages/pluggableWidgets/shrinking-header-web/README.md
@@ -7,6 +7,5 @@ Please see [Shrinking header]() in the Mendix documentation for details.
 1. Position "sticky" behaviour breaks when navigating from another page in the Mendix application. This is due to the css rule `overflow-x: hidden` which is applied to `div class="mx-placeholder" />`.
 
     The solution is to iterate through all ancestor elements and remove this rule. If encountered, the rule can maybe be applied to the body. Discuss with the teach lead why this overflow rule is set at all.
-1. Calculate the max size of wrapping header div. Use a ref to the header and when available check the height and width. Also think about scroll position preservation and whether that affects the calculation.
 1. Remove shrunk class widget config property.
 1. Create a shrinking header that resizes while scrolling.
--- packages/pluggableWidgets/shrinking-header-web/src/components/ShrinkingHeaderThreshold.tsx
+++ packages/pluggableWidgets/shrinking-header-web/src/components/ShrinkingHeaderThreshold.tsx
@@ -1,4 +1,4 @@
-import { createElement, CSSProperties, ReactElement, ReactNode, useEffect, useState } from "react";
+import { createElement, CSSProperties, ReactElement, ReactNode, useCallback, useEffect, useState } from "react";
 import classNames from "classnames";
 import { throttle } from "lodash-es";
 
@@ -18,10 +18,22 @@ export interface ShrinkingHeaderThresholdProps {
 export function ShrinkingHeaderThreshold(props: ShrinkingHeaderThresholdProps): ReactElement {
     const { name, className, style, tabIndex, headerContent, shrunkClassName, shrinkThreshold } = props;
 
+    const [inlineStyle, setInlineStyle] = useState<CSSProperties>({ ...style });
+    const [wrappingHeaderHeight, setWrappingHeaderHeight] = useState<number>(0);
     const [shrunk, setShrunk] = useState(false);
+
     const actualClassName = classNames("widget-shrinking-header-threshold", className, {
         [`${shrunkClassName}`]: shrunk
     });
+
+    const updateHeight = useCallback(
+        (node: HTMLDivElement | null) => {
+            if (node) {
+                setWrappingHeaderHeight(node.offsetHeight);
+            }
+        },
+        [setWrappingHeaderHeight]
+    );
 
     useEffect(() => {
         const evaluateShrunkState = function (this: HTMLElement): void {
@@ -41,9 +53,13 @@ export function ShrinkingHeaderThreshold(props: ShrinkingHeaderThresholdProps): 
         };
     }, [shrinkThreshold, setShrunk]);
 
+    useEffect(() => {
+        setInlineStyle(prevState => ({ ...prevState, height: wrappingHeaderHeight }));
+    }, [setInlineStyle, wrappingHeaderHeight]);
+
     return (
-        <div id={name} className={actualClassName} style={style} tabIndex={tabIndex}>
-            <header>{headerContent}</header>
+        <div id={name} className={actualClassName} style={inlineStyle} tabIndex={tabIndex}>
+            <header ref={updateHeight}>{headerContent}</header>
         </div>
     );
 }
