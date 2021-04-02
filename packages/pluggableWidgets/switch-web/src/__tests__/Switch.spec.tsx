bc3170a26e4665d76334b3f8a2da2cd43741cdcc
Jordan Hunt <jordan.hunt@mendix.com>
2021-04-02 13:58:55 +0200

    feat(switch-web): improve unit tests and add coverage

--- packages/pluggableWidgets/switch-web/src/__tests__/Switch.spec.tsx
+++ packages/pluggableWidgets/switch-web/src/__tests__/Switch.spec.tsx
@@ -1,26 +1,33 @@
-import { ShallowWrapper, shallow } from "enzyme";
+import { actionValue, EditableValueBuilder, Alert, AlertProps } from "@mendix/piw-utils-internal";
+import { mount, ReactWrapper } from "enzyme";
 import { createElement } from "react";
 
-import { Switch } from "../Switch";
-import { actionValue, EditableValueBuilder, Alert } from "@mendix/piw-utils-internal";
+import { Switch as SwitchComponent, SwitchProps } from "../components/Switch";
 import { SwitchContainerProps } from "../../typings/SwitchProps";
+import { Switch } from "../Switch";
 
 describe("Switch", () => {
-    let switchWrapper: ShallowWrapper<SwitchContainerProps, any>;
-    let checkbox: ShallowWrapper<any, any>;
-    let switchButtonWrapper: ShallowWrapper<any, any>;
-    let switchButton: ShallowWrapper<any, any>;
+    let switchWrapper: ReactWrapper<SwitchContainerProps, any>;
+    let switchComponent: ReactWrapper<SwitchProps, any>;
+    let switchComponentWrapper: ReactWrapper<any, any>;
+    let checkbox: ReactWrapper<any, any>;
+    let switchButtonWrapper: ReactWrapper<any, any>;
+    let switchButton: ReactWrapper<any, any>;
+    let alert: ReactWrapper<AlertProps, any>;
     const createAndFindElements = (props: SwitchContainerProps): void => {
-        switchWrapper = shallow(createElement(Switch, props));
-        checkbox = switchWrapper.find(".widget-switch-checkbox");
-        switchButtonWrapper = switchWrapper.find(".widget-switch-btn-wrapper");
-        switchButton = switchWrapper.find(".widget-switch-btn");
+        switchWrapper = mount(createElement(Switch, props));
+        switchComponent = switchWrapper.find(SwitchComponent);
+        switchComponentWrapper = switchComponent.find(".widget-switch");
+        checkbox = switchComponent.find(".widget-switch-checkbox");
+        switchButtonWrapper = switchComponent.find(".widget-switch-btn-wrapper");
+        switchButton = switchComponent.find(".widget-switch-btn");
+        alert = switchComponent.find(Alert);
     };
     const createProps = (props?: Partial<SwitchContainerProps>): SwitchContainerProps => {
         const defaultProps: SwitchContainerProps = {
             name: "switch",
-            class: "",
-            style: undefined,
+            class: "company-switch-widget",
+            style: { color: "red" },
             tabIndex: 0,
             id: "com.mendix.widgets.custom.switch1",
             booleanAttribute: new EditableValueBuilder<boolean>().withValue(false).build(),
@@ -31,42 +38,53 @@ describe("Switch", () => {
         return { ...defaultProps, ...props };
     };
 
-    it("should render the structure correctly", () => {
+    it("with editable value should render the structure correctly", () => {
         createAndFindElements(
             createProps({
-                booleanAttribute: new EditableValueBuilder<boolean>().withValue(true).build()
+                booleanAttribute: new EditableValueBuilder<boolean>().withValue(false).build()
             })
         );
 
-        expect(switchWrapper.getElement()).toEqual(
-            createElement(
-                "div",
-                { className: "widget-switch auto" },
-                createElement("input", {
-                    checked: true,
-                    className: "widget-switch-checkbox enabled",
-                    readOnly: true,
-                    type: "checkbox"
-                }),
-                createElement(
-                    "div",
-                    {
-                        "aria-checked": true,
-                        className: "widget-switch-btn-wrapper widget-switch-btn-wrapper-default checked",
-                        onClick: expect.any(Function),
-                        onKeyDown: expect.any(Function),
-                        "aria-labelledby": "com.mendix.widgets.custom.switch1",
-                        role: "checkbox",
-                        tabIndex: 0
-                    },
-                    createElement("small", { className: "widget-switch-btn right" })
-                ),
-                createElement(Alert)
-            )
-        );
+        expect(switchWrapper).toMatchSnapshot();
     });
 
-    it("that is true should be on", () => {
+    it("with readonly value should render the structure correctly", () => {
+        createAndFindElements(
+            createProps({
+                booleanAttribute: new EditableValueBuilder<boolean>().isReadOnly().withValue(false).build()
+            })
+        );
+
+        expect(switchWrapper).toMatchSnapshot();
+    });
+
+    it("without validation message should render correctly", () => {
+        createAndFindElements(
+            createProps({
+                booleanAttribute: new EditableValueBuilder<boolean>().withValue(false).build()
+            })
+        );
+
+        expect(alert).toMatchSnapshot();
+    });
+
+    it("with validation message should render correctly", () => {
+        createAndFindElements(
+            createProps({
+                booleanAttribute: new EditableValueBuilder<boolean>().withValidation("error").withValue(false).build()
+            })
+        );
+
+        expect(alert).toMatchSnapshot();
+    });
+
+    it("when value is false should have the correct checkbox attribute", () => {
+        createAndFindElements(createProps());
+
+        expect(checkbox.props().checked).toBe(false);
+    });
+
+    it("when value is true should have the correct checkbox attribute", () => {
         createAndFindElements(
             createProps({ booleanAttribute: new EditableValueBuilder<boolean>().withValue(true).build() })
         );
@@ -74,39 +92,65 @@ describe("Switch", () => {
         expect(checkbox.props().checked).toBe(true);
     });
 
-    it("that is false should be off", () => {
-        createAndFindElements(createProps());
-
-        expect(checkbox.props().checked).toBe(false);
-    });
-
-    it("with the iOS device style renders with the class iOS", () => {
+    it("with iOS device style should render correct class", () => {
         createAndFindElements(createProps({ deviceStyle: "iOS" }));
-
-        expect(switchWrapper.hasClass("iOS")).toBe(true);
+        console.log(switchComponentWrapper);
+        expect(switchComponentWrapper.hasClass("iOS")).toBe(true);
     });
 
-    it("with the android device style renders with the class android", () => {
+    it("with android device style should render correct class", () => {
         createAndFindElements(createProps({ deviceStyle: "android" }));
 
-        expect(switchWrapper.hasClass("android")).toBe(true);
+        expect(switchComponentWrapper.hasClass("android")).toBe(true);
     });
 
-    it("with the auto device style renders with the class auto", () => {
+    it("with auto device style should render correct class", () => {
         createAndFindElements(createProps({ deviceStyle: "auto" }));
 
-        expect(switchWrapper.hasClass("auto")).toBe(true);
+        expect(switchComponentWrapper.hasClass("auto")).toBe(true);
     });
 
-    describe("that is enabled", () => {
-        it("should not have the disabled class", () => {
-            createAndFindElements(createProps({}));
+    it("with tabIndex passed renders correctly", () => {
+        createAndFindElements(createProps({ tabIndex: 1 }));
+
+        expect(switchButtonWrapper.props().tabIndex).toEqual(1);
+    });
+
+    it("without tabIndex passed renders correctly", () => {
+        createAndFindElements(createProps({ tabIndex: undefined }));
+
+        expect(switchButtonWrapper.props().tabIndex).toEqual(0);
+    });
+
+    describe("when editable", () => {
+        it("should render elements with correct attributes", () => {
+            createAndFindElements(createProps());
 
             expect(checkbox.hasClass("enabled")).toBe(true);
             expect(switchButtonWrapper.hasClass("enabled")).toBe(false);
         });
 
-        it("should handle click events", () => {
+        it("should invoke preventDefault onClick", () => {
+            const props = createProps({ action: actionValue() });
+            createAndFindElements(props);
+            const eventMock = { preventDefault: jest.fn() };
+
+            switchButtonWrapper.simulate("click", eventMock);
+
+            expect(eventMock.preventDefault).toHaveBeenCalled();
+        });
+
+        it("should invoke preventDefault on space keydown", () => {
+            const props = createProps({ action: actionValue() });
+            createAndFindElements(props);
+            const eventMock = { preventDefault: jest.fn(), key: " " };
+
+            switchButtonWrapper.simulate("keydown", eventMock);
+
+            expect(eventMock.preventDefault).toHaveBeenCalled();
+        });
+
+        it("should invoke action on click", () => {
             const props = createProps({ action: actionValue() });
             createAndFindElements(props);
 
@@ -114,10 +158,123 @@ describe("Switch", () => {
 
             expect(props.action?.execute).toHaveBeenCalled();
         });
+
+        it("should invoke action on space keydown", () => {
+            const props = createProps({ action: actionValue() });
+            createAndFindElements(props);
+
+            switchButtonWrapper.simulate("keydown", { key: " " });
+
+            expect(props.action?.execute).toHaveBeenCalled();
+        });
+
+        it("should not invoke action on keydown of any key but space", () => {
+            const props = createProps({ action: actionValue() });
+            createAndFindElements(props);
+
+            switchButtonWrapper.simulate("keydown", { key: "enter" });
+
+            expect(props.action?.execute).not.toHaveBeenCalled();
+        });
+
+        describe("when value is available", () => {
+            it("should toggle the attributes value onClick", () => {
+                const props = createProps({
+                    booleanAttribute: new EditableValueBuilder<boolean>().withValue(false).build()
+                });
+                createAndFindElements(props);
+
+                switchButton.simulate("click");
+
+                expect(props.booleanAttribute.setValue).toHaveBeenCalled();
+                expect(props.booleanAttribute.value).toEqual(true);
+
+                switchButton.simulate("click");
+
+                expect(props.booleanAttribute.setValue).toHaveBeenCalled();
+                expect(props.booleanAttribute.value).toEqual(false);
+            });
+
+            it("should toggle the attribute value on space keydown", () => {
+                const props = createProps({
+                    booleanAttribute: new EditableValueBuilder<boolean>().withValue(false).build()
+                });
+                createAndFindElements(props);
+
+                switchButton.simulate("keydown", { key: " " });
+
+                expect(props.booleanAttribute.setValue).toHaveBeenCalled();
+                expect(props.booleanAttribute.value).toEqual(true);
+
+                switchButton.simulate("keydown", { key: " " });
+
+                expect(props.booleanAttribute.setValue).toHaveBeenCalled();
+                expect(props.booleanAttribute.value).toEqual(false);
+            });
+
+            it("should not toggle the attribute on keydown of any key but space", () => {
+                const props = createProps({
+                    booleanAttribute: new EditableValueBuilder<boolean>().withValue(false).build()
+                });
+                createAndFindElements(props);
+
+                switchButton.simulate("keydown", { key: "enter" });
+
+                expect(props.booleanAttribute.setValue).not.toHaveBeenCalled();
+            });
+        });
+
+        describe("when value is loading", () => {
+            it("should not toggle the attributes value", () => {
+                const props = createProps({
+                    booleanAttribute: new EditableValueBuilder<boolean>().withValue(false).isLoading().build()
+                });
+                createAndFindElements(props);
+
+                switchButton.simulate("click");
+
+                expect(props.booleanAttribute.setValue).not.toHaveBeenCalled();
+            });
+
+            it("should not toggle the attributes value on space keydown", () => {
+                const props = createProps({
+                    booleanAttribute: new EditableValueBuilder<boolean>().withValue(false).isLoading().build()
+                });
+                createAndFindElements(props);
+
+                switchButton.simulate("keydown", { key: " " });
+
+                expect(props.booleanAttribute.setValue).not.toHaveBeenCalled();
+            });
+        });
+
+        describe("when value is unavailable", () => {
+            it("should not toggle the attributes value", () => {
+                const props = createProps({
+                    booleanAttribute: new EditableValueBuilder<boolean>().withValue(false).isUnavailable().build()
+                });
+                createAndFindElements(props);
+
+                switchButton.simulate("click");
+
+                expect(props.booleanAttribute.setValue).not.toHaveBeenCalled();
+            });
+
+            it("should not toggle the attributes value on space keydown", () => {
+                const props = createProps({
+                    booleanAttribute: new EditableValueBuilder<boolean>().withValue(false).isUnavailable().build()
+                });
+                createAndFindElements(props);
+
+                switchButton.simulate("keydown", { key: " " });
+
+                expect(props.booleanAttribute.setValue).not.toHaveBeenCalled();
+            });
+        });
     });
 
-    describe("that is disabled", () => {
-        it("should have the disabled class", () => {
+    describe("when readonly", () => {
+        it("should render elements with correct attributes", () => {
             createAndFindElements(
                 createProps({ booleanAttribute: new EditableValueBuilder<boolean>().isReadOnly().build() })
             );
@@ -126,7 +283,7 @@ describe("Switch", () => {
             expect(switchButtonWrapper.hasClass("disabled")).toBe(true);
         });
 
-        it("should not handle a click event", () => {
+        it("should not invoke action", () => {
             const props = createProps({
                 booleanAttribute: new EditableValueBuilder<boolean>().isReadOnly().build(),
                 action: actionValue()
@@ -136,6 +293,37 @@ describe("Switch", () => {
             switchButton.simulate("click");
 
             expect(props.action?.execute).not.toHaveBeenCalled();
+        });
+
+        it("should not change the attributes value", () => {
+            const props = createProps({
+                booleanAttribute: new EditableValueBuilder<boolean>().isReadOnly().build()
+            });
+            createAndFindElements(props);
+
+            switchButton.simulate("click");
+
+            expect(props.booleanAttribute.setValue).not.toHaveBeenCalled();
+        });
+
+        it("should invoke preventDefault onClick", () => {
+            const props = createProps({ action: actionValue() });
+            createAndFindElements(props);
+            const eventMock = { preventDefault: jest.fn() };
+
+            switchButtonWrapper.simulate("click", eventMock);
+
+            expect(eventMock.preventDefault).toHaveBeenCalled();
+        });
+
+        it("should invoke preventDefault on keydown space", () => {
+            const props = createProps({ action: actionValue() });
+            createAndFindElements(props);
+            const eventMock = { preventDefault: jest.fn(), key: " " };
+
+            switchButtonWrapper.simulate("keydown", eventMock);
+
+            expect(eventMock.preventDefault).toHaveBeenCalled();
         });
     });
 });
