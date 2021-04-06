2466be324fe8fda8a9d9635e7a85373adabd8882
Jordan Hunt <jordan.hunt@mendix.com>
2021-04-06 10:44:34 +0200

    feat(switch-web): review: simplify

--- packages/pluggableWidgets/switch-web/src/Switch.editorPreview.tsx
+++ packages/pluggableWidgets/switch-web/src/Switch.editorPreview.tsx
@@ -2,7 +2,7 @@ import { createElement, ReactElement } from "react";
 import { parseStyle } from "@mendix/piw-utils-internal";
 
 import { SwitchPreviewProps } from "../typings/SwitchProps";
-import { Switch } from "./components/Switch";
+import Switch from "./components/Switch";
 
 export function preview(props: SwitchPreviewProps): ReactElement {
     // TODO: The widget generator is out of sync with Studio Pro design mode. Change PIW preview props typing (class -> className) generation to remove the ts-ignore below
