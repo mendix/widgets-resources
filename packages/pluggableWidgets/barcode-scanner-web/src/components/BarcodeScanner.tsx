f3e96caacfff4af5da0ecf015ef6a5d5454397a3
Keraito <chakshunyu@gmail.com>
2021-04-12 10:50:51 +0200

    feat(barcode-scanner-web): scaffold widget package

---
+++ packages/pluggableWidgets/barcode-scanner-web/src/components/BarcodeScanner.tsx
@@ -0,0 +1,8 @@
+import { createElement, ReactElement } from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface BarcodeScannerProps {}
+
+export function BarcodeScanner(_props: BarcodeScannerProps): ReactElement {
+    return <div></div>;
+}
