672a4cd2f0feb64ba0de3a843137a518517acb4b
Keraito <chakshunyu@gmail.com>
2021-04-15 14:51:39 +0200

    feat(barcode-scanner-web): refine rollup config error catching

--- packages/pluggableWidgets/barcode-scanner-web/rollup.config.js
+++ packages/pluggableWidgets/barcode-scanner-web/rollup.config.js
@@ -3,10 +3,12 @@ export default args => {
     result.forEach(config => {
         const onwarn = config.onwarn;
         config.onwarn = warning => {
-            // TODO: The library itself throws a lot of esm module related errors on compilation.
-            // For now they don't seem related to us and everything still works, so I ignored
-            // all of them, but this should not catch every error and just throw it into the abyss.
-            if (warning.loc.file.includes("@zxing/library")) {
+            // The library itself throws a lot of esm module related errors on compilation.
+            // For now they don't seem related to us and everything still works, so I ignored them.
+            if (
+                warning.loc.file.includes("@zxing/library") &&
+                (warning.code === "THIS_IS_UNDEFINED" || warning.code === "SOURCEMAP_ERROR")
+            ) {
                 return;
             }
             onwarn(warning);
