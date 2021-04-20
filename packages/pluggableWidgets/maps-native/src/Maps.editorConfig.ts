603495a92665eb4a267eea18164d38fce84b2540
Wesley <wesley.sieses@mendix.com>
2021-04-20 14:51:12 +0200

    fix(maps-native): fix error checks

--- packages/pluggableWidgets/maps-native/src/Maps.editorConfig.ts
+++ packages/pluggableWidgets/maps-native/src/Maps.editorConfig.ts
@@ -5,6 +5,7 @@ export function check(values: MapsPreviewProps): Problem[] {
     const errors: Problem[] = [];
     const zoomLevels = ["world", "continent", "country", "city", "town", "streets", "building"];
 
+    const defaultZoomLevelIndex = zoomLevels.indexOf(values.defaultZoomLevel);
     const minZoomLevelIndex = zoomLevels.indexOf(values.minZoomLevel);
     const maxZoomLevelIndex = zoomLevels.indexOf(values.maxZoomLevel);
 
@@ -14,11 +15,25 @@ export function check(values: MapsPreviewProps): Problem[] {
             severity: "error",
             message: "The minimum zoom level can not be smaller than the maximum zoom level."
         });
-    } else if (minZoomLevelIndex < maxZoomLevelIndex) {
+    } else if (maxZoomLevelIndex < minZoomLevelIndex) {
         errors.push({
             property: "maxZoomLevel",
             severity: "error",
             message: "The maximum zoom level can not be bigger than the minimum zoom level."
+        });
+    }
+
+    if (defaultZoomLevelIndex < minZoomLevelIndex) {
+        errors.push({
+            property: "defaultZoomLevel",
+            severity: "error",
+            message: "The default zoom level can not be bigger than the minimum zoom level."
+        });
+    } else if (defaultZoomLevelIndex > maxZoomLevelIndex) {
+        errors.push({
+            property: "defaultZoomLevel",
+            severity: "error",
+            message: "The default zoom level can not be smaller than the maximum zoom level."
         });
     }
 
