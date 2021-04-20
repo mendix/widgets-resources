4f5f7d4709e20a054e954844e3d280fe5c5aa01c
Keraito <chakshunyu@gmail.com>
2021-04-20 12:26:52 +0200

    feat(barcode-scanner-web): implement toggle for showing the overlay mask functionality

--- packages/pluggableWidgets/barcode-scanner-web/src/BarcodeScanner.editorPreview.tsx
+++ packages/pluggableWidgets/barcode-scanner-web/src/BarcodeScanner.editorPreview.tsx
@@ -2,6 +2,6 @@ import { createElement, ReactElement } from "react";
 import { BarcodeScannerPreviewProps } from "../typings/BarcodeScannerProps";
 import { BarcodeScanner } from "./components/BarcodeScanner";
 
-export function preview(_props: BarcodeScannerPreviewProps): ReactElement {
-    return <BarcodeScanner onClose={undefined} onDetect={undefined} />;
+export function preview(props: BarcodeScannerPreviewProps): ReactElement {
+    return <BarcodeScanner onClose={undefined} onDetect={undefined} showMask={props.showMask} />;
 }
