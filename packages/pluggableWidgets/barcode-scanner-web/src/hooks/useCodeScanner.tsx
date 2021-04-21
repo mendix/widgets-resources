c5ea0cf774ef325c5dcbc6af886799a32501280a
Keraito <chakshunyu@gmail.com>
2021-04-21 14:40:03 +0200

    feat(barcode-scanner-web): remove onDetect from useCodeScanner dependencies and optimize performance

--- packages/pluggableWidgets/barcode-scanner-web/src/hooks/useCodeScanner.tsx
+++ packages/pluggableWidgets/barcode-scanner-web/src/hooks/useCodeScanner.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { BarcodeFormat, BrowserMultiFormatReader, DecodeHintType } from "@zxing/library";
 
 const hints = new Map();
@@ -7,17 +7,18 @@ hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
 
 type CodeScannerHook = (
     streamObject: MediaStream | null,
-    videoElement: HTMLVideoElement | null | undefined,
-    onDetect: ((data: string) => void) | undefined
-) => void;
+    videoElement: HTMLVideoElement | null | undefined
+) => { codeResult: string | null };
 
-export const useCodeScanner: CodeScannerHook = (streamObject, videoElement, onDetect) => {
+export const useCodeScanner: CodeScannerHook = (streamObject, videoElement) => {
+    const [codeResult, setCodeResult] = useState<string | null>(null);
+
     useEffect(() => {
         const browserReader = new BrowserMultiFormatReader(hints);
         async function check(stream: MediaStream, element: HTMLVideoElement): Promise<void> {
             try {
                 const result = await browserReader.decodeOnceFromStream(stream, element);
-                onDetect?.(result.getText());
+                setCodeResult(result.getText());
             } catch (e) {
                 // TODO: handle error case
                 console.log("something went wrong", e);
@@ -25,9 +26,40 @@ export const useCodeScanner: CodeScannerHook = (streamObject, videoElement, onDe
         }
         if (streamObject && videoElement) {
             check(streamObject, videoElement);
-            // return () => {
-            //     browserReader.reset();
-            // };
         }
-    }, [streamObject, videoElement, onDetect]);
+    }, [streamObject, videoElement]);
+    useWhyDidYouUpdate("useCodeScanner", { streamObject, videoElement });
+    return { codeResult };
 };
+
+// Hook
+function useWhyDidYouUpdate(name: string, props: Record<string, any>): void {
+    // Get a mutable ref object where we can store props ...
+    // ... for comparison next time this hook runs.
+    const previousProps = useRef<Record<string, any>>({});
+    useEffect(() => {
+        if (previousProps.current) {
+            // Get all keys from previous and current props
+            const allKeys = Object.keys({ ...previousProps.current, ...props });
+            // Use this object to keep track of changed props
+            const changesObj: Record<string, any> = {};
+            // Iterate through keys
+            allKeys.forEach(key => {
+                // If previous is different from current
+                if (previousProps.current[key] !== props[key]) {
+                    // Add to changesObj
+                    changesObj[key] = {
+                        from: previousProps.current[key],
+                        to: props[key]
+                    };
+                }
+            });
+            // If changesObj not empty then output to console
+            if (Object.keys(changesObj).length) {
+                console.log("[why-did-you-update]", name, changesObj);
+            }
+        }
+        // Finally update previousProps with current props for next hook call
+        previousProps.current = props;
+    });
+}
