7ebad7ccf4cdcbc6064be991aaadd4563d47169e
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2021-04-26 12:05:50 +0200

    feat(accordion-web): add group object list property

---
+++ packages/pluggableWidgets/accordion-web/Accordion.editorConfig.ts
@@ -0,0 +1,23 @@
+import { hidePropertyIn, Properties, transformGroupsIntoTabs } from "@mendix/piw-utils-internal";
+
+import { AccordionContainerProps } from "./typings/AccordionProps";
+
+export function getProperties(
+    values: AccordionContainerProps,
+    defaultProperties: Properties,
+    platform: "web" | "desktop"
+): Properties {
+    values.groups.forEach((group, index) => {
+        if (group.headerRenderMode === "text") {
+            hidePropertyIn(defaultProperties, values, "groups", index, "headerContent");
+        } else {
+            hidePropertyIn(defaultProperties, values, "groups", index, "headerText");
+        }
+    });
+
+    if (platform === "web") {
+        transformGroupsIntoTabs(defaultProperties);
+    }
+
+    return defaultProperties;
+}
--- packages/pluggableWidgets/accordion-web/typings/AccordionProps.d.ts
+++ packages/pluggableWidgets/accordion-web/typings/AccordionProps.d.ts
@@ -3,18 +3,33 @@
  * WARNING: All changes made to this file will be overwritten
  * @author Mendix UI Content Team
  */
-import { CSSProperties } from "react";
+import { ComponentType, CSSProperties, ReactNode } from "react";
+import { DynamicValue } from "mendix";
+
+export type HeaderRenderModeEnum = "text" | "custom";
+
+export interface GroupsType {
+    headerRenderMode: HeaderRenderModeEnum;
+    headerText: DynamicValue<string>;
+    headerContent?: ReactNode;
+}
+
+export interface GroupsPreviewType {
+    headerRenderMode: HeaderRenderModeEnum;
+    headerText: string;
+    headerContent: { widgetCount: number; renderer: ComponentType<{caption?: string}> };
+}
 
 export interface AccordionContainerProps {
     name: string;
     class: string;
     style?: CSSProperties;
     tabIndex?: number;
-
+    groups: GroupsType[];
 }
 
 export interface AccordionPreviewProps {
     class: string;
     style: string;
-
+    groups: GroupsPreviewType[];
 }
