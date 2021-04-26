dcbfaa97f6186cc681ff186122e9faa28f2da0b2
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2021-04-26 15:33:12 +0200

    feat(accordion-web): add group rendering

--- packages/pluggableWidgets/accordion-web/src/components/Accordion.tsx
+++ packages/pluggableWidgets/accordion-web/src/components/Accordion.tsx
@@ -1,5 +1,31 @@
-import { ReactElement } from "react";
+import { createElement, ReactElement, ReactNode, useMemo } from "react";
 
-export default function Accordion(): ReactElement | null {
-    return null;
+import { AccordionContainerProps } from "../../typings/AccordionProps";
+
+interface AccordionGroup {
+    header: ReactNode;
+    content: ReactNode;
+}
+
+export interface AccordionProps extends Pick<AccordionContainerProps, "class" | "style" | "tabIndex"> {
+    groups: AccordionGroup[];
+}
+
+export default function Accordion(props: AccordionProps): ReactElement | null {
+    const { class: classNames, groups, style, tabIndex } = props;
+
+    const renderedGroups = useMemo(() => {
+        return groups.map((group, index) => (
+            <section key={index}>
+                <header>{group.header}</header>
+                <div>{group.content}</div>
+            </section>
+        ));
+    }, [groups]);
+
+    return (
+        <div className={classNames} style={style} tabIndex={tabIndex}>
+            {renderedGroups}
+        </div>
+    );
 }
