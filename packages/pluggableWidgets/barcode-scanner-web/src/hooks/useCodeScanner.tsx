a0ff7c0f7a23381d72b525fd1cf9f4a5773a05fb
Keraito <chakshunyu@gmail.com>
2021-04-28 09:50:38 +0200

    feat(barcode-scanner-web): add appropriate TODOs, cleanup debug code, add code comments

--- packages/pluggableWidgets/barcode-scanner-web/src/hooks/useCodeScanner.tsx
+++ packages/pluggableWidgets/barcode-scanner-web/src/hooks/useCodeScanner.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { BarcodeFormat, BrowserMultiFormatReader, DecodeHintType } from "@zxing/library";
 
 const hints = new Map();
+// `BarcodeFormat` is a TypeScript enum. Calling `Object.values` on it returns an array of string and ints, we only want the latter.
 const formats = Object.values(BarcodeFormat).filter(format => typeof format !== "string") as BarcodeFormat[];
 hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
 
@@ -39,38 +40,6 @@ export const useCodeScanner: CodeScannerHook = (streamObject, videoElement) => {
             isCanceled = true;
         };
     }, [streamObject, videoElement]);
-    useWhyDidYouUpdate("useCodeScanner", { streamObject, videoElement });
+
     return { codeResult, error };
 };
-
-// Hook
-function useWhyDidYouUpdate(name: string, props: Record<string, any>): void {
-    // Get a mutable ref object where we can store props ...
-    // ... for comparison next time this hook runs.
-    const previousProps = useRef<Record<string, any>>({});
-    useEffect(() => {
-        if (previousProps.current) {
-            // Get all keys from previous and current props
-            const allKeys = Object.keys({ ...previousProps.current, ...props });
-            // Use this object to keep track of changed props
-            const changesObj: Record<string, any> = {};
-            // Iterate through keys
-            allKeys.forEach(key => {
-                // If previous is different from current
-                if (previousProps.current[key] !== props[key]) {
-                    // Add to changesObj
-                    changesObj[key] = {
-                        from: previousProps.current[key],
-                        to: props[key]
-                    };
-                }
-            });
-            // If changesObj not empty then output to console
-            if (Object.keys(changesObj).length) {
-                console.log("[why-did-you-update]", name, changesObj);
-            }
-        }
-        // Finally update previousProps with current props for next hook call
-        previousProps.current = props;
-    });
-}
