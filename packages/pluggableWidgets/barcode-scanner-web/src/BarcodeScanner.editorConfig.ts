8675eda4545f7b14ff836b1f530017622e2d317b
Keraito <chakshunyu@gmail.com>
2021-04-28 12:32:16 +0200

    feat(barcode-scanner-web): clean up old configurations and fix build script

--- packages/pluggableWidgets/barcode-scanner-web/src/BarcodeScanner.editorConfig.ts
+++ packages/pluggableWidgets/barcode-scanner-web/src/BarcodeScanner.editorConfig.ts
@@ -1,1 +1,7 @@
+import { StructurePreviewProps } from "@mendix/piw-utils-internal";
+import { BarcodeScannerPreviewProps } from "../typings/BarcodeScannerProps";
+
 // TODO: Structure preview.
+export function getPreview(_values: BarcodeScannerPreviewProps): StructurePreviewProps | null {
+    return null;
+}
