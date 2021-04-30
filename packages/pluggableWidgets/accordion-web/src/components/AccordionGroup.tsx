2858dff55da933159aec6280d3dc1fe0e7d832e2
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2021-04-30 16:39:28 +0200

    feat(accordion-web): implement collapsed prop for AccordionGroup
    - Create a prop with which you can influence the collapsed state of the group

--- packages/pluggableWidgets/accordion-web/src/components/AccordionGroup.tsx
+++ packages/pluggableWidgets/accordion-web/src/components/AccordionGroup.tsx
@@ -1,26 +1,36 @@
-import { createElement, CSSProperties, ReactElement, ReactNode, useCallback, useState } from "react";
+import { createElement, CSSProperties, ReactElement, ReactNode, useCallback, useRef, useState } from "react";
 
 export interface AccordionGroupProps {
     header: ReactNode;
     content: ReactNode;
     collapsible: boolean;
+    collapsed?: boolean;
 }
 
-// collapsible false -> always show all content, not clickable
-// collapsible true + single --> start all closed, allow only one open
-// collapsible true + multiple --> start all closed, allow multiple open
-
 export default function AccordionGroup(props: AccordionGroupProps): ReactElement | null {
-    const { header, content, collapsible } = props;
+    const { header, content, collapsible, collapsed } = props;
+
+    const divCollapsed = useRef(collapsed);
 
     // The collapsible prop is never going to change afterwards, so the state doesn't need to be updated at a later stage based on this prop.
-    const [divInlineStyle, setDivInlineStyle] = useState<CSSProperties>({ display: collapsible ? "none" : undefined });
-    const [mountContent, setMountContent] = useState(!collapsible);
+    const [divInlineStyle, setDivInlineStyle] = useState<CSSProperties>({
+        display: collapsible && (divCollapsed.current || divCollapsed.current === undefined) ? "none" : undefined
+    });
+    const [mountContent, setMountContent] = useState(
+        !(collapsible && (divCollapsed.current || divCollapsed.current === undefined))
+    );
 
     const toggleContentVisibility = useCallback(() => {
         setMountContent(true);
         setDivInlineStyle(prevState => ({ display: prevState.display ? undefined : "none" }));
     }, [setDivInlineStyle, setMountContent]);
+
+    if (collapsed !== divCollapsed.current) {
+        divCollapsed.current = collapsed;
+        setDivInlineStyle({
+            display: collapsible && (divCollapsed.current || divCollapsed.current === undefined) ? "none" : undefined
+        });
+    }
 
     return (
         <section>
