a8f124d11952080e1922611142ca03c36dba21d1
Keraito <chakshunyu@gmail.com>
2021-05-10 17:05:47 +0200

    feat(barcode-scanner-web): remove support for RSS_Expanded as it's not production rdy yet

--- packages/pluggableWidgets/barcode-scanner-web/src/hooks/useCodeScanner.tsx
+++ packages/pluggableWidgets/barcode-scanner-web/src/hooks/useCodeScanner.tsx
@@ -2,8 +2,12 @@ import { useEffect, useState } from "react";
 import { BarcodeFormat, BrowserMultiFormatReader, DecodeHintType } from "@zxing/library";
 
 const hints = new Map();
+// RSS_Expanded is not production ready yet.
+const exclusions: BarcodeFormat[] = [BarcodeFormat.RSS_EXPANDED];
 // `BarcodeFormat` is a TypeScript enum. Calling `Object.values` on it returns an array of string and ints, we only want the latter.
-const formats = Object.values(BarcodeFormat).filter(format => typeof format !== "string") as BarcodeFormat[];
+const formats = Object.values(BarcodeFormat)
+    .filter((format: string | BarcodeFormat) => typeof format !== "string")
+    .filter((format: BarcodeFormat) => !exclusions.includes(format)) as BarcodeFormat[];
 hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
 
 export type CodeScannerHookError = "ERROR_CODE_SCANNER";
