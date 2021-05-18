85a7667a27b0c31c9d264f0705dd07dd74a5d64d
Keraito <chakshunyu@gmail.com>
2021-05-18 14:14:06 +0200

    feat(barcode-scanner-web): transform group into tabs for Studio

--- packages/pluggableWidgets/barcode-scanner-web/src/BarcodeScanner.editorConfig.ts
+++ packages/pluggableWidgets/barcode-scanner-web/src/BarcodeScanner.editorConfig.ts
@@ -1,5 +1,17 @@
-import { StructurePreviewProps } from "@mendix/piw-utils-internal";
+import { Properties, StructurePreviewProps, transformGroupsIntoTabs } from "@mendix/piw-utils-internal";
+import { BarcodeScannerContainerProps } from "../typings/BarcodeScannerProps";
 import BarcodeScannerSvg from "./assets/barcodescanner.svg";
+
+export function getProperties(
+    _: BarcodeScannerContainerProps,
+    defaultProperties: Properties,
+    platform: "web" | "desktop"
+): Properties {
+    if (platform === "web") {
+        transformGroupsIntoTabs(defaultProperties);
+    }
+    return defaultProperties;
+}
 
 export function getPreview(): StructurePreviewProps | null {
     return {
