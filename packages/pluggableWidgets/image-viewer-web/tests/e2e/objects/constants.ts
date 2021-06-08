f9f6344216d616befd73ca8927adbb74a3fa74ab
Leonardo de Souza <leonardo.de.souza@mendix.com>
2021-06-08 16:15:48 +0200

    test(image-viewer-web): enable the test:e2e again

---
+++ packages/pluggableWidgets/image-viewer-web/tests/e2e/objects/constants.ts
@@ -0,0 +1,4 @@
+export const dynamicImage = "https://silkui.outsystems.com/IframePreview/img/IframePreview.landscape_3.png";
+export const dynamicImageNoUrl = "emptyUrl";
+export const staticImage = "img/Atlas_UI_Resources$Content$indonesia.jpg";
+export const staticUrl = "https://picsum.photos/200/300";
