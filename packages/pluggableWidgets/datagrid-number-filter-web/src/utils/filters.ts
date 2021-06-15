c129cd9dbc5bb2457fb62b9e98afce2683c203a7
Diego Antonelli <diego.antonelli@mendix.com>
2021-06-15 15:09:56 +0200

    feat(datagrid-number-filter-web): implementing view state

---
+++ packages/pluggableWidgets/datagrid-number-filter-web/src/utils/filters.ts
@@ -0,0 +1,40 @@
+import { DefaultFilterEnum } from "../../typings/DatagridNumberFilterProps";
+import { Big } from "big.js";
+import { FilterValue } from "./provider";
+
+export declare type DefaultFilterValue = {
+    type: DefaultFilterEnum;
+    value: Big;
+};
+
+export function translateFilters(filters: FilterValue[]): DefaultFilterValue | undefined {
+    if (filters && filters.length === 1) {
+        const [filter] = filters;
+        let type: DefaultFilterEnum = "equal";
+        switch (filter.type) {
+            case ">":
+                type = "greater";
+                break;
+            case ">=":
+                type = "greaterEqual";
+                break;
+            case "=":
+                type = "equal";
+                break;
+            case "!=":
+                type = "notEqual";
+                break;
+            case "<":
+                type = "smaller";
+                break;
+            case "<=":
+                type = "smallerEqual";
+                break;
+        }
+        return {
+            type,
+            value: filter.value
+        };
+    }
+    return undefined;
+}
