48aec7bd566fac5ddd0e5de1030b8663e71fbf42
Leonardo de Souza <leonardo.de.souza@mendix.com>
2021-06-21 17:18:45 +0200

    test: fix rating e2e test

--- packages/pluggableWidgets/rating-web/tests/e2e/specs/Rating.spec.ts
+++ packages/pluggableWidgets/rating-web/tests/e2e/specs/Rating.spec.ts
@@ -1,14 +1,18 @@
+import page from "../../../../../../configs/e2e/src/pages/page";
+
 describe("Rating", () => {
     beforeAll(() => {
-        browser.url("/p/home");
+        page.open();
     });
 
     it("compares with a screenshot baseline and checks if all rating elements are rendered as expected", () => {
         browser.setWindowRect(0, 0, 1200, 900);
-        const screenshotElem = $(".mx-name-mainLayoutGrid");
+        const screenshotElem = $(".mx-name-ratingContent");
+        const ratingWidget = $(".mx-name-rating1");
         screenshotElem.waitForDisplayed({ timeout: 5000 });
-        browser.pause(2000);
+        ratingWidget.waitForDisplayed({ timeout: 5000 });
         browser.saveElement(screenshotElem, "ratingPageContent");
+
         expect(browser.checkElement(screenshotElem, "ratingPageContent")).toBeLessThan(0.4);
     });
 });
