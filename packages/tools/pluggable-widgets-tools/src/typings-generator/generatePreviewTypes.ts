bd387fe59652b782f207691401b4031e0491d6ab
Keraito <chakshunyu@gmail.com>
2021-06-23 10:29:45 +0200

    Revert "feat(pluggable-widgets-tools): update typing generator for preview types of icon and image"
    This reverts commit 9ff1b52e23e30a0e1780571241918a439709802b.

--- packages/tools/pluggable-widgets-tools/src/typings-generator/generatePreviewTypes.ts
+++ packages/tools/pluggable-widgets-tools/src/typings-generator/generatePreviewTypes.ts
@@ -29,9 +29,9 @@ function toPreviewPropType(prop: Property, generatedTypes: string[]): string {
         case "decimal":
             return "number | null";
         case "icon":
-            return "WebIcon | null";
+            return '{ type: "glyph"; iconClass: string; } | { type: "image"; imageUrl: string; } | null';
         case "image":
-            return '{ type: "static"; imageUrl: string } | { type: "dynamic"; entity: string } | null';
+            return "string";
         case "file":
             return "string";
         case "datasource":
