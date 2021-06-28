7928bd4ee332cb88909d739b63ce0a48a5a178c0
Wesley <wesley.sieses@mendix.com>
2021-06-28 12:59:17 +0200

    fix(): move types to typings directory

--- packages/jsActions/native-mobile-resources/src/camera/TakePictureAdvanced.ts
+++ packages/jsActions/native-mobile-resources/src/camera/TakePictureAdvanced.ts
@@ -15,65 +15,7 @@ import {
     launchImageLibrary
 } from "react-native-image-picker";
 import { getLocales } from "react-native-localize";
-
-type PictureSource = "camera" | "imageLibrary" | "either";
-
-type PictureQuality = "original" | "low" | "medium" | "high" | "custom";
-
-type ImagePickerV2Response = {
-    customButton: string;
-    didCancel: boolean;
-    error: string;
-    data: string;
-    uri: string;
-    origURL?: string;
-    isVertical: boolean;
-    width: number;
-    height: number;
-    fileSize: number;
-    type?: string;
-    fileName?: string;
-    path?: string;
-    latitude?: number;
-    longitude?: number;
-    timestamp?: string;
-};
-
-type ImagePickerV2Options = {
-    title?: string;
-    cancelButtonTitle?: string;
-    takePhotoButtonTitle?: string;
-    chooseFromLibraryButtonTitle?: string;
-    chooseWhichLibraryTitle?: string;
-    customButtons?: Array<{
-        name?: string;
-        title?: string;
-    }>;
-    cameraType?: "front" | "back";
-    mediaType?: "photo" | "video" | "mixed";
-    maxWidth?: number;
-    maxHeight?: number;
-    quality?: number;
-    videoQuality?: "low" | "medium" | "high";
-    durationLimit?: number;
-    rotation?: number;
-    allowsEditing?: boolean;
-    noData?: boolean;
-    storageOptions?: {
-        skipBackup?: boolean;
-        path?: string;
-        cameraRoll?: boolean;
-        waitUntilSaved?: boolean;
-        privateDirectory?: boolean;
-    };
-    permissionDenied?: {
-        title: string;
-        text: string;
-        reTryTitle: string;
-        okTitle: string;
-    };
-    tintColor?: number | string;
-};
+import { ImagePickerV2Options, ImagePickerV2Response, PictureQuality, PictureSource } from "../../typings/Camera";
 
 /**
  * Take a picture using the camera or import one from the image library on the device.
