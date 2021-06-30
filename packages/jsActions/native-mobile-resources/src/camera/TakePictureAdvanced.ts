3a45f55af7af10cca2f0d903ce09b9cd06402f90
Wesley <wesley.sieses@mendix.com>
2021-06-30 11:19:12 +0200

    fix(): add the right type for the actions return value

--- packages/jsActions/native-mobile-resources/src/camera/TakePictureAdvanced.ts
+++ packages/jsActions/native-mobile-resources/src/camera/TakePictureAdvanced.ts
@@ -40,7 +40,7 @@ export async function TakePictureAdvanced(
     pictureQuality?: PictureQuality,
     maximumWidth?: Big,
     maximumHeight?: Big
-): Promise<boolean> {
+): Promise<mendix.lib.MxObject> {
     // BEGIN USER CODE
 
     if (!picture) {
