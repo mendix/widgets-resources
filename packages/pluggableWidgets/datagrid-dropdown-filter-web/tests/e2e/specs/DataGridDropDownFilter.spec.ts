bf09d5acf5a3ed4f339540a6c17884f87116bef4
Leonardo de Souza <leonardo.de.souza@mendix.com>
2021-07-07 16:23:26 +0200

    test(datagrid-dropdown-filter-web): add e2e test to dg2 dropdown filter

---
+++ packages/pluggableWidgets/datagrid-dropdown-filter-web/tests/e2e/specs/DataGridDropDownFilter.spec.ts
@@ -0,0 +1,101 @@
+import page from "../../../../../../configs/e2e/src/pages/page";
+import datagrid from "../../../../datagrid-web/tests/e2e/objects/datagrid.widget";
+
+describe("datagrid-dropdown-filter-web", () => {
+    beforeEach(() => {
+        page.open(); // resets page
+    });
+
+    describe("using enumeration as attribute", () => {
+        it("shows the expected result", () => {
+            const grid = page.getWidget("datagrid1");
+            const dropdown = page.getElement(".dropdown-container", grid);
+
+            dropdown.click();
+
+            const dropdownOption = page.getElement(".dropdown-list > li:nth-child(1)", dropdown);
+
+            dropdownOption.click();
+
+            grid.waitUntil(
+                () => {
+                    return datagrid.getAllRows(page.getElements(".mx-name-datagrid1 .td")).length !== 12;
+                },
+                {
+                    timeout: 3000,
+                    timeoutMsg: "expected result to be different after 3s"
+                }
+            );
+            const items = page.getElements(".td", grid);
+
+            expect(datagrid.getAllRows(items)).toEqual(["10", "test", "test", "Yes", ""]);
+        });
+
+        it("shows the expected result when multiple selected items", () => {
+            const grid = page.getWidget("datagrid1");
+            const dropdown = page.getElement(".dropdown-container", grid);
+
+            dropdown.click();
+
+            const dropdownOption = page.getElement(".dropdown-list > li:nth-child(1)", dropdown);
+
+            const dropdownOption2 = page.getElement(".dropdown-list > li:nth-child(2)", dropdown);
+
+            dropdownOption.click();
+
+            dropdownOption2.click();
+
+            grid.waitUntil(
+                () => {
+                    return datagrid.getAllRows(page.getElements(".mx-name-datagrid1 .td")).length !== 12;
+                },
+                {
+                    timeout: 3000,
+                    timeoutMsg: "expected result to be different after 3s"
+                }
+            );
+            const items = page.getElements(".td", grid);
+
+            expect(datagrid.getAllRows(items)).toEqual([
+                "10",
+                "test",
+                "test",
+                "Yes",
+                "",
+                "20",
+                "test2",
+                "test2",
+                "Yes",
+                ""
+            ]);
+        });
+    });
+
+    describe("using boolean as attribute", () => {
+        it("shows the expected result", () => {
+            const grid = page.getWidget("datagrid1");
+            const dropdown = grid.$$(".dropdown-container")[1];
+
+            dropdown.click();
+
+            const dropdownOption = page.getElement(".dropdown-list > li:nth-child(3) > div", dropdown);
+
+            expect(dropdownOption.getText()).toContain("No");
+
+            dropdownOption.click();
+
+            grid.waitUntil(
+                () => {
+                    return datagrid.getAllRows(page.getElements(".mx-name-datagrid1 .td")).length !== 12;
+                },
+                {
+                    timeout: 3000,
+                    timeoutMsg: "expected result to be different after 3s"
+                }
+            );
+            const items = page.getElements(".td", grid);
+
+            expect(datagrid.getAllRows(items)).toEqual([]);
+        });
+    });
+});
