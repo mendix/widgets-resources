a26e14d6920b273d8d4f5eabe780189528aadcbb
Diego Antonelli <diego.antonelli@mendix.com>
2021-07-13 10:29:53 +0200

    feat(scroll-container-web): adding extra test cases

--- packages/pluggableWidgets/scroll-container-web/src/components/__tests__/ScrollContainer.spec.tsx
+++ packages/pluggableWidgets/scroll-container-web/src/components/__tests__/ScrollContainer.spec.tsx
@@ -7,4 +7,18 @@ describe("Scroll Container", () => {
         const scrollContainer = shallow(<ScrollContainer className="scroll-container-test" />);
         expect(scrollContainer).toMatchSnapshot();
     });
+
+    describe("with fixed width", () => {
+        it("renders correctly the structure with pixels", () => {
+            const scrollContainer = shallow(<ScrollContainer className="scroll-container-pixels" width={500} />);
+            expect(scrollContainer).toMatchSnapshot();
+        });
+
+        it("renders correctly the structure with percentage", () => {
+            const scrollContainer = shallow(
+                <ScrollContainer className="scroll-container-percentage" percentage={70} />
+            );
+            expect(scrollContainer).toMatchSnapshot();
+        });
+    });
 });
