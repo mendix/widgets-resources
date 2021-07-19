04fd79a6e563551be2b6b359a364784c550ec356
I. Isaku <16718722+IIsaku@users.noreply.github.com>
2021-07-19 16:54:44 +0200

    feat(shrinking-header-web): improve widget properties

--- packages/pluggableWidgets/shrinking-header-web/src/ShrinkingHeader.editorConfig.ts
+++ packages/pluggableWidgets/shrinking-header-web/src/ShrinkingHeader.editorConfig.ts
@@ -1,12 +1,20 @@
-import { hidePropertiesIn, hidePropertyIn, Properties } from "@mendix/piw-utils-internal";
+import { hidePropertiesIn, hidePropertyIn, Properties, transformGroupsIntoTabs } from "@mendix/piw-utils-internal";
 
 import { ShrinkingHeaderPreviewProps } from "../typings/ShrinkingHeaderProps";
 
-export function getProperties(values: ShrinkingHeaderPreviewProps, defaultProperties: Properties): Properties {
+export function getProperties(
+    values: ShrinkingHeaderPreviewProps,
+    defaultProperties: Properties,
+    platform: "web" | "desktop"
+): Properties {
     if (values.threshold) {
         hidePropertiesIn(defaultProperties, values, ["initHeight", "shrunkHeight"]);
     } else {
         hidePropertyIn(defaultProperties, values, "shrinkThreshold");
+    }
+
+    if (platform === "web") {
+        transformGroupsIntoTabs(defaultProperties);
     }
 
     return defaultProperties;
