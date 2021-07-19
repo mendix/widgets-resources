972fe7ca4fb188e29fed9459315b378ef40a1efc
I. Isaku <16718722+IIsaku@users.noreply.github.com>
2021-07-19 16:15:04 +0200

    feat(shrinking-header-web): override mx-placeholder style with CSS
    - Override mx-placeholder overflow-x hidden through CSS to not limit it to inline styles only.
    - Refactor root component to contain common props in one constant.

--- packages/pluggableWidgets/shrinking-header-web/src/ShrinkingHeader.tsx
+++ packages/pluggableWidgets/shrinking-header-web/src/ShrinkingHeader.tsx
@@ -1,4 +1,4 @@
-import { createElement, ReactElement, useCallback, useEffect, useState } from "react";
+import { createElement, ReactElement } from "react";
 
 import { ShrinkingHeaderThreshold } from "./components/ShrinkingHeaderThreshold";
 import { ShrinkingHeaderLinear } from "./components/ShrinkingHeaderLinear";
@@ -6,49 +6,9 @@ import { ShrinkingHeaderLinear } from "./components/ShrinkingHeaderLinear";
 import { ShrinkingHeaderContainerProps } from "../typings/ShrinkingHeaderProps";
 
 export default function ShrinkingHeader(props: ShrinkingHeaderContainerProps): ReactElement | null {
-    const {
-        name,
-        tabIndex,
-        style,
-        class: className,
-        content,
-        threshold,
-        shrinkThreshold,
-        initHeight,
-        shrunkHeight
-    } = props;
+    const { threshold, shrinkThreshold, initHeight, shrunkHeight, ...commonProps } = props;
 
     const actualShrinkThreshold = shrinkThreshold.value ? Number(shrinkThreshold.value.toString()) : undefined;
-
-    const [shrinkingHeaderThresholdElement, setShrinkingHeaderThresholdElement] = useState<HTMLDivElement>();
-
-    const setShrinkingHeaderThreshold = useCallback(
-        (node: HTMLDivElement | null) => {
-            setShrinkingHeaderThresholdElement(node ?? undefined);
-        },
-        [setShrinkingHeaderThresholdElement]
-    );
-
-    /*
-        Position "sticky" behaviour breaks when navigating from another page in the Mendix application.
-        This is due to the css rule `overflow-x: hidden` which is applied to `div class="mx-placeholder" />`.
-        This useEffect iterates through all ancestor elements and removes this rule, except for the body element.
-     */
-    useEffect(() => {
-        if (shrinkingHeaderThresholdElement) {
-            const removeOverflowRules = function (element: HTMLElement): void {
-                if (element.tagName !== "body") {
-                    element.style.overflow = "";
-                }
-                const parentElement = element.parentElement;
-                if (parentElement) {
-                    removeOverflowRules(parentElement);
-                }
-            };
-
-            removeOverflowRules(shrinkingHeaderThresholdElement);
-        }
-    }, [shrinkingHeaderThresholdElement]);
 
     if (!actualShrinkThreshold) {
         return null;
@@ -57,12 +17,8 @@ export default function ShrinkingHeader(props: ShrinkingHeaderContainerProps): R
     if (threshold) {
         return (
             <ShrinkingHeaderThreshold
-                rootElementRef={setShrinkingHeaderThreshold}
-                name={name}
-                tabIndex={tabIndex}
-                style={style}
-                className={className}
-                content={content}
+                {...commonProps}
+                className={commonProps.class}
                 shrinkThreshold={actualShrinkThreshold}
             />
         );
@@ -70,12 +26,8 @@ export default function ShrinkingHeader(props: ShrinkingHeaderContainerProps): R
 
     return (
         <ShrinkingHeaderLinear
-            rootElementRef={setShrinkingHeaderThreshold}
-            name={name}
-            tabIndex={tabIndex}
-            style={style}
-            className={className}
-            content={content}
+            {...commonProps}
+            className={commonProps.class}
             initHeight={initHeight}
             shrunkHeight={shrunkHeight}
         />
