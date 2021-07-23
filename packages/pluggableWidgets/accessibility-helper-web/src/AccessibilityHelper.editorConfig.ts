07f4d6a2e94f895f28289ae11fb3b0744a7ab8bc
Keraito <chakshunyu@gmail.com>
2021-07-23 12:31:40 +0200

    feat(accessibility-helper-web): add structure preview

--- packages/pluggableWidgets/accessibility-helper-web/src/AccessibilityHelper.editorConfig.ts
+++ packages/pluggableWidgets/accessibility-helper-web/src/AccessibilityHelper.editorConfig.ts
@@ -1,4 +1,13 @@
-import { hidePropertyIn, Properties, Problem } from "@mendix/piw-utils-internal";
+import {
+    hidePropertyIn,
+    Properties,
+    Problem,
+    StructurePreviewProps,
+    DropZoneProps,
+    RowLayoutProps,
+    // ContainerProps,
+    TextProps
+} from "@mendix/piw-utils-internal";
 import { AttributesListPreviewType, AccessibilityHelperPreviewProps } from "../typings/AccessibilityHelperProps";
 
 const PROHIBITED_ATTRIBUTES = ["class", "style", "widgetid", "data-mendix-id"];
@@ -28,4 +37,58 @@ export function check(values: AccessibilityHelperPreviewProps): Problem[] {
     });
 
     return errors;
+}
+
+export function getPreview(values: AccessibilityHelperPreviewProps): StructurePreviewProps | null {
+    return {
+        type: "Container",
+        borders: true,
+        borderWidth: 1,
+        children: [
+            {
+                type: "RowLayout",
+                backgroundColor: values.content.widgetCount > 0 ? undefined : "#F8F8F8",
+                children: [
+                    {
+                        type: "DropZone",
+                        property: values.content,
+                        placeholder: "Place content here"
+                    } as DropZoneProps
+                ]
+            } as RowLayoutProps,
+            {
+                type: "RowLayout",
+                columnSize: "grow",
+                children: [
+                    {
+                        type: "Container",
+                        grow: 1,
+                        children: []
+                    },
+                    {
+                        type: "Container",
+                        grow: 0,
+                        padding: 8,
+                        children: [
+                            {
+                                type: "Text",
+                                bold: true,
+                                content: buildCaption(values)
+                            } as TextProps
+                        ]
+                    }
+                ]
+            }
+        ]
+    };
+}
+
+function buildCaption(values: AccessibilityHelperPreviewProps): string {
+    if (values.targetSelector.length > 0) {
+        if (values.attributesList.length > 0) {
+            return `Target ${values.targetSelector}`;
+        }
+        return "Set [HTML Attributes list]";
+    }
+    return "Target [Target selector]";
 }
