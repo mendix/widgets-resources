7b2561db4de1f56e12c515969d1ad12e250cf472
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2021-07-23 11:35:30 +0200

    feat(sidebar-web): register same sidebar name error handling

--- packages/pluggableWidgets/sidebar-web/src/components/Sidebar.tsx
+++ packages/pluggableWidgets/sidebar-web/src/components/Sidebar.tsx
@@ -14,10 +14,16 @@ export function Sidebar(props: PropsWithChildren<SidebarProps>): ReactElement {
     const [expanded, setExpanded] = useState(false);
 
     useEffect(() => {
-        const unregisterSidebar = registerSidebar({
-            name: props.name,
-            toggleExpanded: () => setExpanded(prevExpanded => !prevExpanded)
-        });
+        let unregisterSidebar: () => void;
+
+        try {
+            unregisterSidebar = registerSidebar({
+                name: props.name,
+                toggleExpanded: () => setExpanded(prevExpanded => !prevExpanded)
+            });
+        } catch (e) {
+            console.error(e); // TODO: show error in an alert
+        }
 
         return () => unregisterSidebar();
     }, [props.name]);
@@ -43,6 +49,13 @@ function registerSidebar(registration: SidebarRegistration): () => void {
 
     if (!(window as any)[registerLocation]) {
         (window as any)[registerLocation] = new Map();
+    }
+
+    // Widget names aren't unique, so we need to check
+    if ((window as any)[registerLocation].has(registration.name)) {
+        throw Error(
+            "There are multiple sidebar widgets on this page that have the same name. Please give every sidebar a unique name."
+        );
     }
 
     (window as any)[registerLocation].set(registration.name, registration.toggleExpanded);
