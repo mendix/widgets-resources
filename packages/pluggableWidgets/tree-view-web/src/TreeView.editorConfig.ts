d83447b3b727421d6f301dbfbc670683eba573f7
Keraito <chakshunyu@gmail.com>
2021-08-27 11:47:15 +0200

    feat(tree-view-web): implement structure preview

--- packages/pluggableWidgets/tree-view-web/src/TreeView.editorConfig.ts
+++ packages/pluggableWidgets/tree-view-web/src/TreeView.editorConfig.ts
@@ -1,11 +1,17 @@
 import {
+    ContainerProps,
+    DropZoneProps,
     hidePropertiesIn,
     hidePropertyIn,
     Properties,
+    RowLayoutProps,
     StructurePreviewProps,
+    TextProps,
     transformGroupsIntoTabs
 } from "@mendix/piw-utils-internal";
-import { TreeViewPreviewProps } from "../typings/TreeViewProps";
+import { HeaderTypeEnum, TreeViewPreviewProps } from "../typings/TreeViewProps";
+
+import ChevronSVG from "./assets/ChevronStructurePreview.svg";
 
 export function getProperties(
     values: TreeViewPreviewProps,
@@ -42,7 +48,113 @@ export function getProperties(
     return defaultProperties;
 }
 
-export function getPreview(): StructurePreviewProps | null {
-    // TODO:
-    return null;
+export function getPreview(values: TreeViewPreviewProps): StructurePreviewProps | null {
+    const titleHeader: RowLayoutProps = {
+        type: "RowLayout",
+        columnSize: "fixed",
+        backgroundColor: "#daeffb",
+        borders: true,
+        borderWidth: 1,
+        children: [
+            {
+                type: "Container",
+                padding: 4,
+                children: [
+                    {
+                        type: "Text",
+                        content: "Tree node",
+                        fontColor: "#2074c8"
+                    }
+                ]
+            }
+        ]
+    };
+    const treeViewHeader: RowLayoutProps = {
+        type: "RowLayout",
+        backgroundColor: "#F5F5F5",
+        borders: true,
+        borderWidth: 1,
+        children: [
+            {
+                type: "RowLayout",
+                columnSize: "grow",
+                padding: 4,
+                children: [
+                    ...(values.showIcon === "left" ? [getChevronIconPreview(values.headerType)] : []),
+                    values.headerType === "text"
+                        ? ({
+                              type: "Text",
+                              fontSize: 12,
+                              bold: true,
+                              grow: 100,
+                              content: values.headerCaption || "Header caption"
+                          } as TextProps)
+                        : ({
+                              type: "RowLayout",
+                              borders: true,
+                              backgroundColor: "#F8F8F8",
+                              children: [
+                                  {
+                                      type: "DropZone",
+                                      property: values.headerContent,
+                                      placeholder: "Node header: Place widgets here"
+                                  } as DropZoneProps
+                              ]
+                          } as RowLayoutProps),
+
+                    ...(values.showIcon === "right" ? [getChevronIconPreview(values.headerType)] : [])
+                ]
+            }
+        ]
+    };
+
+    const getTreeViewContent: () => StructurePreviewProps[] = () =>
+        values.hasChildren
+            ? [
+                  {
+                      type: "RowLayout",
+                      borders: true,
+                      borderWidth: 1,
+                      children: [
+                          {
+                              type: "Container",
+                              padding: 6,
+                              children: [
+                                  {
+                                      type: "Container",
+                                      borders: true,
+                                      children: [
+                                          {
+                                              type: "DropZone",
+                                              property: values.children,
+                                              placeholder: "Node body: Place widgets here"
+                                          } as DropZoneProps
+                                      ]
+                                  }
+                              ]
+                          }
+                      ]
+                  } as RowLayoutProps
+              ]
+            : [];
+
+    return {
+        type: "Container",
+        children: [titleHeader, treeViewHeader, ...getTreeViewContent()]
+    } as ContainerProps;
+}
+
+function getChevronIconPreview(headerType: HeaderTypeEnum): ContainerProps {
+    return {
+        type: "Container",
+        grow: 1,
+        padding: headerType === "text" ? 8 : 27,
+        children: [
+            {
+                type: "Image",
+                document: decodeURIComponent(ChevronSVG.replace("data:image/svg+xml,", "")),
+                width: 14
+            }
+        ]
+    };
 }
