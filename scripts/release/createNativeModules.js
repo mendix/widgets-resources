8c9c291a0d42649cd194ced528e9ecef85b11d7a
Jordan Hunt <jordan.hunt@mendix.com>
2021-09-02 10:49:04 +0200

    fix(): add secrets to native module release script

--- scripts/release/createNativeModules.js
+++ scripts/release/createNativeModules.js
@@ -38,8 +38,8 @@ async function createNMRModule() {
         testProject: { githubUrl, branchName },
         repository: { url }
     } = require(pkgPath);
-    await execShellCommand('git config user.name "Native Content Automation"');
-    await execShellCommand('git config user.email "widgets@mendix.com"');
+    await execShellCommand(`git config user.name "${process.env.GH_NAME_NATIVE_AUTOMATION}"`);
+    await execShellCommand(`git config user.email "${process.env.GH_EMAIL}"`);
     await execShellCommand(
         `git remote set-url origin https://${process.env.GH_USERNAME}:${process.env.GH_PAT}@${url.replace(
             "https://",
