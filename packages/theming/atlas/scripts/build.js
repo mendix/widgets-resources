8cdafc6097fec2587f39073b13596cc26fbffd9d
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2021-09-09 10:58:15 +0200

    chore(atlas-ui): close readline in build script

--- packages/theming/atlas/scripts/build.js
+++ packages/theming/atlas/scripts/build.js
@@ -76,8 +76,10 @@ async function main() {
 }
 
 function closeOnSigint(watcher) {
+    let rl;
+
     if (process.platform === "win32") {
-        const rl = require("readline").createInterface({
+        rl = require("readline").createInterface({
             input: process.stdin,
             output: process.stdout
         });
@@ -88,6 +90,7 @@ function closeOnSigint(watcher) {
     }
 
     process.on("SIGINT", () => {
+        rl?.close();
         watcher.close();
     });
 }
