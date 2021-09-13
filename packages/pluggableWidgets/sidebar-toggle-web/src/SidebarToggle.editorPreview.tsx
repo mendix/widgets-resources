8d9dbb641c2ccde8a209f9656b6563169fce78f4
Keraito <chakshunyu@gmail.com>
2021-09-13 15:24:35 +0200

    feat(sidebar-toggle-web): remove fix to accommodate incorrect preview icon typings

--- packages/pluggableWidgets/sidebar-toggle-web/src/SidebarToggle.editorPreview.tsx
+++ packages/pluggableWidgets/sidebar-toggle-web/src/SidebarToggle.editorPreview.tsx
@@ -2,15 +2,15 @@ import { createElement, ReactElement } from "react";
 
 import { SidebarTogglePreviewProps } from "../typings/SidebarToggleProps";
 import { Toggle } from "./components/Toggle";
-import { WebIcon } from "mendix";
 import { parseStyle } from "@mendix/piw-utils-internal";
+import { mapPreviewIconToWebIcon } from "@mendix/piw-utils-internal/components/web";
 
 export function preview(props: SidebarTogglePreviewProps): ReactElement | null {
     return (
         <Toggle
             caption={props.caption}
             className={props.class}
-            icon={props.icon as WebIcon}
+            icon={mapPreviewIconToWebIcon(props.icon)}
             render={props.renderMode}
             role={props.role}
             style={parseStyle(props.style)}
