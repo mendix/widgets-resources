47cae576663b7e9a60e77d32f53eb0270fdf5d5b
Leonardo de Souza <leonardo.de.souza@mendix.com>
2021-10-12 17:28:32 +0200

    test(pluggable-widgets-tools): disable in the jest native config

--- packages/tools/pluggable-widgets-tools/test-config/jest.native.config.js
+++ packages/tools/pluggable-widgets-tools/test-config/jest.native.config.js
@@ -28,7 +28,7 @@ module.exports = {
         "mendix/components/native/Image": join(__dirname, "__mocks__/NativeImage"),
         "mendix/filters/builders": join(__dirname, "__mocks__/FilterBuilders")
     },
-    collectCoverage: true,
+    collectCoverage: !process.env.CI,
     coverageDirectory: "<rootDir>/../dist/coverage"
 };
 
