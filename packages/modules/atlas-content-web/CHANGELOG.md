b99809b3e18a89801253ec9ce7d0e6130a9a9636
Diego Antonelli <diego.antonelli@mendix.com>
2021-10-25 13:21:38 +0200

    feat(atlas-content-web): add automation for atlas web content

---
+++ packages/modules/atlas-content-web/CHANGELOG.md
@@ -0,0 +1,27 @@
+# Changelog
+All notable changes to this widget will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [3.0.6] - 2021-09-28
+
+### Changed
+- We updated all widgets
+- We changed building blocks to contain new Image widget
+
+## [3.0.5] - 2021-08-03
+
+### Changed
+- Changed the Building blocks documentation URLs to Atlas 3
+
+## [3.0.4] - 2021-07-27
+
+### Changed
+- We fixed a style issue in Wizard's building blocks.
+
+## [3.0.3] - 2021-06-29
+
+### Changed
+- We defined the module as UI Resource.
---
+++ packages/modules/atlas-content-web/scripts/build.ts
@@ -0,0 +1,81 @@
+import concurrently from "concurrently";
+import { join } from "path";
+import { rm, mkdir } from "shelljs";
+
+const repoRoot = join(__dirname, "../../../../");
+
+main().catch(e => {
+    console.error(e);
+    process.exit(1);
+});
+
+async function main(): Promise<void> {
+    let mode = "build";
+    if (process.argv.includes("start")) {
+        mode = "start";
+    } else if (process.argv.includes("release")) {
+        mode = "release";
+    }
+
+    let outputDir;
+
+    if (mode === "build" || mode === "start") {
+        const MX_PROJECT_PATH = process.env.MX_PROJECT_PATH; // should be an absolute path.
+        outputDir = MX_PROJECT_PATH ? MX_PROJECT_PATH : join(__dirname, "../tests/testProject");
+
+        const toRemoveDirs = [join(outputDir, "themesource/datawidgets")];
+        rm("-rf", toRemoveDirs);
+        console.info(`Ensured the directories ${toRemoveDirs.join(", ")} are removed from your Mendix project`);
+    } else if (mode === "release") {
+        outputDir = join(__dirname, "../dist");
+
+        rm("-rf", outputDir);
+        console.info(`Ensured the directory ${outputDir} is removed`);
+    }
+
+    if (outputDir) {
+        // when targeting a networked windows drive, the cmds executed by concurrently run into a race condition when
+        // creating directories. create them here to avoid the error.
+        mkdir("-p", join(outputDir, "themesource/atlas_web_content"));
+
+        await copyStylesAndAssets(mode === "start", outputDir);
+    }
+}
+
+async function copyStylesAndAssets(watchMode: boolean, destination: string): Promise<void> {
+    console.info(`Copying styles and assets...`);
+    const watchArg = watchMode ? "--watch" : "";
+
+    try {
+        await concurrently(
+            [
+                {
+                    name: "web-themesource-content",
+                    command: `copy-and-watch ${watchArg} "${join(
+                        repoRoot,
+                        "packages/theming/atlas",
+                        "src/themesource/atlas_web_content/web/**/*"
+                    )}" "${join(destination, "themesource/atlas_web_content/web")}"`
+                },
+                {
+                    name: "public-themesource-content",
+                    command: `copy-and-watch ${watchArg} "${join(
+                        repoRoot,
+                        "packages/theming/atlas",
+                        "src/themesource/atlas_web_content/public/**/*"
+                    )}" "${join(destination, "themesource/atlas_web_content/public")}"`
+                }
+            ],
+            {
+                killOthers: ["failure"]
+            }
+        );
+
+        console.log("Copying styles and assets has completed successfully");
+    } catch (commands) {
+        const commandInfo = commands.map(
+            (command: concurrently.ExitInfos) => `{ name: ${command.command.name}, exit code: ${command.exitCode}}`
+        );
+        throw new Error(`One or more commands failed:\n${commandInfo.join("\n")}`);
+    }
+}
