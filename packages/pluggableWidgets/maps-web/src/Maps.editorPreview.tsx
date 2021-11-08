e77b825b7b104d07cb03180559c195132eca7128
Raffi Hovhannisyan <raffi.hovhannisyan@mendix.com>
2021-11-08 10:57:39 +0100

    fix(maps-web): use only google image, remove checks based on provider type

--- packages/pluggableWidgets/maps-web/src/Maps.editorPreview.tsx
+++ packages/pluggableWidgets/maps-web/src/Maps.editorPreview.tsx
@@ -1,36 +1,18 @@
 import { createElement, ReactNode, Fragment, ReactElement } from "react";
 import { MapsPreviewProps } from "../typings/MapsProps";
 import { Alert } from "@mendix/piw-utils-internal/components/web";
-import GoogleMapsSVG from "./assets/GoogleMaps.svg";
-import MapboxSVG from "./assets/Mapbox.svg";
-import OpenStreetMapSVG from "./assets/OpenStreetMap.svg";
-import HereMapsSVG from "./assets/HereMaps.svg";
+import GoogleMapSvg from "./assets/googlemaps_large.svg";
 
 declare function require(name: string): string;
 
 export const preview = (props: MapsPreviewProps): ReactNode => {
     const renderImagePreview = (): ReactElement => {
-        let image;
-        switch (props.mapProvider) {
-            case "googleMaps":
-                image = GoogleMapsSVG;
-                break;
-            case "mapBox":
-                image = MapboxSVG;
-                break;
-            case "openStreet":
-                image = OpenStreetMapSVG;
-                break;
-            case "hereMaps":
-                image = HereMapsSVG;
-                break;
-        }
-
         const style = {
-            backgroundImage: `url("${image}")`,
+            backgroundImage: `url("${GoogleMapSvg}")`,
             backgroundPosition: "center, center",
             backgroundSize: "cover",
-            width: "375px",
+            backgroundRepeat: "no-repeat",
+            width: "100%",
             height: "375px"
         };
         return <div style={style} />;
