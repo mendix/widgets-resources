0c1d9a6005cc89a42c5d8cfcd3a6f8269f41755c
Raffi Hovhannisyan <raffi.hovhannisyan@mendix.com>
2021-11-16 17:28:13 +0100

    fix(maps-web): added class and styles from appearance on container div

--- packages/pluggableWidgets/maps-web/src/Maps.editorPreview.tsx
+++ packages/pluggableWidgets/maps-web/src/Maps.editorPreview.tsx
@@ -1,7 +1,12 @@
-import { createElement, ReactNode, Fragment } from "react";
+import { createElement, ReactNode } from "react";
 import { MapsPreviewProps } from "../typings/MapsProps";
 import { Alert } from "@mendix/piw-utils-internal/components/web";
 import GoogleMapSvg from "./assets/GoogleMapsDesign.svg";
+import { parseStyle } from "@mendix/piw-utils-internal";
+
+interface PreviewProps extends Omit<MapsPreviewProps, "class"> {
+    className: string;
+}
 
 declare function require(name: string): string;
 
@@ -13,16 +18,17 @@ const imagePreviewStyles = {
     width: "100%",
     height: "375px"
 };
-export const preview = (props: MapsPreviewProps): ReactNode => {
+
+export const preview = (props: PreviewProps): ReactNode => {
     return (
-        <Fragment>
+        <div className={props.className} style={parseStyle(props.style)}>
             {(props.mapProvider === "mapBox" || props.mapProvider === "hereMaps") && (
                 <Alert bootstrapStyle="warning">
                     Provider unavailable without API Key, preview is not possible at the moment
                 </Alert>
             )}
             <div style={imagePreviewStyles} />
-        </Fragment>
+        </div>
     );
 };
 
