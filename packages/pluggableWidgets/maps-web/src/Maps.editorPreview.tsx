5f061dbf9502d23861acfc5e9f29e6a48e74cbc1
Raffi Hovhannisyan <raffi.hovhannisyan@mendix.com>
2021-11-16 10:50:00 +0100

    fix(maps-web): simplify styling method

--- packages/pluggableWidgets/maps-web/src/Maps.editorPreview.tsx
+++ packages/pluggableWidgets/maps-web/src/Maps.editorPreview.tsx
@@ -1,23 +1,19 @@
-import { createElement, ReactNode, Fragment, ReactElement } from "react";
+import { createElement, ReactNode, Fragment } from "react";
 import { MapsPreviewProps } from "../typings/MapsProps";
 import { Alert } from "@mendix/piw-utils-internal/components/web";
 import GoogleMapSvg from "./assets/GoogleMapsDesign.svg";
 
 declare function require(name: string): string;
 
+const imagePreviewStyles = {
+    backgroundImage: `url("${GoogleMapSvg}")`,
+    backgroundPosition: "center, center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    width: "100%",
+    height: "375px"
+};
 export const preview = (props: MapsPreviewProps): ReactNode => {
-    const renderImagePreview = (): ReactElement => {
-        const style = {
-            backgroundImage: `url("${GoogleMapSvg}")`,
-            backgroundPosition: "center, center",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            width: "100%",
-            height: "375px"
-        };
-        return <div style={style} />;
-    };
-
     return (
         <Fragment>
             {(props.mapProvider === "mapBox" || props.mapProvider === "hereMaps") && (
@@ -25,7 +21,7 @@ export const preview = (props: MapsPreviewProps): ReactNode => {
                     Provider unavailable without API Key, preview is not possible at the moment
                 </Alert>
             )}
-            {renderImagePreview()}
+            <div style={imagePreviewStyles} />
         </Fragment>
     );
 };
