175624850171a02558d00533899cc4e4c9b9d4a8
Illia Obukhau <vyusfire@gmail.com>
2021-11-24 14:19:54 +0100

    fix(bar-chart-web): hide custom properties in advanced and dev mode

--- packages/pluggableWidgets/bar-chart-web/src/BarChart.editorConfig.ts
+++ packages/pluggableWidgets/bar-chart-web/src/BarChart.editorConfig.ts
@@ -1,12 +1,18 @@
 import { BarChartPreviewProps } from "../typings/BarChartProps";
-import { hideNestedPropertiesIn, Properties } from "@mendix/piw-utils-internal";
+import {
+    hideNestedPropertiesIn,
+    hidePropertiesIn,
+    hidePropertyIn,
+    Properties,
+    transformGroupsIntoTabs
+} from "@mendix/piw-utils-internal";
 
 export function getProperties(
     values: BarChartPreviewProps,
-    defaultProperties: Properties
-    // platform: "web" | "desktop"
+    defaultProperties: Properties,
+    platform: "web" | "desktop"
 ): Properties {
-    // const showAdvancedOptions = values.developerMode !== "basic";
+    const showAdvancedOptions = values.developerMode !== "basic";
     values.series.forEach((dataSeries, index) => {
         if (dataSeries.dataSet === "static") {
             hideNestedPropertiesIn(defaultProperties, values, "series", index, [
@@ -24,7 +30,21 @@ export function getProperties(
                 "staticName"
             ]);
         }
+
+        if (!showAdvancedOptions) {
+            hidePropertyIn(defaultProperties, values, "series", index, "customSeriesOptions");
+        }
     });
+
+    if (platform === "web") {
+        hidePropertyIn(defaultProperties, values, "developerMode");
+
+        transformGroupsIntoTabs(defaultProperties);
+    } else {
+        if (!showAdvancedOptions) {
+            hidePropertiesIn(defaultProperties, values, ["customLayout", "customConfigurations"]);
+        }
+    }
 
     return defaultProperties;
 }
