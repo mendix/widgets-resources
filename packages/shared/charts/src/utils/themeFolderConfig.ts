e28d6898d2e4d59f9e7f34bf97077904c5dd012c
Keraito <chakshunyu@gmail.com>
2021-11-26 15:28:12 +0100

    feat(shared-charts): check if window is present before requesting com.mendix.charts.json

--- packages/shared/charts/src/utils/themeFolderConfig.ts
+++ packages/shared/charts/src/utils/themeFolderConfig.ts
@@ -1,5 +1,8 @@
 export async function fetchThemeFolderConfigFile(): Promise<any> {
     try {
+        if (!window) {
+            return null;
+        }
         const response = await window.fetch(`${window.mx.remoteUrl}com.mendix.charts.json`);
         if (response.ok) {
             return await response.json();
