5caec237cce961569f49b19549dd34791d3f9d38
Raffi Hovhannisyan <raffi.hovhannisyan@mendix.com>
2021-11-29 10:28:36 +0100

    feat(rich-text-web): move editor instance to another component

--- packages/pluggableWidgets/rich-text-web/src/components/RichText.tsx
+++ packages/pluggableWidgets/rich-text-web/src/components/RichText.tsx
@@ -1,6 +1,6 @@
-import { createElement, ReactElement, useState, useEffect } from "react";
-import { useCKEditor, CKEditorConfig, CKEditorHookProps, CKEditorEventAction, CKEditorType } from "ckeditor4-react";
-import { getPreset, defineEnterMode, addPlugin } from "../utils/ckeditorConfigs";
+import { createElement, ReactElement, useState, useMemo } from "react";
+import { useCKEditor, CKEditorHookProps, CKEditorEventAction, CKEditorType } from "ckeditor4-react";
+import { defineEnterMode } from "../utils/ckeditorConfigs";
 import classNames from "classnames";
 import {
     ReadOnlyStyleEnum,
@@ -30,9 +30,24 @@ interface RichTextProps {
     plugins: string[];
 }
 
+function CKInstance({ config }: { config: CKEditorHookProps<string> }) {
+    useCKEditor(config);
+    return null;
+}
+
 export const RichText = (props: RichTextProps): ReactElement => {
     const { editorType, preset, plugins, enterMode, shiftEnterMode, readOnly, readOnlyStyle, value } = props;
     const [element, setElement] = useState<HTMLElement | null>(null);
+    const [currentPreset, setCurrentPreset] = useState(false);
+    const onClick = () => setCurrentPreset(prev => !prev);
+    const preset1 = [
+        { name: "document", groups: ["mode", "document", "doctools"] },
+        { name: "clipboard", groups: ["clipboard", "undo"] }
+    ];
+    const preset2 = [{ name: "clipboard", groups: ["clipboard", "undo"] }];
+
+    console.log(plugins, readOnly, readOnlyStyle, preset);
+    // const [element, setElement] = useState<HTMLElement | null>(null);
     const ckeditorConfig: CKEditorHookProps<any> = {
         element,
         type: editorType,
@@ -40,7 +55,8 @@ export const RichText = (props: RichTextProps): ReactElement => {
             readOnly: false,
             enterMode: defineEnterMode(enterMode),
             shiftEnterMode: defineEnterMode(shiftEnterMode),
-            disableNativeSpellChecker: props.spellChecker === "no"
+            disableNativeSpellChecker: props.spellChecker === "no",
+            toolbarGroups: currentPreset ? preset1 : preset2
         },
         dispatchEvent: ({ type }) => {
             if (type === CKEditorEventAction.beforeLoad) {
@@ -54,42 +70,53 @@ export const RichText = (props: RichTextProps): ReactElement => {
         initContent: value,
         subscribeTo: ["instanceReady", "beforeLoad", "loaded"]
     };
-    const editorPreset: CKEditorConfig | null = getPreset(preset);
-    const { editor, status } = useCKEditor(ckeditorConfig);
-    useEffect(() => {
-        if (!editorPreset) {
-            const config: CKEditorConfig = {};
-            const { toolbarConfig, toolbarGroup } = props;
-            if (toolbarConfig === "basic") {
-                config.toolbarGroups = toolbarGroup;
-            } else {
-                config.toolbar = [...toolbarGroup];
-            }
-        } else {
-            Object.assign(ckeditorConfig.config, editorPreset);
-        }
-        if (status === "loaded") {
-            // editor?.window.CKEDITOR.replace(props.id, { element, config: { toolbar: [] } });
-            if (readOnly) {
-                editor?.setReadOnly(readOnly);
+    // const editorPreset: CKEditorConfig | null = getPreset(preset);
+    const key = useMemo(() => Date.now(), [ckeditorConfig]);
+    console.log(key);
 
-                switch (readOnlyStyle) {
-                    case "bordered":
-                        editor.removeToolbar();
-                }
-            }
-        }
-    }, [props, status]);
+    // useLayoutEffect();
 
-    useEffect(() => {
-        if (plugins.length) {
-            // @ts-ignore
-            plugins.forEach(plugin => addPlugin(plugin, ckeditorConfig));
-        }
-    }, [plugins]);
+    // useEffect(() => {
+    //     if (!editorPreset) {
+    //         const config: CKEditorConfig = {};
+    //         const { toolbarConfig, toolbarGroup } = props;
+    //         if (toolbarConfig === "basic") {
+    //             config.toolbarGroups = toolbarGroup;
+    //         } else {
+    //             config.toolbar = [...toolbarGroup];
+    //         }
+    //     } else {
+    //         Object.assign(ckeditorConfig.config, editorPreset);
+    //     }
+    // }, [props]);
+
+    // useEffect(() => {
+    //     if (status === "loaded") {
+    //         console.log("Toolbar", editor);
+    //         // @ts-ignore
+    //
+    //         // editor?.window.CKEDITOR.replace(props.id, { element, config: { toolbar: [] } });
+    //         if (readOnly) {
+    //             editor?.setReadOnly(readOnly);
+    //
+    //             switch (readOnlyStyle) {
+    //                 case "bordered":
+    //                 // editor.removeToolbar();
+    //             }
+    //         }
+    //     }
+    // }, [status, readOnly]);
+    // useEffect(() => {
+    //     if (plugins.length) {
+    //         // @ts-ignore
+    //         plugins.forEach(plugin => addPlugin(plugin, ckeditorConfig));
+    //     }
+    // }, [plugins]);
     return (
         <div className={classNames(props.class, "widget-rich-text")}>
-            <div ref={setElement} id={props.id} />
+            <button onClick={onClick}>Change preset</button>
+            <div id="editor-sdfjvsdlfvn" ref={setElement} />
+            {element && <CKInstance key={currentPreset.toString()} config={ckeditorConfig} />}
         </div>
     );
 };
