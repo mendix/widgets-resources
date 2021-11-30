2e6b974c41ac7b8fc96380cede9860db68bd99a4
Diego Antonelli <diego.antonelli@mendix.com>
2021-11-30 11:10:23 +0100

    fix(switch-web): removing usage of style and class from code

--- packages/pluggableWidgets/switch-web/src/Switch.editorPreview.tsx
+++ packages/pluggableWidgets/switch-web/src/Switch.editorPreview.tsx
@@ -1,22 +1,10 @@
 import { createElement, ReactElement } from "react";
-import { parseStyle } from "@mendix/piw-utils-internal";
 
 import { SwitchPreviewProps } from "../typings/SwitchProps";
 import Switch from "./components/Switch";
 
 export function preview(props: SwitchPreviewProps): ReactElement {
-    const { className, style } = props;
-
-    return (
-        <Switch
-            id="switch-preview"
-            validation={undefined}
-            editable={!props.readOnly ?? true}
-            isChecked
-            class={className}
-            style={parseStyle(style)}
-        />
-    );
+    return <Switch id="switch-preview" validation={undefined} editable={!props.readOnly ?? true} isChecked />;
 }
 
 export function getPreviewCss() {
