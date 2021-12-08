9e00af86a221eefbd7f06f338f641a934474f583
Diego Antonelli <diego.antonelli@mendix.com>
2021-12-08 19:14:23 +0100

    feat(switch-web): add dark structure preview

--- packages/pluggableWidgets/switch-web/src/Switch.editorConfig.ts
+++ packages/pluggableWidgets/switch-web/src/Switch.editorConfig.ts
@@ -1,11 +1,12 @@
 import { StructurePreviewProps } from "@mendix/piw-utils-internal";
 
 import checkedSVG from "./assets/checked.svg";
+import checkedSVGDark from "./assets/checked-dark.svg";
 
-export function getPreview(): StructurePreviewProps {
+export function getPreview(_: StructurePreviewProps, isDarkMode: boolean): StructurePreviewProps {
     return {
         type: "Image",
-        document: decodeURIComponent(checkedSVG.replace("data:image/svg+xml,", "")),
+        document: decodeURIComponent((isDarkMode ? checkedSVGDark : checkedSVG).replace("data:image/svg+xml,", "")),
         width: 80
     };
 }
