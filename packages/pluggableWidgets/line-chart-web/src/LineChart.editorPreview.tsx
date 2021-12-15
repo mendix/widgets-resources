f69b9159483348b157927b59f8679f7952b93698
Diego Antonelli <diego.antonelli@mendix.com>
2021-12-15 15:34:43 +0100

    test: remove editorPreview files from charts

--- packages/pluggableWidgets/line-chart-web/src/LineChart.editorPreview.tsx
+++ packages/pluggableWidgets/line-chart-web/src/LineChart.editorPreview.tsx
@@ -1,27 +1,6 @@
-import { createElement } from "react";
-import { ChartWidget } from "@mendix/shared-charts";
+import { ReactNode } from "react";
 
-export function preview() {
+export function preview(): ReactNode {
     // TODO:
-    return (
-        <ChartWidget
-            className="widget-chart-line"
-            data={[{ x: [], y: [], customSeriesOptions: "" }]}
-            widthUnit={"percentage"}
-            width={0}
-            heightUnit={"pixels"}
-            height={0}
-            showLegend={false}
-            xAxisLabel={undefined}
-            yAxisLabel={undefined}
-            gridLinesMode={"both"}
-            showSidebarEditor={false}
-            customLayout={""}
-            customConfig={""}
-            configOptions={{}}
-            layoutOptions={{}}
-            seriesOptions={{}}
-            type="LineChart"
-        />
-    );
+    return null;
 }
