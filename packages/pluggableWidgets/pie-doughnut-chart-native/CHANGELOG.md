19b5eb389381562279007a84f6fd5957c071bc7d
Uraz Akgultan <uraz.akgultan@gmail.com>
2021-12-28 19:16:16 +0100

    feat(pie-doughnut-chart-native): add pie chart dark structure preview

--- packages/pluggableWidgets/pie-doughnut-chart-native/CHANGELOG.md
+++ packages/pluggableWidgets/pie-doughnut-chart-native/CHANGELOG.md
@@ -9,6 +9,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ### Added
 - Dark theme icons for Tile and List view.
+- We added dark mode to structure mode preview.
 
 ## [1.0.0] - 2021-10-14
 
--- packages/pluggableWidgets/pie-doughnut-chart-native/src/PieDoughnutChart.editorConfig.ts
+++ packages/pluggableWidgets/pie-doughnut-chart-native/src/PieDoughnutChart.editorConfig.ts
@@ -3,12 +3,16 @@ import { StructurePreviewProps } from "@mendix/piw-utils-internal";
 import { PieDoughnutChartPreviewProps } from "../typings/PieDoughnutChartProps";
 import PieWithLabels from "./assets/pie-w-labels.svg";
 import PieWithoutLabels from "./assets/pie-wo-labels.svg";
+import PieWithLabelsDark from "./assets/pie-w-labels-dark.svg";
+import PieWithoutLabelsDark from "./assets/pie-wo-labels-dark.svg";
 import DoughnutWithLabels from "./assets/doughnut-w-labels.svg";
 import DoughnutWithoutLabels from "./assets/doughnut-wo-labels.svg";
 
-export function getPreview(values: PieDoughnutChartPreviewProps): StructurePreviewProps {
+export function getPreview(values: PieDoughnutChartPreviewProps, isDarkMode: boolean): StructurePreviewProps {
+    const pieWithLabelsSVG = isDarkMode ? PieWithLabelsDark : PieWithLabels;
+    const pieWithoutLabelsSVG = isDarkMode ? PieWithoutLabelsDark : PieWithoutLabels;
     return values.presentation === "pie"
-        ? result(values.showLabels ? PieWithLabels : PieWithoutLabels)
+        ? result(values.showLabels ? pieWithLabelsSVG : pieWithoutLabelsSVG)
         : result(values.showLabels ? DoughnutWithLabels : DoughnutWithoutLabels);
 
     function result(svg: string): StructurePreviewProps {
