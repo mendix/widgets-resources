f26ed38984fbdcb02461a82007c37249d4704017
Uraz Akgultan <uraz.akgultan@gmail.com>
2021-12-28 19:10:01 +0100

    feat(floating-action-button-native): add dark structure preview

--- packages/pluggableWidgets/floating-action-button-native/src/FloatingActionButton.editorConfig.ts
+++ packages/pluggableWidgets/floating-action-button-native/src/FloatingActionButton.editorConfig.ts
@@ -1,24 +1,32 @@
-import { ImageProps, ContainerProps } from "@mendix/piw-utils-internal";
+import { ImageProps, ContainerProps, StructurePreviewProps } from "@mendix/piw-utils-internal";
 import { FloatingActionButtonPreviewProps } from "../typings/FloatingActionButtonProps";
 import StructurePreviewFloatingActionButtonPrimarySVG from "./assets/StructurePreviewFloatingActionButtonPrimary.svg";
+import StructurePreviewFloatingActionButtonPrimaryDarkSVG from "./assets/StructurePreviewFloatingActionButtonPrimaryDark.svg";
 
-const rowLayoutContainer: ContainerProps = { type: "Container", children: [] };
-const rowLayoutImage: ImageProps = {
-    type: "Image",
-    document: decodeURIComponent(StructurePreviewFloatingActionButtonPrimarySVG.replace("data:image/svg+xml,", "")),
-    width: 32,
-    height: 32
+export const getPreview = (
+    values: FloatingActionButtonPreviewProps,
+    isDarkMode: boolean
+): StructurePreviewProps | null => {
+    const rowLayoutContainer: ContainerProps = { type: "Container", children: [] };
+    const rowLayoutImage: ImageProps = {
+        type: "Image",
+        document: isDarkMode
+            ? decodeURIComponent(StructurePreviewFloatingActionButtonPrimaryDarkSVG.replace("data:image/svg+xml,", ""))
+            : decodeURIComponent(StructurePreviewFloatingActionButtonPrimarySVG.replace("data:image/svg+xml,", "")),
+        width: 32,
+        height: 32
+    };
+
+    return {
+        type: "RowLayout",
+        borders: false,
+        padding: 8,
+        columnSize: "grow",
+        children:
+            values.horizontalPosition === "left"
+                ? [rowLayoutImage]
+                : values.horizontalPosition === "center"
+                ? [rowLayoutContainer, rowLayoutImage, rowLayoutContainer]
+                : [rowLayoutContainer, rowLayoutImage]
+    };
 };
-
-export const getPreview = (values: FloatingActionButtonPreviewProps) => ({
-    type: "RowLayout",
-    borders: false,
-    padding: 8,
-    columnSize: "grow",
-    children:
-        values.horizontalPosition === "left"
-            ? [rowLayoutImage]
-            : values.horizontalPosition === "center"
-            ? [rowLayoutContainer, rowLayoutImage, rowLayoutContainer]
-            : [rowLayoutContainer, rowLayoutImage]
-});
