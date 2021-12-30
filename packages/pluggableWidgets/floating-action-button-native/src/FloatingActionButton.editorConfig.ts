5b195e63c020bdbfbe7354d61515fce5273d999b
Uraz Akgultan <uraz.akgultan@gmail.com>
2021-12-30 10:43:27 +0100

    refactor(floating-action-button-native): refactoring code

--- packages/pluggableWidgets/floating-action-button-native/src/FloatingActionButton.editorConfig.ts
+++ packages/pluggableWidgets/floating-action-button-native/src/FloatingActionButton.editorConfig.ts
@@ -10,9 +10,12 @@ export const getPreview = (
     const rowLayoutContainer: ContainerProps = { type: "Container", children: [] };
     const rowLayoutImage: ImageProps = {
         type: "Image",
-        document: isDarkMode
-            ? decodeURIComponent(StructurePreviewFloatingActionButtonPrimaryDarkSVG.replace("data:image/svg+xml,", ""))
-            : decodeURIComponent(StructurePreviewFloatingActionButtonPrimarySVG.replace("data:image/svg+xml,", "")),
+        document: decodeURIComponent(
+            (isDarkMode
+                ? StructurePreviewFloatingActionButtonPrimaryDarkSVG
+                : StructurePreviewFloatingActionButtonPrimarySVG
+            ).replace("data:image/svg+xml,", "")
+        ),
         width: 32,
         height: 32
     };
