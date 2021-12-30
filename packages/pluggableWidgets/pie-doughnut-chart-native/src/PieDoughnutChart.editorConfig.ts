68513a677d85092025bbd567dceae97d747075f7
Uraz Akgultan <uraz.akgultan@gmail.com>
2021-12-30 10:36:56 +0100

    feat(pie-doughnut-chart-native): add dark structure preview to doughnut chart

--- packages/pluggableWidgets/pie-doughnut-chart-native/src/PieDoughnutChart.editorConfig.ts
+++ packages/pluggableWidgets/pie-doughnut-chart-native/src/PieDoughnutChart.editorConfig.ts
@@ -6,14 +6,18 @@ import PieWithoutLabels from "./assets/pie-wo-labels.svg";
 import PieWithLabelsDark from "./assets/pie-w-labels-dark.svg";
 import PieWithoutLabelsDark from "./assets/pie-wo-labels-dark.svg";
 import DoughnutWithLabels from "./assets/doughnut-w-labels.svg";
+import DoughnutWithLabelsDark from "./assets/doughnut-w-labels-dark.svg";
 import DoughnutWithoutLabels from "./assets/doughnut-wo-labels.svg";
+import DoughnutWithoutLabelsDark from "./assets/doughnut-wo-labels-dark.svg";
 
 export function getPreview(values: PieDoughnutChartPreviewProps, isDarkMode: boolean): StructurePreviewProps {
     const pieWithLabelsSVG = isDarkMode ? PieWithLabelsDark : PieWithLabels;
     const pieWithoutLabelsSVG = isDarkMode ? PieWithoutLabelsDark : PieWithoutLabels;
+    const doughnutWithLabelsDarkSVG = isDarkMode ? DoughnutWithLabelsDark : DoughnutWithLabels;
+    const doughnutWithoutLabelsDarkSVG = isDarkMode ? DoughnutWithoutLabelsDark : DoughnutWithoutLabels;
     return values.presentation === "pie"
         ? result(values.showLabels ? pieWithLabelsSVG : pieWithoutLabelsSVG)
-        : result(values.showLabels ? DoughnutWithLabels : DoughnutWithoutLabels);
+        : result(values.showLabels ? doughnutWithLabelsDarkSVG : doughnutWithoutLabelsDarkSVG);
 
     function result(svg: string): StructurePreviewProps {
         return {
