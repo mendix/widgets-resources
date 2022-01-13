a1a0a19c206922770cf1c3ea3d4f5905added42f
Pratik Goyal <pratik.goyal@mendix.com>
2022-01-13 12:07:07 +0100

    test(): change the test identifier

--- packages/pluggableWidgets/repeater-native/e2e/specs/Repeater.spec.ts
+++ packages/pluggableWidgets/repeater-native/e2e/specs/Repeater.spec.ts
@@ -3,12 +3,12 @@ import { Widget, expectToMatchImageSnapshot } from "../../../../../tests/e2e";
 
 describe("Repeater", () => {
     beforeAll(async () => {
-        await waitFor(element(by.id("RepeaterWidgetHome")))
+        await waitFor(element(by.id("btnRepeater")))
             .toBeVisible()
             .whileElement(by.id("scrollContainer1"))
             .scroll(200, "down");
 
-        await Widget("RepeaterWidgetHome").getElement().tap();
+        await Widget("btnRepeater").getElement().tap();
     });
 
     it("renders the default repeater", async () => {
