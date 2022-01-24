ade438b9f63f903b3a8b1acc842e37e1beab749f
ahmed.elsayed <ahmed.elsayed@mendix.com>
2022-01-24 12:13:24 +0100

    fix(): revert xml format configs

--- packages/tools/generator-widget/generators/app/templates/commons/prettier.config.js
+++ packages/tools/generator-widget/generators/app/templates/commons/prettier.config.js
@@ -1,6 +1,5 @@
 const base = require("@mendix/pluggable-widgets-tools/configs/prettier.base.json");
 
 module.exports = {
-    ...base,
-    plugins: [require.resolve("@prettier/plugin-xml")],
+    ...base
 };
