2aa6a0781bfbeb4bad5e253acf9912fe09566c43
Kevin Vlaanderen <kevin.vlaanderen@mendix.com>
2022-01-27 22:15:40 +0100

    feat: add columns; add color to boolean fields

--- scripts/data/components/WidgetsGrid.tsx
+++ scripts/data/components/WidgetsGrid.tsx
@@ -2,12 +2,20 @@ import ReactDataGrid, { Column } from "react-data-grid";
 import { FunctionComponent, useCallback, useState } from "react";
 import { z } from "zod";
 import { WidgetPackageSchema, WidgetSchema } from "../schema";
+import styles from "./WidgetsGrid.module.scss";
 
 type Props = {
     data: Array<z.infer<typeof WidgetPackageSchema>>;
 };
 
 type Row = Pick<z.infer<typeof WidgetPackageSchema>, "version"> & z.infer<typeof WidgetSchema>;
+
+function booleanFormatter(value?: boolean) {
+    const className =
+        value === true ? styles.booleanTrue : value === false ? styles.booleanFalse : styles.booleanUndefined;
+    const text = value === true ? "Yes" : value === false ? "No" : undefined;
+    return <div className={`${styles.boolean} ${className}`}>{text}</div>;
+}
 
 const columns: readonly Column<Row>[] = [
     {
@@ -19,35 +27,48 @@ const columns: readonly Column<Row>[] = [
         name: "Platform"
     },
     {
-        key: "isPluginWidget",
-        name: "Plugin widget?"
-    },
-    {
         key: "offlineCapable",
-        name: "Offline capable?"
+        name: "Offline capable?",
+        formatter: props => booleanFormatter(props.row.offlineCapable)
     },
     {
-        key: "definitionOfDone",
-        name: "DoD"
+        key: "isPluginWidget",
+        name: "Plugin widget?",
+        formatter: props => booleanFormatter(props.row.isPluginWidget)
+    },
+    {
+        key: "hasStructureModePreview",
+        name: "Has structure mode preview?",
+        formatter: props => booleanFormatter(props.row.hasStructureModePreview)
+    },
+    {
+        key: "hasDesignModePreview",
+        name: "Has design mode preview?",
+        formatter: props => booleanFormatter(props.row.hasDesignModePreview)
+    },
+    {
+        key: "hasAllTileIcons",
+        name: "Has all tile icons?",
+        formatter: props => booleanFormatter(props.row.hasAllTileIcons)
+    },
+    {
+        key: "hasAllDarkIcons",
+        name: "Has all dark icons?",
+        formatter: props => booleanFormatter(props.row.hasAllDarkIcons)
     }
 ];
 
 export const WidgetsGrid: FunctionComponent<Props> = ({ data }) => {
     const [rows] = useState(
-        data
-            .flatMap(widgetPackage => widgetPackage.widgets)
-            .map(widget =>
-                columns.reduce((result, column) => ({ ...result, [column.key]: widget[column.key] }), {
-                    id: widget.id
-                })
-            )
+        data.flatMap(widgetPackage =>
+            widgetPackage.widgets.map(widget => ({ version: widgetPackage.version, ...widget }))
+        )
     );
     const rowKeyGetter = useCallback((row: Row) => row.id, []);
 
     return isServer() ? null : (
         <ReactDataGrid
-            className="rdg-light"
-            style={{ height: "100%" }}
+            className={`rdg-light ${styles.grid}`}
             columns={columns}
             rows={rows}
             rowKeyGetter={rowKeyGetter}
