1e1e28a1730df63ecc9422b7eb2158b4197da399
Kevin Vlaanderen <kevin.vlaanderen@mendix.com>
2022-01-31 21:41:23 +0100

    feat: upload site to s3

--- scripts/data/next.config.js
+++ scripts/data/next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    basePath: process.env.BASEPATH
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
