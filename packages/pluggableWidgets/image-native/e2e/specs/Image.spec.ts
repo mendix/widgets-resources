3306b93219eaa5bee7fdf80412ae31597449222b
Pratik Goyal <pratik.goyal@mendix.com>
2022-02-03 23:03:00 +0100

    test(): add image e2e tests

---
+++ packages/pluggableWidgets/image-native/e2e/specs/Image.spec.ts
@@ -0,0 +1,51 @@
+import { expectToMatchScreenshot, tapMenuItem } from "../../../../../detox/src/helpers";
+import { element, by, waitFor, expect, device } from "detox";
+import { Alert } from "../../../../../detox/src/Alert";
+
+describe("Image", () => {
+    beforeEach(async () => {
+        await tapMenuItem("Image");
+    });
+
+    afterEach(async () => {
+        await device.reloadReactNative();
+    });
+
+    it("renders the static image", async () => {
+        const btnImageStatic = element(by.id("btnImageStatic"));
+        await btnImageStatic.tap();
+
+        await expectToMatchScreenshot();
+    });
+
+    it("renders the dynamic image", async () => {
+        const btnImageDynamic = element(by.id("btnImageDynamic"));
+        await btnImageDynamic.tap();
+
+        const dynamicImage = element(by.id("dynamicImageText"));
+        await waitFor(dynamicImage).toBeVisible().withTimeout(2000);
+
+        await expectToMatchScreenshot();
+    });
+
+    it("renders the image with URL", async () => {
+        const btnImageUrl = element(by.id("btnImageUrl"));
+        await btnImageUrl.tap();
+
+        await expectToMatchScreenshot();
+
+        const clickableImage = element(by.id("clickableImage$ImageSmallPressable"));
+        await clickableImage.tap();
+
+        const alert = Alert();
+        await expect(alert.messageElement).toHaveText("Image clicked!");
+        await alert.confirm();
+    });
+
+    it("renders the image with icon", async () => {
+        const btnImageIcon = element(by.id("btnImageIcon"));
+        await btnImageIcon.tap();
+
+        await expectToMatchScreenshot();
+    });
+});
