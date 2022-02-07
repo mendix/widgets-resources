0a4497c2aca1f7d18780a1358dca76aed998f2d1
Diego Antonelli <diego.antonelli@mendix.com>
2022-02-07 11:33:43 +0100

    feat: migrating to cypress-image-diff-js

--- packages/theming/atlas/cypress/support/commands.js
+++ packages/theming/atlas/cypress/support/commands.js
@@ -1,8 +1,3 @@
-import { addMatchImageSnapshotCommand } from "cypress-image-snapshot/command";
+import compareSnapshotCommand from "cypress-image-diff-js/dist/command";
 
-addMatchImageSnapshotCommand({
-    failureThreshold: 0.03, // threshold for entire image
-    failureThresholdType: "percent", // percent of image or number of pixels
-    customDiffConfig: { threshold: 0.1 }, // threshold for each pixel
-    capture: "viewport" // capture viewport in screenshot
-});
+compareSnapshotCommand();
