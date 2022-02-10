991007f01d0fe2eff5d0a32b2b20f02be164f6b4
Diego Antonelli <diego.antonelli@mendix.com>
2022-02-10 16:00:40 +0100

    feat(progress-circle-web): move e2e to cypress
    Signed-off-by: Diego Antonelli <diego.antonelli@mendix.com>

--- packages/pluggableWidgets/progress-circle-web/cypress/integration/ProgressCircle.spec.js
+++ packages/pluggableWidgets/progress-circle-web/cypress/integration/ProgressCircle.spec.js
@@ -1,37 +1,23 @@
-import page from "../../../../../../configs/e2e/src/pages/page";
-import ProgressCircle from "../objects/progressCircle.widget";
-
 describe("Progress Circle", () => {
     it("renders with a value", () => {
-        page.open("p/Home");
-
-        const progressCircle = new ProgressCircle("progressCircleNegative");
-        progressCircle.element.waitForDisplayed();
-
-        expect(progressCircle.valueElement.getText()).toBe("20%");
+        cy.visit("p/Home");
+        cy.get(".mx-name-progressCircleNegative").should("be.visible");
+        cy.get(".mx-name-progressCircleNegative .progressbar-text").should("have.text", "20%");
     });
 
     it("updates the progress percentage when the value is changed", () => {
-        page.open("p/Playground");
-
-        const progressCirclePercentage = new ProgressCircle("progressCirclePercentage");
-
-        const progress = page.getWidget("textBoxProgress");
-        progress.$(".form-control").setValue("67");
-        page.getWidget("textBoxMaximumValue").click();
-
-        expect(progressCirclePercentage.valueElement.getText()).toBe("67%");
-
-        const progressCircleValue = new ProgressCircle("progressCircleValue");
-        const progressCircleNoValue = new ProgressCircle("progressCircleNoValue");
-        const progressCircleAttribute = new ProgressCircle("progressCircleAttribute");
-        const progressCircleStaticText = new ProgressCircle("progressCircleStaticText");
-        const progressCircleStaticTextAttributeDefined = new ProgressCircle("progressCircleStaticTextAttributeDefined");
-
-        expect(progressCircleValue.valueElement.getText()).toBe("67");
-        expect(progressCircleNoValue.valueElement.getText()).toBe("");
-        expect(progressCircleAttribute.valueElement.getText()).toBe("Working with an attribute");
-        expect(progressCircleStaticText.valueElement.getText()).toBe("Static text");
-        expect(progressCircleStaticTextAttributeDefined.valueElement.getText()).toBe("Static text");
+        cy.visit("p/Playground");
+        cy.get(".mx-name-progressCirclePercentage").should("be.visible");
+        cy.get(".mx-name-textBoxProgress input").type("67", { force: true });
+        cy.get(".mx-name-textBoxMaximumValue").click();
+        cy.get(".mx-name-progressCirclePercentage .progressbar-text").should("have.text", "67%");
+        cy.get(".mx-name-progressCircleValue .progressbar-text").should("have.text", "67");
+        cy.get(".mx-name-progressCircleNoValue .progressbar-text").should("have.text", "");
+        cy.get(".mx-name-progressCircleAttribute .progressbar-text").should("have.text", "Working with an attribute");
+        cy.get(".mx-name-progressCircleStaticText .progressbar-text").should("have.text", "Static text");
+        cy.get(".mx-name-progressCircleStaticTextAttributeDefined .progressbar-text").should(
+            "have.text",
+            "Static text"
+        );
     });
 });
