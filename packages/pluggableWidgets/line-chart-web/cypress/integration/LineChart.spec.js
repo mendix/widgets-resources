951f746795c2590680eba3a0070a782f17fe8685
Leonardo de Souza <leonardo.de.souza@mendix.com>
2022-02-10 09:53:17 +0100

    test(line-chart-web): moving e2e to cypress

---
+++ packages/pluggableWidgets/line-chart-web/cypress/integration/LineChart.spec.js
@@ -0,0 +1,122 @@
+describe("line-chart-web", () => {
+    const browserName = Cypress.browser.name;
+
+    before(() => {
+        cy.visit("/");
+    });
+
+    describe("line style", () => {
+        it("renders basic line chart and compares with a screenshot baseline", () => {
+            cy.wait(3000);
+            cy.get(".mx-name-containerBasic").compareSnapshot(`lineChartLineBasic-${browserName}`, 0.1);
+        });
+
+        it("renders line with markers and compares with a screenshot baseline", () => {
+            cy.get(".mx-name-containerMarkers")
+                .scrollIntoView()
+                .compareSnapshot(`lineChartLineMarkers-${browserName}`, 0.1);
+        });
+
+        it("renders colored line with colored markers and compares with a screenshot baseline", () => {
+            cy.get(".mx-name-containerColoredMarkerLine")
+                .scrollIntoView()
+                .compareSnapshot(`lineChartColoredLineMarkers-${browserName}`, 0.1);
+        });
+    });
+
+    describe("interpolation", () => {
+        it("renders linear and compares with a screenshot baseline", () => {
+            cy.get(".mx-name-containerLinear").scrollIntoView().compareSnapshot(`lineChartLinear-${browserName}`, 0.1);
+        });
+
+        it("renders curved and compares with a screenshot baseline", () => {
+            cy.get(".mx-name-containerCurved").scrollIntoView().compareSnapshot(`lineChartCurved-${browserName}`, 0.1);
+        });
+    });
+
+    describe("grid lines", () => {
+        it("renders horizontal grid lines and compares with a screenshot baseline", () => {
+            cy.get(".mx-name-containerVertical")
+                .scrollIntoView()
+                .compareSnapshot(`lineChartGridLinesVertical-${browserName}`, 0.1);
+        });
+
+        it("renders vertical grid lines and compares with a screenshot baseline", () => {
+            cy.get(".mx-name-containerHorizontal")
+                .scrollIntoView()
+                .compareSnapshot(`lineChartGridLinesHorizontal-${browserName}`, 0.1);
+        });
+
+        it("renders both grid lines and compares with a screenshot baseline", () => {
+            cy.get(".mx-name-containerBoth")
+                .scrollIntoView()
+                .compareSnapshot(`lineChartGridLinesBoth-${browserName}`, 0.1);
+        });
+    });
+
+    describe("legend", () => {
+        it("renders with legend and compares with a screenshot baseline", () => {
+            cy.get(".mx-name-containerLegend").scrollIntoView().compareSnapshot(`lineChartLegend-${browserName}`, 0.1);
+        });
+
+        it("renders without legend and compares with a screenshot baseline", () => {
+            cy.get(".mx-name-containerNoLegend")
+                .scrollIntoView()
+                .compareSnapshot(`lineChartNoLegend-${browserName}`, 0.1);
+        });
+    });
+
+    describe("axis label", () => {
+        it("renders x axis label and compares with a screenshot baseline", () => {
+            cy.get(".mx-name-containerXLabel").scrollIntoView().compareSnapshot(`lineChartXLabel-${browserName}`, 0.1);
+        });
+
+        it("renders y axis label legend and compares with a screenshot baseline", () => {
+            cy.get(".mx-name-containerYLabel").scrollIntoView().compareSnapshot(`lineChartYLabel-${browserName}`, 0.1);
+        });
+
+        it("renders x+y axis label legend and compares with a screenshot baseline", () => {
+            cy.get(".mx-name-containerXYLabels")
+                .scrollIntoView()
+                .compareSnapshot(`lineChartXYLabels-${browserName}`, 0.1);
+        });
+    });
+
+    describe("dimensions", () => {
+        it("renders with width: pixels (500px) height: pixels (300px) and compares with a screenshot baseline", () => {
+            cy.get(".mx-name-containerDimensionPixels")
+                .scrollIntoView()
+                .compareSnapshot(`lineChartDimensionPixels-${browserName}`, 0.1);
+        });
+
+        it("renders with width: pixels (500px) height: percentage of width (50%) and compares with a screenshot baseline", () => {
+            cy.get(".mx-name-containerPercentageOfWidth")
+                .scrollIntoView()
+                .compareSnapshot(`lineChartDimensionPercentageOfWidth-${browserName}`, 0.1);
+        });
+
+        it("renders with width: pixels (500px) height: percentage of parent (50%) and compares with a screenshot baseline", () => {
+            cy.get(".mx-name-containerPixelsPercentageOfParent")
+                .scrollIntoView()
+                .compareSnapshot(`lineChartDimensionPixelsPercentageOfParent-${browserName}`, 0.1);
+        });
+
+        it("renders with width: percentage (80%) height: pixels (300px) and compares with a screenshot baseline", () => {
+            cy.get(".mx-name-containerPercentagePixels")
+                .scrollIntoView()
+                .compareSnapshot(`lineChartDimensionPercentagePixels-${browserName}`, 0.1);
+        });
+
+        it("renders with width: percentage (100%) height: percentage of parent (50%) and compares with a screenshot baseline", () => {
+            cy.get(".mx-name-containerPercentageOfParent")
+                .scrollIntoView()
+                .compareSnapshot(`lineChartDimensionPercentageOfParent-${browserName}`, 0.1);
+        });
+
+        it("renders with width: percentage (100%) height: percentage of width (75%) and compares with a screenshot baseline", () => {
+            cy.get(".mx-name-containerDimensionPercentage")
+                .scrollIntoView()
+                .compareSnapshot(`lineChartDimensionPercentage-${browserName}`, 0.1);
+        });
+    });
+});
