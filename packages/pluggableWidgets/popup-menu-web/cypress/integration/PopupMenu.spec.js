2db7099a8ab6c6079deefed5fef23c6980ebc24b
Diego Antonelli <diego.antonelli@mendix.com>
2022-02-10 12:21:14 +0100

    feat(popup-menu-web): move e2e to cypress
    Signed-off-by: Diego Antonelli <diego.antonelli@mendix.com>

--- packages/pluggableWidgets/popup-menu-web/cypress/integration/PopupMenu.spec.js
+++ packages/pluggableWidgets/popup-menu-web/cypress/integration/PopupMenu.spec.js
@@ -1,230 +1,112 @@
-import page from "../../../../../configs/e2e/src/pages/page";
-
 describe("Popup-menu-web", () => {
+    const browserName = Cypress.browser.name;
+    const cleanMendixSession = () => {
+        cy.window().then(window => {
+            // Cypress opens a new session for every test, so it exceeds mendix license limit of 5 sessions, we need to logout after each test.
+            window.mx.session.logout();
+        });
+    };
+
     beforeEach(() => {
         cy.visit("/"); // resets page
     });
 
+    afterEach(() => cleanMendixSession());
+
     describe("using basic option", () => {
         it("compares with a screenshot baseline and checks if popupmenu is rendered in the top left position", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            const sprintrFeedback = $(".sprintrFeedback__sidebar");
-            const popupmenu = page.getWidget("container15");
-            const button = page.getWidget("actionButton10");
-            button.click();
-            popupmenu.waitForDisplayed({ timeout: 5000 });
-            browser.saveElement(popupmenu, "popUpMenuTopLeft", { removeElements: [sprintrFeedback] });
-            expect(browser.checkElement(popupmenu, "popUpMenuTopLeft", { removeElements: [sprintrFeedback] })).toEqual(
-                0
-            );
-            cy.get(".widget-maps").wait(3000).compareSnapshot(`googleMaps-${browserName}`, 0.5);
+            cy.get(".mx-name-actionButton10").click();
+            cy.wait(1000);
+            cy.get(".mx-name-container15").compareSnapshot(`popUpMenuTopLeft-${browserName}`, 0.1);
         });
         it("compares with a screenshot baseline and checks if popupmenu is rendered in the left position", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            const sprintrFeedback = $(".sprintrFeedback__sidebar");
-            const popupmenu = page.getWidget("container15");
-            const button = page.getWidget("actionButton12");
-            button.click();
-            popupmenu.waitForExist();
-            browser.pause(1000);
-            browser.saveElement(popupmenu, "popUpMenuLeft", { removeElements: [sprintrFeedback] });
-            expect(browser.checkElement(popupmenu, "popUpMenuLeft", { removeElements: [sprintrFeedback] })).toEqual(0);
+            cy.get(".mx-name-actionButton12").click();
+            cy.wait(1000);
+            cy.get(".mx-name-container15").compareSnapshot(`popUpMenuLeft-${browserName}`, 0.1);
         });
 
         it("compares with a screenshot baseline and checks if popupmenu is rendered in the top position", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            const sprintrFeedback = $(".sprintrFeedback__sidebar");
-            const popupmenu = page.getWidget("container15");
-            const button = page.getWidget("actionButton15");
-            button.click();
-            popupmenu.waitForExist();
-            browser.pause(1000);
-            browser.saveElement(popupmenu, "popUpMenuTop", { removeElements: [sprintrFeedback] });
-            expect(browser.checkElement(popupmenu, "popUpMenuTop", { removeElements: [sprintrFeedback] })).toEqual(0);
+            cy.get(".mx-name-actionButton15").click();
+            cy.wait(1000);
+            cy.get(".mx-name-container15").compareSnapshot(`popUpMenuTop-${browserName}`, 0.1);
         });
         it("compares with a screenshot baseline and checks if popupmenu is rendered in the top right position", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            const sprintrFeedback = $(".sprintrFeedback__sidebar");
-            const popupmenu = page.getWidget("container15");
-            const button = page.getWidget("actionButton13");
-            button.click();
-            popupmenu.waitForExist();
-            browser.pause(1000);
-            browser.saveElement(popupmenu, "popUpMenuTopRight", { removeElements: [sprintrFeedback] });
-            expect(browser.checkElement(popupmenu, "popUpMenuTopRight", { removeElements: [sprintrFeedback] })).toEqual(
-                0
-            );
+            cy.get(".mx-name-actionButton13").click();
+            cy.wait(1000);
+            cy.get(".mx-name-container15").compareSnapshot(`popUpMenuTopRight-${browserName}`, 0.1);
         });
         it("compares with a screenshot baseline and checks if popupmenu is rendered in the right position", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            const sprintrFeedback = $(".sprintrFeedback__sidebar");
-            const popupmenu = page.getWidget("container15");
-            const button = page.getWidget("actionButton14");
-            button.click();
-            popupmenu.waitForExist();
-            browser.pause(1000);
-            browser.saveElement(popupmenu, "popUpMenuRight", { removeElements: [sprintrFeedback] });
-            expect(browser.checkElement(popupmenu, "popUpMenuRight", { removeElements: [sprintrFeedback] })).toEqual(0);
+            cy.get(".mx-name-actionButton14").click();
+            cy.wait(1000);
+            cy.get(".mx-name-container15").compareSnapshot(`popUpMenuRight-${browserName}`, 0.1);
         });
         it("compares with a screenshot baseline and checks if popupmenu is rendered in the bottom right position", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            const sprintrFeedback = $(".sprintrFeedback__sidebar");
-            const popupmenu = page.getWidget("container15");
-            const button = page.getWidget("actionButton20");
-            button.click();
-            popupmenu.waitForExist();
-            browser.pause(1000);
-            browser.saveElement(popupmenu, "popUpMenuBottomRight", {
-                hideScrollBars: true,
-                removeElements: [sprintrFeedback]
-            });
-            expect(
-                browser.checkElement(popupmenu, "popUpMenuBottomRight", { removeElements: [sprintrFeedback] })
-            ).toEqual(0);
+            cy.get(".mx-name-actionButton20").click();
+            cy.wait(1000);
+            cy.get(".mx-name-container15").compareSnapshot(`popUpMenuBottomRight-${browserName}`, 0.1);
         });
         it("compares with a screenshot baseline and checks if popupmenu is rendered in the bottom left position", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            const sprintrFeedback = $(".sprintrFeedback__sidebar");
-            const popupmenu = page.getWidget("container15");
-            const button = page.getWidget("actionButton18");
-            button.click();
-            popupmenu.waitForExist();
-            browser.pause(1000);
-            browser.saveElement(popupmenu, "popUpMenuBottomLeft", {
-                hideScrollBars: true,
-                removeElements: [sprintrFeedback]
-            });
-            expect(
-                browser.checkElement(popupmenu, "popUpMenuBottomLeft", { removeElements: [sprintrFeedback] })
-            ).toEqual(0);
+            cy.get(".mx-name-actionButton18").click();
+            cy.wait(1000);
+            cy.get(".mx-name-container15").compareSnapshot(`popUpMenuBottomLeft-${browserName}`, 0.1);
         });
         it("compares with a screenshot baseline and checks if popupmenu is rendered in the bottom position", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            const sprintrFeedback = $(".sprintrFeedback__sidebar");
-            const popupmenu = page.getWidget("container15");
-            const button = page.getWidget("actionButton19");
-            button.click();
-            popupmenu.waitForExist();
-            browser.pause(1000);
-            browser.saveElement(popupmenu, "popUpMenuBottom", {
-                hideScrollBars: true,
-                removeElements: [sprintrFeedback]
-            });
-            expect(browser.checkElement(popupmenu, "popUpMenuBottom", { removeElements: [sprintrFeedback] })).toEqual(
-                0
-            );
+            cy.get(".mx-name-actionButton19").click();
+            cy.wait(1000);
+            cy.get(".mx-name-container15").compareSnapshot(`popUpMenuBottom-${browserName}`, 0.1);
         });
         it("shows a new menu list when on hover is triggered", () => {
-            const button = page.getWidget("actionButton1");
-            const buttonHover = page.getWidget("actionButton25");
-            button.click();
-            buttonHover.moveTo();
-            const textHover = page.getWidget("text42");
-            textHover.moveTo();
-            expect(textHover.getText()).toContain("Gooooooo");
+            cy.get(".mx-name-actionButton1").click();
+            cy.wait(1000);
+            cy.get(".mx-name-actionButton25").should("be.visible");
+            cy.get(".mx-name-actionButton25").trigger("mouseover");
+            cy.get(".mx-name-text42").trigger("mouseover");
+            cy.get(".mx-name-text42").should("contain.text", "Gooooooo");
         });
         it("shows a message when one item is clicked", () => {
-            const popupmenu = page.getWidget("pop_upMenu18");
-            const item = page.getElement(".popupmenu-basic-item", popupmenu);
-            const button = page.getWidget("actionButton10");
-            const dialog = page.modalDialog;
-            popupmenu.waitForExist();
-            button.click();
-            item.click();
-            dialog.waitForDisplayed();
-            expect(dialog.getText()).toContain("hello");
+            cy.get(".mx-name-pop_upMenu18").should("be.visible");
+            cy.get(".mx-name-actionButton10").click();
+            cy.get(".mx-name-pop_upMenu18 .popupmenu-basic-item").first().click();
+            cy.get(".modal-dialog").should("be.visible").should("contain.text", "hello");
         });
     });
     describe("using custom option", () => {
         it("compares with a screenshot baseline and checks if custom popupmenu is rendered in the top left position", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            const sprintrFeedback = $(".sprintrFeedback__sidebar");
-            const popupmenu = page.getWidget("container15");
-            const button = page.getWidget("actionButton11");
-            button.click();
-            popupmenu.waitForExist();
-            browser.pause(1000);
-            browser.saveElement(popupmenu, "customPopUpMenuTopLeft", { removeElements: [sprintrFeedback] });
-            expect(
-                browser.checkElement(popupmenu, "customPopUpMenuTopLeft", { removeElements: [sprintrFeedback] })
-            ).toEqual(0);
+            cy.get(".mx-name-actionButton11").click();
+            cy.wait(1000);
+            cy.get(".mx-name-container15").compareSnapshot(`customPopUpMenuTopLeft-${browserName}`, 0.1);
         });
         it("compares with a screenshot baseline and checks if custom popupmenu is rendered in the left position", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            const sprintrFeedback = $(".sprintrFeedback__sidebar");
-            const popupmenu = page.getWidget("container15");
-            const button = page.getWidget("actionButton17");
-            button.click();
-            popupmenu.waitForExist();
-            browser.pause(1000);
-            browser.saveElement(popupmenu, "CustomPopUpMenuLeft", { removeElements: [sprintrFeedback] });
-            expect(
-                browser.checkElement(popupmenu, "CustomPopUpMenuLeft", { removeElements: [sprintrFeedback] })
-            ).toEqual(0);
+            cy.get(".mx-name-actionButton17").click();
+            cy.wait(1000);
+            cy.get(".mx-name-container15").compareSnapshot(`CustomPopUpMenuLeft-${browserName}`, 0.1);
         });
 
         it("compares with a screenshot baseline and checks if custom popupmenu is rendered in the top position", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            const sprintrFeedback = $(".sprintrFeedback__sidebar");
-            const popupmenu = page.getWidget("container15");
-            const button = page.getWidget("actionButton24");
-            button.click();
-            popupmenu.waitForExist();
-            browser.pause(1000);
-            browser.saveElement(popupmenu, "customPopUpMenuTop", { removeElements: [sprintrFeedback] });
-            expect(
-                browser.checkElement(popupmenu, "customPopUpMenuTop", { removeElements: [sprintrFeedback] })
-            ).toEqual(0);
+            cy.get(".mx-name-actionButton24").click();
+            cy.wait(1000);
+            cy.get(".mx-name-container15").compareSnapshot(`customPopUpMenuTop-${browserName}`, 0.1);
         });
         it("compares with a screenshot baseline and checks if custom popupmenu is rendered in the top right position", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            const sprintrFeedback = $(".sprintrFeedback__sidebar");
-            const popupmenu = page.getWidget("container15");
-            const button = page.getWidget("actionButton23");
-            button.click();
-            popupmenu.waitForExist();
-            browser.pause(1000);
-            browser.saveElement(popupmenu, "customPopUpMenuTopRight", { removeElements: [sprintrFeedback] });
-            expect(
-                browser.checkElement(popupmenu, "customPopUpMenuTopRight", { removeElements: [sprintrFeedback] })
-            ).toEqual(0);
+            cy.get(".mx-name-actionButton23").click();
+            cy.wait(1000);
+            cy.get(".mx-name-container15").compareSnapshot(`customPopUpMenuTopRight-${browserName}`, 0.1);
         });
         it("compares with a screenshot baseline and checks if custom popupmenu is rendered in the right position", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            const sprintrFeedback = $(".sprintrFeedback__sidebar");
-            const popupmenu = page.getWidget("container15");
-            const button = page.getWidget("actionButton26");
-            button.click();
-            popupmenu.waitForExist();
-            browser.pause(1000);
-            browser.saveElement(popupmenu, "customPopUpMenuRight", { removeElements: [sprintrFeedback] });
-            expect(
-                browser.checkElement(popupmenu, "customPopUpMenuRight", { removeElements: [sprintrFeedback] })
-            ).toEqual(0);
+            cy.get(".mx-name-actionButton26").click();
+            cy.wait(1000);
+            cy.get(".mx-name-container15").compareSnapshot(`customPopUpMenuRight-${browserName}`, 0.1);
         });
         it("compares with a screenshot baseline and checks if custom popupmenu is rendered in the bottom position", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            const sprintrFeedback = $(".sprintrFeedback__sidebar");
-            const popupmenu = page.getWidget("container15");
-            const button = page.getWidget("actionButton29");
-            button.click();
-            popupmenu.waitForExist();
-            browser.pause(1000);
-            browser.saveElement(popupmenu, "customPopUpMenuBottom", { removeElements: [sprintrFeedback] });
-            expect(
-                browser.checkElement(popupmenu, "customPopUpMenuBottom", { removeElements: [sprintrFeedback] })
-            ).toEqual(0);
+            cy.get(".mx-name-actionButton29").click();
+            cy.wait(1000);
+            cy.get(".mx-name-container15").compareSnapshot(`customPopUpMenuBottom-${browserName}`, 0.1);
         });
         it("shows a message when one item is clicked", () => {
-            const popupmenu = page.getWidget("pop_upMenu13");
-            const item = page.getElement(".mx-name-text35", popupmenu);
-            const button = page.getWidget("actionButton11");
-            const dialog = page.modalDialog;
-            popupmenu.waitForExist();
-            button.click();
-            item.click();
-            dialog.waitForDisplayed();
-            expect(dialog.getText()).toContain("hello");
+            cy.get(".mx-name-pop_upMenu13").should("be.visible");
+            cy.get(".mx-name-actionButton11").click();
+            cy.get(".mx-name-pop_upMenu13 .mx-name-text35").first().click();
+            cy.get(".modal-dialog").should("be.visible").should("contain.text", "hello");
         });
     });
 });
