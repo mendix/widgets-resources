8a53672f8b95d6e4996d8d26431dd033c61124df
Diego Antonelli <diego.antonelli@mendix.com>
2022-02-10 17:21:40 +0100

    feat(range-slider-web): move e2e to cypress
    Signed-off-by: Diego Antonelli <diego.antonelli@mendix.com>

--- packages/pluggableWidgets/range-slider-web/cypress/integration/dataTypes.spec.js
+++ packages/pluggableWidgets/range-slider-web/cypress/integration/dataTypes.spec.js
@@ -1,25 +1,36 @@
-import page from "../../../../../../configs/e2e/src/pages/page";
-import RangeSlider from "../objects/rangeSlider.widget";
-
 describe("Range Slider", () => {
-    const rangeSlider = new RangeSlider("rangeSlider1");
+    const cleanMendixSession = () => {
+        cy.window().then(window => {
+            // Cypress opens a new session for every test, so it exceeds mendix license limit of 5 sessions, we need to logout after each test.
+            window.mx.session.logout();
+        });
+    };
 
-    beforeAll(() => {
-        page.open();
-        rangeSlider.element.waitForDisplayed();
+    beforeEach(() => {
+        cy.visit("/"); // resets page
     });
+
+    afterEach(() => cleanMendixSession());
 
     it("renders slider with interval context", () => {
-        expect(rangeSlider.firstTrack.isDisplayed()).toBeTruthy();
+        cy.get(".mx-name-rangeSlider1 .rc-slider-track.rc-slider-track-1").should("be.visible");
     });
     it("renders slider min value text", () => {
-        expect(rangeSlider.minValueText).toBe("0");
+        cy.get(".mx-name-rangeSlider1 .rc-slider-mark-text").first().should("have.text", "0");
     });
     it("renders slider max value text", () => {
-        expect(rangeSlider.maxValueText).toBe("100");
+        cy.get(".mx-name-rangeSlider1 .rc-slider-mark-text").eq(1).should("have.text", "100");
     });
-    // TODO: Values returned in pixels, so couldn't text precisely
+
     it("upper bound value is higher than lower bound value", () => {
-        expect(rangeSlider.upperBound).toBeGreaterThan(rangeSlider.lowerBound);
+        cy.get(".mx-name-rangeSlider1").should("be.visible");
+        cy.get(".mx-name-rangeSlider1 .rc-slider-handle").then(el => {
+            const [lowerBound, upperBound] = el;
+            const lowerBoundSizes = lowerBound.getBoundingClientRect();
+            const upperBoundSizes = upperBound.getBoundingClientRect();
+
+            // eslint-disable-next-line jest/valid-expect
+            expect(upperBoundSizes.x).to.be.greaterThan(lowerBoundSizes.x);
+        });
     });
 });
