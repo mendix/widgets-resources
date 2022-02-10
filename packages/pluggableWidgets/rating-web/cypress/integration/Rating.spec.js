eeefdb9bad0bc79f1ddb7c3740ced2c0109e9d7c
Diego Antonelli <diego.antonelli@mendix.com>
2022-02-10 17:27:57 +0100

    feat(rating-web): move e2e to cypress
    Signed-off-by: Diego Antonelli <diego.antonelli@mendix.com>

--- packages/pluggableWidgets/rating-web/cypress/integration/Rating.spec.js
+++ packages/pluggableWidgets/rating-web/cypress/integration/Rating.spec.js
@@ -1,19 +1,12 @@
-import page from "../../../../../../configs/e2e/src/pages/page";
-
 describe("Rating", () => {
-    beforeAll(() => {
-        page.open();
+    const browserName = Cypress.browser.name;
+
+    beforeEach(() => {
+        cy.visit("/");
     });
 
     it("compares with a screenshot baseline and checks if all rating elements are rendered as expected", () => {
-        browser.setWindowRect(0, 0, 1200, 900);
-        const screenshotElem = $(".mx-name-ratingContent");
-        const ratingWidget = $(".mx-name-rating1");
-        screenshotElem.waitForDisplayed({ timeout: 5000 });
-        ratingWidget.waitForDisplayed({ timeout: 5000 });
-        browser.pause(1000);
-        browser.saveElement(screenshotElem, "ratingPageContent");
-
-        expect(browser.checkElement(screenshotElem, "ratingPageContent")).toBeLessThan(0.4);
+        cy.get(".mx-name-rating1").should("be.visible");
+        cy.get(".mx-name-ratingContent").compareSnapshot(`ratingPageContent-${browserName}`, 0.4);
     });
 });
