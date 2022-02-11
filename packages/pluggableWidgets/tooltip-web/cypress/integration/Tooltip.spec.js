81a0f1a598bf1800da219163172642fa0bde3173
Diego Antonelli <diego.antonelli@mendix.com>
2022-02-11 15:58:12 +0100

    feat(tooltip-web): move e2e to cypress
    Signed-off-by: Diego Antonelli <diego.antonelli@mendix.com>

--- packages/pluggableWidgets/tooltip-web/cypress/integration/Tooltip.spec.js
+++ packages/pluggableWidgets/tooltip-web/cypress/integration/Tooltip.spec.js
@@ -1,102 +1,77 @@
-import page from "../../../../../../configs/e2e/src/pages/page";
-
 describe("tooltip-web", () => {
-    beforeAll(() => {
-        page.open("p/arrow"); // resets page
-    });
+    const browserName = Cypress.browser.name;
+    const cleanMendixSession = () => {
+        cy.window().then(window => {
+            // Cypress opens a new session for every test, so it exceeds mendix license limit of 5 sessions, we need to logout after each test.
+            window.mx.session.logout();
+        });
+    };
+
+    afterEach(() => cleanMendixSession());
 
     describe("render method: text", () => {
         it("compares with a screenshot baseline and checks if tooltip arrow start is rendered as expected", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            $(".mx-name-actionButtonStart").moveTo();
-            const screenshotElem = $(".mx-scrollcontainer-center");
-
-            browser.saveElement(screenshotElem, "tooltipArrowStart");
-            expect(browser.checkElement(screenshotElem, "tooltipArrowStart")).toEqual(0);
+            cy.visit("/p/arrow");
+            cy.get(".mx-name-actionButtonStart").focus();
+            cy.get(".mx-scrollcontainer-center").compareSnapshot(`tooltipArrowStart-${browserName}`, 0.1);
         });
 
         it("compares with a screenshot baseline and checks if tooltip arrow end is rendered as expected", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            $(".mx-name-actionButtonEnd").moveTo();
-            const screenshotElem = $(".mx-scrollcontainer-center");
-
-            browser.saveElement(screenshotElem, "tooltipArrowEnd");
-            expect(browser.checkElement(screenshotElem, "tooltipArrowEnd")).toEqual(0);
+            cy.visit("/p/arrow");
+            cy.get(".mx-name-actionButtonEnd").focus();
+            cy.get(".mx-scrollcontainer-center").compareSnapshot(`tooltipArrowEnd-${browserName}`, 0.1);
         });
 
         it("compares with a screenshot baseline and checks if tooltip arrow center is rendered as expected", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            $(".mx-name-actionButtonCenter").moveTo();
-            const screenshotElem = $(".mx-scrollcontainer-center");
-
-            browser.saveElement(screenshotElem, "tooltipArrowCenter");
-            expect(browser.checkElement(screenshotElem, "tooltipArrowCenter")).toEqual(0);
+            cy.visit("/p/arrow");
+            cy.get(".mx-name-actionButtonCenter").focus();
+            cy.get(".mx-scrollcontainer-center").compareSnapshot(`tooltipArrowCenter-${browserName}`, 0.1);
         });
 
         it("compares with a screenshot baseline and checks if tooltip position is rendered on top", () => {
-            page.open("p/position");
-            browser.setWindowRect(0, 0, 1200, 900);
-            $(".mx-name-actionButtonTop").moveTo();
-            const screenshotElem = $(".mx-scrollcontainer-center");
-
-            browser.saveElement(screenshotElem, "tooltipPositionTop");
-            expect(browser.checkElement(screenshotElem, "tooltipPositionTop")).toEqual(0);
+            cy.visit("/p/position");
+            cy.get(".mx-name-actionButtonTop").focus();
+            cy.get(".mx-scrollcontainer-center").compareSnapshot(`tooltipPositionTop-${browserName}`, 0.1);
         });
 
         it("compares with a screenshot baseline and checks if tooltip position is rendered on left", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            $(".mx-name-actionButtonLeft").moveTo();
-            const screenshotElem = $(".mx-scrollcontainer-center");
-
-            browser.saveElement(screenshotElem, "tooltipPositionLeft");
-            expect(browser.checkElement(screenshotElem, "tooltipPositionLeft")).toEqual(0);
+            cy.visit("/p/position");
+            cy.get(".mx-name-actionButtonLeft").focus();
+            cy.get(".mx-scrollcontainer-center").compareSnapshot(`tooltipPositionLeft-${browserName}`, 0.1);
         });
 
         it("compares with a screenshot baseline and checks if tooltip position is rendered on right", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            $(".mx-name-actionButtonRight").moveTo();
-            const screenshotElem = $(".mx-scrollcontainer-center");
-
-            browser.saveElement(screenshotElem, "tooltipPositionRight");
-            expect(browser.checkElement(screenshotElem, "tooltipPositionRight")).toEqual(0);
+            cy.visit("/p/position");
+            cy.get(".mx-name-actionButtonRight").focus();
+            cy.get(".mx-scrollcontainer-center").compareSnapshot(`tooltipPositionRight-${browserName}`, 0.1);
         });
 
         it("compares with a screenshot baseline and checks if tooltip position is rendered on bottom", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            $(".mx-name-actionButtonBottom").moveTo();
-            const screenshotElem = $(".mx-scrollcontainer-center");
-
-            browser.saveElement(screenshotElem, "tooltipPositionBottom");
-            expect(browser.checkElement(screenshotElem, "tooltipPositionBottom")).toEqual(0);
+            cy.visit("/p/position");
+            cy.get(".mx-name-actionButtonBottom").focus();
+            cy.get(".mx-scrollcontainer-center").compareSnapshot(`tooltipPositionBottom-${browserName}`, 0.1);
         });
         it("compares with a screenshot baseline and checks if tooltip position is flipped when it doesn't have space on the left", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            $(".mx-name-actionButtonFlip").moveTo();
-            const screenshotElem = $(".mx-scrollcontainer-center");
-
-            browser.saveElement(screenshotElem, "tooltipPositionFlipped");
-            expect(browser.checkElement(screenshotElem, "tooltipPositionFlipped")).toEqual(0);
+            cy.visit("/p/position");
+            cy.get(".mx-name-actionButtonFlip").focus();
+            cy.get(".mx-scrollcontainer-center").compareSnapshot(`tooltipPositionFlipped-${browserName}`, 0.1);
         });
     });
 
     describe("render method: custom", () => {
         it("verifies tooltip shown custom content and compares with a screenshot baseline", () => {
-            browser.setWindowRect(0, 0, 1200, 900);
-            $(".mx-name-navigationTree3-3").click();
-            $(".mx-name-actionButtonCustom").moveTo();
-            const screenshotElem = $(".mx-scrollcontainer-center");
-
-            browser.saveElement(screenshotElem, "tooltipCustom");
-            expect(browser.checkElement(screenshotElem, "tooltipCustom")).toEqual(0);
+            cy.visit("/p/arrow");
+            cy.get(".mx-name-navigationTree3-3").click();
+            cy.wait(1000);
+            cy.get(".mx-name-actionButtonCustom").focus();
+            cy.get(".mx-scrollcontainer-center").compareSnapshot(`tooltipCustom-${browserName}`, 0.1);
         });
 
         it("verifies if tooltip is opened on click", () => {
-            page.open("p/click");
-            $(".mx-name-actionButtonClick").click();
-            const screenshotElem = $(".mx-scrollcontainer-center");
-
-            browser.saveElement(screenshotElem, "tooltipClick");
-            expect(browser.checkElement(screenshotElem, "tooltipClick")).toEqual(0);
+            cy.visit("/p/click");
+            cy.get(".mx-name-actionButtonClick").click();
+            cy.wait(1000);
+            cy.get(".mx-scrollcontainer-center").compareSnapshot(`tooltipClick-${browserName}`, 0.1);
         });
     });
 });
