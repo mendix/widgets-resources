8ed29e27a6cb84aab4550011fa02ce1c0d760a6b
Diego Antonelli <diego.antonelli@mendix.com>
2022-02-11 10:56:03 +0100

    feat(switch-web): move 2e to cypress
    Signed-off-by: Diego Antonelli <diego.antonelli@mendix.com>

--- packages/pluggableWidgets/switch-web/cypress/integration/Switch.spec.js
+++ packages/pluggableWidgets/switch-web/cypress/integration/Switch.spec.js
@@ -1,51 +1,44 @@
-import page from "../pages/home.page";
-
-const switchBackgroundColor = "#64bd63";
-const radioValueTrue = "Yes";
-const popupValue = "IT WORKS";
-
 describe("Switch", () => {
+    const cleanMendixSession = () => {
+        cy.window().then(window => {
+            // Cypress opens a new session for every test, so it exceeds mendix license limit of 5 sessions, we need to logout after each test.
+            window.mx.session.logout();
+        });
+    };
+
+    beforeEach(() => {
+        cy.visit("/"); // resets page
+    });
+
+    afterEach(() => cleanMendixSession());
+
     it("changes color when checked", () => {
-        page.open();
-        page.switch1.waitForDisplayed();
-
-        const switchValue = page.switch1.$(".widget-switch-btn-wrapper");
-
-        const content = switchValue.getCSSProperty("background-color");
-        expect(content.parsed.hex).toBe(switchBackgroundColor);
+        cy.get(".mx-name-switch1").should("be.visible");
+        cy.get(".mx-name-switch1 .widget-switch-btn-wrapper").should(
+            "have.css",
+            "background-color",
+            "rgb(100, 189, 99)"
+        );
     });
 
     it("is updated by an attribute", () => {
-        page.open();
-        page.radioButton.waitForDisplayed();
-        page.radioButton.click();
-
-        page.switch2.waitForDisplayed();
-
-        const switchValue = page.switch1.$(".widget-switch-btn-wrapper");
-        const content = switchValue.getAttribute("class");
-        const ariaChecked = switchValue.getAttribute("aria-checked");
-        expect(content).toContain("checked");
-        expect(ariaChecked).toBe("true");
+        cy.get(".mx-name-radioButtons6 label").first().click();
+        cy.get(".mx-name-switch2").should("be.visible");
+        cy.get(".mx-name-switch2 .widget-switch-btn-wrapper").invoke("attr", "class").should("contain", "checked");
+        cy.get(".mx-name-switch2 .widget-switch-btn-wrapper").invoke("attr", "aria-checked").should("eq", "true");
     });
 
     it("updates attribute when clicked", () => {
-        page.open();
-        page.switch2.waitForDisplayed();
-        page.switch2.click();
-
-        page.radioButton.waitForDisplayed();
-        expect(page.radioButton.getText()).toContain(radioValueTrue);
+        cy.get(".mx-name-switch2").should("be.visible");
+        cy.get(".mx-name-switch2 .widget-switch-btn-wrapper").first().click();
+        cy.get(".mx-name-radioButtons6 input:checked").should("have.value", "true");
     });
 
     it("opens popup when clicked", () => {
-        page.open();
-        page.switch3.waitForDisplayed();
-        page.switch3.click();
-
-        setTimeout(() => {
-            page.popup.waitForDisplayed();
-            expect(page.popup.getText()).toContain(popupValue);
-        }, 3000);
+        cy.get(".mx-name-switch3").should("be.visible");
+        cy.get(".mx-name-switch3 .widget-switch-btn-wrapper").first().click();
+        cy.get(".mx-name-radioButtons7 input:checked").should("have.value", "true");
+        cy.wait(1000);
+        cy.get(".modal-dialog .modal-body").should("be.visible").and("contain.text", "IT WORKS");
     });
 });
