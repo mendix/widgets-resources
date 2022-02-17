1e77ea2d17699f393ea418c8d13a05bc3bebb30c
Illia Obukhau <vyusfire@gmail.com>
2022-02-17 14:37:48 +0100

    feat(rich-text-web): add design-mode preview for RichText

--- packages/pluggableWidgets/rich-text-web/src/RichText.editorPreview.tsx
+++ packages/pluggableWidgets/rich-text-web/src/RichText.editorPreview.tsx
@@ -1,1 +1,14 @@
-export const preview = (): null => null;
+import { createElement, ReactElement } from "react";
+import RichTextPreviewSVGLight from "./assets/rich-text-preview-light.svg";
+import { RichTextPreviewProps } from "../typings/RichTextProps";
+
+export const preview = (props: RichTextPreviewProps): ReactElement => {
+    let doc = decodeURIComponent(RichTextPreviewSVGLight.replace("data:image/svg+xml,", "")).replace(
+        "width='602' height='148'",
+        ""
+    );
+
+    doc = props.stringAttribute ? doc.replace("[No attribute selected]", `[${props.stringAttribute}]`) : doc;
+
+    return <div className="widget-rich-text" style={{ maxWidth: 602 }} dangerouslySetInnerHTML={{ __html: doc }}></div>;
+};
