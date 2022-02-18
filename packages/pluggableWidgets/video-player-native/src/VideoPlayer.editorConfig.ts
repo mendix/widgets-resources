f3faa7f63f145ef3173b3d074006edf6c9ee0fe6
Jordan Hunt <jordan.hunt@mendix.com>
2022-02-18 15:04:33 +0200

    fix(video-player-native): structure mode preview

--- packages/pluggableWidgets/video-player-native/src/VideoPlayer.editorConfig.ts
+++ packages/pluggableWidgets/video-player-native/src/VideoPlayer.editorConfig.ts
@@ -1,8 +1,8 @@
 import { StructurePreviewProps } from "@mendix/piw-utils-internal";
 
 import { VideoPlayerPreviewProps } from "../typings/VideoPlayerProps";
-import StructurePreviewWithControlsSVG from "../assets/structure-preview-video-with-controls.svg";
-import StructurePreviewWithoutControlsSVG from "../assets/structure-preview-video-without-controls.svg";
+import StructurePreviewWithControlsSVG from "./assets/structure-preview-video-with-controls.svg";
+import StructurePreviewWithoutControlsSVG from "./assets/structure-preview-video-without-controls.svg";
 
 export function getPreview(values: VideoPlayerPreviewProps): StructurePreviewProps | null {
     const image = values.showControls ? StructurePreviewWithControlsSVG : StructurePreviewWithoutControlsSVG;
