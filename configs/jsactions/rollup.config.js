b3ab1a04d96ba1993516334349781716c17a9e88
Jordan Hunt <jordan.hunt@mendix.com>
2022-03-03 13:57:22 +0100

    fix(): correct description

--- configs/jsactions/rollup.config.js
+++ configs/jsactions/rollup.config.js
@@ -61,7 +61,7 @@ export default async args => {
                           async () => {
                               if (!isWeb) {
                                   if (args.configProject === "nativemobileresources") {
-                                      // `fbjs/lib/invariant` is being used silently by @react-native-community/cameraroll; it is not listed as a dependency not peerDependency.
+                                      // `fbjs/lib/invariant` is being used silently by @react-native-community/cameraroll; it is not listed as a dependency nor peerDependency.
                                       // https://github.dev/react-native-cameraroll/react-native-cameraroll/blob/7c269a837d095a2cb5f4ce13b54ab3060455b17f/js/CameraRoll.js#L14
                                       const path = join(outDir, "node_modules", "fbjs", "lib");
                                       mkdirSync(path, { recursive: true });
@@ -70,7 +70,7 @@ export default async args => {
                                           join(path, "invariant.js")
                                       );
                                   } else if (args.configProject === "nanoflowcommons") {
-                                      // `invariant` is being used silently by @react-native-community/geolocation; it is not listed as a dependency not peerDependency.
+                                      // `invariant` is being used silently by @react-native-community/geolocation; it is not listed as a dependency nor peerDependency.
                                       // https://github.dev/react-native-geolocation/react-native-geolocation/blob/1786929f2be581da91082ff857c2393da5e597b3/js/implementation.native.js#L13
                                       await copyAsync(
                                           dirname(require.resolve("invariant")),
