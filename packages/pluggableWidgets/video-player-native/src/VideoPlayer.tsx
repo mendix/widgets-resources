9729f9ff82a7f4979f68d7b51cb0fcdcbeed8c4b
Ahmad-Elsayed <se.ahmedelsayed@gmail.com>
2022-03-10 16:10:41 +0100

    feat(video-payer-native): android fullscreen support

--- packages/pluggableWidgets/video-player-native/src/VideoPlayer.tsx
+++ packages/pluggableWidgets/video-player-native/src/VideoPlayer.tsx
@@ -1,11 +1,21 @@
-import { flattenStyles } from "@mendix/piw-native-utils-internal";
-import { Component, createElement } from "react";
-import { ActivityIndicator, Text, View } from "react-native";
-import Video, { OnLoadData } from "react-native-video";
-import { VideoPlayerProps } from "../typings/VideoPlayerProps";
-import { defaultVideoStyle } from "./ui/Styles";
-
-export type Props = VideoPlayerProps<undefined>;
+import {flattenStyles} from "@mendix/piw-native-utils-internal";
+import {createElement, Fragment, ReactElement, useCallback, useEffect, useRef, useState} from "react";
+import {
+    ActivityIndicator,
+    Modal,
+    Platform, StatusBar,
+    Text,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    View
+} from "react-native";
+import Video, {OnProgressData, VideoProperties} from "react-native-video";
+import SystemNavigationBar from "react-native-system-navigation-bar";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import {VideoPlayerProps} from "../typings/VideoPlayerProps";
+import {defaultVideoStyle, VideoStyle} from "./ui/Styles";
+import {isAvailable} from "@mendix/piw-utils-internal";
+import deepmerge from "deepmerge";
 
 const enum StatusEnum {
     ERROR = "error",
@@ -14,65 +24,161 @@ const enum StatusEnum {
     NOT_READY = "not-ready"
 }
 
-interface State {
-    aspectRatio?: number;
-    status: StatusEnum;
-}
+export function VideoPlayer(props: VideoPlayerProps<VideoStyle>): ReactElement {
+    const [styles, setStyles] = useState(flattenStyles(defaultVideoStyle, props.style));
+    const timeoutRef = useRef<number>();
 
-export class VideoPlayer extends Component<Props, State> {
-    readonly state: State = {
-        status: StatusEnum.NOT_READY
+    const playerRef = useRef<Video>(null);
+    const fullScreenPlayerRef = useRef<Video>(null);
+    const [status, setStatus] = useState(StatusEnum.NOT_READY);
+    const [videoAspectRatio, setVideoAspectRatio] = useState(0);
+    const [fullScreen, setFullScreen] = useState(false);
+    const [showControls, setShowControls] = useState(false);
+    const [currentPlayTime, setCurrentPlayTime] = useState(0);
+
+    useEffect(() => {
+        const alteredStyles = deepmerge({}, styles);
+        if (props.aspectRatio && videoAspectRatio !== 0) {
+            alteredStyles.video.aspectRatio = videoAspectRatio;
+            alteredStyles.container.aspectRatio = videoAspectRatio;
+        } else if (!props.aspectRatio) {
+            alteredStyles.container.aspectRatio = undefined;
+            if (alteredStyles.video.width) {
+                alteredStyles.container.width = alteredStyles.video.width;
+            }
+            if (alteredStyles.video.height) {
+                alteredStyles.container.height = alteredStyles.video.height;
+            }
+        }
+        setStyles(alteredStyles);
+    }, [props.style, props.aspectRatio, videoAspectRatio]);
+
+    const showControlsHandler = useCallback((): void => {
+        clearTimeout(timeoutRef.current);
+
+        setShowControls(true);
+        timeoutRef.current = setTimeout(() => setShowControls(false), 5000);
+    }, []);
+
+    useEffect(() => {
+        if (props.showControls) {
+            showControlsHandler();
+        }
+    }, [fullScreen, props.showControls, showControlsHandler]);
+
+    const onVideoPressHandler = useCallback((): void => {
+        if (!props.showControls) {
+            return;
+        }
+        if (!showControls) {
+            showControlsHandler();
+        } else {
+            clearTimeout(timeoutRef.current);
+            setShowControls(false);
+        }
+    }, [props.showControls, showControls, showControlsHandler]);
+
+    function fullScreenHandler(isFullScreen: boolean): void {
+        console.debug("isFullScreen", isFullScreen)
+        setFullScreen(isFullScreen);
+        if(isFullScreen){
+            SystemNavigationBar.navigationHide()
+        } else {
+            SystemNavigationBar.navigationShow()
+        }
+        StatusBar.setHidden(isFullScreen)
+    }
+
+    const videoProps: VideoProperties = {
+        testID: props.name,
+        source: {uri: isAvailable(props.videoUrl) ? props.videoUrl.value : undefined},
+        muted: props.muted,
+        repeat: props.loop,
+        controls: props.showControls,
+        onLoadStart: () => setStatus(StatusEnum.LOADING),
+        onError: () => setStatus(StatusEnum.ERROR),
+        useTextureView: false,
+        resizeMode: props.aspectRatio ? "contain" : "stretch",
+        onProgress: ({currentTime}: OnProgressData) => currentTime && setCurrentPlayTime(currentTime)
     };
 
-    private readonly onLoadStartHandler = this.onLoadStart.bind(this);
-    private readonly onLoadHandler = this.onLoad.bind(this);
-    private readonly onErrorHandler = this.onError.bind(this);
-    private readonly styles = flattenStyles(defaultVideoStyle, this.props.style);
+    const isAndroid = Platform.OS === "android";
+    const errorText = status === StatusEnum.ERROR && <Text style={styles.errorMessage}>The video failed to load</Text>;
 
-    render(): JSX.Element {
-        const uri = this.props.videoUrl && this.props.videoUrl.value;
-
-        const styles = { ...this.styles.container };
-
-        if (this.props.aspectRatio && this.state.aspectRatio) {
-            styles.aspectRatio = this.state.aspectRatio;
-        }
-
-        return (
-            <View style={styles}>
-                {this.state.status === StatusEnum.LOADING && (
-                    <ActivityIndicator color={this.styles.indicator.color} size="large" />
+    return (
+        <Fragment>
+            {isAndroid && (
+                <Modal
+                    visible={fullScreen}
+                    onRequestClose={() => fullScreenHandler(false)}
+                >
+                    <View style={styles.fullScreenVideoPlayer}>
+                        <TouchableWithoutFeedback onPress={onVideoPressHandler} testID="fullscreen-overlay">
+                            <Video
+                                {...videoProps}
+                                ref={fullScreenPlayerRef}
+                                paused={false}
+                                onLoad={data => {
+                                    setStatus(StatusEnum.READY);
+                                    setVideoAspectRatio(data.naturalSize.width / data.naturalSize.height);
+                                    fullScreenPlayerRef.current?.seek(currentPlayTime);
+                                }}
+                                testID={`${props.name}-fullscreen`}
+                                style={styles.fullScreenVideoStyle}
+                            />
+                        </TouchableWithoutFeedback>
+                        {showControls && (
+                            <TouchableOpacity
+                                style={styles.controlBtnContainerStyle}
+                                onPress={() => fullScreenHandler(false)}
+                                testID="btn-fullscreen-exit"
+                            >
+                                <Icon name="fullscreen-exit" color="white" size={22}/>
+                            </TouchableOpacity>
+                        )}
+                        {status === StatusEnum.LOADING && (
+                            <ActivityIndicator
+                                color={styles.indicator.color}
+                                size="large"
+                                style={styles.fullScreenActivityIndicatorStyle}
+                            />
+                        )}
+                        {errorText}
+                    </View>
+                </Modal>
+            )}
+            <View style={styles.container}>
+                {status === StatusEnum.LOADING && <ActivityIndicator color={styles.indicator.color} size="large"/>}
+                {errorText}
+                {!fullScreen && (
+                    <TouchableWithoutFeedback style={styles.container} onPress={onVideoPressHandler}>
+                        <Video
+                            {...videoProps}
+                            paused={!props.autoStart && !currentPlayTime}
+                            onLoad={data => {
+                                setStatus(StatusEnum.READY);
+                                setVideoAspectRatio(data.naturalSize.width / data.naturalSize.height);
+                                playerRef.current?.seek(currentPlayTime);
+                            }}
+                            ref={playerRef}
+                            style={status !== StatusEnum.READY ? {height: 0} : {
+                                width: '100%',
+                                aspectRatio: videoAspectRatio && 16 / 9
+                            }
+                            }
+                        />
+                    </TouchableWithoutFeedback>
                 )}
-                {this.state.status === StatusEnum.ERROR && (
-                    <Text style={this.styles.errorMessage}>The video failed to load :(</Text>
+                {isAndroid && showControls && (
+                    <TouchableOpacity
+                        onPress={() => fullScreenHandler(true)}
+                        style={styles.controlBtnContainerStyle}
+                        testID="btn-fullscreen"
+                    >
+                        <Icon name="fullscreen" color="white" size={22}/>
+                    </TouchableOpacity>
                 )}
-                <Video
-                    testID={this.props.name}
-                    source={{ uri }}
-                    paused={!this.props.autoStart}
-                    muted={this.props.muted}
-                    repeat={this.props.loop}
-                    controls={this.props.showControls}
-                    onLoadStart={this.onLoadStartHandler}
-                    onLoad={this.onLoadHandler}
-                    onError={this.onErrorHandler}
-                    style={this.state.status !== StatusEnum.READY ? { height: 0 } : this.styles.video}
-                    useTextureView={false}
-                    resizeMode="contain"
-                />
             </View>
-        );
-    }
-
-    private onLoadStart(): void {
-        this.setState({ status: StatusEnum.LOADING, aspectRatio: undefined });
-    }
-
-    private onLoad(data: OnLoadData): void {
-        this.setState({ status: StatusEnum.READY, aspectRatio: data.naturalSize.width / data.naturalSize.height });
-    }
-
-    private onError(): void {
-        this.setState({ status: StatusEnum.ERROR, aspectRatio: undefined });
-    }
+        </Fragment>
+    );
 }
--- packages/pluggableWidgets/video-player-native/src/ui/Styles.ts
+++ packages/pluggableWidgets/video-player-native/src/ui/Styles.ts
@@ -1,13 +1,17 @@
 import { Style } from "@mendix/piw-native-utils-internal";
-import { TextStyle, ViewStyle } from "react-native";
+import {TextStyle, ViewStyle} from "react-native";
 
-interface VideoStyle extends Style {
+export interface VideoStyle extends Style {
     container: ViewStyle;
     indicator: {
         color: string;
     };
     video: ViewStyle;
     errorMessage: TextStyle;
+    fullScreenVideoPlayer: ViewStyle;
+    controlBtnContainerStyle: ViewStyle;
+    fullScreenVideoStyle:  ViewStyle;
+    fullScreenActivityIndicatorStyle: ViewStyle;
 }
 
 export const defaultVideoStyle: VideoStyle = {
@@ -15,16 +19,39 @@ export const defaultVideoStyle: VideoStyle = {
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: "black",
-        aspectRatio: 16 / 9
     },
     indicator: {
         color: "white"
     },
     video: {
         width: "100%",
-        height: "100%"
+        height: undefined
     },
     errorMessage: {
         color: "white"
+    },
+    fullScreenVideoPlayer: {
+        justifyContent: "center",
+        backgroundColor: "black",
+        margin: 0
+    },
+    controlBtnContainerStyle: {
+        position: "absolute",
+        top: "10%",
+        right: 10,
+        padding: 2,
+        backgroundColor: "rgba(0,0,0,0.5)",
+        borderRadius: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    fullScreenVideoStyle: {
+        width: "100%",
+        height: "100%"
+    },
+    fullScreenActivityIndicatorStyle: {
+        position: "absolute",
+        alignSelf: "center"
     }
 };
+
