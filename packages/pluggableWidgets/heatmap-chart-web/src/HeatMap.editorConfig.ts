d758c78081eaf49c3c92935729a0bc6b4e3fa141
raffiar <raffi.hovhannisyan@mendix.com>
2022-03-16 16:28:32 +0100

    feat(heatmap-chart-web): structure preview for heatmap chart

--- packages/pluggableWidgets/heatmap-chart-web/src/HeatMap.editorConfig.ts
+++ packages/pluggableWidgets/heatmap-chart-web/src/HeatMap.editorConfig.ts
@@ -3,9 +3,16 @@ import {
     hidePropertyIn,
     Properties,
     StructurePreviewProps,
-    transformGroupsIntoTabs
+    transformGroupsIntoTabs,
+    ImageProps,
+    ContainerProps
 } from "@mendix/piw-utils-internal";
 import { HeatMapPreviewProps } from "../typings/HeatMapProps";
+
+import HeatMapDark from "./assets/HeatMap.dark.svg";
+import HeatMapLight from "./assets/HeatMap.light.svg";
+import HeatMapLegendDark from "./assets/HeatMap-legend.dark.svg";
+import HeatMapLegendLight from "./assets/HeatMap-legend.light.svg";
 
 export function getProperties(
     values: HeatMapPreviewProps,
@@ -35,6 +42,38 @@ export function getProperties(
     return defaultProperties;
 }
 
-export function getPreview(_values: HeatMapPreviewProps): StructurePreviewProps | null {
-    return null;
+export function getPreview(values: HeatMapPreviewProps, isDarkMode: boolean): StructurePreviewProps | null {
+    const items = {
+        dark: { structure: HeatMapDark, legend: HeatMapLegendDark },
+        light: { structure: HeatMapLight, legend: HeatMapLegendLight }
+    };
+
+    const getImage = (type: "structure" | "legend") => {
+        const colorMode = isDarkMode ? "dark" : "light";
+        return items[colorMode][type];
+    };
+
+    const chartImage = {
+        type: "Image",
+        document: decodeURIComponent(getImage("structure").replace("data:image/svg+xml,", "")),
+        width: 375
+    } as ImageProps;
+
+    const legendImage = {
+        type: "Image",
+        document: decodeURIComponent(getImage("legend").replace("data:image/svg+xml,", "")),
+        width: 57
+    } as ImageProps;
+
+    const filler = {
+        type: "Container",
+        grow: 1,
+        children: []
+    } as ContainerProps;
+
+    return {
+        type: "RowLayout",
+        columnSize: "fixed",
+        children: values.showLegend ? [chartImage, legendImage, filler] : [chartImage, filler]
+    };
 }
