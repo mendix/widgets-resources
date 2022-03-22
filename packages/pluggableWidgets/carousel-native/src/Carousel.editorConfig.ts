f13b15427fb292e66654e91fc3bd010508e7bcf5
Uraz Akgultan <uraz.akgultan@gmail.com>
2022-03-22 18:05:59 +0100

    feat(carousel-native): structure mode preview

---
+++ packages/pluggableWidgets/carousel-native/src/Carousel.editorConfig.ts
@@ -0,0 +1,53 @@
+import { RowLayoutProps, StructurePreviewProps } from "@mendix/piw-utils-internal";
+
+import paginationSVG from "./assets/pagination.svg";
+
+import { CarouselPreviewProps } from "../typings/CarouselProps";
+
+export function getPreview(values: CarouselPreviewProps, isDarkMode: boolean): StructurePreviewProps {
+    const paginationImageRowLayout: RowLayoutProps = {
+        type: "RowLayout",
+        columnSize: "grow",
+        padding: 4,
+        children: [
+            {
+                type: "Container",
+                grow: 1
+            },
+            {
+                type: "Image",
+                document: decodeURIComponent(paginationSVG.replace("data:image/svg+xml,", "")),
+                width: 57
+            },
+            {
+                type: "Container",
+                grow: 1
+            }
+        ]
+    };
+
+    return {
+        type: "Container",
+        backgroundColor: isDarkMode ? "#454545" : "#F5F5F5",
+        borders: true,
+        children: [
+            {
+                type: "Container",
+                padding: 8,
+                children: [
+                    {
+                        type: "Text",
+                        fontColor: isDarkMode ? "#DEDEDE" : "#0A1324",
+                        content: "Carousel"
+                    }
+                ]
+            },
+            {
+                type: "DropZone",
+                placeholder: "Place content here",
+                property: values.content
+            },
+            ...(values.showPagination ? [paginationImageRowLayout] : [])
+        ]
+    };
+}
