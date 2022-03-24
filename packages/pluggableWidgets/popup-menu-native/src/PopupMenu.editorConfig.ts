c893fa712003a46016c78028af5e853579e4bcd2
Uraz Akgultan <uraz.akgultan@gmail.com>
2022-03-24 07:11:16 +0100

    feat(popup-menu-native): structure mode preview

--- packages/pluggableWidgets/popup-menu-native/src/PopupMenu.editorConfig.ts
+++ packages/pluggableWidgets/popup-menu-native/src/PopupMenu.editorConfig.ts
@@ -1,5 +1,100 @@
-import { hidePropertyIn, Properties } from "@mendix/piw-utils-internal";
+import {
+    StructurePreviewProps,
+    hidePropertyIn,
+    Properties,
+    DropZoneProps,
+    ContainerProps,
+    RowLayoutProps
+} from "@mendix/piw-utils-internal";
 import { BasicItemsPreviewType, PopupMenuPreviewProps } from "../typings/PopupMenuProps";
+
+export function getPreview(values: PopupMenuPreviewProps, isDarkMode: boolean): StructurePreviewProps {
+    return {
+        type: "Container",
+        borders: true,
+        children: [
+            {
+                type: "Container",
+                backgroundColor: isDarkMode ? "#454545" : "#F5F5F5",
+                children: [
+                    {
+                        type: "Container",
+                        padding: 8,
+                        children: [
+                            {
+                                type: "Text",
+                                fontColor: isDarkMode ? "#DEDEDE" : "#0A1324",
+                                content: "Pop Up Menu"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                type: "Container",
+                borders: true,
+                children: [
+                    {
+                        type: "DropZone",
+                        placeholder: "Clickable area to show menu",
+                        property: values.menuTriggerer
+                    }
+                ]
+            },
+            { type: "Container", padding: 4 },
+            ...(values.renderMode === "basic"
+                ? values.basicItems.map<ContainerProps>((value, index) => ({
+                      type: "Container",
+                      children: [
+                          ...(value.itemType === "item"
+                              ? ([
+                                    {
+                                        type: "RowLayout",
+                                        columnSize: "fixed",
+                                        children: [
+                                            {
+                                                type: "RowLayout",
+                                                padding: 18,
+                                                borders: true,
+                                                columnSize: "grow",
+                                                children: [
+                                                    {
+                                                        type: "Container",
+                                                        grow: 1
+                                                    },
+                                                    {
+                                                        type: "Text",
+                                                        content: value.caption || `[Item ${index + 1}]`
+                                                    },
+                                                    {
+                                                        type: "Container",
+                                                        grow: 1
+                                                    }
+                                                ]
+                                            },
+                                            {
+                                                type: "Container",
+                                                grow: 2
+                                            }
+                                        ]
+                                    }
+                                ] as RowLayoutProps[])
+                              : ([
+                                    {
+                                        type: "Container",
+                                        padding: 4
+                                    }
+                                ] as ContainerProps[]))
+                      ]
+                  }))
+                : values.customItems.map<DropZoneProps>((value, index) => ({
+                      type: "DropZone",
+                      placeholder: `Items/${index + 1}/Content`,
+                      property: value.content
+                  })))
+        ]
+    };
+}
 
 export function getProperties(values: PopupMenuPreviewProps, defaultProperties: Properties): Properties {
     if (values.renderMode === "basic") {
