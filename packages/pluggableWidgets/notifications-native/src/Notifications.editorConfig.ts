13c987658da8e3dff4fe72a9bc5c644cb1526b9c
Uraz Akgultan <uraz.akgultan@gmail.com>
2022-03-24 08:18:33 +0100

    fix(notifications-native): refactor, add missing icon

--- packages/pluggableWidgets/notifications-native/src/Notifications.editorConfig.ts
+++ packages/pluggableWidgets/notifications-native/src/Notifications.editorConfig.ts
@@ -1,46 +1,76 @@
 import { StructurePreviewProps } from "@mendix/piw-utils-internal";
 
+import notificationIconSvgDark from "./assets/notification.dark.svg";
+import notificationIconSvgLight from "./assets/notification.light.svg";
+
 import { NotificationsPreviewProps } from "../typings/NotificationsProps";
 
 export const getPreview = (values: NotificationsPreviewProps, isDarkMode: boolean): StructurePreviewProps => {
-    const content: StructurePreviewProps = {
-        type: "Container",
-        padding: 12,
-        children: []
-    };
-
-    if (values.actions.length === 0) {
-        content.children = [
-            {
-                type: "Text",
-                content: "Configure your notification"
-            }
-        ];
-    } else {
-        content.children = [
-            {
-                type: "Text",
-                bold: true,
-                italic: values.title === "",
-                content: values.title !== "" ? values.title : "No title"
-            },
-            {
-                type: "Text",
-                italic: values.subtitle === "",
-                content: values.subtitle !== "" ? values.subtitle : "No subtitle"
-            },
-            {
-                type: "Text",
-                italic: values.body === "",
-                content: values.body !== "" ? values.body : "No body"
-            }
-        ];
-    }
-
+    const textColor: string = isDarkMode ? "#C5C5C5" : "#2F3646";
     return {
-        type: "Container",
+        type: "RowLayout",
         borders: true,
-        backgroundColor: isDarkMode ? "#454545" : "#F5F5F5",
-        children: [content]
+        borderRadius: 4,
+        backgroundColor: isDarkMode ? "#4F4F4F" : "#E7E7E9",
+        columnSize: "grow",
+        children: [
+            {
+                type: "Container",
+                padding: 16,
+                children: [
+                    ...((values.actions.length === 0
+                        ? [
+                              {
+                                  type: "Text",
+                                  content: "Configure your notification"
+                              }
+                          ]
+                        : [
+                              {
+                                  type: "Text",
+                                  bold: true,
+                                  fontColor: textColor,
+                                  fontSize: 10,
+                                  italic: !values.title,
+                                  content: values.title ? `{${values.title}}` : "No title"
+                              },
+                              {
+                                  type: "Text",
+                                  fontSize: 9,
+                                  fontColor: textColor,
+                                  italic: !values.subtitle,
+                                  content: values.subtitle ? `{${values.subtitle}}` : "No subtitle"
+                              },
+                              {
+                                  type: "Text",
+                                  fontSize: 8,
+                                  fontColor: textColor,
+                                  italic: !values.body,
+                                  content: values.body ? `{${values.body}}` : "No body"
+                              }
+                          ]) as StructurePreviewProps[])
+                ]
+            },
+            {
+                type: "Container",
+                grow: 1
+            },
+            {
+                type: "Container",
+                padding: 16,
+                children: [
+                    {
+                        type: "Image",
+                        document: decodeURIComponent(
+                            (isDarkMode ? notificationIconSvgDark : notificationIconSvgLight).replace(
+                                "data:image/svg+xml,",
+                                ""
+                            )
+                        ),
+                        width: 14
+                    }
+                ]
+            }
+        ]
     };
 };
