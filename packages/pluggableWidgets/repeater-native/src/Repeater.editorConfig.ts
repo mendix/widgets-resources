bf0a7d2c8f15c569b33d70ee67dafb42e267688c
Uraz Akgultan <uraz.akgultan@gmail.com>
2022-03-24 08:57:46 +0100

    feat(repeater-native): structure mode preview

---
+++ packages/pluggableWidgets/repeater-native/src/Repeater.editorConfig.ts
@@ -0,0 +1,34 @@
+import { StructurePreviewProps } from "@mendix/piw-utils-internal";
+
+import { RepeaterPreviewProps } from "../typings/RepeaterProps";
+
+export function getPreview(values: RepeaterPreviewProps, isDarkMode: boolean): StructurePreviewProps {
+    return {
+        type: "Container",
+        borders: true,
+        children: [
+            {
+                type: "Container",
+                backgroundColor: isDarkMode ? "#454545" : "#F5F5F5",
+                children: [
+                    {
+                        type: "Container",
+                        padding: 4,
+                        children: [
+                            {
+                                type: "Text",
+                                fontColor: isDarkMode ? "#DEDEDE" : "#0A1324",
+                                content: "Repeater"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                type: "DropZone",
+                placeholder: "Content",
+                property: values.content
+            }
+        ]
+    };
+}
