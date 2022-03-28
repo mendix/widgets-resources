482cdaca1992c6e7e73a9c077289b3da30788c8d
Ahmad-Elsayed <se.ahmedelsayed@gmail.com>
2022-03-28 14:07:35 +0200

    fix(background-gradient-native): add backward handling

--- packages/pluggableWidgets/background-gradient-native/src/BackgroundGradient.tsx
+++ packages/pluggableWidgets/background-gradient-native/src/BackgroundGradient.tsx
@@ -1,5 +1,5 @@
 import { ReactElement, createElement } from "react";
-import { Pressable, StyleSheet } from "react-native";
+import { Pressable, StyleSheet, NativeModules, Alert } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
 
 import { executeAction } from "@mendix/piw-utils-internal";
@@ -12,6 +12,10 @@ import { BackgroundGradientProps } from "../typings/BackgroundGradientProps";
 export type props = BackgroundGradientProps<CustomStyle>;
 
 export function BackgroundGradient({ name, colorList, angle, content, opacity, onClick, style }: props): ReactElement {
+    if (!("BVLinearGradient" in NativeModules.UIManager)) {
+        Alert.alert("", "The widget 'Background gradient' requires an updated 'Make It Native 9' application");
+    }
+
     let sortedColorList = colorList.sort((a, b) => Number(a.offset) - Number(b.offset));
     // checking if the color list only have one color item , then we should duplicate it. one color list throws an exception on android.
     if (sortedColorList.length === 1) {
