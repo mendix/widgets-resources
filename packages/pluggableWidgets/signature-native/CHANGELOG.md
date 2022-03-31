554c0df1f5fa02b013cc5b5bb037929f0c5abdb6
Arjan Blok <arjan.blok@gmail.com>
2022-03-31 13:39:10 +0200

    test(): fix review comments

--- packages/pluggableWidgets/signature-native/CHANGELOG.md
+++ packages/pluggableWidgets/signature-native/CHANGELOG.md
@@ -6,11 +6,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ## [Unreleased]
 
-### Added
-
--   Added test id
-
-## [2.1.0] - 2022-1-24
+## [1.0.0] - 2022-3-31
 
 ### Added
 
--- packages/pluggableWidgets/signature-native/e2e/specs/Signature.spec.ts
+++ packages/pluggableWidgets/signature-native/e2e/specs/Signature.spec.ts
@@ -1,7 +1,7 @@
 import { element, by } from "detox";
 import { expectToMatchScreenshot, resetDevice, tapMenuItem } from "../../../../../detox/src/helpers";
 
-describe("Signature", () => {
+describe("Signature widget", () => {
     beforeEach(async () => {
         await tapMenuItem("Signature");
     });
@@ -10,7 +10,7 @@ describe("Signature", () => {
         await resetDevice();
     });
 
-    it("should be able to save a signature", async () => {
+    it("should be able to save a complex signature", async () => {
         await element(by.id("signature")).swipe("up", "slow", 0.4, 0.5, 0.5);
         await element(by.id("signature")).swipe("right", "slow", 0.4, 0.5, 0.5);
         await element(by.id("signature")).swipe("down", "slow", 0.4, 0.5, 0.5);
@@ -20,5 +20,13 @@ describe("Signature", () => {
         await element(by.id("signature$SaveButton$Touchable")).tap();
         const attributes = (await element(by.id("textArea")).getAttributes()) as Detox.ElementAttributes;
         expect(attributes.text!.startsWith("data:image/png;base64,")).toBeTruthy();
+    });
+
+    it("should be able to clear a signature", async () => {
+        await element(by.id("signature")).swipe("down", "slow", 0.9, 0.5, 0.1);
+        await expectToMatchScreenshot();
+
+        await element(by.id("signature$ClearButton$Touchable")).tap();
+        await expectToMatchScreenshot();
     });
 });
