04d1ec90391b5f84bff85bb85cd6e14b308abca7
Jordan Hunt <jordan.hunt@mendix.com>
2022-04-06 15:26:49 +0200

    feat(): remove ports, manually start emulator

--- detox/scripts/setup-android.js
+++ detox/scripts/setup-android.js
@@ -1,5 +1,6 @@
 const { ANDROID_SDK_VERSION, ANDROID_DEVICE_TYPE } = require("../detox.config");
 const { downloadFile, execCommand } = require("./helpers");
+const { execSync } = require("child_process");
 
 main().catch(e => {
     console.error(e);
@@ -18,9 +19,20 @@ async function main() {
     execCommand("sdkmanager --licenses");
 
     console.log("Creating Android emulator...");
+    const emulatorName = `NATIVE_${ANDROID_DEVICE_TYPE}_${ANDROID_SDK_VERSION}`;
     execCommand(
-        `avdmanager -s create avd -n NATIVE_${ANDROID_DEVICE_TYPE}_${ANDROID_SDK_VERSION} -k 'system-images;android-${ANDROID_SDK_VERSION};google_apis;x86_64' -f -d '${ANDROID_DEVICE_TYPE}' -c 1000M`
+        `avdmanager -s create avd -n ${emulatorName} -k 'system-images;android-${ANDROID_SDK_VERSION};google_apis;x86_64' -f -d '${ANDROID_DEVICE_TYPE}' -c 1000M`
     );
 
-    console.log("Done!");
+    // inspired by https://github.com/edvinasbartkus/react-native-detox-github-actions/blob/master/.github/workflows/android.yml
+    if (process.env.CI) {
+        console.log("Attempting to manually start emulator...");
+        execSync(
+            `nohup $ANDROID_HOME/emulator/emulator -avd ${emulatorName} -no-audio -no-snapshot -no-window &
+            $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\\r') ]]; do sleep 1; done; input keyevent 82' $ANDROID_HOME/platform-tools/adb devices`
+        );
+        console.log("Started emulator!");
+    }
+
+    console.log("Android setup done!");
 }
