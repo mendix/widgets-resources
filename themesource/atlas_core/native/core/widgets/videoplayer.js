5ceb94a7686bdfeeefda50ebf7e8ee418b47d983
leonardomendix <leonardomendix@mendix.com>
2022-04-07 15:48:32 +0000

    Automatic update of widgets, themes and modules

--- themesource/atlas_core/native/core/widgets/videoplayer.js
+++ themesource/atlas_core/native/core/widgets/videoplayer.js
@@ -19,5 +19,17 @@ export const com_mendix_widget_native_videoplayer_VideoPlayer = {
     },
     video: {
     // All ViewStyle properties are allowed
+    },
+    fullScreenVideoPlayer: {
+    // All ViewStyle properties are allowed
+    },
+    controlBtnContainerStyle: {
+    // All ViewStyle properties are allowed
+    },
+    fullScreenVideoStyle: {
+    // All ViewStyle properties are allowed
+    },
+    fullScreenActivityIndicatorStyle: {
+    // All ViewStyle properties are allowed
     }
 };
