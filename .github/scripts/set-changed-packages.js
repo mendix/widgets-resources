aa4c89e19caf8dcd93aa72b352181414b1d25fd7
Jordan Hunt <jordan.hunt@mendix.com>
2022-04-20 13:19:24 +0200

    feat(): wrap with stringify

--- .github/scripts/set-changed-packages.js
+++ .github/scripts/set-changed-packages.js
@@ -3,6 +3,9 @@ const { execSync } = require("child_process");
 const [arg] = process.argv.slice(2);
 const changedPackages = execSync(`npx lerna list --json --since origin/master${arg ? ` --scope '${arg}'` : ""}`);
 
-execSync(`echo ::set-output name=CHANGED_PACKAGES::${changedPackages.toString().replaceAll("\n", "")}`, {
-    stdio: "inherit"
-});
+execSync(
+    `echo ::set-output name=CHANGED_PACKAGES::${JSON.stringify(changedPackages.toString().replaceAll("\n", ""))}`,
+    {
+        stdio: "inherit"
+    }
+);
