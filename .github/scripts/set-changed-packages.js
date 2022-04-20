da5a2b7da6441e88cda50c3d616d636552790a96
Jordan Hunt <jordan.hunt@mendix.com>
2022-04-20 11:55:39 +0200

    feat(): use string iso buffer

--- .github/scripts/set-changed-packages.js
+++ .github/scripts/set-changed-packages.js
@@ -3,4 +3,6 @@ const { execSync } = require("child_process");
 const [arg] = process.argv.slice(2);
 const changedPackages = execSync(`npx lerna list --json --since origin/master${arg ? ` --scope '${arg}'` : ""}`);
 
-execSync(`echo ::set-output name=CHANGED_PACKAGES::${changedPackages}`, { stdio: "inherit" });
+execSync(`echo ::set-output name=CHANGED_PACKAGES::${changedPackages.toString().replaceAll("\n", "")}`, {
+    stdio: "inherit"
+});
