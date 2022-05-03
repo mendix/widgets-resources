359a28e239376ed549c6db964d29435db678f197
Jordan Hunt <jordan.hunt@mendix.com>
2022-05-03 17:53:18 +0200

    feat(): add trigger output and calc changes for master

--- .github/scripts/set-changed-packages.js
+++ .github/scripts/set-changed-packages.js
@@ -1,7 +1,11 @@
 const { execSync } = require("child_process");
 
-const [arg] = process.argv.slice(2);
-const changedPackages = execSync(`npx lerna list --json --since origin/master${arg ? ` --scope '${arg}'` : ""}`);
+const [scope] = process.argv.slice(2);
+const trigger = process.env.TRIGGER;
+const isMergeRequest = trigger.includes("merge-request");
+const changedPackages = execSync(
+    `npx lerna list --json${isMergeRequest ? ` --since origin/master${scope ? ` --scope '${scope}'` : ""}` : ""}`
+);
 
 execSync(
     `echo ::set-output name=CHANGED_PACKAGES::${JSON.stringify(changedPackages.toString().replaceAll("\n", ""))}`,
