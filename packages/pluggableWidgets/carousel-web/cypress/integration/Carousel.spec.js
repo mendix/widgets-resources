11ab25429c739db441b2008737b06ef22c3c0752
Leonardo de Souza <leonardo.de.souza@mendix.com>
2022-05-04 12:52:39 +0200

    test(carousel-web): add retries

--- packages/pluggableWidgets/carousel-web/cypress/integration/Carousel.spec.js
+++ packages/pluggableWidgets/carousel-web/cypress/integration/Carousel.spec.js
@@ -1,22 +1,23 @@
 describe("Carousel", () => {
     before(() => {
         cy.visit("/");
-        cy.wait(2000);
+        cy.wait(5000);
     });
-    it("disables the left arrow when showing the first item", () => {
-        cy.get(".swiper-button-prev").should("not.exist");
+    it("disables the left arrow when showing the first item", { retries: 3 }, () => {
+        cy.get(".swiper-button-prev").should("exist").and("have.class", "swiper-button-disabled");
     });
 
-    it("enables the left arrow when it navigates from the first item", () => {
+    it("enables the left arrow when it navigates from the first item", { retries: 3 }, () => {
         cy.get(".swiper-button-next").click({ force: true });
         cy.get(".swiper-button-prev").should("be.visible");
     });
 
-    it("disables the right arrow when on the last image item", () => {
+    it("disables the right arrow when on the last image item", { retries: 3 }, () => {
         cy.get(".swiper-button-next").should("have.class", "swiper-button-disabled");
     });
 
-    it("shows enlarged image when image is clicked", () => {
+    it("shows enlarged image when image is clicked", { retries: 3 }, () => {
+        cy.get(".swiper-button-prev").click({ force: true });
         cy.get(".mx-name-Image01").first().click({ force: true });
         cy.get(".mx-imagezoom-image").should("be.visible");
     });
