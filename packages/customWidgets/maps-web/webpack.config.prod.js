7edaa1dba9eeb6ae635c8166520bf0e30566047c
Keraito <chakshunyu@gmail.com>
2022-05-05 15:23:36 +0200

    feat(maps-web): fix bug where merging of webpack configs leaves webpack with 2 plugins for extracting CSS, which causes the resulting folder in the MPK bundle to be lowercased. This causes breaking changes for users trying to upgrade to v1.0.10.

--- packages/customWidgets/maps-web/webpack.config.prod.js
+++ packages/customWidgets/maps-web/webpack.config.prod.js
@@ -33,4 +33,12 @@ const customPreviewConfig = {
     }
 };
 
+/**
+ * First plugin of the baseConfig extracts the CSS into `custom/maps/ui/Maps.css` (note the lowercase of the folder).
+ * This for some reason causes the folder to be entirely renamed to lowercase, also for the JS files.
+ * This causes breaking changes for users that try to update towards v1.0.10 (which introduces this bug).
+ * Removing the first plugin solves this issue.
+ *  */ 
+baseConfig[0].plugins.shift();
+
 module.exports = [merge(baseConfig[0], customConfig), merge(baseConfig[1], customPreviewConfig)];
