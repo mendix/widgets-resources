7ae7e548f660e09092b34d0c0a247c55fe6f48a0
Leonardo de Souza <leonardo.de.souza@mendix.com>
2022-05-12 17:25:44 +0200

    test(rich-text-web): enable external website check

--- packages/pluggableWidgets/rich-text-web/cypress/integration/RichText.spec.js
+++ packages/pluggableWidgets/rich-text-web/cypress/integration/RichText.spec.js
@@ -15,21 +15,26 @@ describe("RichText", () => {
                 .then(cy.wrap)
         );
     };
+    before(() => {
+        cy.visit("/");
+        cy.contains("Generate Data").click();
+    });
 
     it("compares with a screenshot baseline and checks if inline basic mode are rendered as expected", () => {
-        cy.visit("/p/basic");
+        cy.visit("/#/basic");
         cy.wait(3000);
         cy.get(".mx-name-richText1").should("be.visible");
         cy.get(".mx-name-richText1").compareSnapshot(`inlineBasicMode-${browserName}`, 0.4);
     });
 
     it("compares with a screenshot baseline and checks if toolbar basic mode are rendered as expected", () => {
+        cy.visit("/#/basic");
         cy.get(".mx-name-richText4").should("be.visible");
         cy.get(".mx-name-richText4").compareSnapshot(`toolbarBasicMode-${browserName}`, 0.4);
     });
 
     it("compares with a screenshot baseline and checks if inline advanced mode are rendered as expected", () => {
-        cy.visit("/p/advanced");
+        cy.visit("/#/advanced");
         cy.wait(2000);
         cy.get(".mx-name-richText1").should("be.visible");
         cy.get(".mx-name-richText1").compareSnapshot(`inlineAdvancedMode-${browserName}`, 0.4);
@@ -63,7 +68,7 @@ describe("RichText", () => {
     });
 
     it("compares with a screenshot baseline and checks if read-only text are rendered as expected", () => {
-        cy.visit("/p/read-only");
+        cy.visit("/#/read-only");
         cy.wait(2000);
         cy.get(".mx-name-richText2").should("be.visible");
         cy.get(".mx-name-richText2").compareSnapshot(`readOnlyText-${browserName}`, 0.4);
@@ -79,8 +84,17 @@ describe("RichText", () => {
         cy.get(".mx-name-richText4").compareSnapshot(`readOnlyBorderedToolbar-${browserName}`, 0.4);
     });
 
-    it("opens a link in the read-only mode", () => {
+    // Only run the test if Cypress is run via Chrome
+    it("opens a link in the read-only mode", { browser: "chrome" }, () => {
+        // For checking a different page, which is out of the localhost this test need to disable cross-origin security setting, and that's only possible when using Chrome
+        // https://docs.cypress.io/guides/guides/web-security#Disabling-Web-Security
+        cy.window().then(win => {
+            cy.stub(win, "open", url => {
+                win.location.href = "https://mendix.com/";
+            }).as("popup");
+        });
         getIframeBody().contains("Clickable Link").click({ ctrlKey: true });
-        cy.url().should("contain", "mendix.com");
+        cy.get("@popup").should("be.called");
+        cy.location("href").should("contain", "mendix.com");
     });
 });
