267a81497d64762a0288628450f862855946ca86
Leonardo de Souza <leonardo.de.souza@mendix.com>
2022-06-27 11:43:37 +0200

    test(document-viewer-web): enable chrome headed mode

--- packages/pluggableWidgets/document-viewer-web/cypress/integration/DocumentViewer.spec.js
+++ packages/pluggableWidgets/document-viewer-web/cypress/integration/DocumentViewer.spec.js
@@ -6,7 +6,7 @@ describe("document-viewer-web", () => {
     });
 
     it("compares with a screenshot baseline and checks if displays a PDF file from URL", () => {
-        cy.wait(3000);
+        cy.wait(5000);
         cy.get(".mx-name-documentViewer1").should("be.visible");
         cy.get(".mx-name-documentViewer1").compareSnapshot(`documentViewerPDFUrl-${browserName}`, 0.1);
     });
