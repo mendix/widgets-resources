5fbdf3c845b28b65111ded5ee84dcf7457d088e2
Leonardo de Souza <leonardo.de.souza@mendix.com>
2022-07-07 11:43:20 +0200

    test(datagrid-number-filter-web): fix update data widgets styles

---
+++ scripts/test/updateDataWidgets.js
@@ -0,0 +1,31 @@
+const { exec } = require("child_process");
+const { join } = require("path");
+const rCopy = require("recursive-copy");
+const { promisify } = require("util");
+
+main().catch(e => {
+    console.error(e);
+    process.exit(-1);
+});
+
+async function main() {
+    await copyLatestDataWidgets();
+}
+
+async function copyLatestDataWidgets() {
+    console.log("Copying latest data widgets from mono repo...");
+
+    const dataWidgetsSrc = join(process.cwd(), "../../modules/data-widgets");
+
+    try {
+        await promisify(exec)("npm run release:module", { cwd: dataWidgetsSrc });
+    } catch (e) {
+        throw new Error(`Failed to create a release distribution of Data Widgets: ${e}`);
+    }
+
+    try {
+        await promisify(rCopy)(join(dataWidgetsSrc, "dist"), "tests/testProject");
+    } catch (e) {
+        throw new Error("Failed to copy Data Widgets release distribution to test/testProject");
+    }
+}
