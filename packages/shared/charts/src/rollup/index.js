2486b33ba137946ffa81dfbdb8b65fee232149c2
Illia Obukhau <vyusfire@gmail.com>
2022-08-04 13:26:45 +0200

    fix(): remvoe code altering output path

--- packages/shared/charts/src/rollup/index.js
+++ packages/shared/charts/src/rollup/index.js
@@ -118,11 +118,8 @@ function sharedChardConfig(args, chartName) {
         );
     }
 
-    // We change the output because chart widget packages were wrongly named with uppercase first letter in the past.
-    jsConfig.output.file = join(process.cwd(), `dist/tmp/widgets/com/mendix/widget/web/${chartName}/${chartName}.js`);
-    mJsConfig.output.file = join(process.cwd(), `dist/tmp/widgets/com/mendix/widget/web/${chartName}/${chartName}.mjs`);
-
     result.forEach(config => {
+        console.log(config);
         const onwarn = config.onwarn;
         if (onwarn) {
             config.onwarn = warning => {
