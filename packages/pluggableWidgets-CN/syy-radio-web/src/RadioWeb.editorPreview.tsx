811072c63088639b1b8a30e5ce778336bec7d942
ouhuichan <98056@runjian.com>
2022-08-19 11:38:04 +0800

    feat(syy-radio-web): 控件preview

--- packages/pluggableWidgets-CN/syy-radio-web/src/RadioWeb.editorPreview.tsx
+++ packages/pluggableWidgets-CN/syy-radio-web/src/RadioWeb.editorPreview.tsx
@@ -1,72 +1,18 @@
-import { ReactNode, createElement, useMemo } from "react";
+import { ReactNode, createElement } from "react";
 import { RadioWebPreviewProps } from "../typings/RadioWebProps";
 import { RadioComponent } from "./components/RadioComponent";
+import "antd/lib/radio/style/css.js";
 
-declare function require(name: string): string;
-
-export function RadioWeb(props: RadioWebPreviewProps): ReactNode {
-    const {
-        data,
-        attribute,
-        attributedata,
-        selectedValue,
-        optionType,
-        radioName,
-        disabled,
-        size,
-        direction,
-        title,
-        datasType,
-        staticData,
-        buttonStyle,
-        isText
-    } = props;
-
-    const handleValue = (val: any, item: any): any => {
-        if (val.get) {
-            return val.get(item).value;
-        }
-        return val(item).value;
-    };
+export function preview(props: RadioWebPreviewProps): ReactNode {
+    const { attributedata, selectedValue, optionType, radioName, disabled, size, direction, buttonStyle, isText } =
+        props;
 
     // 格式化options
-    const dataOption: any[] = [];
-    // const dataOption: any[] = useMemo(() => {
-    //     const options: any = [];
-    //     if (datasType === "dynamic") {
-    //         if (attributedata) {
-    //             attributedata.universe?.forEach(name => {
-    //                 options.push({
-    //                     label: attributedata.formatter.format(name),
-    //                     value: name
-    //                 });
-    //             });
-    //         } else {
-    //             if (data) {
-    //                 data.map((item: any) => {
-    //                     const value = handleValue(attribute, item);
-    //                     const label = title ? handleValue(title, item) : value;
-    //                     return options.push({
-    //                         label,
-    //                         value
-    //                     });
-    //                 });
-    //             }
-    //         }
-    //     } else {
-    //         if (staticData.length > 0) {
-    //             staticData.forEach((item: any) => {
-    //                 options.push({
-    //                     label: item.manuaTitle,
-    //                     value: item.manualValue,
-    //                     disabled: item.manualDisabled
-    //                 });
-    //             });
-    //         }
-    //     }
-    //     return options;
-    // }, [attribute, attributedata, data, staticData, title, datasType]);
-
+    const dataOption: any[] = [
+        { label: "Apple", value: "Apple" },
+        { label: "Pear", value: "Pear" },
+        { label: "Orange", value: "Orange" }
+    ];
     // 静态数据
     if (isText) {
         return (
@@ -85,8 +31,4 @@ export function RadioWeb(props: RadioWebPreviewProps): ReactNode {
             defaultValue={attributedata ? attributedata : selectedValue}
         />
     );
-}
-
-export function getPreviewCss(): string {
-    return require("./ui/index.css");
 }
